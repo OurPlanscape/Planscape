187b973c26817228b05e084b1fc04b6ad1170322
Hana Lee <leehana@alumni.stanford.edu>
2022-10-21 16:10:08 -0700

    Added Material styling to login and signup forms

--- src/interface/src/app/app.module.ts
+++ src/interface/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
@@ -50,6 +53,9 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     FormsModule,
     CommonModule,
     MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [
     AuthService,
