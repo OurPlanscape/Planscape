07fe9f3599d590856f459e3016f18738ec8bb2b0
cynthia huang <cynhuang@google.com>
2022-10-20 18:53:07 -0700

    * Migrated to scss for easier styling
    * Updated styling with material theming
    
    * Added 'top-bar' component
    
    * Removed mat-toolbar from 'navigation' component
    
    * Added toggling of 'navigation' sidebar

--- src/interface/src/app/signup/signup.component.ts
+++ src/interface/src/app/signup/signup.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from './../auth.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
