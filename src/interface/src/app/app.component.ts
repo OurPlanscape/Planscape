efa94385e0833b213b6a7ebb2c9c8832e4a49b73
cynthia huang <cynhuang@google.com>
2022-10-20 18:53:07 -0700

    * Migrated to scss for easier styling
    * Updated styling with material theming
    
    * Added 'top-bar' component
    
    * Removed mat-toolbar from 'navigation' component
    
    * Added toggling of 'navigation' sidebar

--- src/interface/src/app/app.component.ts
+++ src/interface/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'interface';
+  sidebarOpen = false;
+
+  toggleSidebar(event: Event) {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
 }
