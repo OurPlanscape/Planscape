8c21309729ad4b016ab686380c6a915d0a93ee23
Elsie Ling <elsieling@google.com>
2022-10-21 11:41:43 -0700

    Adding HUC-12 and CalMAPPER layers in javascript files. Rendering is a little slow, so we may want to move this, but this is where I've gotten to for now.

--- src/planscape/explore/views.py
+++ src/planscape/explore/views.py
@@ -1,7 +1,21 @@
+from ctypes import sizeof
+from pyexpat import features
 from django.views.generic.base import TemplateView
-
+import json, requests, urllib
 
 class ExploreView(TemplateView):
-    """Markers map view."""
-
     template_name = "explore.html"
+
+    def get_context_data(self, **kwargs):
+        url = "https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/ArcGIS/rest/services/CMDash_v3_view/FeatureServer/2/query?"
+        params = {
+            'where': '1=1',
+            'outFields' : 'PROJECT_NAME,PROJECT_STATUS',
+            'f': 'GEOJSON'
+        }
+        url_final = url + urllib.parse.urlencode(params)
+        response = requests.get(url=url_final).text
+
+        context = super().get_context_data(**kwargs)
+        context['existing_projects'] = response
+        return context
