8c65a626cab1f16254afa69b7e7d034555160c90
cynthia huang <cynhuang@google.com>
2022-10-20 18:53:07 -0700

    * Migrated to scss for easier styling
    * Updated styling with material theming
    
    * Added 'top-bar' component
    
    * Removed mat-toolbar from 'navigation' component
    
    * Added toggling of 'navigation' sidebar

--- src/interface/src/app/home/home.component.ts
+++ src/interface/src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from './../auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
