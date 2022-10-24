464c91f7695340c58c92c28c589fa9d1326bd5e4
Hana Lee <leehana@alumni.stanford.edu>
2022-10-24 16:50:32 -0700

    Added non-functioning layer selection to map view

--- src/interface/src/app/app.module.ts
+++ src/interface/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -56,6 +57,7 @@ import { RegionSelectionComponent } from './region-selection/region-selection.co
     FormsModule,
     CommonModule,
     MatIconModule,
+    MatRadioModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
