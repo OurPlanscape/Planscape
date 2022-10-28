7e8ee78c92d82656b9a5f820fc96c2fadabac770
cynthia huang <cynhuang@google.com>
2022-10-27 20:23:10 -0700

    update tests

--- src/interface/src/app/region-selection/region-selection.component.spec.ts
+++ src/interface/src/app/region-selection/region-selection.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Region } from '../types';
 
 import { RegionSelectionComponent } from './region-selection.component';
 
@@ -19,5 +20,23 @@ describe('RegionSelectionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set available region', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const sierraNevadaElement: any = element.querySelectorAll('.region')[0];
+
+    sierraNevadaElement.click();
+
+    expect(component.selectedRegion).toEqual(Region.SIERRA_NEVADA);
+  });
+
+  it('should disable unavailable regions', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const regionElement: any = element.querySelectorAll('.region')[2];
+
+    regionElement.click();
+
+    expect(component.selectedRegion).toEqual(undefined);
   });
 });
