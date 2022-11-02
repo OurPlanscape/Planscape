64e51b9828c06227d3ca199bceec72fc1c05e119
Jon Riecke <riecke@google.com>
2022-11-02 17:38:31 -0400

    Updates to documentation on conditions app 	modified:   src/planscape/conditions/migrations/0001_initial.py 	modified:   src/planscape/conditions/models.py

--- src/planscape/conditions/models.py
+++ src/planscape/conditions/models.py
@@ -1,22 +1,39 @@
 from django.contrib.gis.db import models
-from base.condition_types import ConditionLevel, ConditionScoreType
 
 
 class AbstractCondition(models.Model):
-    """An AbstractCondition has a name and optional display and region names."""
+    """An AbstractCondition has a name, and an optional display/region name and level."""
+    # The name of the condition.
     condition_name: models.CharField = models.CharField(max_length=120)
-    # Optional fields for Condition
+
+    # The level of the condition, drawn from the ConditionLevel enum defined in
+    # base/condition_types.py (one of ECOSYSTEM, PILLAR, ELEMENT, METRIC).
+    condition_level: models.IntegerField = models.IntegerField()
+
+    # The name of the condition for use in interfaces.
     display_name: models.CharField = models.CharField(
         max_length=120, null=True)
-    region_name: models.CharField = models.CharField(max_length=120, null=True)
 
+    # The region associated with the condition, drawn from the RegionName enum in
+    # base/region_name.py.
+    region_name: models.CharField = models.CharField(max_length=120, null=True)
+  
 
 class Condition(models.Model):
     """
-    A Condition object is a single raster, referencing the Condition 
-    object, with additional metadata about its level and type.
+    A Condition is a single raster, referencing the AbstractCondition, with additional metadata.
     """
-    condition = models.ForeignKey(AbstractCondition, on_delete=models.CASCADE)  # type: ignore
+    condition = models.ForeignKey(
+        AbstractCondition, on_delete=models.CASCADE)  # type: ignore
+
+    # The raster data associated with the condition.
     geometry = models.RasterField()
-    condition_level = ConditionLevel
-    condition_score_type = ConditionScoreType
+
+    # The type of condition, drawn from the ConditionScoreType enum defined in
+    # base/condition_types.py (CURRENT, FUTURE, ADAPT, etc).
+    condition_score_type: models.IntegerField = models.IntegerField(null=True)
+
+    # True if the Condition represents raw, uninterpreted data.  This is appropriate only
+    # when condition_score_type = CURRENT and condition_level = METRIC, and is ignored
+    # otherwise.
+    is_raw:models.BooleanField = models.BooleanField(null=True)
