8402f154605a4c27fce0a316c7640c13443aec3a
Hana Lee <leehana@alumni.stanford.edu>
2022-11-02 17:01:40 -0700

    Renamed BoundaryService -> MapService and added layer controls

---
+++ src/interface/src/app/map.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapService {
+  constructor(private http: HttpClient) {}
+
+  getBoundaryShapes() {
+    // Get the shapes from the REST server.
+    return this.http.get('http://127.0.0.1:8000/boundary/boundary_details/?boundary_name=tcsi_huc12');
+  }
+
+  // Queries the CalMAPPER ArcGIS Web Feature Service for known land management projects without filtering.
+  getExistingProjects() {
+    const params = {
+      'where': '1=1',
+      'outFields' : 'PROJECT_NAME,PROJECT_STATUS',
+      'f': 'GEOJSON'
+    }
+
+    const url = "https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/ArcGIS/rest/services/CMDash_v3_view/FeatureServer/2/query?" + new URLSearchParams(params).toString();
+    return this.http.get(url);
+  }
+}
