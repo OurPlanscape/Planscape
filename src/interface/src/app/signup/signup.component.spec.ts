14ea4eb87fbc6837913f6ab5a09a5c5be3fdda5e
Hana Lee <leehana@alumni.stanford.edu>
2022-11-04 16:32:35 -0700

    Unit tests for LoginComponent and SignupComponent

---
+++ src/interface/src/app/signup/signup.component.spec.ts
@@ -0,0 +1,54 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+
+import { AuthService } from './../auth.service';
+import { SignupComponent } from './signup.component';
+
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
+
+  beforeEach(() => {
+    const routerStub = () => ({ navigate: (array: string[]) => ({}) });
+    const fakeAuthService = jasmine.createSpyObj<AuthService>(
+      'AuthService',
+      { signup: of({}) },
+      {},
+    );
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [SignupComponent],
+      providers: [
+        { provide: Router, useFactory: routerStub },
+        { provide: AuthService, useValue: fakeAuthService }
+      ]
+    });
+    fixture = TestBed.createComponent(SignupComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('can load instance', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('onSubmit', () => {
+    it('makes expected calls', () => {
+      spyOn(component, 'signup').and.callThrough();
+      component.onSubmit();
+      expect(component.signup).toHaveBeenCalled();
+    });
+  });
+
+  describe('login', () => {
+    it('makes expected calls', () => {
+      const routerStub: Router = fixture.debugElement.injector.get(Router);
+      spyOn(routerStub, 'navigate').and.callThrough();
+      component.login();
+      expect(routerStub.navigate).toHaveBeenCalled();
+    });
+  });
+});
