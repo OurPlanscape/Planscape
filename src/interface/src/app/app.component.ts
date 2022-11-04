15212549164997811597acf9a8f3c9e689c9299e
Hana Lee <leehana@alumni.stanford.edu>
2022-11-04 15:24:25 -0700

    Moved isLoggedIn subscriptions from service to component

--- src/interface/src/app/app.component.ts
+++ src/interface/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   sidebarOpen = false;
+
+  constructor(private authService: AuthService) {}
 
   toggleSidebar(event: Event) {
     this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  ngOnInit(): void {
+    // Refresh the user's logged in status when the app initializes.
+    this.authService.refreshToken().subscribe().unsubscribe();
   }
 
 }
