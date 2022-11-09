3e3ef48b6c452b2b9a1536627e41e51a709980d2
Hana Lee <leehana@alumni.stanford.edu>
2022-11-09 14:28:34 -0800

    Delete deadcode

--- src/interface/src/app/map.service.ts
+++ src/interface/src/app/map.service.ts
@@ -48,13 +48,6 @@ export class MapService {
 
   // Queries the CalMAPPER ArcGIS Web Feature Service for known land management projects without filtering.
   getExistingProjects(): Observable<GeoJSON.GeoJSON> {
-    const params = {
-      'where': '1=1',
-      'outFields' : 'PROJECT_NAME,PROJECT_STATUS',
-      'f': 'GEOJSON'
-    }
-
-    const url = "https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/ArcGIS/rest/services/CMDash_v3_view/FeatureServer/2/query?" + new URLSearchParams(params).toString();
     return this.http.get<string>('http://127.0.0.1:8000/projects').pipe(map((response: string) => {
       return JSON.parse(response);
     }));
