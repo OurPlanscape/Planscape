ed2b4a4cec0db62649bb599abe03775700bfc430
Jon Riecke <riecke@google.com>
2022-11-17 15:08:43 -0500

    Do the right thing when loading regions/boundaries 	modified:   src/interface/src/app/map/map.component.ts

--- src/interface/src/app/map/map.component.ts
+++ src/interface/src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { Observable, Subject, take, takeUntil } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import * as L from 'leaflet';
 import 'leaflet-draw';
 
@@ -63,18 +64,26 @@ export class MapComponent implements AfterViewInit, OnDestroy {
     }
 
   ngAfterViewInit(): void {
-    var region: Region|null = null;
     this.initMap();
-    this.selectedRegion$.subscribe((selectedRegion) => {
+    this.selectedRegion$.pipe(take(1)).subscribe((selectedRegion) => {
       this.displayRegionBoundary(selectedRegion);
-      region = selectedRegion;
     });
-    this.boundaryService.getHUC12BoundaryShapes(region).pipe(take(1)).subscribe((boundary: GeoJSON.GeoJSON) => {
-      this.initHUC12BoundaryLayer(boundary);
+
+    this.selectedRegion$.pipe(
+      take(1),
+      switchMap((selectedRegion) => {
+        return this.boundaryService.getHUC12BoundaryShapes(selectedRegion).pipe(take(1));
+    })).subscribe((boundary:GeoJSON.GeoJSON) => {
+        this.initHUC12BoundaryLayer(boundary);
     });
-    this.boundaryService.getCountyBoundaryShapes(region).pipe(take(1)).subscribe((boundary: GeoJSON.GeoJSON) => {
-      this.initCountyBoundaryLayer(boundary);
+    this.selectedRegion$.pipe(
+      take(1),
+      switchMap((selectedRegion) => {
+        return this.boundaryService.getCountyBoundaryShapes(selectedRegion).pipe(take(1));
+    })).subscribe((boundary: GeoJSON.GeoJSON) => {
+        this.initCountyBoundaryLayer(boundary);
     });
+
     this.boundaryService.getExistingProjects().pipe(take(1)).subscribe((existingProjects: GeoJSON.GeoJSON) => {
       console.log('existing projects', existingProjects);
       this.initCalMapperLayer(existingProjects);
