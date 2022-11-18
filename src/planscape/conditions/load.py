42512a3e32590cffeb13e55ed462039484398486
Elsie Ling <elsieling@google.com>
2022-11-18 14:40:08 -0800

    Add initial load file

--- src/planscape/conditions/load.py
+++ src/planscape/conditions/load.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from decouple import config
 from typing import cast
+import sys
+import numpy
 
 from django.contrib.gis.utils.layermapping import LayerMapping
 from django.db.models.signals import pre_save
@@ -11,56 +13,57 @@ from base.condition_types import ConditionLevel, ConditionScoreType
 from base.region_name import RegionName
 from config.conditions_config import PillarConfig
 from .models import BaseCondition, Condition
+from base.conditions import *
+from eval.compute_conditions import *
+from osgeo import gdal, osr
 
-PLANSCAPE_ROOT_DIRECTORY = cast(str, config('PLANSCAPE_ROOT_DIRECTORY'))
+
+PLANSCAPE_ROOT_DIRECTORY = '/Users/elsieling/cnra/env'
 
 
-def run(verbose=True):
+def run(region: str):
     """Loads the rasters defined by the configuration into the database."""
-    # Function that connects a  object with a Boundary object
-    # in the database.
-    def presave_callback_generator(fkey):
-        def cb(sender, instance, *args, **kwargs):
-            instance.boundary = fkey
-        return cb
-
-    # Build the configuration object.
     config_path = os.path.join(
-        PLANSCAPE_ROOT_DIRECTORY, 'src/config/metrics.json')
+    PLANSCAPE_ROOT_DIRECTORY, 'Planscape/src/planscape/config/conditions.json')
     config = PillarConfig(config_path)
+    # data_path = os.path.join(PLANSCAPE_ROOT_DIRECTORY, 'data')
 
-    # Read the shapefiles and add Boundary and BoundaryDetail objects.
-    data_path = os.path.join(PLANSCAPE_ROOT_DIRECTORY, 'data')
+    # Save RRK base and normalized metrics to DB
+    region = config.get_region(region)
+    for pillar in config.get_pillars(region):
+        for element in config.get_elements(pillar):
+            for metric in config.get_metrics(element):
+                    print(metric['metric_name'])
+                    base_metric = BaseCondition(condition_name=metric['metric_name'], condition_level=ConditionLevel.METRIC,
+                        region_name=region['region_name'])
+                    metric_path = os.path.join(PLANSCAPE_ROOT_DIRECTORY, metric['filepath']) + '.tif'
+                    condition = Condition(condition_dataset=base_metric, geometry=raster, condition_score_type=ConditionScoreType.CURRENT)
+            # Compute elements/pillars/ecosystem scores and save to database
+            # base_element = BaseCondition(condition_name=element['element_name'], condition_level=ConditionLevel.ELEMENT, region_name=region['region_name'])
+            # reader = ConditionReader()
+            # element_nparray = score_element(reader, element, ConditionScoreType.CURRENT, recompute=True)
+            
 
-    print("Loading...")
-    raster = GDALRaster(os.path.join(
-        data_path, 'conditions/tcsi/forest_resilience/current.tif'), write=True)
-    dataset = BaseCondition(condition_name='forest_resilience', condition_level=ConditionLevel.PILLAR,
-                            region_name=RegionName.TCSI)
-    print("Saving")
-    dataset.save()
-    condition = Condition(condition_dataset=dataset, geometry=raster,
-                          condition_score_type=ConditionScoreType.CURRENT)
-    condition.save()                      
 
-    # for boundary in config.get_metric('tcsi', '')
-    #    # Create the new top-level Boundary
-    #    boundary_name = boundary['boundary_name']
-    #    print("Creating Boundary " + boundary_name)
-    #    query = Boundary.objects.filter(boundary_name__exact=boundary_name)
-    #    if len(query) > 0:
-    #        print("Boundary " + boundary_name + " already exists; deleting.")
-    #        query.delete()
-    #    boundary_obj = Boundary(boundary_name=boundary_name)
-    #    boundary_obj.save()
 
-    #    shapefile_field_mapping = dict(boundary['shapefile_field_mapping'])
-    #    shapefile_field_mapping['geometry'] = boundary['geometry_type']
-    #    filepath = Path(os.path.join(data_path, boundary['filepath']))
-    #    srs = boundary['source_srs']
-    #    lm = LayerMapping(BoundaryDetails, filepath,
-    #                      shapefile_field_mapping, source_srs=srs, transform=True)
-    #    presave_callback = presave_callback_generator(boundary_obj)
-    #    pre_save.connect(presave_callback, sender=BoundaryDetails)
-    #    lm.save(strict=True, verbose=verbose)
-    #    pre_save.disconnect(presave_callback, sender=BoundaryDetails)
+
+
+
+    # query = BaseCondition.objects.filter(condition_name=condition_name)
+    # if len(query) > 0:
+    #     print("BaseCondition " + condition_name + " already exists; deleting.")
+    #     query.delete()
+    # dataset.save()
+    # print("Saved base condition: " + dataset.condition_name)
+
+
+
+    # condition.save()              
+    # print("Saved condition with BaseCondition: " + str(condition.pk))        
+
+    # print("Verifying that raster was written")
+    # query = Condition.objects.get(pk=condition.pk)
+    # print(query.geometry.srid)
+
+
+
