fd79117ac982533feb33b8fd368b7aadceefc3de
Jessica Lee <jessxlee@google.com>
2022-11-23 11:28:10 -0800

    Add REST endpoint to get conditions data based on region name

--- src/planscape/conditions/views.py
+++ src/planscape/conditions/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpRequest, HttpResponse, QueryDict
+from django.http import HttpRequest, HttpResponse, JsonResponse, QueryDict
 from django.db import connection
 
+import json, os
+
+PLANSCAPE_ROOT_DIRECTORY = '/Users/jessxlee/cnra/'
 
 # Name of the table and column from models.py.
 RASTER_TABLE = 'condition_raster'
@@ -34,7 +37,29 @@ def get_wms(params: QueryDict):
         row = cursor.fetchone()
     return row
 
+def get_region(params: QueryDict):
+    # Get region name
+    assert isinstance(params['region_name'], str)
+    region_name = params['region_name']
+    print(region_name)
+
+    # Read from conditions config
+    config_path = os.path.join(
+       PLANSCAPE_ROOT_DIRECTORY, 'Planscape/src/planscape/config/conditions.json')
+    conditions_config = json.load(open(config_path, 'r'))
+
+    # Extract specific region data from JSON
+    for region in conditions_config['regions']:
+        print(region)
+        if region_name == region['region_name']:
+            return region
+
+    return None
 
 def wms(request: HttpRequest) -> HttpResponse:
     image = get_wms(request.GET)
     return HttpResponse(image, content_type=request.GET['format'])
+
+def regions(request: HttpRequest) -> HttpResponse :
+    region = get_region(request.GET)
+    return JsonResponse(region)
