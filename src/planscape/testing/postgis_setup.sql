7ebd1318e82c6073627dc567da42295d7e07233f
Jon Riecke <riecke@google.com>
2022-11-28 08:12:34 -0500

    Better setup script 	modified:   src/planscape/testing/postgis_setup.sql

--- src/planscape/testing/postgis_setup.sql
+++ src/planscape/testing/postgis_setup.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_raster;
-ALTER USER planscape CREATEDB;
 SET postgis.enable_outdb_rasters = True;
 SET postgis.gdal_enabled_drivers = 'GTiff JPEG PNG';
+CREATE USER planscape WITH PASSWORD 'pass';
+ALTER USER planscape CREATEDB;
