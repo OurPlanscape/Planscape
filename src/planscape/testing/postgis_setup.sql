c58614fa2c1fb1c37bc1248ad2b27692479b6960
Jon Riecke <riecke@google.com>
2022-11-28 08:46:04 -0500

    Try to set up GDAL drivers 	modified:   src/planscape/testing/postgis_setup.sql

--- src/planscape/testing/postgis_setup.sql
+++ src/planscape/testing/postgis_setup.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_raster;
-SET postgis.enable_outdb_rasters = True;
-SET postgis.gdal_enabled_drivers = 'GTiff JPEG PNG';
+ALTER DATABASE template1 SET postgis.enable_outdb_rasters = True;
+ALTER DATABASE template1 SET postgis.gdal_enabled_drivers = 'GTiff JPEG PNG';
 CREATE USER planscape WITH PASSWORD 'pass';
 ALTER USER planscape CREATEDB;
 ALTER TABLE spatial_ref_sys OWNER TO planscape;
