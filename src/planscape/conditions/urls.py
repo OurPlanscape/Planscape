96503e2af3d8468c8f2867471e85f33123c14d5a
Jessica Lee <jessxlee@google.com>
2022-11-28 09:13:08 -0800

    resolved PR changes

--- src/planscape/conditions/urls.py
+++ src/planscape/conditions/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'conditions'
+
 urlpatterns = [
     path('wms/', views.wms),
-    path('regions/', views.regions)
+    path('config/', views.config)
 ]
