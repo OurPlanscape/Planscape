0d237a62590e6a4a0b0375043971ddca4668f8bb
Hana Lee <leehana@alumni.stanford.edu>
2022-11-29 15:17:07 -0800

    Fix unit tests

--- src/interface/src/app/types/data.types.ts
+++ src/interface/src/app/types/data.types.ts
@@ -1,26 +1,26 @@
 export interface ConditionsConfig {
-  display_name: string;
-  filepath: string;
-  region_name: string;
-  pillars: PillarConfig[];
+  display_name?: string;
+  filepath?: string;
+  region_name?: string;
+  pillars?: PillarConfig[];
 }
 
 export interface ElementConfig {
-  display_name: string;
-  element_name: string;
-  filepath: string;
-  metrics: MetricConfig[];
+  display_name?: string;
+  element_name?: string;
+  filepath?: string;
+  metrics?: MetricConfig[];
 }
 
 export interface MetricConfig {
   metric_name: string;
   filepath: string;
-  display_name: string;
+  display_name?: string;
 }
 
 export interface PillarConfig {
-  display_name: string;
-  pillar_name: string;
-  filepath: string;
-  elements: ElementConfig[];
+  display_name?: string;
+  pillar_name?: string;
+  filepath?: string;
+  elements?: ElementConfig[];
 }
