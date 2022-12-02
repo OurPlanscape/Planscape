22cb0d41c929942e233d0d20867338a7b76f18ae
Jon Riecke <riecke@google.com>
2022-12-02 09:55:39 -0500

    Initial version of the plan project 	new file:   src/planscape/plan/__init__.py 	new file:   src/planscape/plan/admin.py 	new file:   src/planscape/plan/apps.py 	new file:   src/planscape/plan/migrations/0001_initial.py 	new file:   src/planscape/plan/migrations/__init__.py 	new file:   src/planscape/plan/models.py 	new file:   src/planscape/plan/tests.py 	new file:   src/planscape/plan/views.py 	modified:   src/planscape/planscape/settings.py

---
+++ src/planscape/plan/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+
+
+class Plan(models.Model):
+    """
+    A Plan is associated with one User, the owner, and one Region.  It has a name,
+    status (locked/public), and a geometry representing the planning area.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)  # type: ignore
+
+    # The region_name for the plan; should be one of the valid region names in base.region_names.
+    region_name: models.CharField = models.CharField(max_length=120)
+
+    # Whether the plan has been made "public".
+    public: models.BooleanField = models.BooleanField(null=True)
+
+    # Whether the plan has been "locked".
+    locked: models.BooleanField = models.BooleanField(null=True)
+
+    # The planning area of the plan.
+    geometry = models.MultiPolygonField(srid=4269, null=True)
--- src/planscape/planscape/settings.py
+++ src/planscape/planscape/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_gis',
     'boundary',
     'conditions',
+    'plan',
     'leaflet',
     'corsheaders',
     'allauth',
