649a03cac17f6f1fe3fb54d0b7926a2fbb5fb401
Jon Riecke <riecke@google.com>
2022-12-06 11:38:22 -0500

    Regularize region naming 	modified:   src/interface/src/app/services/map.service.ts 	modified:   src/planscape/base/region_name.py 	modified:   src/planscape/boundary/viewsets.py

--- src/planscape/base/region_name.py
+++ src/planscape/base/region_name.py
@@ -6,11 +6,10 @@ import enum
 
 class RegionName(str, enum.Enum):
     """Valid Regions."""
-    # Tahoe Central-Sierra Initiative
-    TCSI = 'tcsi'
 
     # California Wildfire and Forest Resilience Task Force regions.
     SIERRA_CASCADE_INYO = 'sierra_cascade_inyo'
     NORTH_COAST_INLAND = 'north_coast_inland'
     COASTAL_INLAND = 'coastal_inland'
     SOUTHERN_CALIFORNIA = 'southern_california'
+
