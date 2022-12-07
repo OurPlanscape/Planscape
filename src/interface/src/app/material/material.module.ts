8f55986e0a9c54d6db66cf8b5521e951de497ce7
Hana Lee <leehana@alumni.stanford.edu>
2022-12-07 13:42:12 -0800

    popover menu with placeholder condition info

--- src/interface/src/app/material/material.module.ts
+++ src/interface/src/app/material/material.module.ts
@@ -9,6 +9,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
@@ -31,6 +32,7 @@ import { MatTreeModule } from '@angular/material/tree';
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatMenuModule,
     MatProgressSpinnerModule,
     MatRadioModule,
     MatSelectModule,
