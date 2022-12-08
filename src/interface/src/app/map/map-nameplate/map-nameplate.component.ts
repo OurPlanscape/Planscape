873fc7b20cc0fcf524b6b925d3ca60e0508212da
Hana Lee <leehana@alumni.stanford.edu>
2022-12-07 16:07:48 -0800

    fix tests

--- src/interface/src/app/map/map-nameplate/map-nameplate.component.ts
+++ src/interface/src/app/map/map-nameplate/map-nameplate.component.ts
@@ -1,24 +1,24 @@
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { filter } from 'rxjs/operators';
-import { Observable, BehaviorSubject, of } from 'rxjs';
-import { Component, Input, OnInit, AfterViewInit, AfterContentInit, AfterContentChecked, ChangeDetectorRef } from '@angular/core';
 import { Map } from 'src/app/types';
 
-const NAMEPLATE_RIGHT_MARGIN = 16;
+export const NAMEPLATE_RIGHT_MARGIN = 16;
 
 @Component({
   selector: 'app-map-nameplate',
   templateUrl: './map-nameplate.component.html',
   styleUrls: ['./map-nameplate.component.scss'],
 })
-export class MapNameplateComponent implements OnInit {
+export class MapNameplateComponent implements AfterViewInit {
   @Input() map: Map | null = null;
   @Input() selected: boolean = false;
   @Input() width$: Observable<number | null> = of(null);
 
   widthInPx: string = '100%';
 
-  ngOnInit() {
-    this.width$.pipe(filter(width => !!width)).subscribe(width => {
+  ngAfterViewInit() {
+    this.width$.pipe(filter((width) => !!width)).subscribe((width) => {
       // Timeout is required to avoid triggering changes too quickly,
       // which causes errors with Angular's change detection.
       setTimeout(() => {
@@ -32,7 +32,9 @@ export class MapNameplateComponent implements OnInit {
    */
   private widthToPx(width: number | null): string {
     if (width != null) {
-      return Number(width - NAMEPLATE_RIGHT_MARGIN).toString().concat('px');
+      return Number(width - NAMEPLATE_RIGHT_MARGIN)
+        .toString()
+        .concat('px');
     }
     return '100%';
   }
