4ffac7a9577e917e55eeabec92978a116d11f5c4
Elsie Ling <elsieling@google.com>
2022-12-12 17:00:46 -0800

    add a test database option to to allow us to run unit tests on postgres

--- src/planscape/planscape/settings.py
+++ src/planscape/planscape/settings.py
@@ -97,10 +97,9 @@ DATABASES = {
         "PASSWORD": "pass",
         "PORT": 5432,
         "USER": "planscape",
-    },
-    'sqllite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': 'auto_test',
+        }
     }
 }
 
