a4bde5985c1074f9588cc9ff9272c1d2e3de6717
Hana Lee <leehana@alumni.stanford.edu>
2022-12-13 15:16:14 -0800

    Creature feature module with route guard, service, and feature flag directive

---
+++ src/interface/src/app/features/features.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FeatureFlagDirective } from './feature-flag.directive';
+import { FeatureService } from './feature.service';
+
+@NgModule({
+  declarations: [
+    FeatureFlagDirective
+  ],
+  imports: [CommonModule],
+  providers: [FeatureService],
+  exports: [FeatureFlagDirective],
+})
+export class FeaturesModule {}
