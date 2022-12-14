9f43151737faea199b653fb8e467b42f1a0facc0
cynthia huang <cynhuang@google.com>
2022-12-13 19:02:17 -0800

    output the uploaded file to parent component

--- src/interface/src/app/shared/file-uploader/file-uploader.component.ts
+++ src/interface/src/app/shared/file-uploader/file-uploader.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'file-uploader',
@@ -6,7 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./file-uploader.component.scss']
 })
 export class FileUploaderComponent {
+  /** File selected event. */
+  @Output() fileEvent = new EventEmitter<any>();
+
   fileName = '';
+  file: File | null = null;
 
   @Input()
     requiredFileType: string = 'application/zip';
@@ -14,6 +18,17 @@ export class FileUploaderComponent {
   onFileSelected(event: Event) {
     const target = event.target as HTMLInputElement;
     const files = target.files as FileList;
-    console.log(files);
+    if (target.files) {
+      this.file = files.item(0);
+      this.fileName = this.file?.name || '';
+      this.emitEvent(this.file!);
+    }
+  }
+
+  /**
+   * Triggers file upload event.
+   */
+   private emitEvent(file: File) {
+    this.fileEvent.emit({type: 'area_upload', value: file});
   }
 }
