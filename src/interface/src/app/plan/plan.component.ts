973cf54d4f347eb1acf5f4291ad1df338a6d4a38
Hana Lee <leehana@alumni.stanford.edu>
2022-12-15 15:50:09 -0800

    Add fake plan polygon to plan map view

--- src/interface/src/app/plan/plan.component.ts
+++ src/interface/src/app/plan/plan.component.ts
@@ -1,7 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 import * as L from 'leaflet';
+
+import { Plan, Region } from '../types';
 
 @Component({
   selector: 'app-plan',
@@ -10,8 +11,22 @@ import * as L from 'leaflet';
 })
 export class PlanComponent implements AfterViewInit {
   private map!: L.Map;
+  private plan: Plan | null = null;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    // TODO(leehana): Use a fake plan until we can query plans from the DB
+    this.plan = {
+      id: 'fake',
+      name: 'fake',
+      ownerId: 'fake',
+      region: Region.SIERRA_NEVADA,
+      planningArea: new L.Polygon([
+        new L.LatLng(38.715517043571914, -120.42857302225725),
+        new L.LatLng(38.47079787227401, -120.5164425608172),
+        new L.LatLng(38.52668443555346, -120.11828371421737),
+      ]).toGeoJSON(),
+    };
+  }
 
   ngAfterViewInit(): void {
     this.map = L.map('map', {
@@ -22,6 +37,20 @@ export class PlanComponent implements AfterViewInit {
       pmIgnore: false,
     });
     this.map.attributionControl.setPosition('topright');
+
+    // Add planning area to map and frame it in view
+    if (!!this.plan) {
+      const planningArea = L.geoJSON(this.plan.planningArea, {
+        pane: 'overlayPane',
+        style: {
+          color: '#7b61ff',
+          fillColor: '#7b61ff',
+          fillOpacity: 0.2,
+          weight: 3,
+        },
+      }).addTo(this.map);
+      this.map.fitBounds(planningArea.getBounds());
+    }
   }
 
   /** Creates a basemap layer using the Stadia.AlidadeSmooth tiles. */
