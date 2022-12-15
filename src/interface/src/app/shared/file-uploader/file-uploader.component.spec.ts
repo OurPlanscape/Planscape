bdd4426d4aa009274c38c8cc36ff611f5df4e9fb
cynthia huang <cynhuang@google.com>
2022-12-15 15:40:59 -0800

    add tests for file-uploader component

--- src/interface/src/app/shared/file-uploader/file-uploader.component.spec.ts
+++ src/interface/src/app/shared/file-uploader/file-uploader.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { FileUploaderComponent } from './file-uploader.component';
 
-describe('FileUploaderComponent', () => {
+fdescribe('FileUploaderComponent', () => {
   let component: FileUploaderComponent;
   let fixture: ComponentFixture<FileUploaderComponent>;
 
@@ -17,7 +18,29 @@ describe('FileUploaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should update the file model when a file is loaded', () => {
+    const fileList = new DataTransfer();
+    fileList.items.add(new File([''], 'test-file.zip'));
+    const inputDebugEl  = fixture.debugElement.query(By.css('input[type=file]'));
+    inputDebugEl.nativeElement.files = fileList.files;
+
+    inputDebugEl.nativeElement.dispatchEvent(new InputEvent('change'));
+    fixture.detectChanges();
+
+    expect(component.file).toBeTruthy()
+    expect(component.fileName).toBe('test-file.zip')
   });
+
+  it('file upload event should emit the file', () => {
+    const element = fixture.nativeElement;
+    const input = element.querySelector('.file-input');
+    spyOn(component, 'onFileUploaded').and.callThrough();
+    spyOn(component.fileEvent, 'emit');
+
+    input.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+
+    expect(component.onFileUploaded).toHaveBeenCalled();
+    expect(component.fileEvent.emit).toHaveBeenCalled();
+});
 });
