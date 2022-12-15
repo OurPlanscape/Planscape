8facbf80f070c7c77a28c56ac119e5359180d4f0
Hana Lee <leehana@alumni.stanford.edu>
2022-12-15 15:17:12 -0800

    Add leaflet map to plan view

--- src/interface/src/app/plan/plan.component.ts
+++ src/interface/src/app/plan/plan.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
   styleUrls: ['./plan.component.scss'],
 })
-export class PlanComponent {
+export class PlanComponent implements AfterViewInit {
+  private map!: L.Map;
+
   constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    this.map = L.map('map', {
+      center: [38.646, -120.548],
+      zoom: 9,
+      layers: [this.stadiaAlidadeTiles()],
+      zoomControl: false,
+      pmIgnore: false,
+    });
+    this.map.attributionControl.setPosition('topright');
+  }
+
+  /** Creates a basemap layer using the Stadia.AlidadeSmooth tiles. */
+  private stadiaAlidadeTiles() {
+    return L.tileLayer(
+      'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://stadiamaps.com/" target="_blank" rel="noreferrer">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank" rel="noreferrer">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org" target="_blank" rel="noreferrer">OpenStreetMap</a> contributors',
+      }
+    );
+  }
 
   expandMap() {
     this.router.navigate(['map']);
