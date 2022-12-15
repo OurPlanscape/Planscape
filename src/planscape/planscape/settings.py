fddd6693eda5c1e8360bf8f8ee20bcce870749bf
Jon Riecke <riecke@google.com>
2022-12-14 20:56:11 -0500

    Add functionality to the docker deployment: 1. Use docker-compose to build containers 2. Add uwsgi for Django 3. Connect Django and nginx containers via a socket 4. Expose only port 80 from nginx 5. Proxy /planscape-backend requests to uwsgi 	new file:   src/docker-compose.yaml 	modified:   src/interface/Dockerfile 	modified:   src/interface/nginx/nginx.conf 	new file:   src/interface/nginx/uwsgi_params 	modified:   src/interface/src/app/backend-constants.ts 	modified:   src/planscape/Dockerfile 	new file:   src/planscape/planscape/planscape.sock 	modified:   src/planscape/planscape/settings.py 	modified:   src/planscape/planscape/urls.py 	new file:   src/planscape/planscape/uwsgi.ini 	modified:   src/planscape/requirements.txt

--- src/planscape/planscape/settings.py
+++ src/planscape/planscape/settings.py
@@ -209,3 +209,22 @@ CACHES = {
         'LOCATION': config('PLANSCAPE_CACHE_LOCATION', 'planscape-cache'),
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    "formatters": {
+        "verbose": {
+            "format": "[{asctime}] {levelname}: {name}.{message}",
+            "style": "{",
+        },
+    },
+
+    'handlers': {
+         "console": {'level': 'DEBUG', "class": "logging.StreamHandler", "formatter": "verbose"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
