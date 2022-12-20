2ec71cfa303084c712f58490f00b2dade2c78d93
cynthia huang <cynhuang@google.com>
2022-12-19 18:11:48 -0800

    make progress panel clickable

--- src/interface/src/app/plan/progress-panel/progress-panel.component.ts
+++ src/interface/src/app/plan/progress-panel/progress-panel.component.ts
@@ -1,43 +1,61 @@
-import { AfterViewInit, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { FlatTreeControl } from '@angular/cdk/tree';
-import { MatTreeFlatDataSource, MatTreeFlattener } from "@angular/material/tree";
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
 
 import { Plan } from '../../types';
 
 interface ProgressTask {
+  id: number;
   stepNumber?: number;
   taskName: string;
   done: boolean;
+  clickable: boolean;
   children?: ProgressTask[];
 }
 
 /** Flat node with expandable and level information */
 interface ProgressFlatNode {
   expandable: boolean;
-  stepNumber?: number;
   name: string;
   level: number;
+  id: number;
+  stepNumber?: number;
+  clickable: boolean;
   done: boolean;
 }
 
 @Component({
   selector: 'progress-panel',
   templateUrl: './progress-panel.component.html',
-  styleUrls: ['./progress-panel.component.scss']
+  styleUrls: ['./progress-panel.component.scss'],
 })
 export class ProgressPanelComponent implements OnChanges, AfterViewInit {
-  @Input()
-  plan: Plan | null = null;
+  @Input() plan: Plan | null = null;
+  currentTaskId: number = 3;
 
-  private mapProgressTaskToFlatNode = (node: ProgressTask, level: number): ProgressFlatNode => {
+  private mapProgressTaskToFlatNode = (
+    node: ProgressTask,
+    level: number
+  ): ProgressFlatNode => {
     const progressFlatNode: ProgressFlatNode = {
       expandable: !!node.children && node.children.length > 0,
       name: node.taskName,
       level: level,
+      id: node.id,
       done: node.done,
+      clickable: node.clickable,
     };
     if ('stepNumber' in node) {
-      progressFlatNode['stepNumber'] = node.stepNumber
+      progressFlatNode['stepNumber'] = node.stepNumber;
     }
     return progressFlatNode;
   };
@@ -54,54 +72,7 @@ export class ProgressPanelComponent implements OnChanges, AfterViewInit {
     (node) => node.children
   );
 
-  dataSource = new MatTreeFlatDataSource(
-    this.treeControl, this.treeFlattener);
-
-  progressTaskList: ProgressTask[] = [
-      {
-        stepNumber: 1,
-        taskName: 'Identify planning area',
-        done: true,
-      },
-      {
-        stepNumber: 2,
-        taskName: 'Create a Plan',
-        done: true,
-      },
-      {
-        stepNumber: 3,
-        taskName: 'Create scenarios',
-        done: false,
-        children: [
-          {
-            taskName: 'Set priorities',
-            done: false,
-          },
-          {
-            taskName: 'Set constraints',
-            done: false,
-          },
-          {
-            taskName: 'Explore options',
-            done: false,
-          },
-          {
-            taskName: 'Save scenarios',
-            done: false,
-          },
-        ]
-      },
-      {
-        stepNumber: 4,
-        taskName: 'Compare scenarios',
-        done: false,
-      },
-      {
-        stepNumber: 5,
-        taskName: 'Save plan',
-        done: false,
-      }
-    ]
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   constructor() {}
 
@@ -114,12 +85,74 @@ export class ProgressPanelComponent implements OnChanges, AfterViewInit {
   }
 
   setProgressList() {
-    console.log(this.plan);
-    this.dataSource.data = this.progressTaskList;
+    this.dataSource.data = [
+      {
+        id: 1,
+        stepNumber: 1,
+        taskName: 'Identify planning area',
+        done: true,
+        clickable: false,
+      },
+      {
+        id: 2,
+        stepNumber: 2,
+        taskName: 'Create a Plan',
+        done: true,
+        clickable: false,
+      },
+      {
+        id: 3,
+        stepNumber: 3,
+        taskName: 'Create scenarios',
+        done: false,
+        clickable: false,
+        children: [
+          {
+            id: 4,
+            taskName: 'Set priorities',
+            done: false,
+            clickable: true,
+          },
+          {
+            id: 5,
+            taskName: 'Set constraints',
+            done: false,
+            clickable: true,
+          },
+          {
+            id: 6,
+            taskName: 'Explore options',
+            done: false,
+            clickable: true,
+          },
+          {
+            id: 7,
+            taskName: 'Save scenarios',
+            done: false,
+            clickable: true,
+          },
+        ],
+      },
+      {
+        id: 8,
+        stepNumber: 4,
+        taskName: 'Compare scenarios',
+        done: false,
+        clickable: true,
+      },
+      {
+        id: 9,
+        stepNumber: 5,
+        taskName: 'Save plan',
+        done: false,
+        clickable: true,
+      },
+    ];
   }
 
-  trackByTask(_: number, task: ProgressTask) {
-    return task.taskName;
+  goToStep(node: ProgressFlatNode) {
+    console.log('clicked ' + node.name);
+    this.currentTaskId = node.id;
   }
 
   /** Used to compute whether a node in the progress tree has children. */
@@ -127,5 +160,4 @@ export class ProgressPanelComponent implements OnChanges, AfterViewInit {
 
   /** Used to compute whether a node is a child. */
   isChild = (node: ProgressFlatNode) => node.level >= 1;
-
 }
