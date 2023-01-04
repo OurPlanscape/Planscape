8586529709984cc914c8892f1751a7d8082af0b0
Jon Riecke <riecke@google.com>
2023-01-04 10:01:28 -0500

    Restore boundary/tests.py 	modified:   src/planscape/boundary/tests.py

--- src/planscape/boundary/tests.py
+++ src/planscape/boundary/tests.py
@@ -1,11 +1,4 @@
 from django.test import TestCase
-from django.urls import reverse
+from boundary.models import Boundary, BoundaryDetails
 
-from .models import Boundary, BoundaryDetails
-
-
-class BoundaryTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username='testuser')
-        self.user.set_password('12345')
-        self.user.save()
+# Create your tests here.
