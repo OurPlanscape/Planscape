707bbfa5a97123e3bbe38be2203948840565d0b7
Jon Riecke <riecke@google.com>
2023-01-04 09:50:55 -0500

    Return geometry from get_plan,list_plans_by_owner 	modified:   src/planscape/plan/serializers.py 	modified:   src/planscape/plan/tests.py 	modified:   src/planscape/plan/views.py

--- src/planscape/plan/tests.py
+++ src/planscape/plan/tests.py
@@ -1,4 +1,7 @@
+import json
+
 from django.contrib.auth.models import User
+from django.contrib.gis.geos import GEOSGeometry
 from django.test import TransactionTestCase
 from django.urls import reverse
 
@@ -79,7 +82,7 @@ class PlanTest(TransactionTestCase):
         response = self.client.post(
             reverse('plan:create'),
             {'name': 'plan', 'geometry': {'features': [
-                {'geometry': {'type': 'MultiPolygon', 'coordinates': [[[1, 2], [2, 3], [3, 4], [1, 2]]]}}]}},
+                {'geometry': {'type': 'MultiPolygon', 'coordinates': [[[[1, 2], [2, 3], [3, 4], [1, 2]]]]}}]}},
             content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(Plan.objects.all()), 1)
@@ -89,7 +92,7 @@ class PlanTest(TransactionTestCase):
         response = self.client.post(
             reverse('plan:create'),
             {'name': 'plan', 'region_name': 'north_coast_inland', 'geometry': {'features': [
-                {'geometry': {'type': 'MultiPolygon', 'coordinates': [[[1, 2], [2, 3], [3, 4], [1, 2]]]}}]}},
+                {'geometry': {'type': 'MultiPolygon', 'coordinates': [[[[1, 2], [2, 3], [3, 4], [1, 2]]]]}}]}},
             content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(Plan.objects.all()), 1)
@@ -100,24 +103,28 @@ class GetPlanTest(TransactionTestCase):
         self.user = User.objects.create(username='testuser')
         self.user.set_password('12345')
         self.user.save()
+        self.geometry = {'type': 'MultiPolygon',
+                         'coordinates': [[[[1, 2], [2, 3], [3, 4], [1, 2]]]]}
+        stored_geometry = GEOSGeometry(json.dumps(self.geometry))
         self.plan_no_user = Plan.objects.create(
-            owner=None, name='ownerless', region_name='sierra_cascade_inyo')
+            owner=None, name='ownerless', region_name='sierra_cascade_inyo', geometry=stored_geometry)
         self.plan_no_user.save()
         self.plan_with_user = Plan.objects.create(
             owner=self.user, name='with_owner', region_name='sierra_cascade_inyo')
         self.plan_with_user.save()
 
     def test_get_plan_with_user(self):
-        response = self.client.get(reverse('plan:get_plan'), {'id': self.plan_with_user.pk}, 
-            content_type="application/json")
+        response = self.client.get(reverse('plan:get_plan'), {'id': self.plan_with_user.pk},
+                                   content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['name'], 'with_owner')
 
     def test_get_plan_no_user(self):
-        response = self.client.get(reverse('plan:get_plan'), {'id': self.plan_no_user.pk}, 
-            content_type="application/json")
+        response = self.client.get(reverse('plan:get_plan'), {'id': self.plan_no_user.pk},
+                                   content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['name'], 'ownerless')
+        self.assertEqual(response.json()['geometry'], self.geometry)
 
 
 class ListPlansTest(TransactionTestCase):
@@ -125,8 +132,11 @@ class ListPlansTest(TransactionTestCase):
         self.user = User.objects.create(username='testuser')
         self.user.set_password('12345')
         self.user.save()
+        self.geometry = {'type': 'MultiPolygon',
+                         'coordinates': [[[[1, 2], [2, 3], [3, 4], [1, 2]]]]}
+        stored_geometry = GEOSGeometry(json.dumps(self.geometry))
         self.plan_A_no_user = Plan.objects.create(
-            owner=None, name='A_ownerless', region_name='sierra_cascade_inyo')
+            owner=None, name='A_ownerless', region_name='sierra_cascade_inyo', geometry=stored_geometry)
         self.plan_A_no_user.save()
         self.plan_B_no_user = Plan.objects.create(
             owner=None, name='B_ownerless', region_name='sierra_cascade_inyo')
@@ -139,13 +149,16 @@ class ListPlansTest(TransactionTestCase):
         self.plan_B_with_user.save()
 
     def test_list_plans_by_owner_with_user(self):
-        response = self.client.get(reverse('plan:list_plans_by_owner'), {'owner': self.user.pk}, 
-            content_type="application/json")
+        response = self.client.get(reverse('plan:list_plans_by_owner'), {'owner': self.user.pk},
+                                   content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 2)
 
     def test_list_plans_by_owner_no_user(self):
-        response = self.client.get(reverse('plan:list_plans_by_owner'), {}, 
-            content_type="application/json")
+        response = self.client.get(reverse('plan:list_plans_by_owner'), {},
+                                   content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 2)
+        for plan in response.json():
+            if plan['owner'] == 'A_ownerless':
+                self.assertEqual(plan['geometry'], self.geometry)
