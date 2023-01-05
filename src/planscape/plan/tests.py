f913ea52eb99a5d9a20bae16011c1467b2571351
Jon Riecke <riecke@google.com>
2023-01-05 13:44:23 -0500

    Add creation_time and # projects/scenarios to APIs 	new file:   src/planscape/plan/migrations/0004_plan_creation_time.py 	modified:   src/planscape/plan/models.py 	modified:   src/planscape/plan/serializers.py 	modified:   src/planscape/plan/tests.py 	modified:   src/planscape/plan/views.py

--- src/planscape/plan/tests.py
+++ src/planscape/plan/tests.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 from django.contrib.gis.geos import GEOSGeometry
 from django.test import TransactionTestCase
 from django.urls import reverse
-
-from .models import Plan
 from planscape.settings import PLANSCAPE_GUEST_CAN_SAVE
+
+from .models import Plan, Project, Scenario
 
 
 class PlanTest(TransactionTestCase):
@@ -99,6 +99,23 @@ class PlanTest(TransactionTestCase):
         self.assertEqual(len(Plan.objects.all()), 1)
 
 
+def create_plan(owner: User | None, name: str, geometry: GEOSGeometry | None, scenarios: list[int]):
+    """
+    Creates a plan with the given owner, name, geometry, and projects with the
+    number of scenarios.
+    """
+    plan = Plan.objects.create(
+        owner=owner, name=name, region_name='sierra_cascade_inyo', geometry=geometry)
+    plan.save()
+    for num_scenarios in scenarios:
+        project = Project.objects.create(owner=owner, plan=plan)
+        project.save()
+        for _ in range(num_scenarios):
+            scenario = Scenario.objects.create(project=project)
+            scenario.save()
+    return plan
+
+
 class GetPlanTest(TransactionTestCase):
     def setUp(self):
         self.user = User.objects.create(username='testuser')
@@ -107,18 +124,15 @@ class GetPlanTest(TransactionTestCase):
         self.geometry = {'type': 'MultiPolygon',
                          'coordinates': [[[[1, 2], [2, 3], [3, 4], [1, 2]]]]}
         stored_geometry = GEOSGeometry(json.dumps(self.geometry))
-        self.plan_no_user = Plan.objects.create(
-            owner=None, name='ownerless', region_name='sierra_cascade_inyo', geometry=stored_geometry)
-        self.plan_no_user.save()
-        self.plan_with_user = Plan.objects.create(
-            owner=self.user, name='with_owner', region_name='sierra_cascade_inyo')
-        self.plan_with_user.save()
+        self.plan_no_user = create_plan(None, 'ownerless', stored_geometry, [])
+        self.plan_with_user = create_plan(
+            self.user, 'owned', None, [])
 
     def test_get_plan_with_user(self):
         response = self.client.get(reverse('plan:get_plan'), {'id': self.plan_with_user.pk},
                                    content_type="application/json")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['name'], 'with_owner')
+        self.assertEqual(response.json()['name'], 'owned')
 
     def test_get_plan_no_user(self):
         response = self.client.get(reverse('plan:get_plan'), {'id': self.plan_no_user.pk},
@@ -136,24 +150,10 @@ class ListPlansTest(TransactionTestCase):
         self.geometry = {'type': 'MultiPolygon',
                          'coordinates': [[[[1, 2], [2, 3], [3, 4], [1, 2]]]]}
         stored_geometry = GEOSGeometry(json.dumps(self.geometry))
-        self.plan_A_no_user = Plan.objects.create(
-            owner=None, name='A_ownerless', region_name='sierra_cascade_inyo', geometry=stored_geometry)
-        self.plan_A_no_user.save()
-        self.plan_B_no_user = Plan.objects.create(
-            owner=None, name='B_ownerless', region_name='sierra_cascade_inyo')
-        self.plan_B_no_user.save()
-        self.plan_A_with_user = Plan.objects.create(
-            owner=self.user, name='A_with_owner', region_name='sierra_cascade_inyo')
-        self.plan_A_with_user.save()
-        self.plan_B_with_user = Plan.objects.create(
-            owner=self.user, name='B_with_owner', region_name='sierra_cascade_inyo')
-        self.plan_B_with_user.save()
-
-    def test_list_plans_by_owner_with_user(self):
-        response = self.client.get(reverse('plan:list_plans_by_owner'), {'owner': self.user.pk},
-                                   content_type="application/json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
+        self.plan1 = create_plan(None, 'plan1', None, [])
+        self.plan2 = create_plan(None, 'plan2', stored_geometry, [0])
+        self.plan3 = create_plan(self.user, 'plan3', stored_geometry, [1])
+        self.plan4 = create_plan(self.user, 'plan4', stored_geometry, [2, 1])
 
     def test_list_plans_by_owner_no_user(self):
         response = self.client.get(reverse('plan:list_plans_by_owner'), {},
@@ -161,8 +161,30 @@ class ListPlansTest(TransactionTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 2)
         for plan in response.json():
-            if plan['owner'] == 'A_ownerless':
-                self.assertEqual(plan['geometry'], self.geometry)
+            if plan['name'] == 'plan1':
+                self.assertEqual(plan['projects'], 0)
+                self.assertEqual(plan['scenarios'], 0)
+            elif plan['name'] == 'plan2':
+                self.assertEqual(plan['projects'], 1)
+                self.assertEqual(plan['scenarios'], 0)
+            else:
+                self.assertTrue(False)
+
+    def test_list_plans_by_owner_with_user(self):
+        response = self.client.get(reverse('plan:list_plans_by_owner'), {'owner': self.user.pk},
+                                   content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+        for plan in response.json():
+            self.assertEqual(plan['geometry'], self.geometry)
+            if plan['name'] == 'plan3':
+                self.assertEqual(plan['projects'], 1)
+                self.assertEqual(plan['scenarios'], 1)
+            elif plan['name'] == 'plan4':
+                self.assertEqual(plan['projects'], 2)
+                self.assertEqual(plan['scenarios'], 3)
+            else:
+                self.assertTrue(False)
 
 
 class ProjectTest(TransactionTestCase):
