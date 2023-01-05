5ef89bdc5defdd3a49888f692d905f01188e9b50
Hana Lee <leehana@alumni.stanford.edu>
2023-01-05 10:36:03 -0800

    add create button to plan table view

--- src/interface/src/app/plan/plan-table/plan-table.component.spec.ts
+++ src/interface/src/app/plan/plan-table/plan-table.component.spec.ts
@@ -1,7 +1,11 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'src/app/material/material.module';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { of } from 'rxjs';
+import { MaterialModule } from 'src/app/material/material.module';
 import { PlanPreview, Region } from 'src/app/types';
 
 import { PlanService } from './../../services/plan.service';
@@ -16,19 +20,25 @@ describe('PlanTableComponent', () => {
 
   let component: PlanTableComponent;
   let fixture: ComponentFixture<PlanTableComponent>;
+  let loader: HarnessLoader;
   let fakeService: PlanService = jasmine.createSpyObj('PlanService', {
     listPlansByUser: of([fakePlan]),
   });
+  let routerStub = () => ({ navigate: (array: string[]) => ({}) });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BrowserAnimationsModule, MaterialModule],
       declarations: [PlanTableComponent],
-      providers: [{ provide: PlanService, useValue: fakeService }],
+      providers: [
+        { provide: PlanService, useValue: fakeService },
+        { provide: Router, useFactory: routerStub },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PlanTableComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
@@ -39,5 +49,15 @@ describe('PlanTableComponent', () => {
   it('should fetch plans from the DB', () => {
     expect(fakeService.listPlansByUser).toHaveBeenCalled();
     expect(component.datasource.data).toEqual([fakePlan]);
+  });
+
+  it('create button should navigate to region selection', async () => {
+    const routerStub: Router = fixture.debugElement.injector.get(Router);
+    spyOn(routerStub, 'navigate').and.callThrough();
+    const button = await loader.getHarness(MatButtonHarness);
+
+    await button.click();
+
+    expect(routerStub.navigate).toHaveBeenCalledOnceWith(['region']);
   });
 });
