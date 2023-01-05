3482dd8163d1870db86eea3d8e6aef428e51c7eb
Jon Riecke <riecke@google.com>
2023-01-05 13:44:23 -0500

    Add creation_time and # projects/scenarios to APIs 	new file:   src/planscape/plan/migrations/0004_plan_creation_time.py 	modified:   src/planscape/plan/models.py 	modified:   src/planscape/plan/serializers.py 	modified:   src/planscape/plan/tests.py 	modified:   src/planscape/plan/views.py

--- src/planscape/plan/models.py
+++ src/planscape/plan/models.py
@@ -1,16 +1,18 @@
+import math
+
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models
 
-import math 
 
 class Plan(models.Model):
     """
     A Plan is associated with one User, the owner, and one Region.  It has a name,
     status (locked/public), and a geometry representing the planning area.
     """
-    # TODO: Change "null=True" so that owner is not nullable. Currently owner can be null because 
+    # TODO: Change "null=True" so that owner is not nullable. Currently owner can be null because
     # we want alpha users to not be signed in.
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # type: ignore
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True)  # type: ignore
 
     # The name of the plan.
     name: models.CharField = models.CharField(max_length=120)
@@ -27,45 +29,57 @@ class Plan(models.Model):
     # The planning area of the plan.
     geometry = models.MultiPolygonField(srid=4269, null=True)
 
-class Project(models.Model): 
+    # The creation time of the plan, automatically set when the plan is created.
+    creation_time: models.DateTimeField = models.DateTimeField(
+        null=True, auto_now_add=True)
+
+
+class Project(models.Model):
     """
     A Project is associated with one User, the owner, and one Plan. It has optional user-specified
     project parameters, e.g. constraints.
     """
-    # TODO: Change "null=True" so that owner is not nullable. Currently owner can be null because 
+    # TODO: Change "null=True" so that owner is not nullable. Currently owner can be null because
     # we want alpha users to not be signed in.
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # type: ignore
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True)  # type: ignore
 
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)  # type: ignore
 
     # Project Parameters:
 
     # The maximum cost constraint. If null, no max cost.
-    max_cost:models.IntegerField = models.IntegerField(null=True) 
+    max_cost: models.IntegerField = models.IntegerField(null=True)
 
-    # TODO: Add more project parameters like min_acres_treated and 
-    # permitted_ownership = (1=federal, 2=state, 4=private) 
+    # TODO: Add more project parameters like min_acres_treated and
+    # permitted_ownership = (1=federal, 2=state, 4=private)
 
-class GeneratedProjectAreas(models.Model): 
+
+class GeneratedProjectAreas(models.Model):
     """
     GeneratedProjectAreas are associated with one Project. It has geometries representing 
     the project area, and an estimate of the area treated.
     """
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)  # type: ignore
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE)  # type: ignore
 
     # The project area geometries. May be one or more polygons that represent the project area.
-    project_area = models.MultiPolygonField(srid=4269, null=True) 
+    project_area = models.MultiPolygonField(srid=4269, null=True)
 
     # The sum total of the project area geometries.
-    estimated_area_treated:models.IntegerField = models.IntegerField(null=True) 
+    estimated_area_treated: models.IntegerField = models.IntegerField(
+        null=True)
+
 
 class Scenario(models.Model):
     """
     A Scenario is associated with one User, the owner, and one Project. It has optional user-specified
     prioritization parameters.
     """
-    # TODO: Change "null=True" so that owner is not nullable. Currently owner can be null because 
+    # TODO: Change "null=True" so that owner is not nullable. Currently owner can be null because
     # we want alpha users to not be signed in.
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # type: ignore
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True)  # type: ignore
 
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)  # type: ignore
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE)  # type: ignore
