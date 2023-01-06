134509bfa63b2a4bdc27be82930272defd8cc017
Hana Lee <leehana@alumni.stanford.edu>
2023-01-06 15:27:15 -0800

    plan timestamps and multiple delete ids

--- src/interface/src/app/services/plan.service.spec.ts
+++ src/interface/src/app/services/plan.service.spec.ts
@@ -56,18 +56,33 @@ describe('PlanService', () => {
   });
 
   describe('deletePlan', () => {
-    it('should make HTTP post request to DB', () => {
-      service.deletePlan('1').subscribe((res) => {
-        expect(res).toEqual(1);
+    it('should make HTTP post request to DB for a single ID', () => {
+      service.deletePlan(['1']).subscribe((res) => {
+        expect(res).toEqual('1');
       });
       const req = httpTestingController.expectOne(
         BackendConstants.END_POINT.concat('/plan/delete/?id=1')
       );
 
       expect(req.request.method).toEqual('POST');
-      expect(req.request.body['id']).toEqual(1);
+      expect(req.request.body['id']).toEqual('1');
 
-      req.flush(1);
+      req.flush('1');
+      httpTestingController.verify();
+    });
+
+    it('should make HTTP post request to DB for multiple IDs', () => {
+      service.deletePlan(['1', '2', '3']).subscribe((res) => {
+        expect(res).toEqual('1,2,3');
+      });
+      const req = httpTestingController.expectOne(
+        BackendConstants.END_POINT.concat('/plan/delete/?id=1,2,3')
+      );
+
+      expect(req.request.method).toEqual('POST');
+      expect(req.request.body['id']).toEqual('1,2,3');
+
+      req.flush('1,2,3');
       httpTestingController.verify();
     });
   });
@@ -106,6 +121,7 @@ describe('PlanService', () => {
         name: mockPlan.name,
         region: mockPlan.region,
         savedScenarios: 1,
+        createdTimestamp: 5000,
       };
 
       const backendPlan: BackendPlan = {
@@ -115,6 +131,7 @@ describe('PlanService', () => {
         region_name: mockPlan.region,
         geometry: mockPlan.planningArea,
         scenarios: 1,
+        creation_timestamp: 5,
       };
 
       service.listPlansByUser(null).subscribe((res) => {
