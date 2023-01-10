0dad43f7283cce1278c8a6ffe19bdc8a595509ca
Laurens Geffert <geffert@google.com>
2023-01-10 11:30:41 -0800

    updating CRS

--- analysis/code/calculate_buildings_raster.R
+++ analysis/code/calculate_buildings_raster.R
@@ -3,22 +3,22 @@ library(raster)
 library(fasterize)
 library(tidyverse)
 library(sf)
-library(microbenchmark)
 
 # load buildings data for California
 filepath <- 'analysis/data/California Building Outlines/California buildings.geojson'
 buildings <- st_read(filepath)
 
 # load an RRK metric for raster extent and resolution
-accel_fn <- 'analysis/data/Sierra Nevada ACCEL/fireAdaptComm/DamagePotential_WUI_2022_300m_normalized.tif'
-accel_extent <- raster(accel_fn)
+rrk_fn <- 'analysis/data/RRK - Sierra Nevada/forest_resilience/forest_composition_normalized.tif'
+rrk_extent <- raster(rrk_fn)
 # set all values to zero (just to be safe)
-values(accel_extent) <- 0
-names(accel_extent) <- 'buildings_present'
+values(rrk_extent) <- 0
+names(rrk_extent) <- 'buildings_present'
 # set correct CRS
-proj4string(accel_extent) <- CRS("+init=EPSG:3310")
+proj4string(rrk_extent) <- CRS('+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs')
 
 # # test speed of functions
+# library(microbenchmark)
 # pols <- buildings %>%
 #   sample_n(1e3) %>%
 #   mutate(n = 1)
@@ -32,30 +32,36 @@ proj4string(accel_extent) <- CRS("+init=EPSG:3310")
 # check extent and alignment
 buildings_sample <- buildings %>%
   sample_n(1e3) %>%
-  mutate(n = 1) %>%
-  st_transform(crs(accel_extent))
+  select() %>%
+  st_transform(crs(rrk_extent))
 
 ggplot() +
   geom_sf(data = buildings_sample) +
   geom_sf(
-    data = st_as_sfc(st_bbox(buildings)),
+    data = buildings %>%
+      st_transform(crs(rrk_extent)) %>%
+      st_bbox() %>%
+      st_as_sfc(),
     fill = NA,
     color = 'red') +
   geom_sf(
-    data = st_as_sfc(st_bbox(accel_extent)),
+    data = st_as_sfc(st_bbox(rrk_extent)),
     fill = NA,
     color = 'blue')
 
 # rasterize, checking if buildings are present in each cell
 buildings_raster <- buildings %>%
   mutate(n = 1) %>%
-  st_transform(crs(accel_extent)) %>%
+  st_transform(crs(rrk_extent)) %>%
   fasterize(
-    raster = accel_extent,
+    raster = rrk_extent,
     field = 'n',
     fun = 'sum',
     background = 0)
 buildings_raster
 
-writeRaster(buildings_raster, 'analysis/output/buildings_raster.tif')
+writeRaster(
+  x = buildings_raster,
+  filename = 'analysis/output/buildings_raster.tif',
+  overwrite = TRUE)
 
