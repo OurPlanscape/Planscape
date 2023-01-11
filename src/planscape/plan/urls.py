0fbf65c86b387b1d56d930176631bc92135b915f
Elsie Ling <elsieling@google.com>
2023-01-11 15:28:55 -0800

    add get_project endpoint

--- src/planscape/plan/urls.py
+++ src/planscape/plan/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from plan.views import (create_plan, create_project, delete, get_plan,
-                        list_plans_by_owner, create_project_area)
+                        list_plans_by_owner, create_project_area, get_project, get_project_areas)
 
 app_name = 'plan'
 
@@ -12,5 +12,7 @@ urlpatterns = [
     path('list_plans_by_owner/', list_plans_by_owner, name='list_plans_by_owner'),
     # Projects
     path('create_project/', create_project, name='create_project'),
+    path('get_project/', get_project, name='get_project'),
     path('create_project_area/', create_project_area, name='create_project_area'),
+    path('get_project_areas/', get_project_areas, name='get_project_areas'),
 ]
