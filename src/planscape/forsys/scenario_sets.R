37d19b44e8f4c902ff33f4177d32e71f1022cae7
Wendy Nie <niew@google.com>
2023-01-12 15:04:04 -0800

    adjust to recent changes in forsys: adds params write_outputs and scenario_output_fields, adjusts parsing from ETrt_*_PCP to ETrt_*, makes header names into variables

--- src/planscape/forsys/scenario_sets.R
+++ src/planscape/forsys/scenario_sets.R
@@ -2,21 +2,24 @@ library(dplyr)
 library(forsys)
 library(sf)
 
-scenario_sets <- function(input_stand_data, priorities) {
-  input_stand_data
+scenario_sets <- function(input_stand_data, priorities, stand_id_field,
+                          proj_id_field, stand_area_field, stand_cost_field) {
+
+  scenario_output_fields <- append(priorities, c(stand_area_field, stand_cost_field))
 
   stand_dat <- test_forest %>% st_drop_geometry()
   run_outputs <- forsys::run(
     return_outputs = TRUE,
-    scenario_name = "test_scenario",
+    write_outputs = FALSE,
     stand_data = input_stand_data,
-    stand_id_field = "stand_id",
-    proj_id_field = "proj_id",
-    stand_area_field = "area",
+    stand_id_field = stand_id_field,
+    proj_id_field = proj_id_field,
+    stand_area_field = stand_area_field,
     scenario_priorities = priorities,
-    scenario_weighting_values = c("1 5 1"), 
+    scenario_weighting_values = c("1 5 1"),
+    scenario_output_fields = scenario_output_fields, 
     proj_fixed_target =  FALSE,
-    proj_target_field = "area",
+    proj_target_field = stand_area_field,
     proj_target_value = 1.0
   )
 
