c7a5569c2ea929d572ccf526d5183e520a715397
Wendy Nie <niew@google.com>
2023-01-13 15:21:14 -0800

    urls.py for first-pass implementation of plan/score

--- src/planscape/plan/urls.py
+++ src/planscape/plan/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from plan.views import (create, create_project, delete, get_plan,
+from plan.views import (create, create_project, delete, get_plan, get_scores,
                         list_plans_by_owner)
 
 app_name = 'plan'
@@ -10,6 +10,7 @@ urlpatterns = [
     path('delete/', delete, name='delete'),
     path('get_plan/', get_plan, name='get_plan'),
     path('list_plans_by_owner/', list_plans_by_owner, name='list_plans_by_owner'),
+    path('scores/', get_scores, name='get_scores'),
     # Projects
     path('create_project/', create_project, name='create_project')
 ]
