7e26c4fab90f4da234e1a01f1258afa657428f0b
Jon Riecke <riecke@google.com>
2023-01-18 08:46:17 -0500

    Disable scroll zoom 	modified:   src/interface/src/app/map/map-manager.ts

--- src/interface/src/app/map/map-manager.ts
+++ src/interface/src/app/map/map-manager.ts
@@ -78,6 +78,7 @@ export class MapManager {
       layers: [map.baseLayerRef],
       zoomControl: false,
       pmIgnore: false,
+      scrollWheelZoom: false,
     });
 
     // Add zoom controls to bottom right corner
