175ddd401725971dbcedc3e227c34794fd5b39e1
Elsie Ling <elsieling@google.com>
2023-01-19 12:33:24 -0800

    Add preliminary endpoint for ITS projects

--- src/planscape/existing_projects/urls.py
+++ src/planscape/existing_projects/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CalMAPPER.as_view())
+    path('calmapper/', views.CalMAPPER.as_view()),
+    path('its/', views.MillionAcres_ITS.as_view())
 ]
