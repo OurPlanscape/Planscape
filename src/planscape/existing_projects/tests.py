e9a64970b8708be18ebddf61f63d99a3896d7d4b
Elsie Ling <elsieling@google.com>
2023-01-19 20:01:24 -0800

    add test for its endpoint

--- src/planscape/existing_projects/tests.py
+++ src/planscape/existing_projects/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+import json
 
-# Create your tests here.
+class ITSTest(TestCase):
+        
+    def test_x(self):
+        response = self.client.get(
+            reverse('existing_projects:its'), {}, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.json())["error"]["code"], 499)
+        self.assertEqual(json.loads(response.json())["error"]["message"], "Token Required")
