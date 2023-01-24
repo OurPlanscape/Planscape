20f8c51ff2c372d2855521b050a90320e712da00
Wendy Nie <niew@google.com>
2023-01-24 00:06:45 -0800

    adds model for mean condition scores

--- src/planscape/conditions/models.py
+++ src/planscape/conditions/models.py
@@ -70,3 +70,19 @@ class ConditionRaster(models.Model):
 
     # A tile in the raster.
     raster = models.RasterField(null=True, srid=CRS_FOR_RASTERS)
+
+class ConditionScores(models.Model):
+    """
+    Condition scores are computed from statistics aggregated across all relevant stands
+    within a project area or planning area.
+    """
+    # Either plan or project should be present.
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, null=True)
+    project_area = models.ForeignKey(
+        ProjectArea, on_delete=models.CASCADE, null=True)
+    
+    # Condition
+    condition = models.ForeignKey(
+        Condition, on_delete=models.CASCADE, null=False)
+
+    mean_score = models.FloatField(null=False)
