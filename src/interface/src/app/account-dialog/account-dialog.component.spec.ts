267c509ff1a4a399d142ff5727cda6118f4e657d
Hana Lee <leehana@alumni.stanford.edu>
2023-01-25 17:03:33 -0800

    update login

--- src/interface/src/app/account-dialog/account-dialog.component.spec.ts
+++ src/interface/src/app/account-dialog/account-dialog.component.spec.ts
@@ -1,28 +1,27 @@
-import { of, BehaviorSubject, Subject } from 'rxjs';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { MaterialModule } from 'src/app/material/material.module';
+
 import { AuthService } from '../services';
+import { User } from './../services/auth.service';
 import { AccountDialogComponent } from './account-dialog.component';
 
 describe('AccountDialogComponent', () => {
   let component: AccountDialogComponent;
   let fixture: ComponentFixture<AccountDialogComponent>;
-  let fakeLoggedInStatus: Subject<boolean>;
 
   beforeEach(() => {
     const fakeAuthService = jasmine.createSpyObj<AuthService>(
       'AuthService',
-      {
-        getLoggedInUser: of({ username: 'username' }),
-      },
       {},
+      {
+        loggedInUser$: new BehaviorSubject<User | null>(null),
+      }
     );
-    fakeLoggedInStatus = new BehaviorSubject(true);
-    fakeAuthService.isLoggedIn$ = fakeLoggedInStatus;
     TestBed.configureTestingModule({
-      schemas: [NO_ERRORS_SCHEMA],
+      imports: [MaterialModule],
       declarations: [AccountDialogComponent],
-      providers: [{ provide: AuthService, useValue: fakeAuthService }]
+      providers: [{ provide: AuthService, useValue: fakeAuthService }],
     });
     fixture = TestBed.createComponent(AccountDialogComponent);
     component = fixture.componentInstance;
@@ -31,21 +30,5 @@ describe('AccountDialogComponent', () => {
 
   it('can load instance', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('updates user observable when logged in status changes to false', (done) => {
-    fakeLoggedInStatus.next(false);
-    component.user$.subscribe(user => {
-      expect(user).toEqual({ username: 'Guest' });
-      done();
-    });
-  });
-
-  it('updates user observable when logged in status changes to true', (done) => {
-    fakeLoggedInStatus.next(true);
-    component.user$.subscribe(user => {
-      expect(user).toEqual({ username: 'username' });
-      done();
-    });
   });
 });
