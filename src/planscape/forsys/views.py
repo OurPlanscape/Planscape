5c137cef36a70de9d41703aa09e13e814d3dff2b
Wendy Nie <niew@google.com>
2023-01-29 13:18:53 -0800

    simplifies forsys project area ranking call

--- src/planscape/forsys/views.py
+++ src/planscape/forsys/views.py
@@ -1,22 +1,19 @@
-import json
 import logging
-import numpy as np
 import os
+
+import numpy as np
 import pandas as pd
-import rpy2
-
-from forsys.get_forsys_inputs import ForsysProjectAreaRankingRequestParams
-from forsys.parse_forsys_output import ForsysScenarioSetOutput
-from forsys.raster_merger import RasterMerger
-
-from boundary.models import BoundaryDetails
-from conditions.models import BaseCondition, Condition, ConditionRaster
+from conditions.models import BaseCondition, Condition
+from conditions.raster_utils import compute_condition_score_from_raster
 from django.conf import settings
-from django.contrib.gis.gdal import CoordTransform, Envelope, GDALRaster, SpatialReference
-from django.contrib.gis.geos import MultiPolygon, Point, Polygon
-from django.db.models.query import QuerySet
+from django.contrib.gis.gdal import CoordTransform, SpatialReference
 from django.http import (HttpRequest, HttpResponse, HttpResponseBadRequest,
                          JsonResponse)
+from forsys.get_forsys_inputs import ForsysProjectAreaRankingRequestParams
+from forsys.parse_forsys_output import ForsysScenarioSetOutput
+from planscape import settings
+
+import rpy2
 
 # Configures global logging.
 logger = logging.getLogger(__name__)
@@ -51,219 +48,13 @@ def convert_dictionary_of_lists_to_rdf(
     return rdf
 
 
-def get_boundary_debug_info(
-        boundaries: QuerySet, project_area: MultiPolygon) -> dict:
-    boundary_response = []
-    for b in boundaries:
-        geo = b.geometry
-        p = project_area.clone()
-        p.transform(CoordTransform(SpatialReference(
-            project_area.srid), SpatialReference(geo.srid)))
-        boundary_response.append(
-            "%s (id=%s, intersection area=%f)" %
-            (b.shape_name, b.boundary_id, geo.intersection(project_area).area))
-    return boundary_response
-
-
-def get_raster_debug_info(rasters: dict) -> dict:
-    raster_response = []
-    for c in rasters:
-        r = rasters[c]
-        d = r.bands[0].data()
-        count = np.count_nonzero(~np.isnan(d))
-        mean = np.sum(d[~np.isnan(d)]) / count if count > 0 else 0
-        shape = np.shape(d)
-        raster_response.append(
-            "%s (non-nan area: %d, mean: %f, shape: %d x %d)" %
-            (c, count, mean, shape[0],
-             shape[1]))
-    return raster_response
-
-
-def get_condition_rasters(condition: str, region: str) -> QuerySet:
-    conditions = BaseCondition.objects.filter(
-        condition_name=condition).filter(region_name=region).all()
-    if len(conditions) == 0:
-        raise LookupError(
-            "no condition with name, %s, exists in region, %s" %
-            (condition, region))
-    if len(conditions) > 1:
-        raise LookupError(
-            "more than 1 condition with name, %s, exists in region, %s" %
-            (condition, region))
-    c = conditions[0]
-    condition_files = Condition.objects.filter(condition_dataset_id=c.id).all()
-    if len(condition_files) == 0:
-        raise LookupError(
-            "no condition filename exists for condition id, %d" % (c.id))
-    if len(condition_files) > 1:
-        raise LookupError(
-            "more than 1 condition filename exists for condition id, %d" %
-            (c.id))
-
-    condition_rasters = ConditionRaster.objects.filter(
-        name=condition_files[0].raster_name)
-    return condition_rasters
-
-
-def raster_extent_overlaps_project_area(
-        raster: GDALRaster, project_area: MultiPolygon) -> bool:
-    e = raster.extent
-    e_polygon = Polygon(((e[0], e[1]),
-                         (e[2], e[1]),
-                         (e[2], e[3]),
-                         (e[0], e[3]),
-                         (e[0], e[1])))
-    return e_polygon.overlaps(project_area)
-
-
-def fetch_condition_rasters(
-        priorities: list[str],
-        region: str, project_area: MultiPolygon) -> dict:
-    all_rasters = {}
-
-    for p in priorities:
-        condition_rasters = get_condition_rasters(p, region)
-
-        raster_merger = RasterMerger()
-
-        for cr in condition_rasters:
-            r = cr.raster
-
-            # Checking for overlapping extents is faster than issuing a query
-            # that checks for overlaps.
-            if not raster_extent_overlaps_project_area(r, project_area):
-                continue
-            raster_merger.add_raster(r)
-
-        all_rasters[p] = raster_merger.merged_raster
-
-    return all_rasters
-
-
-def convert_extent_to_raster_indices(extent: Envelope, origin: GDALRaster.origin, scale: GDALRaster.scale) -> Envelope:
-    min_x = 0
-    min_y = 0
-    max_x = 0
-    max_y = 0
-    if scale[0] > 0:
-        min_x = int(np.floor((extent[0] - origin[0]) / scale[0]))
-        max_x = int(np.ceil((extent[2] - origin[0]) / scale[0]))
-    else:
-        min_x = int(np.floor((extent[2] - origin[0]) / scale[0]))
-        max_x = int(np.ceil((extent[0] - origin[0]) / scale[0]))
-
-    if scale[1] > 0:
-        min_y = int(np.floor((extent[1] - origin[1]) / scale[1]))
-        max_y = int(np.ceil((extent[3] - origin[1]) / scale[1]))
-    else:
-        min_y = int(np.floor((extent[3] - origin[1]) / scale[1]))
-        max_y = int(np.ceil((extent[1] - origin[1]) / scale[1]))
-
-    return (min_x, min_y, max_x, max_y)
-
-
-def multipolygon_contains_point(
-        origin: GDALRaster.origin, scale: GDALRaster.scale, x: int, y: int,
-        multipolygon: MultiPolygon) -> bool:
-    # TODO: adjust these equations for the case where skew != [0, 0]
-    xpoly = origin[0] + x*scale[0]
-    ypoly = origin[1] + y*scale[1]
-    p = Point((xpoly, ypoly))
-    return p.within(multipolygon)
-
-
-def count_raster_data(data: np.ndarray, extent_indices: Envelope,
-                      origin: GDALRaster.origin, scale: GDALRaster.scale,
-                      multipolygon: MultiPolygon) -> [int, float]:
-    count = 0
-    sum = 0
-    for y in range(extent_indices[1], extent_indices[3] + 1, 1):
-        for x in range(extent_indices[0], extent_indices[2] + 1, 1):
-            d = data[y][x]
-            if np.isnan(d):
-                continue
-            if not multipolygon_contains_point(
-                    origin, scale, x, y, multipolygon):
-                continue
-            sum = sum + d
-            count = count + 1
-    return count, sum
-
-
-def get_condition_data(raster: GDALRaster, multipolygon: MultiPolygon) -> dict:
-    scale = raster.scale
-    origin = raster.origin
-
-    multipolygon_extent_indices = convert_extent_to_raster_indices(
-        multipolygon.extent, origin, scale)
-
-    data = raster.bands[0].data()
-
-    count, sum = count_raster_data(
-        data, multipolygon_extent_indices, origin, scale, multipolygon)
-
-    if count == 0:
-        return {"mean": 0, "count": 0}
-    return {"mean": sum / count, "count": count}
-
-
-def transform_into_forsys_df_data(condition_rasters: QuerySet,
-                                  boundaries: QuerySet, project_area: Polygon,
-                                  project_area_id: int,
-                                  forsys_proj_id_header: str,
-                                  forsys_stand_id_header: str,
-                                  forsys_area_header: str,
-                                  forsys_cost_header: str) -> dict[str, list]:
-    kConditionPrefix = "cond"
-    kPriorityPrefix = "p"
-    # TODO: fix cost estimation once rasters become available.
-    kUnitAreaCost = 5000
-
-    data = {}
-    data[forsys_proj_id_header] = []
-    data[forsys_stand_id_header] = []
-    data['shape_name'] = []
-    for c in condition_rasters.keys():
-        data[kConditionPrefix + "_" + c] = []
-        data[kPriorityPrefix + "_" + c] = []
-        data[forsys_area_header] = []
-        data[forsys_cost_header] = []
-
-    for b in boundaries:
-        geo = b.geometry.clone()
-        geo.transform(CoordTransform(SpatialReference(geo.srid),
-                      SpatialReference(settings.CRS_9822_PROJ4)))
-        geo = project_area.intersection(geo)
-
-        # TODO: set project_area ID from an external source
-        data[forsys_proj_id_header].append(project_area_id)
-        # TODO: double-check that it makes sense to use this ID.
-        data[forsys_stand_id_header].append(b.id)
-        # TODO: double-check that this field is necessary.
-        data['shape_name'].append(b.shape_name)
-        data[forsys_area_header].append(geo.area)
-        # TODO: adjust cost as a function of treatment type.
-        data[forsys_cost_header].append(kUnitAreaCost * geo.area)
-
-        for c in condition_rasters.keys():
-            d = get_condition_data(condition_rasters[c], geo)
-            data[kConditionPrefix + "_" + c].append(d['mean'])
-            # TODO: adjust improvement score as a function of treatment type.
-            data[kPriorityPrefix + "_" +
-                 c].append((1.0 - d['mean']) * d['count'])
-
-    return data
-
-
 # Runs a forsys scenario sets call.
 def run_forsys_scenario_sets(
         forsys_input_dict: dict[str, list],
-        priorities: list[str], forsys_proj_id_header: str,
-        forsys_stand_id_header: str, forsys_area_header: str,
-        forsys_cost_header: str) -> ForsysScenarioSetOutput:
-    kPriorityPrefix = "p"
-
+        priorities: list[str],
+        forsys_proj_id_header: str, forsys_stand_id_header: str,
+        forsys_area_header: str, forsys_cost_header: str,
+        forsys_priority_header_prefix: str) -> ForsysScenarioSetOutput:
     import rpy2.robjects as robjects
     robjects.r.source(os.path.join(
         settings.BASE_DIR, 'forsys/scenario_sets.R'))
@@ -276,7 +67,7 @@ def run_forsys_scenario_sets(
 
     priority_headers = []
     for p in priorities:
-        priority_headers.append(kPriorityPrefix + "_" + p)
+        priority_headers.append(forsys_priority_header_prefix + p)
 
     forsys_output = scenario_sets_function_r(forsys_input, robjects.StrVector(
         priority_headers),
@@ -298,78 +89,66 @@ def run_forsys_scenario_sets(
 def scenario_set(request: HttpRequest) -> HttpResponse:
     try:
         params = ForsysProjectAreaRankingRequestParams(request.GET)
-        save_debug_info = params.save_debug_info
         region = params.region
         priorities = params.priorities
         project_areas = params.project_areas
 
-        # TODO: remove this because it's likely not be neceessary if we're ranking project areas rather than stands.
-        huc12_id = 43
+        condition_ids_to_names = {
+            c.pk: c.condition_name
+            for c in BaseCondition.objects.filter(region_name=region).filter(
+                condition_name__in=priorities).all()}
+        conditions = Condition.objects.filter(
+            condition_dataset_id__in=condition_ids_to_names.keys()).filter(
+            is_raw=False).all()
 
         forsys_project_id_header = "proj_id"
         forsys_stand_id_header = "stand_id"
         forsys_area_header = "area"
         forsys_cost_header = "cost"
-        forsys_input_df = {}
-        for id in project_areas.keys():
-            project_area = project_areas[id]
-            project_area_raster = project_area.clone()
-            project_area_raster.transform(CoordTransform(SpatialReference(
-                project_area.srid), SpatialReference(settings.CRS_9822_PROJ4)))
+        kConditionPrefix = "c_"
+        kPriorityPrefix = "p_"
+        data = {}
+        data[forsys_project_id_header] = []
+        data[forsys_stand_id_header] = []
+        data[forsys_area_header] = []
+        data[forsys_cost_header] = []
+        for c in conditions:
+            name = condition_ids_to_names[c.condition_dataset_id]
+            data[kConditionPrefix + name] = []
+            data[kPriorityPrefix + name] = []
+        for proj_id in project_areas.keys():
+            geo = project_areas[proj_id]
 
-            response = {}
-            if (save_debug_info):
-                response['debug'] = {}
+            raster_geo = geo.clone()
+            raster_geo.transform(
+                CoordTransform(
+                    SpatialReference(raster_geo.srid),
+                    SpatialReference(settings.CRS_9822_PROJ4)))
+            raster_geo.srid = settings.CRS_FOR_RASTERS
 
-            # Filters boundaries by boundary_id.
-            # TODO: add more stand options. For the existing solution, project
-            # areas drawn manually are divided into stands according to HUC-12
-            # boundaries.
-            # TODO: double-check, in this case, that "__intersects" works when
-            # project_area and boundary geometry have different srid's.
-            boundaries = BoundaryDetails.objects.filter(
-                boundary_id=huc12_id).filter(
-                geometry__intersects=project_area)
-            if (save_debug_info):
-                response['debug']['huc-12 boundaries'] = get_boundary_debug_info(
-                    boundaries, project_area)
+            data[forsys_project_id_header].append(proj_id)
+            data[forsys_stand_id_header].append(proj_id)
+            data[forsys_area_header].append(raster_geo.area)
+            data[forsys_cost_header].append(raster_geo.area * 5000)
+            for c in conditions:
+                name = condition_ids_to_names[c.condition_dataset_id]
+                score = compute_condition_score_from_raster(
+                    raster_geo, c.raster_name)
+                data[kConditionPrefix + name].append(score)
+                data[kPriorityPrefix + name].append(1.0 - score)
 
-            # Fetches priority rasters for the given project area.
-            condition_rasters = fetch_condition_rasters(
-                priorities, region, project_area_raster)
-            if (save_debug_info):
-                response['debug']['rasters'] = get_raster_debug_info(
-                    condition_rasters)
+        forsys_output = run_forsys_scenario_sets(
+            data, priorities, forsys_project_id_header,
+            forsys_stand_id_header, forsys_area_header, forsys_cost_header,
+            kPriorityPrefix)
 
-            # Transforms rasters into dataframes.
-            # TODO: instead of using HUC-12 boundaries to delineate stands, add
-            # options for using individual pixels and individual latitudinal
-            # bars.
-            dataframe_data = transform_into_forsys_df_data(
-                condition_rasters, boundaries, project_area_raster, id,
-                forsys_project_id_header, forsys_stand_id_header,
-                forsys_area_header, forsys_cost_header)
-            if len(forsys_input_df.keys()) == 0:
-                forsys_input_df = dataframe_data
-            else:
-                for k in forsys_input_df.keys():
-                    for v in dataframe_data[k]:
-                        forsys_input_df[k].append(v)
-
-        dataframe = pd.DataFrame(data=forsys_input_df)
+        response = {}
         response['forsys'] = {}
-        response['forsys']['input_df'] = dataframe.to_json()
-
-        forsys_scenario_sets_output = run_forsys_scenario_sets(
-            forsys_input_df, priorities, forsys_project_id_header,
-            forsys_stand_id_header, forsys_area_header, forsys_cost_header)
-
-        response['forsys']['output_project'] = json.dumps(
-            forsys_scenario_sets_output.scenarios)
+        response['forsys']['input'] = data
+        response['forsys']['output_project'] = forsys_output.scenarios
 
         # TODO: configure response to potentially show stand coordinates and
         # other signals necessary for the UI.
-
         return HttpResponse(
             JsonResponse(response),
             content_type='application/json')
