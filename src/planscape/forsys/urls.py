28d27c79318c03716dbf5aab79d3d93474f2790c
Wendy Nie <niew@google.com>
2023-02-07 11:28:16 -0800

    reformats urls.py

--- src/planscape/forsys/urls.py
+++ src/planscape/forsys/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 app_name = 'forsys'
 
-urlpatterns = [
-    path('rank_projects/multiple_scenarios/', views.rank_projects_for_multiple_scenarios, name='rank_projects_for_multiple_scenarios'),
-]
+urlpatterns = [path('rank_projects/multiple_scenarios/',
+                    views.rank_projects_for_multiple_scenarios, name='rank_projects_for_multiple_scenarios'), ]
