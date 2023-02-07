fb8817362ec1df993549875a4bc3a9757f867f1f
Wendy Nie <niew@google.com>
2023-02-07 12:15:03 -0800

    %s/rank_projects_for_multiple_scenarios/rank_project_areas_for_multiple_scenarios

--- src/planscape/forsys/urls.py
+++ src/planscape/forsys/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'forsys'
 
-urlpatterns = [path('rank_project_areas/multiple_scenarios/',
-                    views.rank_project_areas_for_multiple_scenarios, name='rank_projects_for_multiple_scenarios')]
+urlpatterns = [
+    path(
+        'rank_project_areas/multiple_scenarios/',
+        views.rank_project_areas_for_multiple_scenarios,
+        name='rank_project_areas_for_multiple_scenarios')]
