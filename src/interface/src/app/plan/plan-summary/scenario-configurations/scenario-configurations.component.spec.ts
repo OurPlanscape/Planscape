3427d1e07ff51ada5b24cdd60069c6a36818a75e
Hana Lee <leehana@alumni.stanford.edu>
2023-02-08 14:32:45 -0800

    map condition name to condition display name

--- src/interface/src/app/plan/plan-summary/scenario-configurations/scenario-configurations.component.spec.ts
+++ src/interface/src/app/plan/plan-summary/scenario-configurations/scenario-configurations.component.spec.ts
@@ -1,21 +1,34 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProjectConfig } from 'src/app/types';
-import { of } from 'rxjs';
-import { PlanService } from 'src/app/services';
+import { BehaviorSubject, of } from 'rxjs';
+import { MapService, PlanService } from 'src/app/services';
 import { MaterialModule } from 'src/app/material/material.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScenarioConfigurationsComponent } from './scenario-configurations.component';
 
-describe('UnsavedScenariosComponent', () => {
+describe('ScenarioConfigurationsComponent', () => {
   let component: ScenarioConfigurationsComponent;
   let fixture: ComponentFixture<ScenarioConfigurationsComponent>;
+  let fakeMapService: MapService;
   let fakePlanService: PlanService;
 
   const fakeConfig: ProjectConfig = {
-    id: 1
+    id: 1,
+    priorities: ['fake_priority']
   };
 
   beforeEach(async () => {
+    let nameMap = new Map<string, string>();
+    nameMap.set('fake_priority', 'fake_display_priority');
+
+    fakeMapService = jasmine.createSpyObj<MapService>(
+      'MapService',
+      {},
+      {
+        conditionNameToDisplayNameMap$: new BehaviorSubject(nameMap)
+      }
+    );
     fakePlanService = jasmine.createSpyObj<PlanService>(
       'PlanService',
       {
@@ -28,6 +41,9 @@ describe('UnsavedScenariosComponent', () => {
       imports: [ MaterialModule ],
       declarations: [ ScenarioConfigurationsComponent ],
       providers: [
+        {
+          provide: MapService, useValue: fakeMapService
+        },
         {
           provide: PlanService, useValue: fakePlanService
         }
@@ -47,5 +63,9 @@ describe('UnsavedScenariosComponent', () => {
   it('should get a list of scenario configs', () => {
     expect(fakePlanService.getProjects).toHaveBeenCalled();
     expect(component.configurations).toEqual([fakeConfig]);
+  });
+
+  it('should map priorities to display names', () => {
+    expect(component.displayPriorities(fakeConfig.priorities!)).toEqual(['fake_display_priority']);
   });
 });
