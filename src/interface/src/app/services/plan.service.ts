495ec6a208e7402a29e24523418054859289eb5d
Hana Lee <leehana@alumni.stanford.edu>
2023-02-09 13:30:42 -0800

    add getProject to PlanService

--- src/interface/src/app/services/plan.service.ts
+++ src/interface/src/app/services/plan.service.ts
@@ -149,7 +149,7 @@ export class PlanService {
   }
 
   /** Fetches the projects for a plan from the backend. */
-  getProjects(planId: string): Observable<ProjectConfig[]> {
+  getProjectsForPlan(planId: string): Observable<ProjectConfig[]> {
     let url = BackendConstants.END_POINT.concat(
       '/plan/list_projects_for_plan/?plan_id=',
       planId
@@ -165,6 +165,22 @@ export class PlanService {
             this.convertToProjectConfig(config)
           )
         )
+      );
+  }
+
+  /** Fetches a project by its ID from the backend. */
+  getProject(projectId: number): Observable<ProjectConfig> {
+    let url = BackendConstants.END_POINT.concat(
+      '/plan/get_project/?id=',
+      projectId.toString()
+    );
+    return this.http
+      .get(url, {
+        withCredentials: true,
+      })
+      .pipe(
+        take(1),
+        map((response) => this.convertToProjectConfig(response))
       );
   }
 
