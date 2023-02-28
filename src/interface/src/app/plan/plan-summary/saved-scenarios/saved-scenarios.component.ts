46622d98694158c7d154824ce92c031821c5b03c
Hana Lee <leehana@alumni.stanford.edu>
2023-02-28 13:33:54 -0800

    scenario table logic (UI only)

--- src/interface/src/app/plan/plan-summary/saved-scenarios/saved-scenarios.component.ts
+++ src/interface/src/app/plan/plan-summary/saved-scenarios/saved-scenarios.component.ts
@@ -4,6 +4,11 @@ import { take } from 'rxjs';
 import { PlanService } from 'src/app/services';
 import { Plan, Scenario } from 'src/app/types';
 
+interface ScenarioRow extends Scenario {
+  selected?: boolean;
+  starred?: boolean;
+}
+
 @Component({
   selector: 'app-saved-scenarios',
   templateUrl: './saved-scenarios.component.html',
@@ -13,8 +18,17 @@ export class SavedScenariosComponent implements OnInit {
   @Input() plan: Plan | null = null;
   @Output() createScenarioEvent = new EventEmitter<void>();
 
-  scenarios: Scenario[] = [];
-  displayedColumns: string[] = ['id', 'createdTimestamp'];
+  scenarios: ScenarioRow[] = [];
+  displayedColumns: string[] = [
+    'id',
+    'starred',
+    'projectAreas',
+    'acresTreated',
+    'estimatedCost',
+    'status',
+    'owner',
+    'createdTimestamp',
+  ];
 
   constructor(
     private planService: PlanService,
@@ -35,7 +49,27 @@ export class SavedScenariosComponent implements OnInit {
     this.createScenarioEvent.emit();
   }
 
-  viewScenario(id: string): void {
+  viewScenario(id?: string): void {
+    if (!id) {
+      id = this.scenarios.find((scenario) => scenario.selected)?.id;
+    }
     this.router.navigate(['scenario', id], { relativeTo: this.route });
+  }
+
+  showDeleteButton(): boolean {
+    return this.scenarios.filter((scenario) => scenario.selected).length > 0;
+  }
+
+  showViewButton(): boolean {
+    return this.scenarios.filter((scenario) => scenario.selected).length === 1;
+  }
+
+  deleteSelectedScenarios(): void {
+    console.log(
+      'Deleting scenarios',
+      this.scenarios
+        .filter((scenario) => scenario.selected)
+        .map((scenario) => scenario.id)
+    );
   }
 }
