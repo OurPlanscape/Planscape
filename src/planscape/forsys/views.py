d2a306373d99671312a09cb192573a32d689faa2
Wendy Nie <niew@google.com>
2023-02-28 19:52:11 -0800

    completes e2e plumbing for hierarchical clustering, fixes typos, prints mean condition score graphs rather than priority score graphs, which represent sums

--- src/planscape/forsys/views.py
+++ src/planscape/forsys/views.py
@@ -28,10 +28,12 @@ import rpy2
 # Configures global logging.
 logger = logging.getLogger(__name__)
 
+
 # Sets up cProfile profiler.
 # This is for measuring runtime.
 def _set_up_cprofiler(pr: cProfile.Profile) -> None:
     pr.enable()
+
 
 # Tears down Cprofile profiler and writes data to a log.
 # This is for measuring runtime.
@@ -185,10 +187,11 @@ def run_forsys_generate_project_areas_for_a_single_scenario(
         forsys_input_dict: dict[str, list],
         forsys_proj_id_header: str, forsys_stand_id_header: str,
         forsys_area_header: str, forsys_cost_header: str,
-        forsys_geo_wkt_header: str, forsys_priority_headers: list[str],
+        forsys_geo_wkt_header: str, forsys_priority_headers: list[str], forsys_condition_headers: list[str],
         forsys_priority_weights: list[float],
         output_scenario_name: str | None,
-        output_scenario_tag: str | None) -> ForsysGenerationOutputForASingleScenario:
+        output_scenario_tag: str | None
+) -> ForsysGenerationOutputForASingleScenario:
     import rpy2.robjects as robjects
     robjects.r.source(os.path.join(
         settings.BASE_DIR, 'forsys/generate_projects_for_a_single_scenario.R'))
@@ -199,6 +202,7 @@ def run_forsys_generate_project_areas_for_a_single_scenario(
 
     forsys_output = generate_projects_for_a_single_scenario_function_r(
         forsys_input, robjects.StrVector(forsys_priority_headers),
+        robjects.StrVector(forsys_condition_headers),
         robjects.FloatVector(forsys_priority_weights),
         forsys_stand_id_header, forsys_proj_id_header, forsys_area_header,
         forsys_cost_header, forsys_geo_wkt_header,
@@ -228,7 +232,7 @@ def generate_project_areas_for_a_single_scenario(
             forsys_input.forsys_input, headers.FORSYS_PROJECT_ID_HEADER,
             headers.FORSYS_STAND_ID_HEADER, headers.FORSYS_AREA_HEADER,
             headers.FORSYS_COST_HEADER, headers.FORSYS_GEO_WKT_HEADER,
-            headers.priority_headers, params.priority_weights,
+            headers.priority_headers, headers.condition_headers, params.priority_weights,
             "test_scenario" if settings.DEBUG else None,
             datetime.now().astimezone(
                 timezone('US/Pacific')
@@ -258,6 +262,6 @@ def generate_project_areas_for_a_single_scenario(
 if settings.DEBUG:
     if not os.path.exists('output'):
         os.makedirs('output')
-    memfp = open('output/memprofiler.log','w+')
+    memfp = open('output/memprofiler.log', 'w+')
     generate_project_areas_for_a_single_scenario = profile(stream=memfp)(
         generate_project_areas_for_a_single_scenario)
