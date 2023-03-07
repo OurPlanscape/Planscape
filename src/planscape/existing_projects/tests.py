ba926b6c681b0e9d4f3210174a920e219c2d91ae
Wendy Nie <niew@google.com>
2023-03-07 01:04:23 -0800

    adds a TODO linking to the last commit

--- src/planscape/existing_projects/tests.py
+++ src/planscape/existing_projects/tests.py
@@ -7,3 +7,4 @@ class ITSTest(TestCase):
         response = self.client.get(
             reverse('existing_projects:its'), {}, content_type='application/json')
         self.assertEqual(response.status_code, 200)
+        # TODO: restore the response json parse statements removed in commit, https://github.com/OurPlanscape/Planscape/pull/630/commits/605c1d036a8a09916f7fe2dd3448acd90f25218e.
