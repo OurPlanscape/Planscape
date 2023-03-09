c5233e8f73cb7dacec0d6b0c14ebd41b96a6cc27
Elsie Ling <elsieling@google.com>
2023-03-09 15:19:27 -0800

    recreate changes

--- src/planscape/plan/urls.py
+++ src/planscape/plan/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from plan.views import (create_plan, create_project, create_project_area,
-                        create_scenario, delete, delete_projects, get_plan,
-                        get_project, get_project_areas, get_scenario,
-                        get_scores, list_plans_by_owner,
-                        list_projects_for_plan, list_scenarios_for_plan,
-                        update_project, delete_scenarios,
-                        favorite_scenario, unfavorite_scenario)
+                        create_project_areas_for_project, create_scenario,
+                        delete, delete_projects, delete_scenarios,
+                        favorite_scenario, get_plan, get_project,
+                        get_project_areas, get_scenario, get_scores,
+                        list_plans_by_owner, list_projects_for_plan,
+                        list_scenarios_for_plan, unfavorite_scenario,
+                        update_project)
 
 app_name = 'plan'
 
@@ -24,6 +25,8 @@ urlpatterns = [
     path('update_project/', update_project, name='update_project'),
     path('delete_projects/', delete_projects, name='delete_projects'),
     path('create_project_area/', create_project_area, name='create_project_area'),
+    path('create_project_areas_for_project/', create_project_areas_for_project,
+         name='create_project_areas_for_project'),
     path('get_project_areas/', get_project_areas, name='get_project_areas'),
     # Scenarios
     path('create_scenario/', create_scenario, name='create_scenario'),
