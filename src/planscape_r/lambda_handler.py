e10ae343bc11f9564fd02b66914d2aec3c0bd35e
Elsie L <elsieling@google.com>
2023-03-10 12:55:59 -0800

    Update lambda_handler.py
    Signed-off-by: Elsie L <elsieling@google.com>

--- src/planscape_r/lambda_handler.py
+++ src/planscape_r/lambda_handler.py
@@ -1,7 +1,14 @@
+import os
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
+from rpy2.rinterface_lib import openrlib
+import rpy2.situation
 
 def lambda_handler(event, context): 
+    c = get_config()
+    libpath = os.environ.get('LD_LIBRARY_PATH', '')
+    os.environ['LD_LIBRARY_PATH'] = (rpy2.situation.r_ld_library_path_from_subprocess(openrlib.R_HOME) + libpath)
+    
     r=robjects.r
     base = importr('base')
     print(base._libPaths())
@@ -11,3 +18,7 @@ def lambda_handler(event, context):
     return { 
         'message' : str(result)
     }
+
+
+
+
