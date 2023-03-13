03a2f1052be32b90ca18a7427dcee1e0da2d7e36
Elsie L <elsieling@google.com>
2023-03-12 23:06:31 -0700

    Update lambda_handler.py
    Signed-off-by: Elsie L <elsieling@google.com>

--- src/planscape_r_conda/lambda_handler.py
+++ src/planscape_r_conda/lambda_handler.py
@@ -1,9 +1,7 @@
 import rpy2.robjects as robjects
-from rpy2.robjects.packages import importr
 
 def lambda_handler(event, context):     
     r=robjects.r
-    base = importr('base')
     r.source('rank.R')
     r_f = robjects.r['times2']
     result = r_f(4)
