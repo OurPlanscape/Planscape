015d4553ad25c3c6b277f9576ecbfbeb429412ab
cynthia huang <cynhuang@google.com>
2023-03-14 14:10:51 -0700

    display actual config info

--- src/interface/src/app/plan/plan-summary/saved-scenarios/saved-scenarios.component.spec.ts
+++ src/interface/src/app/plan/plan-summary/saved-scenarios/saved-scenarios.component.spec.ts
@@ -33,6 +33,10 @@ describe('SavedScenariosComponent', () => {
           {
             id: '1',
             createdTimestamp: 100,
+            projectConfig: {
+              id: 1,
+              max_budget: 200,
+            },
           },
         ]),
         deleteScenarios: of(['1']),
--- src/interface/src/app/plan/scenario-details/outcome/outcome.component.ts
+++ src/interface/src/app/plan/scenario-details/outcome/outcome.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, SimpleChanges, OnChanges, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { take, BehaviorSubject } from 'rxjs';
+import { User, AuthService } from 'src/app/services';
 
 import { Scenario, Plan, ProjectArea } from 'src/app/types';
 
@@ -9,18 +11,25 @@ import { Scenario, Plan, ProjectArea } from 'src/app/types';
   templateUrl: './outcome.component.html',
   styleUrls: ['./outcome.component.scss'],
 })
-export class OutcomeComponent implements OnChanges {
+export class OutcomeComponent implements OnInit, OnChanges {
   @Input() plan: Plan | null = null;
   @Input() scenario: Scenario | null = null;
+  currentUser$ = new BehaviorSubject<User | null>(null);
   scenarioNotes: FormGroup;
   totalAcresTreated: number = 0;
   totalCostRange: string = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder) {
     // TODO: Call update scenario on submit.
     this.scenarioNotes = fb.group({
       notes: '',
     });
+  }
+
+  ngOnInit(): void {
+    this.authService.loggedInUser$.pipe(take(1)).subscribe(this.currentUser$)
   }
 
   ngOnChanges(changes: SimpleChanges): void {
