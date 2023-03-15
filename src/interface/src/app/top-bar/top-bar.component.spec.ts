3e8aab6ee380ca93a3e3f8e0f2303a529b34a4ad
Hana Lee <leehana@alumni.stanford.edu>
2023-03-15 13:06:51 -0700

    show username of logged in user in top bar

--- src/interface/src/app/top-bar/top-bar.component.spec.ts
+++ src/interface/src/app/top-bar/top-bar.component.spec.ts
@@ -1,18 +1,19 @@
-import { BehaviorSubject } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialog } from '@angular/material/dialog';
 import { By } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
 
+import { MaterialModule } from '../material/material.module';
+import { AuthService, SessionService, User } from '../services';
+import { Region } from '../types';
 import { AccountDialogComponent } from './../account-dialog/account-dialog.component';
 import { TopBarComponent } from './top-bar.component';
-import { SessionService } from '../services';
-import { Region } from '../types';
 
 describe('TopBarComponent', () => {
   let component: TopBarComponent;
   let fixture: ComponentFixture<TopBarComponent>;
+  let mockAuthService: Partial<AuthService>;
   let mockSessionService: Partial<SessionService>;
 
   beforeEach(async () => {
@@ -21,20 +22,24 @@ describe('TopBarComponent', () => {
       {
         open: undefined,
       },
-      {});
+      {}
+    );
+    mockAuthService = {
+      loggedInUser$: new BehaviorSubject<User | null>(null),
+    };
     mockSessionService = {
-      region$: new BehaviorSubject<Region|null>(null),
+      region$: new BehaviorSubject<Region | null>(null),
       setRegion: () => {},
     };
     await TestBed.configureTestingModule({
-      imports: [ MatDialogModule, MatIconModule, MatToolbarModule ],
-      declarations: [ TopBarComponent ],
+      imports: [HttpClientTestingModule, MaterialModule],
+      declarations: [TopBarComponent],
       providers: [
+        { provide: AuthService, useValue: mockAuthService },
         { provide: MatDialog, useValue: fakeMatDialog },
         { provide: SessionService, useValue: mockSessionService },
       ],
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TopBarComponent);
     component = fixture.componentInstance;
@@ -49,7 +54,9 @@ describe('TopBarComponent', () => {
     spyOn(component.toggleEvent, 'emit');
 
     // Act: click on the menu icon
-    const menuButton = fixture.debugElement.query(By.css('[data-testid="menu-button"]'));
+    const menuButton = fixture.debugElement.query(
+      By.css('[data-testid="menu-button"]')
+    );
     const clickEvent = new MouseEvent('click');
     menuButton.triggerEventHandler('click', clickEvent);
 
@@ -58,12 +65,13 @@ describe('TopBarComponent', () => {
   });
 
   it('should open account dialog', () => {
-    const fakeMatDialog: MatDialog = fixture.debugElement.injector.get(
-      MatDialog
-    );
+    const fakeMatDialog: MatDialog =
+      fixture.debugElement.injector.get(MatDialog);
 
     // Act: click on the account icon
-    const accountButton = fixture.debugElement.query(By.css('[data-testid="account-button"]'));
+    const accountButton = fixture.debugElement.query(
+      By.css('[data-testid="account-button"]')
+    );
     const clickEvent = new MouseEvent('click');
     accountButton.triggerEventHandler('click', clickEvent);
 
@@ -74,7 +82,9 @@ describe('TopBarComponent', () => {
   describe('Region selection dropdown', () => {
     it('should set the region', () => {
       const setRegionSpy = spyOn<any>(mockSessionService, 'setRegion');
-      const regionDropdown = fixture.debugElement.query(By.css('[class="region-dropdown"]')).nativeElement;
+      const regionDropdown = fixture.debugElement.query(
+        By.css('[class="region-dropdown"]')
+      ).nativeElement;
 
       regionDropdown.value = regionDropdown.options[0].value;
       regionDropdown.dispatchEvent(new Event('change'));
@@ -84,4 +94,15 @@ describe('TopBarComponent', () => {
     });
   });
 
+  describe('username', () => {
+    it('should be "Guest" when no user is logged in', () => {
+      expect(component.username).toEqual('Guest');
+    });
+
+    it('should be the username of the logged in user', () => {
+      mockAuthService.loggedInUser$?.next({ username: 'User' });
+
+      expect(component.username).toEqual('User');
+    });
+  });
 });
