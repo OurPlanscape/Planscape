259b442950dc601c3000e34303f47ea599c3093b
Hana Lee <leehana@alumni.stanford.edu>
2023-03-15 16:39:15 -0700

    revamp signup page

--- src/interface/src/app/signup/signup.component.spec.ts
+++ src/interface/src/app/signup/signup.component.spec.ts
@@ -1,31 +1,31 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
 
+import { MaterialModule } from '../material/material.module';
 import { AuthService } from '../services';
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let fakeAuthService: AuthService;
 
   beforeEach(() => {
     const routerStub = () => ({ navigate: (array: string[]) => ({}) });
-    const fakeAuthService = jasmine.createSpyObj<AuthService>(
+    fakeAuthService = jasmine.createSpyObj<AuthService>(
       'AuthService',
       { signup: of({}) },
-      {},
+      {}
     );
     TestBed.configureTestingModule({
-      imports: [FormsModule],
-      schemas: [NO_ERRORS_SCHEMA],
+      imports: [FormsModule, MaterialModule, ReactiveFormsModule],
       declarations: [SignupComponent],
       providers: [
         { provide: Router, useFactory: routerStub },
-        { provide: AuthService, useValue: fakeAuthService }
-      ]
+        { provide: AuthService, useValue: fakeAuthService },
+      ],
     });
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
@@ -35,13 +35,50 @@ describe('SignupComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('onSubmit', () => {
-    it('calls signup()', () => {
-      spyOn(component, 'signup').and.callThrough();
+  describe('signup form', () => {
+    it('disables continue button when first half of form is incomplete', () => {
+      expect(component.enableContinue()).toBeFalse();
+    });
 
-      component.onSubmit();
+    it('enables continue button when first half of form is complete', () => {
+      component.form.get('firstName')?.setValue('Foo');
+      component.form.get('lastName')?.setValue('Bar');
 
-      expect(component.signup).toHaveBeenCalled();
+      expect(component.enableContinue()).toBeTrue();
+    });
+
+    it('disables submit when password fields are not equal', () => {
+      component.form.get('password1')?.setValue('foo');
+      component.form.get('password2')?.setValue('bar');
+
+      expect(component.form.valid).toBeFalse();
+    });
+
+    it('enables submit when all fields are valid', () => {
+      component.form.get('firstName')?.setValue('Foo');
+      component.form.get('lastName')?.setValue('Bar');
+      component.form.get('email')?.setValue('test@test.com');
+      component.form.get('password1')?.setValue('password');
+      component.form.get('password2')?.setValue('password');
+
+      expect(component.form.valid).toBeTrue();
+    });
+  });
+
+  describe('signup', () => {
+    it('calls authentication service', () => {
+      component.form.get('email')?.setValue('test@test.com');
+      component.form.get('password1')?.setValue('password');
+      component.form.get('password2')?.setValue('password');
+
+      component.signup();
+
+      expect(fakeAuthService.signup).toHaveBeenCalledOnceWith(
+        'test@test.com',
+        'test@test.com',
+        'password',
+        'password'
+      );
     });
   });
 
--- src/interface/src/app/signup/signup.component.ts
+++ src/interface/src/app/signup/signup.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from './../services';
@@ -6,27 +12,47 @@ import { AuthService } from './../services';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
-
   error: any;
 
-  model: any = {};
+  form: FormGroup;
+  step: number = 0;
 
   constructor(
     private authService: AuthService,
-    private router: Router,
-  ) { }
-
-  onSubmit() {
-    this.signup(this.model.username, this.model.email, this.model.password1, this.model.password2);
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group(
+      {
+        firstName: this.formBuilder.control('', Validators.required),
+        lastName: this.formBuilder.control('', Validators.required),
+        department: this.formBuilder.control(''),
+        email: this.formBuilder.control('', [
+          Validators.required,
+          Validators.email,
+        ]),
+        password1: this.formBuilder.control('', [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+        password2: this.formBuilder.control('', Validators.required),
+      },
+      {
+        validator: this.passwordsMatchValidator,
+      }
+    );
   }
 
-  signup(username: string, email: string, password1: string, password2: string) {
-    this.authService.signup(username, email, password1, password2).subscribe(
-      _ => this.router.navigate(['map']),
-      error => this.error = error
+  signup() {
+    const email: string = this.form.get('email')?.value;
+    const password1: string = this.form.get('password1')?.value;
+    const password2: string = this.form.get('password2')?.value;
+    this.authService.signup(email, email, password1, password2).subscribe(
+      (_) => this.router.navigate(['map']),
+      (error) => (this.error = error)
     );
   }
 
@@ -34,4 +60,15 @@ export class SignupComponent {
     this.router.navigate(['login']);
   }
 
+  enableContinue(): boolean {
+    return !!(
+      this.form.get('firstName')?.valid && this.form.get('lastName')?.valid
+    );
+  }
+
+  private passwordsMatchValidator(group: AbstractControl) {
+    const password1 = group.get('password1')?.value;
+    const password2 = group.get('password2')?.value;
+    return password1 === password2 ? null : { passwordsNotEqual: true };
+  }
 }
