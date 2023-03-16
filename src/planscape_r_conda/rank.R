9735a1217e7a8585c3b056cfd53e5c1b4cd21a3b
Elsie L <elsieling@google.com>
2023-03-15 18:27:08 -0700

    Update rank.R
    Signed-off-by: Elsie L <elsieling@google.com>

--- src/planscape_r_conda/rank.R
+++ src/planscape_r_conda/rank.R
@@ -12,21 +12,21 @@ times2 <- function(number) {
   print(head(stand_dat))
   print("elsie 2")
   
-#   run_outputs <- forsys::run(
-#     return_outputs = FALSE,
-#     run_with_patchmax = FALSE,
-#     write_outputs = FALSE,
-#     scenario_name = "test_scenario",
-#     stand_data = stand_dat,
-#     stand_id_field = "stand_id",
-#     proj_id_field = "proj_id",
-#     stand_area_field = "area_ha",
-#     scenario_priorities = "priority1",
-#     scenario_output_fields = c("area_ha", "priority1", "priority2", "priority3", "priority4"),
-#     proj_fixed_target =  TRUE,
-#     proj_target_field = "area_ha",
-#     proj_target_value = number
-#   )
+  run_outputs <- forsys::run(
+    return_outputs = FALSE,
+    run_with_patchmax = FALSE,
+    write_outputs = FALSE,
+    scenario_name = "test_scenario",
+    stand_data = stand_dat,
+    stand_id_field = "stand_id",
+    proj_id_field = "proj_id",
+    stand_area_field = "area_ha",
+    scenario_priorities = "priority1",
+    scenario_output_fields = c("area_ha", "priority1", "priority2", "priority3", "priority4"),
+    proj_fixed_target =  TRUE,
+    proj_target_field = "area_ha",
+    proj_target_value = number
+  )
 #   print("elsie 3")
 #   return(run_outputs)
 }
