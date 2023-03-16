fed8180305de75c73803553c417b893ec6a3a8de
Elsie L <elsieling@google.com>
2023-03-15 21:10:20 -0700

    Update rank.R
    Signed-off-by: Elsie L <elsieling@google.com>

--- src/planscape_r_conda/rank.R
+++ src/planscape_r_conda/rank.R
@@ -1,21 +1,23 @@
 times2 <- function(number) {
+  library(forsys)
   library(sf)
   library(dplyr)
-  library(forsys)
-
-  data(test_forest)
-  print("elsie 1")
-
-# stand_dat <- test_forest %>% st_drop_geometry()
-  stand_dat <- test_forest 
-  print("elsie 2")
   
+  test_forest <- test_forest %>% forsys::combine_priorities(
+  fields = c('priority1','priority2'), 
+  weights = c(1,1), 
+  new_field = 'combo_priority')
+
+  print("elsie 1")
+  
+  stand_dat <- test_forest %>% st_drop_geometry()
+  
+  print("elsie 2")
+
   run_outputs <- forsys::run(
-    return_outputs = FALSE,
-    run_with_patchmax = FALSE,
-    write_outputs = FALSE,
+    return_outputs = TRUE,
     scenario_name = "test_scenario",
-    stand_data =  stand_dat,
+    stand_data = stand_dat,
     stand_id_field = "stand_id",
     proj_id_field = "proj_id",
     stand_area_field = "area_ha",
@@ -23,8 +25,8 @@ times2 <- function(number) {
     scenario_output_fields = c("area_ha", "priority1", "priority2", "priority3", "priority4"),
     proj_fixed_target =  TRUE,
     proj_target_field = "area_ha",
-    proj_target_value = 1
+    proj_target_value = 2000
   )
-#   print("elsie 3")
-#   return(run_outputs)
+  print("elsie 3")
+  return(run_outputs)
 }
