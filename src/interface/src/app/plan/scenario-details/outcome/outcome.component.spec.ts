273629602b80fb9c89cbc18c5a9a65f3345bfe2e
cynthia huang <cynhuang@google.com>
2023-03-24 12:12:43 -0700

    calculate actual acres treated and fix my math

--- src/interface/src/app/plan/scenario-details/outcome/outcome.component.spec.ts
+++ src/interface/src/app/plan/scenario-details/outcome/outcome.component.spec.ts
@@ -25,21 +25,43 @@ describe('OutcomeComponent', () => {
         loggedInUser$: new BehaviorSubject<User | null>(null),
       }
     );
-
+    const fakeGeoJson: GeoJSON.GeoJSON = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'MultiPolygon',
+            coordinates: [
+              [
+                [
+                  [10, 20],
+                  [10, 30],
+                  [15, 15],
+                ],
+              ],
+            ],
+          },
+          properties: {
+            shape_name: 'Test',
+          },
+        },
+      ],
+    };
     fakeScenario = {
       id: '1',
       notes: 'bee happy',
       projectAreas: [{
           id: 10,
-          estimatedAreaTreated: 2000,
+          projectArea: fakeGeoJson,
         },
         {
           id: 11,
-          estimatedAreaTreated: 5000,
+          projectArea: fakeGeoJson,
         },
         {
           id: 12,
-          estimatedAreaTreated: 6000,
+          projectArea: fakeGeoJson,
         },
       ],
     }
@@ -75,6 +97,6 @@ describe('OutcomeComponent', () => {
   });
 
   it('should calculate the total acres treated', () => {
-    expect(component.totalAcresTreated).toEqual(13000);
+    expect(component.totalAcresTreated).toEqual(207878832);
   });
 });
