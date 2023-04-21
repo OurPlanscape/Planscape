9f6d14970c3a484658f5a7b7faa8ce1446c1f87b
Sachi Yuan <sachiyuan@google.com>
2023-04-21 14:39:05 -0700

    Feedback form open in new tab

--- src/interface/src/app/app.module.ts
+++ src/interface/src/app/app.module.ts
@@ -44,6 +44,7 @@ import { SignupComponent } from './signup/signup.component';
 import { StringifyMapConfigPipe } from './stringify-map-config.pipe';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { DeleteAccountDialogComponent } from './account-dialog/delete-account-dialog/delete-account-dialog.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
@@ -66,6 +67,7 @@ import { DeleteAccountDialogComponent } from './account-dialog/delete-account-di
     PlanTableComponent,
     SignInDialogComponent,
     DeleteAccountDialogComponent,
+    FeedbackComponent,
   ],
   imports: [
     AppRoutingModule,
