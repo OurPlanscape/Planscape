0d8b0f9b948a9cb482d9081f950a4b2efe10908b
runforyourlives <runforyourlives@gmail.com>
2023-04-23 23:01:42 -0700

    Remove dev-only GA ID.  Both environment.ts and environment.prod.ts will eventually be .gitignore'ed and manually managed on the dev/prod boxes.

--- src/interface/src/environments/environment.prod.ts
+++ src/interface/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   backend_endpoint: 'http://localhost/planscape-backend'
-  google_analytics_id: 'G-4XVSK5FG2M' // TODO: replace with the real prod ID
+  google_analytics_id: '' // Replace with actual ID for prod.
 };
