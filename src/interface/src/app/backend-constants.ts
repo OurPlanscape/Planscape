cfcf5fd84868f81d7dd0b0df5e784189f87c528b
Sachi Yuan <sachiyuan@google.com>
2023-05-04 13:33:43 -0700

    Moved data from Wood element to Wood Product Industry element, added Late Seral Stage layer, changed backend constants to grab tile_endpoint from environment file

--- src/interface/src/app/backend-constants.ts
+++ src/interface/src/app/backend-constants.ts
@@ -2,6 +2,6 @@ import { environment  } from "src/environments/environment";
 
 export const BackendConstants = {
   END_POINT: environment.backend_endpoint,
-  TILES_END_POINT: 'https://dev-geo.planscape.org/geoserver/sierra-nevada/wms?', //TODO Make url managed by environment
+  TILES_END_POINT: environment.tile_endpoint,
 } as const;
  
