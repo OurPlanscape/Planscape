0e96f7e55326e47c94a8026dfa8db4e0df9ebe74
Sachi Yuan <sachiyuan@google.com>
2023-05-04 11:37:57 -0700

    Point tiles endpoint to GeoServer url (to be managed by env variables later)

--- src/interface/src/app/backend-constants.ts
+++ src/interface/src/app/backend-constants.ts
@@ -2,5 +2,6 @@ import { environment  } from "src/environments/environment";
 
 export const BackendConstants = {
   END_POINT: environment.backend_endpoint,
-  TILES_END_POINT: 'https://planscape-tiles.s3.us-west-1.amazonaws.com/'
+  TILES_END_POINT: 'https://dev-geo.planscape.org/geoserver/sierra-nevada/wms?', //TODO Make url managed by environment
 } as const;
+ 
