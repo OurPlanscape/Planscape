c8a455ab12b8a07ae207c7f695134fc92253b4fb
Sachi Yuan <sachiyuan@google.com>
2023-08-11 12:42:06 -0700

    Add physical and budget constraint controls to scenario config page, update load and project config upload functions with new forms

--- src/interface/src/app/plan/create-scenarios/constraints-panel/constraints-panel.component.ts
+++ src/interface/src/app/plan/create-scenarios/constraints-panel/constraints-panel.component.ts
@@ -8,28 +8,4 @@ import { FormGroup, Validators } from '@angular/forms';
 })
 export class ConstraintsPanelComponent {
   @Input() constraintsForm: FormGroup | undefined;
-  @Output() formNextEvent = new EventEmitter<void>();
-  @Output() formBackEvent = new EventEmitter<void>();
-
-  enableMaxCost(): boolean {
-    return !!this.constraintsForm?.get('budgetForm.estimatedCost')?.value;
-  }
-
-  toggleRequiredExcludeDistance() {
-    this.constraintsForm!.controls['excludeDistance'].setValidators(
-      this.constraintsForm!.controls['excludeAreasByDistance'].value
-        ? [Validators.required]
-        : null
-    );
-    this.constraintsForm!.controls['excludeDistance'].updateValueAndValidity();
-  }
-
-  toggleRequiredExcludeSlope() {
-    this.constraintsForm!.controls['excludeSlope'].setValidators(
-      this.constraintsForm!.controls['excludeAreasByDegrees'].value
-        ? [Validators.required]
-        : null
-    );
-    this.constraintsForm!.controls['excludeSlope'].updateValueAndValidity();
-  }
 }
