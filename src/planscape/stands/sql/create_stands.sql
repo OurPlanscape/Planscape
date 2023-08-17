22586fffd90be742f596d82c0cd423d57c8962f9
George Silva <georger.silva@gmail.com>
2023-08-17 12:40:04 -0300

    addressing comments

--- src/planscape/stands/sql/create_stands.sql
+++ src/planscape/stands/sql/create_stands.sql
@@ -3,15 +3,15 @@ CREATE OR REPLACE FUNCTION create_stands(
 		hex_length float,
 		size_indicator varchar,
 		clean bool
-	) RETURNS int as $$
-DECLARE 
+	) RETURNS int AS $$
+DECLARE
 	my_timestamp timestamp := timezone('utc', now());
 BEGIN
 	IF clean THEN
 		DELETE FROM stands_stand ss
 		WHERE size = size_indicator;
 	END IF;
-	
+
 	INSERT INTO stands_stand (created_at, size, geometry, area_m2) (
 		SELECT
 			my_timestamp AS "created_at",
