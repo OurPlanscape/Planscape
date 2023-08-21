e36de905d7a2628e752e7dda25148dd614fbea88
George Silva <georger.silva@gmail.com>
2023-08-21 16:30:03 -0300

    fix indentation & convert all tabs to spaces

--- src/planscape/stands/sql/create_generate_stand_metrics.sql
+++ src/planscape/stands/sql/create_generate_stand_metrics.sql
@@ -2,31 +2,32 @@ CREATE OR REPLACE FUNCTION generate_stand_metrics(_condition_id INT, _clean bool
 RETURNS VOID as $$
 DECLARE
     raster_geometry geometry;
-	stand record;
-	stats stand_stats%rowtype;
-	my_timestamp timestamp := timezone('utc', now());
+    stand record;
+    stats stand_stats%rowtype;
+    my_timestamp timestamp := timezone('utc', now());
 BEGIN
 
     IF _clean THEN
         DELETE FROM stands_standmetric WHERE condition_id = condition_id;
     END IF;
 
-	raster_geometry := (SELECT
-                            st_transform(ST_Envelope(ST_Collect(ST_Envelope(raster))), 4269) 
-                        FROM
-					        conditions_conditionraster cc 
-                        WHERE 
-                            condition_id = _condition_id
-					    GROUP BY
-                            condition_id);
-	
+    raster_geometry := (
+        SELECT
+            st_transform(ST_Envelope(ST_Collect(ST_Envelope(raster))), 4269)
+        FROM
+            conditions_conditionraster cc
+        WHERE
+            condition_id = _condition_id
+        GROUP BY
+            condition_id
+    );
 
-	FOR stand IN
-		SELECT * FROM stands_stand
-        WHERE 
+    FOR stand IN
+        SELECT * FROM stands_stand
+        WHERE
             ss.geometry && raster_geometry AND
             ST_Intersects(ss.geometry, raster_geometry)
-	LOOP
+    LOOP
         stats := compute_stand_stats(stand.id, _condition_id);
 
         insert into stands_standmetric (
@@ -48,6 +49,6 @@ BEGIN
             stats.condition_id,
             stats.stand_id
         );
-	END LOOP;
+    END LOOP;
 END
 $$ LANGUAGE plpgsql;
