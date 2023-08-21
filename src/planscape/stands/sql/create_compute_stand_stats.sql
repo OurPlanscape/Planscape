07d148482928ddad2cf56effafc2505b638a893b
George Silva <georger.silva@gmail.com>
2023-08-21 14:17:35 -0300

    create compute_stand_stats function

--- src/planscape/stands/sql/create_compute_stand_stats.sql
+++ src/planscape/stands/sql/create_compute_stand_stats.sql
@@ -1,35 +1,57 @@
-CREATE OR REPLACE FUNCTION compute_stand_stats(stand_id integer, condition_id int)
+CREATE TYPE stand_stats AS (
+    stand_id     BIGINT,
+    condition_id BIGINT,
+    min          DOUBLE precision,
+    avg          DOUBLE precision,
+    max          DOUBLE precision,
+    SUM          double precision,
+    count        double precision
+);
+
+CREATE OR REPLACE FUNCTION compute_stand_stats(stand_id INT, condition_id INT)
   RETURNS TABLE (
-  	id 	   integer,
-    minval float,
-    maxval float,
-    avgval float,
-    sum    float,
-    count  integer
+  	stand_id 	     integer,
+  	condition_id   integer,
+    min            float,
+    avg            float,
+    max            float,
+    sum            float,
+    count          integer
   ) AS $$
 
-  WITH stand AS (SELECT 
-  					id,
-  					ST_Transform(ss.geometry, 3857) AS geometry
-	             FROM stands_stand ss
-                 WHERE id = stand_id),
+    WITH stand AS (
+      SELECT 
+        id,
+        ST_Transform(ss.geometry, 3857) AS geometry
+      FROM stands_stand ss
+      WHERE
+        id = stand_id
+    ),
 
-  stats AS (SELECT 
-  			s.id as "stand_id",
-  			(ST_SummaryStats(ST_Clip(cc.raster, s.geometry, FALSE))).*
-            FROM conditions_conditionraster cc, stand s
-            WHERE 
-        	 	cc.condition_id = condition_id AND
-				s.geometry && cc.raster AND
-				ST_Intersects(s.geometry, cc.raster))
+    stats AS (
+      SELECT 
+        row_number() OVER (ORDER BY s.id) AS id,
+        s.id AS "stand_id",
+        (ST_SummaryStats(ST_Clip(cc.raster, s.geometry))).*
+      FROM conditions_conditionraster cc, stand s
+      WHERE 
+        cc.condition_id = condition_id AND
+        s.geometry && cc.raster AND
+        ST_Intersects(s.geometry, cc.raster)
+    )
 
-  SELECT stand_id,
-  		 min(ss.min) AS minval,
-         max(ss.max) AS maxval,
-         sum(ss.mean * ss.count)/sum(ss.count) AS avgval,
-         sum(ss.sum) AS sum,
-         sum(ss.count) AS count
-  FROM stats ss
-  group by stand_id
+    SELECT stand_id,
+      condition_id,
+      min(ss.min) AS min,
+      max(ss.max) AS max,
+      sum(ss.mean * ss.count)/sum(ss.count) AS avg,
+      sum(ss.sum) AS sum,
+      sum(ss.count) AS count
+    FROM 
+      stats ss
+    GROUP BY
+      stand_id
 
-$$ LANGUAGE SQL;
+$$ LANGUAGE sql
+IMMUTABLE
+PARALLEL SAFE;
