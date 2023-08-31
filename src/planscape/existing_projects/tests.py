13093aae92f099e05a289c741e094b278e70926e
George Silva <georger.silva@gmail.com>
2023-08-31 15:09:28 -0300

    mock the call to the external service, so we don't get blocked on tests

--- src/planscape/existing_projects/tests.py
+++ src/planscape/existing_projects/tests.py
@@ -1,10 +1,19 @@
+from unittest.mock import patch
 from django.test import TestCase
 from django.urls import reverse
+from requests.models import Response
+
 
 class ITSTest(TestCase):
-        
     def test_token_missing(self):
-        response = self.client.get(
-            reverse('existing_projects:its'), {}, content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        # TODO: restore the response json parse statements removed in commit, https://github.com/OurPlanscape/Planscape/pull/630/commits/605c1d036a8a09916f7fe2dd3448acd90f25218e.
+        response = Response()
+        response.code = 200
+        response._content = b'{ "key" : "a" }'
+        with patch(
+            "existing_projects.views.requests.get", return_value=response
+        ) as get:
+            response = self.client.get(
+                reverse("existing_projects:its"), {}, content_type="application/json"
+            )
+
+            self.assertEqual(response.status_code, 200)
