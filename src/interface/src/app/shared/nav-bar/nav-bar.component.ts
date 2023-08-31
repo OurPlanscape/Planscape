e6df24e039bb91c615bf475e7f649cde60c6886d
Pablo Lopez <pablo.lopez@splice.com>
2023-08-31 16:59:34 -0300

    added actions to copy link, print, and added menu

--- src/interface/src/app/shared/nav-bar/nav-bar.component.ts
+++ src/interface/src/app/shared/nav-bar/nav-bar.component.ts
@@ -8,4 +8,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class NavBarComponent {
   @Input() breadcrumbs: string[] = [];
   @Output() goBack = new EventEmitter<void>();
+
+  copyLink() {
+    navigator.clipboard.writeText(window.location.href);
+  }
+
+  print() {
+    window.print();
+  }
 }
