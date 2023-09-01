fa7605b435d8cb48d71018542d10d85a1db5d66d
Pablo Lopez <pablo.lopez@splice.com>
2023-09-01 17:12:00 -0300

    home and planning areas

---
+++ src/interface/src/app/home/planning-areas/planning-areas.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlanningAreasComponent } from './planning-areas.component';
+
+describe('PlanningAreasComponent', () => {
+  let component: PlanningAreasComponent;
+  let fixture: ComponentFixture<PlanningAreasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PlanningAreasComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PlanningAreasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
