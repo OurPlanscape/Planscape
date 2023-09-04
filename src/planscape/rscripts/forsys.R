a4f2bd62b461903936ceec4bf47b4f6e03516aa1
George Silva <georger.silva@gmail.com>
2023-09-04 12:53:57 -0300

    adds functions to read data from the database and call forsys

--- src/planscape/rscripts/forsys.R
+++ src/planscape/rscripts/forsys.R
@@ -6,6 +6,9 @@ library("DBI")
 library("RPostgreSQL")
 library("optparse")
 library("rjson")
+library("glue")
+library("forsys")
+library("sf")
 
 readRenviron("src/planscape/planscape/.env")
 
@@ -37,20 +40,107 @@ get_connection <- function() {
 }
 
 get_scenario_data <- function(connection, scenario_id) {
-  query <- "SELECT * FROM planning_scenario WHERE id = $1;"
+  query <- "SELECT
+              s.id,
+              s.name,
+              s.configuration
+            FROM
+              planning_scenario s
+            WHERE s.id = $1;"
   result <- dbGetQuery(connection, query, params = list(scenario_id))
   return(head(result, 1))
 }
 
+get_stands <- function(connection, scenario_id) {
+  q <- "
+  WITH plan_scenario AS (
+    SELECT
+      pp.id as \"planning_area_id\",
+      ps.id as \"scenario_id\",
+      pp.geometry
+  FROM planning_planningarea pp
+  LEFT JOIN planning_scenario ps ON (ps.planning_area_id = pp.id)
+  WHERE
+      ps.id = {scenario_id}
+  )
+  SELECT
+      ss.id as \"stand_id\",
+      ST_Transform(ss.geometry, 3310) as \"geometry\",
+      ss.area_m2 / 10000 as \"area_ha\"
+  FROM stands_stand ss, plan_scenario
+  WHERE
+      ss.geometry && plan_scenario.geometry AND
+      ST_Intersects(ss.geometry, plan_scenario.geometry)"
+  query <- glue_sql(q, scenario_id = scenario_id, .con = connection)
+
+  result <- st_read(
+    dsn = connection,
+    layer = NULL,
+    query = query,
+    geometry_column = "geometry"
+  )
+  return(result)
+}
+
+get_stand_metrics <- function(connection, condition_id, stand_ids) {
+  query <- glue_sql(
+    "SELECT
+      stand_id,
+      avg
+     FROM stands_standmetric
+     WHERE
+       condition_id = {condition_id} AND
+       stand_id IN ({stand_ids*}) AND
+       avg is NOT NULL",
+    condition_id = condition_id,
+    stand_ids = stand_ids,
+    .con = connection
+  )
+  result <- dbGetQuery(connection, query)
+  return(result)
+}
+
+merge_data <- function(stands, metrics) {
+  data <- merge(x = stands, y = metrics, by = "stand_id")
+  return(data)
+}
 
 now_utc <- function() {
   strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%S")
 }
 
+get_stand_data <- function(connection, scenario_id, condition_id) {
+  stands <- get_stands(connection, scenario_id)
+  metrics <- get_stand_metrics(connection, condition_id, stands$stand_id)
+  return(merge_data(stands, metrics))
+}
 
-call_forsys <- function(scenario) {
+call_forsys <- function(connection, scenario, condition_id) {
   now <- now_utc()
   configuration <- fromJSON(toString(scenario["configuration"]))
+
+  stand_data <- get_stand_data(connection, scenario$id, condition_id)
+
+  out <- forsys::run(
+    return_outputs = TRUE,
+    write_outputs = TRUE,
+    overwrite_output = TRUE,
+    scenario_name = scenario$name,
+    scenario_output_fields = c("area_ha", "avg"),
+    scenario_priorities = c("avg"),
+    stand_data = stand_data,
+    stand_area_field = "area_ha",
+    stand_id_field = "stand_id",
+    run_with_patchmax = TRUE,
+    patchmax_proj_size = 6000,
+    patchmax_proj_size_min = 1000,
+    patchmax_proj_number = 5,
+    patchmax_SDW = 0.5,
+    patchmax_EPW = 1,
+    patchmax_sample_frac = 0.1,
+    annual_target_field = "area_ha",
+    annual_target = 100000
+  )
 
   print("Calling FORSYS")
   print("scenario")
