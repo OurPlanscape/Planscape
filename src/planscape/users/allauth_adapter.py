28e841208ad85617949e3f94996de3db8188f6da
Janice Leung <janicejl@google.com>
2023-09-07 16:45:54 -0700

    Make path validate instead of confirm.

--- src/planscape/users/allauth_adapter.py
+++ src/planscape/users/allauth_adapter.py
@@ -6,5 +6,5 @@ from allauth.account.adapter import DefaultAccountAdapter
 class CustomAllauthAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         referrer = request.META.get('HTTP_REFERER')
-        confirmation_url = urljoin(referrer, f"confirm/{emailconfirmation.key}")
+        confirmation_url = urljoin(referrer, f"validate/{emailconfirmation.key}")
         return confirmation_url
