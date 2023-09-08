1d510020d361cfe0ea8e86c30e6c0849017d2e7f
Janice Leung <janicejl@google.com>
2023-09-07 17:21:36 -0700

    Override email template.

--- src/planscape/planscape/settings.py
+++ src/planscape/planscape/settings.py
@@ -93,7 +93,10 @@ ROOT_URLCONF = "planscape.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates/allauth")
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
