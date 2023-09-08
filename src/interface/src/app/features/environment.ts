d80786b6fccb0395f46a0bdb4bf2ad29119de652
Xinya Yang <xinyayang0506@gmail.com>
2023-09-08 13:43:23 -0700

    config changes

---
+++ src/interface/src/app/features/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    backend_endpoint: 'http://localhost:8000/planscape-backend',
+    google_analytics_id: 'G-Q9DDPH2717',  // this is our "dev" Google Analytics measurement ID
+    tile_endpoint: 'https://dev-geo.planscape.org/geoserver/',
+    download_endpoint: 'https://dev-geo.planscape.org',
+};
