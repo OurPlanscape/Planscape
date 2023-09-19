8b3568e110c5f57f5caf71f699cbe4dc0aaa24e5
Pablo Lopez <pablo.lopez@splice.com>
2023-09-19 13:26:26 -0300

    fix lint and removing unused components

--- src/interface/src/app/plan/scenario-not-started/scenario-not-started.component.ts
+++ src/interface/src/app/plan/scenario-not-started/scenario-not-started.component.ts
@@ -3,13 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-scenario-not-started',
   templateUrl: './scenario-not-started.component.html',
-  styleUrls: ['./scenario-not-started.component.scss']
+  styleUrls: ['./scenario-not-started.component.scss'],
 })
-export class ScenarioNotStartedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ScenarioNotStartedComponent {}
