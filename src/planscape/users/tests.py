b9a81c84dcdf51ccbce10760e40ea4e7415af24a
janicejl <janicejl@google.com>
2023-09-19 20:07:00 -0700

    [PLAM-38] Reset email text (#985)
    * Email text match provided text.
    
    * Add tests.
    
    * format.

--- src/planscape/users/tests.py
+++ src/planscape/users/tests.py
@@ -29,6 +29,7 @@ class CreateUserTest(TransactionTestCase):
         self.assertEqual(
             mail.outbox[0].subject, "[Planscape] Please Confirm Your E-mail Address"
         )
+        self.assertIn("Team Planscape", mail.outbox[0].body)
 
 
 class DeleteUserTest(TransactionTestCase):
@@ -138,6 +139,7 @@ class PasswordResetTest(TransactionTestCase):
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, "[Planscape] Password Reset E-mail")
         self.assertIn("http://localhost:4200/reset", mail.outbox[0].body)
+        self.assertIn("Team Planscape", mail.outbox[0].body)
 
     def test_reset_confirmation_email(self):
         # POST request to get reset password link.
@@ -165,6 +167,7 @@ class PasswordResetTest(TransactionTestCase):
         # Check that password reset confirmation email was received.
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[1].subject, "[Planscape] Password Reset")
+        self.assertIn("Team Planscape", mail.outbox[1].body)
 
 
 class PasswordChangeTest(TransactionTestCase):
@@ -204,3 +207,4 @@ class PasswordChangeTest(TransactionTestCase):
         # Check that password reset confirmation email was received.
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, "[Planscape] Password Changed")
+        self.assertIn("Team Planscape", mail.outbox[0].body)
