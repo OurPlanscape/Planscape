9524f4161de21defc0127a3a1bed36e9be33da94
Pablo Lopez <pablo.lopez@splice.com>
2023-09-21 01:33:09 -0300

    added tests for pipes and table formats

--- src/interface/src/app/pipes/currency-in-k.pipe.ts
+++ src/interface/src/app/pipes/currency-in-k.pipe.ts
@@ -1,10 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 
+/**
+ * Todo, should this return 1M or 1,000K ?
+ * Update tests and name accordingly
+ */
 @Pipe({
   name: 'currencyInK',
 })
 export class CurrencyInKPipe implements PipeTransform {
+  readonly denominator = 'K';
   constructor(public currencyPipe: CurrencyPipe) {}
 
   transform(value: number): string | null {
@@ -12,12 +17,8 @@ export class CurrencyInKPipe implements PipeTransform {
       return '$0';
     }
     return (
-      this.currencyPipe.transform(
-        Math.round(value / 1000),
-        'USD',
-        'symbol',
-        '1.0'
-      ) + 'K'
+      this.currencyPipe.transform(value / 1000, 'USD', 'symbol', '1.0-2') +
+      this.denominator
     );
   }
 }
