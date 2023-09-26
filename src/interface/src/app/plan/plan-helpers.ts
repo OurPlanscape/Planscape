53229fcf361de081f5c1750a34e51f071b5bdca3
Sachi Yuan <sachiyuan@google.com>
2023-09-25 21:44:50 -0700

    Add planning area note auto-saving and load-in

--- src/interface/src/app/plan/plan-helpers.ts
+++ src/interface/src/app/plan/plan-helpers.ts
@@ -1,6 +1,8 @@
 import area from '@turf/area';
 import { FeatureCollection } from 'geojson';
 
+export const NOTE_SAVE_INTERVAL = 5000;
+
 const SQUARE_METERS_PER_ACRE = 0.0002471054;
 
 export function calculateAcres(planningArea: GeoJSON.GeoJSON) {
