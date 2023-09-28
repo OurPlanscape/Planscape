37b8584e96ba4320b326f790fbf2129c5038b428
Janice Leung <janicejl@google.com>
2023-09-27 19:19:44 -0700

    Confirmation dialog post reset.

--- src/interface/src/app/password-reset/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/interface/src/app/password-reset/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,22 +1,75 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from 'src/app/material/material.module';
 
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
   let fixture: ComponentFixture<ConfirmationDialogComponent>;
+  let loader: HarnessLoader;
+  const routerStub = () => ({ navigate: (_: string[]) => ({}) });
 
   beforeEach(async () => {
+    const fakeDialog = jasmine.createSpyObj(
+      'MatDialog',
+      {
+        open: undefined,
+      },
+      {}
+    );
+    const fakeDialogRef = jasmine.createSpyObj(
+      'MatDialogRef',
+      {
+        close: undefined,
+      },
+      {}
+    );
     await TestBed.configureTestingModule({
+      imports: [MaterialModule, RouterTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [ConfirmationDialogComponent],
+      providers: [
+        { provide: Router, useFactory: routerStub },
+        {
+          provide: MatDialog,
+          useValue: fakeDialog,
+        },
+        {
+          provide: MatDialogRef<ConfirmationDialogComponent>,
+          useValue: fakeDialogRef,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationDialogComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('OK button should close dialog', async () => {
+    const router = fixture.debugElement.injector.get(Router);
+    spyOn(router, 'navigate').and.callThrough();
+
+    const dialogRef = fixture.debugElement.injector.get(MatDialogRef);
+    const okButton: MatButtonHarness = await loader.getHarness(
+      MatButtonHarness.with({ text: /OK/ })
+    );
+
+    await okButton.click();
+
+    expect(dialogRef.close).toHaveBeenCalledOnceWith();
+    expect(router.navigate).toHaveBeenCalledOnceWith(['login']);
   });
 });
