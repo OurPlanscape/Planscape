e1b3582af38165063252be8989e72bc39c0c870b
Pablo Lopez <pablo.lopez@splice.com>
2023-10-03 18:29:44 -0300

    center map on planning area

---
+++ src/interface/src/app/map/map.constants.ts
@@ -0,0 +1,28 @@
+export enum AreaCreationAction {
+  NONE = 0,
+  DRAW = 1,
+  UPLOAD = 2,
+}
+
+export const LEGEND = {
+  labels: [
+    'Highest',
+    'Higher',
+    'High',
+    'Mid-high',
+    'Mid-low',
+    'Low',
+    'Lower',
+    'Lowest',
+  ],
+  colors: [
+    '#f65345',
+    '#e9884f',
+    '#e5ab64',
+    '#e6c67a',
+    '#cccfa7',
+    '#a5c5a6',
+    '#74afa5',
+    '#508295',
+  ],
+};
