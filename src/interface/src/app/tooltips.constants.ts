734efb52aaabb11b3005cedebdba9483e8858785
Pablo Lopez <pablo.lopez@splice.com>
2023-10-04 11:37:24 -0300

    remove unused file

--- src/interface/src/app/tooltips.constants.ts
+++
@@ -1,7 +0,0 @@
-export const TREATMENT_GOALS =
-  'Treatment goals allow you to select one goal that will generate a scenario based on your landscape. Each of the goals uses a different set of input variables, thresholds and primary metrics to generate your result. You can only select one goal per run, however you can do as many scenario runs as you choose by selecting different treatment goal questions and parameters each time.  Hover over the options below for full question details.';
-
-export const PROJECT_AREAS = [
-  'Project Areas are the areas in your planning area that are identified as most viable for treatment. The default is to have Planscape generate your project areas from the treatment goal questions and any constraints that you apply. These project areas will help you see where the optimal treatment areas are on your landscape.',
-  'In some instances, you may already have defined project areas and you want to just apply constraints to those - whether it be to isolate land that is owned by specific groups, to exclude excessive slopes or areas too far from the road. In that case you will need to upload your shapefile into the tool, and then set your constraints. Planscape will then generate your results.',
-];
