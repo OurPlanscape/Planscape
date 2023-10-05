7220caaa603dbb4ea1b65a769b8c2cc0379abec4
lastminutediorama <mkinsella@sig-gis.com>
2023-10-05 18:48:43 -0500

    placeholder for validation page

---
+++ src/interface/src/app/account-validation/account-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-validation',
+  templateUrl: './account-validation.component.html',
+  styleUrls: ['./account-validation.component.scss']
+})
+export class AccountValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
