620af5f07c426c06aa8a4aad5c89eb56b96db892
lastminutediorama <mkinsella@sig-gis.com>
2023-11-02 19:32:10 -0500

    add shared snackbar config with timeout

--- src/interface/src/app/shared/constants.ts
+++ src/interface/src/app/shared/constants.ts
@@ -1,6 +1,14 @@
+import { MatSnackBarConfig } from "@angular/material/snack-bar";
+
 /**
  * @desc Error message indicating some unknown error occurred while attempting a
  * password reset
  */
 export const MSG_RESET_PASSWORD_ERROR =
   'Unable to reset your password at this time. Please try again later.';
+
+export const SNACKBAR_SUCCESS_CONFIG : MatSnackBarConfig<any> = {
+  duration: 4000,
+  panelClass: ['snackbar-success'],
+  verticalPosition: 'top',
+};
