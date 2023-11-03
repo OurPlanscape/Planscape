3cafdb68d11abde658f347e61782a557e322a314
Pablo Lopez <pablo.lopez@splice.com>
2023-11-03 09:13:14 -0300

    added outside region dialog

---
+++ src/interface/src/app/map/outside-region-dialog/outside-region-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outside-region-dialog',
+  templateUrl: './outside-region-dialog.component.html',
+  styleUrls: ['./outside-region-dialog.component.scss']
+})
+export class OutsideRegionDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
