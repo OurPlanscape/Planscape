7cf8d2fae8207af9c03f4bdd7f8347e3ce1143a1
George Silva <863039+george-silva@users.noreply.github.com>
2023-11-08 13:35:09 -0300

    only remove when we have a single prio

--- src/planscape/rscripts/forsys.R
+++ src/planscape/rscripts/forsys.R
@@ -615,9 +615,12 @@ main <- function(scenario_id) {
     scenario,
     configuration$scenario_output_fields
   )
-  # we use this to drop priorities, that are repeated in here - we need those
-  # so front-end can show data from priorities as well
-  outputs <- outputs[! outputs$condition_id %in% priorities$condition_id]
+  
+  if (nrow(priorities) == 1) {
+    # we use this to drop priorities, that are repeated in here - we need those
+    # so front-end can show data from priorities as well
+    outputs <- outputs[! outputs$condition_id %in% priorities$condition_id]
+  }
   restrictions <- get_priorities(
     connection,
     scenario,
