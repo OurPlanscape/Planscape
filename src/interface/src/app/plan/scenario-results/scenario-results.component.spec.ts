bdc94cddb5a2f47582e5badfb252dff55b6ad8c4
Pablo Lopez <pflopez@gmail.com>
2023-11-10 13:42:39 -0300

    adding option of downloading shapefiles on scenario results (#1128)

--- src/interface/src/app/plan/scenario-results/scenario-results.component.spec.ts
+++ src/interface/src/app/plan/scenario-results/scenario-results.component.spec.ts
@@ -3,24 +3,82 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScenarioResultsComponent } from './scenario-results.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+import { FileSaverService } from '../../services/file-saver.service';
+import { ScenarioService } from '../../services/scenario.service';
+import { MockComponent, MockProvider } from 'ng-mocks';
+import { ProjectAreasMetricsComponent } from '../project-areas-metrics/project-areas-metrics.component';
+import { ProjectAreasComponent } from '../project-areas/project-areas.component';
+import { of } from 'rxjs';
 
 describe('ScenarioResultsComponent', () => {
   let component: ScenarioResultsComponent;
   let fixture: ComponentFixture<ScenarioResultsComponent>;
+  let scenarioService: ScenarioService;
+  let fileSaverService: FileSaverService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ScenarioResultsComponent],
+      declarations: [
+        ScenarioResultsComponent,
+        MockComponent(ProjectAreasMetricsComponent),
+        MockComponent(ProjectAreasComponent),
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [HttpClientTestingModule],
+      providers: [
+        MockProvider(ScenarioService, {
+          downloadCsvData: () => of(true),
+          downloadShapeFiles: () => of(true),
+        }),
+        MockProvider(FileSaverService),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ScenarioResultsComponent);
     component = fixture.componentInstance;
+    component.scenarioId = '1234';
+    component.scenarioName = 'A great scenario result';
+
+    scenarioService = TestBed.inject(ScenarioService);
+    fileSaverService = TestBed.inject(FileSaverService);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('downloading files', () => {
+    beforeEach(() => {
+      spyOn(scenarioService, 'downloadCsvData').and.callThrough();
+      spyOn(scenarioService, 'downloadShapeFiles').and.callThrough();
+      spyOn(fileSaverService, 'saveAs');
+    });
+
+    it('should download csv file with scenario name', () => {
+      const button = fixture.debugElement.query(
+        By.css('[data-id="downloadCsv"]')
+      );
+      button.nativeElement.click();
+
+      expect(scenarioService.downloadCsvData).toHaveBeenCalledWith('1234');
+      expect(fileSaverService.saveAs).toHaveBeenCalledWith(
+        jasmine.any(Blob),
+        'A great scenario result csv'
+      );
+    });
+    it('should download shapefile file with scenario name', () => {
+      const button = fixture.debugElement.query(
+        By.css('[data-id="downloadShapeFiles"]')
+      );
+      button.nativeElement.click();
+      expect(scenarioService.downloadShapeFiles).toHaveBeenCalledWith('1234');
+      expect(fileSaverService.saveAs).toHaveBeenCalledWith(
+        jasmine.any(Blob),
+        'A great scenario result shapefiles'
+      );
+    });
   });
 });
--- src/interface/src/app/plan/scenario-results/scenario-results.component.ts
+++ src/interface/src/app/plan/scenario-results/scenario-results.component.ts
@@ -1,14 +1,9 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  Output,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ProjectAreaReport } from '../project-areas/project-areas.component';
 import { ScenarioResult } from '../../types';
 import { parseResultsToProjectAreas } from '../plan-helpers';
+import { ScenarioService } from '../../services/scenario.service';
+import { FileSaverService } from '../../services/file-saver.service';
 
 @Component({
   selector: 'app-scenario-results',
@@ -16,15 +11,20 @@ import { parseResultsToProjectAreas } from '../plan-helpers';
   styleUrls: ['./scenario-results.component.scss'],
 })
 export class ScenarioResultsComponent implements OnChanges {
+  @Input() scenarioId!: string;
+  @Input() scenarioName = 'scenario_results';
   @Input() results: ScenarioResult | null = null;
   @Input() scenarioChartData: any[] = [];
   @Input() priorities: string[] = [];
-  @Output() downloadCsv = new EventEmitter();
 
   areas: ProjectAreaReport[] = [];
-  scenarioOutputFieldsConfigs: any = {};
   data: any[] = [];
   selectedCharts: any[] = [];
+
+  constructor(
+    private scenarioService: ScenarioService,
+    private fileServerService: FileSaverService
+  ) {}
 
   ngOnChanges(changes: SimpleChanges) {
     // parse ScenarioResult
@@ -32,6 +32,34 @@ export class ScenarioResultsComponent implements OnChanges {
       this.areas = parseResultsToProjectAreas(this.results);
       this.data = this.scenarioChartData;
       this.selectedCharts = this.data.slice(0, 4);
+    }
+  }
+
+  downloadCsv() {
+    const filename = this.scenarioName + ' csv';
+    if (this.scenarioId) {
+      this.scenarioService
+        .downloadCsvData(this.scenarioId)
+        .subscribe((data) => {
+          const blob = new Blob([data], {
+            type: 'application/zip',
+          });
+          this.fileServerService.saveAs(blob, filename);
+        });
+    }
+  }
+
+  downloadShapeFiles() {
+    const filename = this.scenarioName + ' shapefiles';
+    if (this.scenarioId) {
+      this.scenarioService
+        .downloadShapeFiles(this.scenarioId)
+        .subscribe((data) => {
+          const blob = new Blob([data], {
+            type: 'application/zip',
+          });
+          this.fileServerService.saveAs(blob, filename);
+        });
     }
   }
 }
