674d14fedd26f3e839dd9fa6fc8848397fc77d24
lastminutediorama <mkinsella@sig-gis.com>
2023-11-20 12:48:40 -0600

    replace comma

--- src/interface/src/app/home/about/about.component.ts
+++ src/interface/src/app/home/about/about.component.ts
@@ -10,6 +10,6 @@ export class AboutComponent {
     'Open source and free to use, for state and federal planners, as well as the public',
     'Supports regional planning for fire resilience and ecological benefits across broader landscapes',
     'Utilizes the latest Regional Resource Kits as the primary data source',
-    'Incorporates the best state and federal science and models'
+    'Incorporates the best state and federal science and models',
   ];
 }
