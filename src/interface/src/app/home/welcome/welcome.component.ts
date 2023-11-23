151fa95ff1964e1c4c463090630c018ff6fb0186
Pablo Lopez <pflopez@gmail.com>
2023-11-23 12:48:21 -0300

    enable north cal behind feature flag (#1149)

--- src/interface/src/app/home/welcome/welcome.component.ts
+++ src/interface/src/app/home/welcome/welcome.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FeatureService } from '../../features/feature.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  showsNorthCal = this.features.isFeatureEnabled('show_north_cal');
+
+  constructor(private features: FeatureService) {}
+}
