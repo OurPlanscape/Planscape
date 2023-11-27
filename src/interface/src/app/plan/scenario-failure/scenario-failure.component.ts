8589116631bdd4b2770949d57bca2317a65a906c
Pablo Lopez <pablo.lopez@splice.com>
2023-11-27 16:35:15 -0300

    add export option on failure scenario

--- src/interface/src/app/plan/scenario-failure/scenario-failure.component.ts
+++ src/interface/src/app/plan/scenario-failure/scenario-failure.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ScenarioService } from '../../services/scenario.service';
+import { FileSaverService } from '../../services/file-saver.service';
 
 @Component({
   selector: 'app-scenario-failure',
@@ -6,6 +8,27 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./scenario-failure.component.scss'],
 })
 export class ScenarioFailureComponent {
+  @Input() scenarioName: string = '';
+  @Input() scenarioId: string = '';
   @Output() goBack = new EventEmitter();
-  constructor() {}
+
+  constructor(
+    private scenarioService: ScenarioService,
+    private fileServerService: FileSaverService
+  ) {}
+
+  downloadCsv() {
+    const filename = this.scenarioName + ' csv';
+    if (this.scenarioId) {
+      this.scenarioService
+        .downloadCsvData(this.scenarioId)
+        .subscribe((data) => {
+          const blob = new Blob([data], {
+            type: 'application/zip',
+          });
+
+          this.fileServerService.saveAs(blob, filename);
+        });
+    }
+  }
 }
