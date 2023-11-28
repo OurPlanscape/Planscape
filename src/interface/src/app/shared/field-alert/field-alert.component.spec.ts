bce76475c1f377c23fb46f8b52d6915fa1b74069
Mark <100235124+lastminutediorama@users.noreply.github.com>
2023-11-28 10:41:20 -0600

    Updates UX for login screen and credential errors (#1142)
    * match login screen to Figma mockup
    * Custom component for field alert

---
+++ src/interface/src/app/shared/field-alert/field-alert.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FieldAlertComponent } from './field-alert.component';
+
+describe('FieldAlertComponent', () => {
+  let component: FieldAlertComponent;
+  let fixture: ComponentFixture<FieldAlertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FieldAlertComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FieldAlertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/interface/src/app/shared/field-alert/field-alert.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-field-alert',
+  templateUrl: './field-alert.component.html',
+  styleUrls: ['./field-alert.component.scss'],
+})
+export class FieldAlertComponent implements OnInit {
+  @Input() visible: boolean = false;
+  @Input() message!: string;
+  @Input() title!: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.message !== '') {
+      this.visible = true;
+    }
+  }
+}
