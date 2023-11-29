af447cedb0738df8f035dfe0fbae40e32c3fe0d6
Pablo Lopez <pflopez@gmail.com>
2023-11-29 12:44:46 -0300

    Account mgmt (#1164)
    * added components and routes
    
    * adding styles
    
    * updated tests

--- src/interface/src/app/app.module.ts
+++ src/interface/src/app/app.module.ts
@@ -54,6 +54,11 @@ import { ConfirmationDialogComponent } from './password-reset/confirmation-dialo
 import { AccountValidationComponent } from './account-validation/account-validation.component';
 import { MapModule } from './map/map.module';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { AccountPageComponent } from './account/account-page/account-page.component';
+import { DetailsComponent } from './account/details/details.component';
+import { CredentialsComponent } from './account/credentials/credentials.component';
+import { DeleteAccountComponent } from './account/delete-account/delete-account.component';
+import { MenuComponent } from './account/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -80,6 +85,11 @@ import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
     ConfirmationDialogComponent,
     AccountValidationComponent,
     DeleteDialogComponent,
+    AccountPageComponent,
+    DetailsComponent,
+    CredentialsComponent,
+    DeleteAccountComponent,
+    MenuComponent,
   ],
   imports: [
     AppRoutingModule,
