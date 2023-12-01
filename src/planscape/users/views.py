315406e98fdbf21133361c046faa6c452ca8eb17
Pablo Lopez <pablo.lopez@splice.com>
2023-12-01 17:04:55 -0300

    clean unused function on users/views

--- src/planscape/users/views.py
+++ src/planscape/users/views.py
@@ -53,32 +53,6 @@ def delete_user(request: HttpRequest) -> HttpResponse:
         return HttpResponseBadRequest("Ill-formed request: " + str(e))
 
 
-def update_name(request: HttpRequest) -> HttpResponse:
-    try:
-        logged_in_user = get_user(request)
-        if logged_in_user is None:
-            raise ValueError("Must be logged in")
-        body = json.loads(request.body)
-
-        print(body)
-        user_email_to_delete = body.get("email", None)
-        password = body.get("password", None)
-        if user_email_to_delete is None or not isinstance(user_email_to_delete, str):
-            raise ValueError("User email must be provided as a string")
-        if not logged_in_user.check_password(password):
-            raise ValueError("Invalid password")
-        if user_email_to_delete != logged_in_user.email:
-            raise ValueError("Cannot delete another user account")
-
-        # Deactivate user account
-        logged_in_user.is_active = False
-        logged_in_user.save()
-
-        return JsonResponse({"deleted": True})
-    except Exception as e:
-        return HttpResponseBadRequest("Ill-formed request: " + str(e))
-
-
 def is_verified_user(request: HttpRequest) -> HttpResponse:
     try:
         logged_in_user = get_user(request)
