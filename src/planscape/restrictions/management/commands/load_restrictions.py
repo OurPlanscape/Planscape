a813dc53a4af093d0a8b4d10449bccd207b0b903
George Silva <georger.silva@gmail.com>
2023-12-07 13:40:10 -0300

    creates loader command for excluded areas

---
+++ src/planscape/restrictions/management/commands/load_restrictions.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand, CommandParser
+from django.db import connection
+from restrictions.loader import MAPPINGS, load_data
+from restrictions.models import Restriction
+
+
+class Command(BaseCommand):
+    help = "Loads restrictions data."
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "--type",
+            type=str,
+        )
+
+        parser.add_argument(
+            "--input-file",
+            type=str,
+        )
+
+        parser.add_argument(
+            "--clear",
+            default=True,
+            action="store_true",
+            help="If set to false, the command will not remove previous restrictions data.",
+        )
+
+    def clear(self, type):
+        Restriction.objects.filter(type=type).delete()
+
+    def handle(self, *args, **options):
+        type = options.get("type")
+        input_file = options.get("input_file")
+        clear = options.get("clear")
+        if type not in list(MAPPINGS.keys()):
+            self.stderr.write("INVALID TYPE")
+            return
+
+        if clear:
+            self.clear(type)
+
+        load_data(
+            input_file,
+            MAPPINGS[type],
+            type,
+        )
