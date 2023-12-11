bc6a46234e94bf42b5308cbd3e446ca65a7f540f
Pablo Lopez <pflopez@gmail.com>
2023-12-11 11:29:04 -0300

    more password validation (#1184)
    * more password validation
    
    * cleanup and also marking current vs new as invalid
    
    * added password matcher to show error
    
    * in progress
    
    * adding validation matchers
    
    * change password validation
    
    * using error matchers
    
    * using field alert

--- src/interface/src/app/account/change-password/change-password.component.ts
+++ src/interface/src/app/account/change-password/change-password.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services';
-import { take } from 'rxjs';
+import { map, take } from 'rxjs';
 import {
   passwordMustBeNewValidator,
   passwordsMustMatchValidator,
 } from '../../validators/passwords';
+import { FormMessageType } from '../../types';
+import { PasswordStateMatcher } from '../../validators/error-matchers';
 
-type State = 'view' | 'editing' | 'saving' | 'error';
+type State = 'view' | 'editing' | 'saving';
 
 @Component({
   selector: 'app-change-password',
@@ -22,15 +24,35 @@ export class ChangePasswordComponent {
   state: State = 'view';
   form: FormGroup;
   error: any;
+  success = false;
+  username$ = this.authService.loggedInUser$.pipe(
+    map((user) => user?.username)
+  );
+
+  currentPasswordStateMatcher = new PasswordStateMatcher([]);
+  passwordStateMatcher = new PasswordStateMatcher([
+    'newPasswordMustBeNew',
+    'newPasswordsMustMatch',
+  ]);
+  confirmPasswordStateMatcher = new PasswordStateMatcher([
+    'newPasswordsMustMatch',
+  ]);
+  showHint = false;
+
+  readonly FormMessageType = FormMessageType;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService
   ) {
-    this.form = this.fb.group(
+    this.form = this.createForm();
+  }
+
+  private createForm() {
+    return this.fb.group(
       {
         current: this.fb.control('', [Validators.required]),
-        password: this.fb.control('', [
+        newPassword: this.fb.control('', [
           Validators.required,
           Validators.minLength(8),
         ]),
@@ -38,8 +60,8 @@ export class ChangePasswordComponent {
       },
       {
         validators: [
-          passwordMustBeNewValidator('current', 'password'),
-          passwordsMustMatchValidator('password', 'passwordConfirm'),
+          passwordMustBeNewValidator('current', 'newPassword'),
+          passwordsMustMatchValidator('newPassword', 'passwordConfirm'),
         ],
       }
     );
@@ -48,29 +70,52 @@ export class ChangePasswordComponent {
   saveForm() {
     if (this.form.invalid) return;
     this.state = 'saving';
+    this.error = null;
+
+    const current = this.form.get('current');
+    const newPassword = this.form.get('newPassword');
+    const passwordConfirm = this.form.get('passwordConfirm');
 
     this.authService
       .changePassword(
-        this.form.get('current')?.value,
-        this.form.get('password')?.value,
-        this.form.get('passwordConfirm')?.value
+        current?.value,
+        newPassword?.value,
+        passwordConfirm?.value
       )
       .pipe(take(1))
       .subscribe(
         (_) => {
           this.error = null;
-          // show success
-          // this.snackbar.open(
-          //   'Updated password successfully',
-          //   'Dismiss',
-          //   SNACK_NOTICE_CONFIG
-          // );
+          this.success = true;
           this.state = 'view';
         },
         (err: any) => {
+          if (err.error.old_password) {
+            current?.setErrors({ invalid: true });
+          }
+          if (err.error.new_password2) {
+            newPassword?.setErrors({ invalid: true });
+            passwordConfirm?.setErrors({ invalid: true });
+          }
           this.error = Object.values(err.error);
-          this.state = 'error';
+          this.state = 'editing';
         }
       );
+  }
+
+  cancel() {
+    this.state = 'view';
+    this.resetAll();
+  }
+
+  edit() {
+    this.state = 'editing';
+    this.resetAll();
+  }
+
+  resetAll() {
+    this.error = null;
+    this.success = false;
+    this.form = this.createForm();
   }
 }
