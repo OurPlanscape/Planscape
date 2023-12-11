608094c2d1e01ce3e85167a2ee4a23c7f83cd613
Mark <100235124+lastminutediorama@users.noreply.github.com>
2023-12-11 10:31:42 -0600

    Add thank you page (#1182)
    * add a thank you page

---
+++ src/interface/src/app/home/learn-more/learn-more.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learn-more',
+  templateUrl: './learn-more.component.html',
+  styleUrls: ['./learn-more.component.scss'],
+})
+export class LearnMoreComponent {
+  constructor() {}
+}
