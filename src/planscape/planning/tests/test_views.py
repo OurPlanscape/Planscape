e90bcceed263a52afb886d27b3e07f4b3ec69338
Mark <100235124+lastminutediorama@users.noreply.github.com>
2024-01-08 11:47:11 -0600

    PLAN-745 Add shared links (#1245)
    * add endpoints for shared links and cron job for deleting old links

--- src/planscape/planning/tests/test_views.py
+++ src/planscape/planning/tests/test_views.py
@@ -1913,3 +1913,74 @@ class DeleteScenarioTest(TransactionTestCase):
         self.assertEqual(Scenario.objects.count(), 4)
         self.assertEqual(ScenarioResult.objects.count(), 4)
         self.assertRegex(str(response.content), r"Must specify scenario id")
+
+
+class CreateSharedLinkTest(TransactionTestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser")
+        self.user.set_password("12345")
+        self.user.save()
+
+    def test_create_shared_link(self):
+        view_state = {
+            "page_attributes": ["a", "b", "c", "d"],
+            "control_values": [
+                {"question1": "yes"},
+                {"question2": "no"},
+                {"question3": "maybe"},
+            ],
+            "long": "-100.00",
+            "lat": "-100.01",
+            "zoom": "+500",
+        }
+        view_json = json.dumps(view_state)
+        self.client.force_login(self.user)
+        # generate the new link with a 'view-state'
+        response = self.client.post(
+            reverse("planning:create_shared_link"),
+            {"view_state": view_json},
+            content_type="application/json",
+        )
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            json_response["link_code"].isalnum(), "Returned string is not alphanumeric"
+        )
+
+    def test_retrieving_new_link(self):
+        view_state = {
+            "page_attributes": ["a", "b", "c", "d"],
+            "control_values": [
+                {"question1": "yes"},
+                {"question2": "no"},
+                {"question3": "maybe"},
+            ],
+            "long": "-100.00",
+            "lat": "-100.01",
+            "zoom": "+500",
+        }
+        view_json = json.dumps(view_state)
+        self.client.force_login(self.user)
+        # generate the new link with a 'view-state'
+        response = self.client.post(
+            reverse("planning:create_shared_link"),
+            {"view_state": view_json},
+            content_type="application/json",
+        )
+        # then fetch the data with the new url
+        json_response = json.loads(response.content)
+        link_code = json_response["link_code"]
+        shared_link_response = self.client.get(
+            reverse("planning:get_shared_link", kwargs={"link_code": link_code}),
+            content_type="application/json",
+        )
+        json_get_response = json.loads(shared_link_response.content)
+        self.assertJSONEqual(json_get_response["view_state"], view_state)
+
+    def test_retrieving_bad_link(self):
+        # then fetch the data with a bad link code
+        shared_link_response = self.client.get(
+            reverse("planning:get_shared_link", kwargs={"link_code": "madeuplink"}),
+            content_type="application/json",
+        )
+        self.assertEqual(shared_link_response.status_code, 404)
