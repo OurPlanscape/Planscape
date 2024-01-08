d238dbf8c98910e0dad54b1c27cd7cf6a8cb9985
lastminutediorama <mkinsella@sig-gis.com>
2024-01-08 10:09:51 -0600

    add cron portion back to branch

---
+++ src/planscape/planning/cron.py
@@ -0,0 +1,16 @@
+from planning.models import SharedLink
+from django.utils import timezone
+from planscape import settings
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def delete_old_shared_links(interval=settings.SHARED_LINKS_NUM_DAYS_VALID):
+    expire_date = timezone.now() - timezone.timedelta(days=interval)
+    links_to_delete = SharedLink.objects.filter(created_at__lte=expire_date)
+    try:
+        counted, _ = links_to_delete.delete()
+        logger.info(f"Deleted {counted} links from shared links table.")
+    except Exception:
+        logger.error("Could not delete links from shared links table.")
