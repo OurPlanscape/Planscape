d5ad8a2779121ad1546e9aaee6e88a3fc598f172
George Silva <863039+george-silva@users.noreply.github.com>
2024-01-09 09:27:00 -0300

    another majority

--- src/planscape/rscripts/forsys.R
+++ src/planscape/rscripts/forsys.R
@@ -64,6 +64,7 @@ METRIC_COLUMNS <- list(
   low_income_population_proportional = "sum"
   wui = "majority",
   mean_percent_fire_return_interval_departure_condition_class = "majority",
+  f3veg100 = "majority"
 )
 
 
