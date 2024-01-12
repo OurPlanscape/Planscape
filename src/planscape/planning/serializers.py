acb578ab517818f407cc499a6fc6945898888827
George Silva <863039+george-silva@users.noreply.github.com>
2024-01-12 14:23:40 -0300

    pablos comments

--- src/planscape/planning/serializers.py
+++ src/planscape/planning/serializers.py
@@ -52,7 +52,7 @@ class ScenarioResultSerializer(serializers.ModelSerializer):
 
 
 class ConfigurationSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    question_id = serializers.IntegerField(allow_null=True, allow_empty=True)
     weights = serializers.ListField(child=serializers.IntegerField(), allow_empty=True)
     est_cost = serializers.FloatField(min_value=1)
     max_budget = serializers.FloatField(
