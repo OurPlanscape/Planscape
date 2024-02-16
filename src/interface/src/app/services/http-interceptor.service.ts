5807521f642f1c56ea345e12d945ce42cbace55e
Pablo Lopez <pablo.lopez@splice.com>
2024-02-16 16:14:16 -0300

    adding interceptor

---
+++ src/interface/src/app/services/http-interceptor.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { catchError, Observable, switchMap } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService) {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (
+          error instanceof HttpErrorResponse &&
+          !request.url.includes('auth/signin') &&
+          error.status === 401
+        ) {
+          console.log('catching error and requesting refrehs');
+          return this.handle401Error(request, next);
+        }
+
+        throw error;
+      })
+    );
+  }
+
+  handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    return this.auth
+      .refreshLoggedInUser()
+      .pipe(switchMap(() => next.handle(request)));
+  }
+}
