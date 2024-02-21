fab5929b6671e9230a3049d209c7aab90b0806f0
Pablo Lopez <pflopez@gmail.com>
2024-02-21 13:44:03 -0300

    adding roles and permissions models and migrations (#1287)
    * adding roles and permissions models and migrations
    
    * moved collaboration out of planning, cleanup
    
    * adding created at and updated at mixin, removing uuid
    
    * added utils with utils for checking permissions
    
    * added utils file for checking roles, removed creator as role on db
    
    * update roles and permissions
    
    * added more methods and tests
    
    * added tests and methods for permissions
    
    * added sceanario archive test and updated migration
    
    * fixed permissions migration, updated utils, still wip
    
    * fixed test
    
    * updated methods
    
    * organizing stuff, updating tests, adding mixin
    
    * cleanup
    
    * added readme
    
    * updated name and added missing field for generic foreign key
    
    * updating migrations to avoid extra migration
    
    * updated relation name
    
    * updated relationship name

---
+++ src/planscape/collaboration/permissions.py
@@ -0,0 +1,99 @@
+from collaboration.utils import check_for_permission, is_creator
+from planning.models import PlanningArea, Scenario
+from django.contrib.auth.models import User
+
+
+class CheckPermissionMixin:
+    @staticmethod
+    def can_view():
+        raise NotImplementedError("Subclasses must implement can_add.")
+
+    @staticmethod
+    def can_add():
+        raise NotImplementedError("Subclasses must implement can_add.")
+
+    @staticmethod
+    def can_change():
+        raise NotImplementedError("Subclasses must implement can_add.")
+
+    @staticmethod
+    def can_remove():
+        raise NotImplementedError("Subclasses must implement can_remove.")
+
+
+class PlanningAreaPermission(CheckPermissionMixin):
+    @staticmethod
+    def can_view(user: User, planning_area: PlanningArea):
+        if is_creator(user, planning_area):
+            return True
+
+        return check_for_permission(user.id, planning_area, "view_planningarea")
+
+    @staticmethod
+    def can_add(user: User, planning_area: PlanningArea):
+        return is_creator(user, planning_area)
+
+    @staticmethod
+    def can_change(user: User, planning_area: PlanningArea):
+        return is_creator(user, planning_area)
+
+    @staticmethod
+    def can_remove(user: User, planning_area: PlanningArea):
+        return is_creator(user, planning_area)
+
+
+class CollaboratorPermission(CheckPermissionMixin):
+    @staticmethod
+    def can_view(user: User, planning_area: PlanningArea):
+        if is_creator(user, planning_area):
+            return True
+
+        return check_for_permission(user.id, planning_area, "view_collaborator")
+
+    @staticmethod
+    def can_add(user: User, planning_area: PlanningArea):
+        if is_creator(user, planning_area):
+            return True
+
+        return check_for_permission(user.id, planning_area, "add_collaborator")
+
+    @staticmethod
+    def can_change(user: User, planning_area: PlanningArea):
+        if is_creator(user, planning_area):
+            return True
+
+        return check_for_permission(user.id, planning_area, "change_collaborator")
+
+    @staticmethod
+    def can_delete(user: User, planning_area: PlanningArea):
+        if is_creator(user, planning_area):
+            return True
+
+        return check_for_permission(user.id, planning_area, "delete_collaborator")
+
+
+class ScenarioPermission(CheckPermissionMixin):
+    @staticmethod
+    def can_view(user: User, scenario: Scenario):
+        if is_creator(user, scenario.planning_area):
+            return True
+
+        return check_for_permission(user.id, scenario.planning_area, "view_scenario")
+
+    @staticmethod
+    def can_add(user: User, scenario: Scenario):
+        if is_creator(user, scenario.planning_area):
+            return True
+
+        return check_for_permission(user.id, scenario.planning_area, "add_scenario")
+
+    @staticmethod
+    def can_change(user: User, scenario: Scenario):
+        if is_creator(user, scenario.planning_area) or scenario.user.pk == user.pk:
+            return True
+
+        return check_for_permission(user.id, scenario.planning_area, "change_scenario")
+
+    @staticmethod
+    def can_delete(user: User, scenario: Scenario):
+        return is_creator(user, scenario.planning_area) or scenario.user.pk == user.pk
