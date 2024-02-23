092929517aa015e1acc0e84f0d870b8221cfb119
George Silva <863039+george-silva@users.noreply.github.com>
2024-02-23 12:04:37 -0300

    some adjustments

---
+++ src/planscape/collaboration/services.py
@@ -0,0 +1,42 @@
+from collaboration.models import Role, UserObjectRole
+from django.db import transaction
+from django.db.models import Model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+from collaboration.tasks import send_invitation
+from planning.models import PlanningArea
+
+
+User = get_user_model()
+
+
+def get_model_from_entity(target_entity: str) -> Model:
+    return ContentType.objects.get(model=target_entity)
+
+
+@transaction.atomic()
+def create_invite(
+    inviter,
+    email: str,
+    role: Role,
+    target_entity: str,
+    object_pk: int,
+    message: str,
+):
+    entity_type = get_model_from_entity(target_entity)
+    collaborator = User.objects.filter(email=email).first()
+    collaborator_exists = collaborator is not None
+    object_role, created = UserObjectRole.objects.update_or_create(
+        email=email,
+        content_type=entity_type,
+        object_pk=object_pk,
+        defaults={
+            "role": role,
+            "inviter": inviter,
+            "collaborator": collaborator,
+        },
+    )
+
+    send_invitation.delay(object_role.pk, collaborator_exists, message)
+
+    return object_role
