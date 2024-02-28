c174d0c84a97c7083f461a26d98e52c8a28cf3be
George Silva <863039+george-silva@users.noreply.github.com>
2024-02-28 13:33:57 -0300

    unit tests

--- src/planscape/collaboration/tests/test_views.py
+++ src/planscape/collaboration/tests/test_views.py
@@ -57,3 +57,42 @@ class CreateSharedLinkTest(APITransactionTestCase):
             reverse("collaboration:create_invite"), data, format="json"
         )
         self.assertEqual(response.status_code, 403)
+
+
+class GetInvitationsTest(APITransactionTestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(username="testuser1")
+        self.user1.set_password("12345")
+        self.user1.save()
+        self.user2 = User.objects.create(username="testuser2")
+        self.user2.set_password("12345")
+        self.user2.save()
+        self.pa1 = PlanningArea.objects.create(user=self.user1, region_name="foo")
+        self.pa2 = PlanningArea.objects.create(user=self.user2, region_name="foo")
+
+    def test_returns_all_invites(self):
+        self.client.force_authenticate(self.user1)
+        response = self.client.get(
+            reverse(
+                "collaboration:get_invitations",
+                kwargs={"target_entity": "planningarea", "object_pk": self.pa1.pk},
+            ),
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        assert len(data) == 0
+
+    def test_returns_one_invite_not_you(self):
+        UserObjectRole.objects.create(
+            collaborator=self.user2, inviter=self.user1, content_object=self.pa1
+        )
+        self.client.force_authenticate(self.user1)
+        response = self.client.get(
+            reverse(
+                "collaboration:get_invitations",
+                kwargs={"target_entity": "planningarea", "object_pk": self.pa1.pk},
+            ),
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        assert len(data) == 1
