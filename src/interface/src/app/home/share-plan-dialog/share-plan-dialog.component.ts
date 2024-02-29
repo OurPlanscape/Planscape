bae9f8e7102ebede533ad32106e52d74889d2baa
Pablo Lopez <pflopez@gmail.com>
2024-02-29 09:45:04 -0300

    added list on invites on share plan modal (#1322)
    * added list on invites on share plan modal
    
    * showing the user name as owner

--- src/interface/src/app/home/share-plan-dialog/share-plan-dialog.component.ts
+++ src/interface/src/app/home/share-plan-dialog/share-plan-dialog.component.ts
@@ -1,22 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { of } from 'rxjs';
-import { FormMessageType } from '../../types';
+import { FormMessageType, User } from '../../types';
 import { SNACK_BOTTOM_NOTICE_CONFIG } from '../../shared/constants';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { INVITE_ROLE, InvitesService } from '../../services/invites.service';
+import { InvitesService } from '../../services/invites.service';
+import { Invite, INVITE_ROLE } from '../../types/invite.types';
+import { filter, map, tap } from 'rxjs';
+import { AuthService } from '../../services';
 
 const Roles: Record<INVITE_ROLE, string> = {
   Viewer: 'Viewer',
   Collaborator: 'Collaborator',
   Owner: 'Owner',
 };
-
-export interface Invite {
-  name: string;
-  role: string;
-  email: string;
-}
 
 @Component({
   selector: 'app-share-plan-dialog',
@@ -28,6 +24,7 @@ export class SharePlanDialogComponent {
     private matSnackBar: MatSnackBar,
     private dialogRef: MatDialogRef<SharePlanDialogComponent>,
     private inviteService: InvitesService,
+    private authService: AuthService,
     @Inject(MAT_DIALOG_DATA) public data: { name: string; id: number }
   ) {}
 
@@ -37,17 +34,16 @@ export class SharePlanDialogComponent {
   showHelp = false;
   submitting = false;
   message = '';
+  isLoading = true;
 
-  invites$ = of([
-    { name: 'John Doe', role: 'Owner', email: 'john@doe.com' },
-    { name: 'Richard Doe', role: 'Collaborator', email: 'richard@doe.com' },
-    { name: 'John Doe', role: 'Owner', email: 'john@doe.com' },
-    { name: 'Richard Doe', role: 'Collaborator', email: 'richard@doe.com' },
-    { name: 'John Doe', role: 'Owner', email: 'john@doe.com' },
-    { name: 'Richard Doe', role: 'Collaborator', email: 'richard@doe.com' },
-    { name: 'John Doe', role: 'Owner', email: 'john@doe.com' },
-    { name: 'Richard Doe', role: 'Collaborator', email: 'richard@doe.com' },
-  ] as Invite[]);
+  invites$ = this.inviteService
+    .getInvites(this.data.id)
+    .pipe(tap((_) => (this.isLoading = false)));
+
+  fullname$ = this.authService.loggedInUser$.pipe(
+    filter((user): user is User => !!user),
+    map((user) => [user.firstName, user.lastName].join(' '))
+  );
 
   roles = Object.keys(Roles);
 
