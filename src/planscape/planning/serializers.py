b0355bcf4e80e35f761a89767c8e9659998c2063
George Silva <863039+george-silva@users.noreply.github.com>
2024-03-05 17:21:19 -0300

    big refactor to remove bad past decisions

--- src/planscape/planning/serializers.py
+++ src/planscape/planning/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
-from rest_framework.serializers import CharField, DateTimeField, IntegerField
 from rest_framework_gis import serializers as gis_serializers
 from django.conf import settings
-from collaboration.models import Permissions
 from collaboration.services import get_role, get_permissions
 from planning.models import PlanningArea, Scenario, ScenarioResult, SharedLink
 from planning.services import get_acreage
@@ -10,20 +8,23 @@ from stands.models import StandSizeChoices
 
 
 # TODO: flesh all serializers more for better maintainability.
-class PlanningAreaSerializer(gis_serializers.GeoFeatureModelSerializer):
-    scenario_count = IntegerField(read_only=True, required=False)
-
+class PlanningAreaSerializer(gis_serializers.GeoModelSerializer):
+    scenario_count = serializers.IntegerField(read_only=True, required=False)
+    region_name = serializers.SerializerMethodField()
     # latest_updated takes into account the plan's scenario's updated timestamps and should
     # be used by clients rather than the row-level updated_at field.
     latest_updated = serializers.SerializerMethodField()
-    notes = CharField(required=False)
-    created_at = DateTimeField(required=False)
+    notes = serializers.CharField(required=False)
+    created_at = serializers.DateTimeField(required=False)
 
     area_m2 = serializers.SerializerMethodField()
     area_acres = serializers.SerializerMethodField()
     creator = serializers.CharField(source="creator_name")
     permissions = serializers.SerializerMethodField()
     role = serializers.SerializerMethodField()
+
+    def get_region_name(self, instance):
+        return instance.get_region_name_display()
 
     def get_area_m2(self, instance):
         geom = instance.geometry.transform(settings.AREA_SRID, clone=True)
@@ -33,7 +34,9 @@ class PlanningAreaSerializer(gis_serializers.GeoFeatureModelSerializer):
         return get_acreage(instance.geometry)
 
     def get_latest_updated(self, instance):
-        return instance.scenario_latest_updated_at or instance.updated_at
+        return (
+            getattr(instance, "scenario_latest_updated_at", None) or instance.updated_at
+        )
 
     def get_role(self, instance):
         user = self.context["request"].user
@@ -58,6 +61,7 @@ class PlanningAreaSerializer(gis_serializers.GeoFeatureModelSerializer):
             "creator",
             "role",
             "permissions",
+            "geometry",
         )
         model = PlanningArea
         geo_field = "geometry"
@@ -143,9 +147,9 @@ class ConfigurationSerializer(serializers.Serializer):
 
 class ScenarioSerializer(serializers.ModelSerializer):
     configuration = ConfigurationSerializer()
-    notes = CharField(required=False)
-    updated_at = DateTimeField(required=False)
-    created_at = DateTimeField(required=False)
+    notes = serializers.CharField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
+    created_at = serializers.DateTimeField(required=False)
     scenario_result = ScenarioResultSerializer(
         required=False,
         read_only=True,
