3bf3faabcefbcf75f295b16279307c1542922eb6
George Silva <863039+george-silva@users.noreply.github.com>
2024-03-08 10:52:31 -0300

    fix links

--- src/planscape/planscape/utils.py
+++ src/planscape/planscape/utils.py
@@ -1,4 +1,5 @@
 import logging
+from django.conf import settings
 
 
 class NotInTestingFilter(logging.Filter):
@@ -6,3 +7,23 @@ class NotInTestingFilter(logging.Filter):
         from django.conf import settings
 
         return not settings.TESTING_MODE
+
+
+def get_base_url(env):
+    match env:
+        case "production":
+            return "https://app.planscape.org"
+        case "staging":
+            return "https://staging.planscape.org"
+        case "demo":
+            return "https://demo.planscape.org"
+        case _:
+            return "https://dev.planscape.org"
+
+
+def get_frontend_url(url: str):
+    if url.startswith("/"):
+        raise ValueError("URLs should not start with /. This is done internally here.")
+
+    env = settings.ENV
+    return f"{get_base_url(env)}/{url}"
