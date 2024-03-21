a1176ce6bb256561ba1f3a2bf600c949848c6013
Pablo Lopez <pablo.lopez@splice.com>
2024-03-20 22:42:04 -0300

    updated to angular 16

--- src/interface/src/app/services/auth.service.ts
+++ src/interface/src/app/services/auth.service.ts
@@ -1,12 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { SNACK_NOTICE_CONFIG } from '../../app/shared/constants';
 import {
@@ -363,7 +358,7 @@ export class AuthService {
  *  in, redirect to the sign-in page.
  */
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     private authService: AuthService,
     private router: Router,
