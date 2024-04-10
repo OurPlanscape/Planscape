76242de5a21e68ef7d2cc511e1bc635bbfe0c964
lastminutediorama <mkinsella@sig-gis.com>
2024-04-10 16:20:46 -0500

    add fail result, assume all scenario tests should pass

--- src/planscape/planning/e2e/tasks.py
+++ src/planscape/planning/e2e/tasks.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from celery import shared_task
 
 from planning.models import Scenario
@@ -13,6 +14,16 @@ def review_results(sid, validation_schema) -> object:
     try:
         scenario = Scenario.objects.get(id=sid)
         res = scenario.results.result
+        if scenario.results.status != "SUCCESS":
+            log.error("FAILED to process scenario: {scenario.id} {scenario.name}")
+            print(f"Forsys Failed for {scenario.name}")
+            return json.dumps(
+                {
+                    "result": "FAILED",
+                    "scenario_id": sid,
+                    "details": f"Forsys failed for {scenario.name}",
+                }
+            )
         return validation_results(sid, validation_schema, res)
     except Exception as e:
         log.error("ERROR: Could not get a scenario result for scenario id %s", sid)
