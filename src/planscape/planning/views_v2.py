fc6f254150c8fb59ce13c0c4e1bced4b54def07f
lastminutediorama <mkinsella@sig-gis.com>
2024-04-24 18:02:23 -0500

    hacky fix to provide a get_for_user() approach

--- src/planscape/planning/views_v2.py
+++ src/planscape/planning/views_v2.py
@@ -1,7 +1,9 @@
 import logging
-from rest_framework import viewsets, filters
+from django.db.models import Q
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
-from rest_framework import status, viewsets
+
+from rest_framework import status, viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -10,6 +12,7 @@ from planning.models import PlanningArea
 from planning.serializers import PlanningAreaSerializer, ListPlanningAreaSerializer
 from planning.filters import PlanningAreaFilter
 from planning.permission import PlanningUserPermission
+from collaboration.models import UserObjectRole
 
 logger = logging.getLogger(__name__)
 
@@ -17,5 +20,20 @@ logger = logging.getLogger(__name__)
 class PlanningAreaViewSet(viewsets.ModelViewSet):
     queryset = PlanningArea.objects.all()
     serializer_class = PlanningAreaSerializer
-    permission_classes = [IsAuthenticated, PlanningUserPermission]
+    permission_classes = [PlanningUserPermission]
     ordering_fields = ["name", "created_at"]
+
+    def get_queryset(self):
+        user = self.request.user
+        content_type_pk = ContentType.objects.get(model="planningarea").pk
+        qs = super().get_queryset()
+        # TODO: there has to be a better way to do this...
+        qs = qs.filter(
+            Q(user=user)
+            | Q(
+                pk__in=UserObjectRole.objects.filter(
+                    collaborator_id=user, content_type_id=content_type_pk
+                ).values_list("object_pk", flat=True)
+            )
+        )
+        return qs
