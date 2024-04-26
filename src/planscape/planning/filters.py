fce2e29cffd48686c14fcc10df483f7e0b890fb6
lastminutediorama <mkinsella@sig-gis.com>
2024-04-25 21:41:30 -0500

    fix region to allow OR queries

--- src/planscape/planning/filters.py
+++ src/planscape/planning/filters.py
@@ -4,13 +4,18 @@ from planning.models import PlanningArea, Scenario, RegionChoices
 
 class PlanningAreaFilter(filters.FilterSet):
     name = filters.CharFilter(lookup_expr="icontains")
-    region_name = filters.MultipleChoiceFilter(
-        choices=RegionChoices.choices,
-    )
+    region_name = filters.CharFilter(field_name="region_name", lookup_expr="in")
 
     class Meta:
         model = PlanningArea
         fields = ["name", "region_name"]
+
+    def filter_queryset(self, queryset):
+        region_filter = self.data.get("region_name")
+        if region_filter:
+            regions = region_filter.split(",")
+            queryset = queryset.filter(region_name__in=regions)
+        return queryset
 
 
 class ScenarioFilter(filters.FilterSet):
