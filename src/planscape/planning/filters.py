6625584953d1ac9e93074cde52d0c7dbc2bb9270
lastminutediorama <mkinsella@sig-gis.com>
2024-04-25 22:17:59 -0500

    add test for updated_by filtering

--- src/planscape/planning/filters.py
+++ src/planscape/planning/filters.py
@@ -5,6 +5,7 @@ from planning.models import PlanningArea, Scenario, RegionChoices
 class PlanningAreaFilter(filters.FilterSet):
     name = filters.CharFilter(lookup_expr="icontains")
     region_name = filters.CharFilter(field_name="region_name", lookup_expr="in")
+    sortby = filters.CharFilter(method="filter_by_sortby")
 
     class Meta:
         model = PlanningArea
@@ -12,9 +13,18 @@ class PlanningAreaFilter(filters.FilterSet):
 
     def filter_queryset(self, queryset):
         region_filter = self.data.get("region_name")
+        name = self.data.get("name")
+        mod_after = self.data.get("mod_after")
+        mod_before = self.data.get("mod_before")
         if region_filter:
             regions = region_filter.split(",")
             queryset = queryset.filter(region_name__in=regions)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if mod_after:
+            queryset = queryset.filter(updated_at__gte=mod_after)
+        if mod_before:
+            queryset = queryset.filter(updated_at__lte=mod_before)
         return queryset
 
 
