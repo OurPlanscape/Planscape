dd7485201696c180f39b6e283a20cd7933c2efd5
Pablo Lopez <pflopez@gmail.com>
2024-05-02 14:42:14 -0300

    Storybook app (#1417)
    * initial
    
    * more storybook
    
    * adding button and colors, wip
    
    * removed boilerplate
    
    * button stories and renaming
    
    * updated storybook after changes
    
    * more storybook stuff
    
    * more button styles
    
    * cleanup on buttons
    
    * remove static content from git
    
    * updated docs
    
    * removed storybook static stuff
    
    * lint
    
    * updates from main
    
    * removing button and more
    
    * lint
    
    * lint
    
    * updated some buttons to use styleguide
    
    * compodoc only for styleguide, added config on tsconfig, moving docs.json
    
    * lint
    
    * added positive variant to buttons
    
    * added button error case

---
+++ src/interface/src/styleguide/button/button.stories.ts
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { argsToTemplate } from '@storybook/angular';
+
+import { ButtonComponent } from './button.component';
+
+type PagePropsAndCustomArgs = ButtonComponent & { content?: string };
+
+/**
+ *
+ * Buttons communicates actions that users can take, such as sharing a document and adding a comment.
+ * They can have several variants as well as optionally an icon.
+ */
+const meta: Meta<PagePropsAndCustomArgs> = {
+  title: 'Components/Buttons',
+  component: ButtonComponent,
+  tags: ['autodocs'],
+  render: ({ content, ...args }) => ({
+    props: args,
+    template: `
+     <button sg-button ${argsToTemplate(args)}>${content}</button>`,
+  }),
+};
+
+export default meta;
+type Story = StoryObj<PagePropsAndCustomArgs>;
+
+export const Default: Story = {
+  render: ({ content, ...args }) => ({
+    props: args,
+    template: `<section class='flex'>
+<button sg-button ${argsToTemplate(args)}>Button ${
+      args.variant || 'Default'
+    }</button>
+<button disabled sg-button ${argsToTemplate(args)}>Disabled ${
+      args.variant || 'Default'
+    }</button>
+<button sg-button ${argsToTemplate(args)} icon='draw'> Icon ${
+      args.variant || 'Default'
+    }</button>
+<button disabled sg-button ${argsToTemplate(args)} icon='draw'> Icon ${
+      args.variant || 'Default'
+    }</button>
+</section>`,
+  }),
+};
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    icon: 'add_box',
+    content: 'A primary button',
+  },
+};
+
+export const Ghost: Story = {
+  args: { variant: 'ghost', icon: 'draw', content: 'Ghost button' },
+};
+
+export const Negative: Story = {
+  args: {
+    variant: 'negative',
+    icon: 'delete',
+    content: 'A distructive action',
+  },
+};
+
+export const Text: Story = {
+  args: {
+    variant: 'text',
+    icon: 'help_outline',
+    content: 'A text-only button',
+  },
+};
