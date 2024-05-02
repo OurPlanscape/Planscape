e53c581257079a761d87f08fbe4086e47e2443d3
lastminutediorama <mkinsella@sig-gis.com>
2024-05-02 18:53:25 -0500

    redo from angular installer

---
+++ src/interface/cypress/e2e/spec.cy.ts
@@ -0,0 +1,11 @@
+describe('Login', () => {
+  it('Visits the initial project page', () => {
+    cy.visit('/login');
+    cy.contains('Sign in to Planscape');
+    cy.get('[formControlName="email"]').type('mkinsella+localdev@sig-gis.com');
+    cy.get('[formControlName="password"]').type('mcw9zej@xet3GWV6dhg');
+    cy.get('button[type="submit"]').click();
+    // wait
+    cy.contains('Planning areas');
+  });
+});
