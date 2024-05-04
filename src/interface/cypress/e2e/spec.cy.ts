d89ca489cc2a4ef616d3086cbdb94daf4e9c5f9d
lastminutediorama <mkinsella@sig-gis.com>
2024-05-04 13:20:08 -0500

    revert to before env

--- src/interface/cypress/e2e/spec.cy.ts
+++ src/interface/cypress/e2e/spec.cy.ts
@@ -1,11 +1,17 @@
-import { randString } from '../support/util';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const test_email1 = 'hi@whatever.example';
+const test_pass1 = 'sureABCdef123';
+const test_firstname = 'FromCode';
+const test_lastname = 'FromCodeAgain';
 
 describe('Login', () => {
   it('Visits the initial project page', () => {
     cy.visit('/login');
     cy.contains('Sign in to Planscape');
-    cy.get('[formControlName="email"]').type('mkinsella+localdev@sig-gis.com');
-    cy.get('[formControlName="password"]').type('mcw9zej@xet3GWV6dhg');
+    cy.get('[formControlName="email"]').type('test_email1');
+    cy.get('[formControlName="password"]').type('test_pass1');
     cy.get('button[type="submit"]').click();
     // wait
     cy.contains('Planning areas');
@@ -13,19 +19,15 @@ describe('Login', () => {
 });
 
 // smoke tests:
-
 describe('Create an account with unmatching password', () => {
   it('Visits the initial project page', () => {
     cy.visit('/signup');
     cy.contains('Create your account');
-    cy.get('[formControlName="firstName"]').type('Test');
-    cy.get('[formControlName="lastName"]').type('Person');
-    cy.get('[formControlName="email"]').type(
-      'newuser' + randString(5) + '@example.test'
-    );
-    cy.get('[formControlName="password1"]').type('ABCdef123!#%');
+    cy.get('[formControlName="firstName"]').type(test_firstname);
+    cy.get('[formControlName="lastName"]').type(test_lastname);
+    cy.get('[formControlName="email"]').type(test_email1);
+    cy.get('[formControlName="password1"]').type(test_pass1);
     cy.get('[formControlName="password2"]').type('Nope!#%');
-
     cy.contains('Given passwords must match.');
     cy.get('button[type="submit"]').should('be.disabled');
   });
@@ -36,27 +38,19 @@ describe('Create an account', () => {
   it('Visits the initial project page', () => {
     cy.visit('/signup');
     cy.contains('Create your account');
-    cy.get('[formControlName="firstName"]').type('Test');
-    cy.get('[formControlName="lastName"]').type('Person');
-    cy.get('[formControlName="email"]').type(
-      'newuser' + randString(5) + '@example.test'
-    );
-    cy.get('[formControlName="password1"]').type('ABCdef123!#%');
-    cy.get('[formControlName="password2"]').type('ABCdef123!#%');
+    cy.get('[formControlName="firstName"]').type(test_firstname);
+    cy.get('[formControlName="lastName"]').type(test_lastname);
+    cy.get('[formControlName="email"]').type(test_email1);
+    cy.get('[formControlName="password1"]').type(test_pass1);
+    cy.get('[formControlName="password2"]').type(test_pass1);
     cy.get('button[type="submit"]').click();
-
     cy.wait(2000).then(() => {
       cy.contains('Thank You!');
     });
     // wait
   });
 });
-// create account
-
 // login
-
 // logout
-
 // core operations:
-
-// run
+run;
