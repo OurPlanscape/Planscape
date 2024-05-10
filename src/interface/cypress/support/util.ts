4f813d6da16db7ecf717a188e3da2feef803e082
lastminutediorama <mkinsella@sig-gis.com>
2024-05-09 22:28:53 -0500

    include pablos map spec

--- src/interface/cypress/support/util.ts
+++ src/interface/cypress/support/util.ts
@@ -1,10 +1,12 @@
-export const baseURL = Cypress.env('TEST_BASE_URL');
+export const baseURL =
+  Cypress.env('TEST_BASE_URL') || 'https://dev.planscape.org';
 export const testUser = Cypress.env('TEST_USER1');
 export const testPass = Cypress.env('TEST_PASS1');
 
 export const urls = {
   LOGIN: '/login',
   SIGNUP: '/signup',
+  MAP: '/map',
 };
 
 export function randString(strlen: number) {
