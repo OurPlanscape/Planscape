238ff5d2f8c83894f8511ea22746f79199bf5c39
George Silva <georger.silva@gmail.com>
2024-05-13 13:10:32 -0300

    creates endpoint for project listing / retrieval

---
+++ src/planscape/projects/routers.py
@@ -0,0 +1,5 @@
+from projects.views import ProjectViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r"projects", ProjectViewSet, basename="projects")
---
+++ src/planscape/projects/views.py
@@ -0,0 +1,18 @@
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+
+from projects.filters import ProjectFilterSet
+from projects.models import Project, ProjectVisibility
+
+
+class ProjectViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
+
+    queryset = Project.objects.filter(visibility=ProjectVisibility.PUBLIC)
+    ordering_fields = ["name", "display_name", "created_at"]
+    filterset_class = ProjectFilterSet
+
+    def get_queryset(self):
+        # TODO: we need to customize
+        # this later. This should return
+        # all the projects the user has access to + the ones that are public
+        return super().get_queryset()
