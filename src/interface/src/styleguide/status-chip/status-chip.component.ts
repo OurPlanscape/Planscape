5f0fd5ee002593090ee041171825575a28d010ae
Pablo Lopez <pflopez@gmail.com>
2024-05-14 15:11:24 -0300

    added status chip to storybook (#1514)

---
+++ src/interface/src/styleguide/status-chip/status-chip.component.ts
@@ -0,0 +1,56 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { NgIf, NgSwitch } from '@angular/common';
+
+export type StatusChipStatus = 'inProgress' | 'success' | 'failed' | 'running';
+
+/**
+ * Status chip to display status inline.
+ * Takes optional label or displays the status styled accordingly.
+ */
+@Component({
+  selector: 'sg-status-chip',
+  standalone: true,
+  imports: [NgIf, NgSwitch],
+  templateUrl: './status-chip.component.html',
+  styleUrl: './status-chip.component.scss',
+})
+export class StatusChipComponent {
+  /**
+   * The status
+   */
+  @Input() status: StatusChipStatus = 'inProgress';
+  /**
+   * Optional label
+   */
+  @Input() label? = '';
+
+  /**
+   * @ignore
+   */
+  readonly defaultTextByStatus: Record<StatusChipStatus, string> = {
+    inProgress: 'In Progress',
+    success: 'Success',
+    failed: 'Failed',
+    running: 'Running',
+  };
+
+  @HostBinding('class.in-progress')
+  get isInProgress() {
+    return this.status === 'inProgress';
+  }
+
+  @HostBinding('class.success')
+  get isSuccess() {
+    return this.status === 'success';
+  }
+
+  @HostBinding('class.failed')
+  get isFailed() {
+    return this.status === 'failed';
+  }
+
+  @HostBinding('class.running')
+  get isRunning() {
+    return this.status === 'running';
+  }
+}
