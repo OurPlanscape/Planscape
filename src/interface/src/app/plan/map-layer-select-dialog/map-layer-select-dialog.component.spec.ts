872200a4506db67bc1c3427932abbc778be9c9d0
lastminutediorama <mkinsella@sig-gis.com>
2024-05-23 09:14:51 -0500

    add scenario result layer control and dialog

---
+++ src/interface/src/app/plan/map-layer-select-dialog/map-layer-select-dialog.component.spec.ts
@@ -0,0 +1,60 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MapLayerSelectDialogComponent } from './map-layer-select-dialog.component';
+import {
+  MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
+  MatLegacyDialog as MatDialog,
+  MatLegacyDialogRef as MatDialogRef,
+} from '@angular/material/legacy-dialog';
+
+describe('MapLayerSelectDialogComponent', () => {
+  let component: MapLayerSelectDialogComponent;
+  let fixture: ComponentFixture<MapLayerSelectDialogComponent>;
+
+  beforeEach(async () => {
+    const fakeData = {
+      user: {
+        email: 'test@test.com',
+      },
+    };
+    const fakeDialog = jasmine.createSpyObj(
+      'MatDialog',
+      {
+        open: undefined,
+      },
+      {}
+    );
+    const fakeDialogRef = jasmine.createSpyObj(
+      'MatDialogRef',
+      {
+        close: undefined,
+      },
+      {}
+    );
+    await TestBed.configureTestingModule({
+      // imports: [MapLayerSelectDialogComponent],
+      declarations: [MapLayerSelectDialogComponent],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: fakeData,
+        },
+        {
+          provide: MatDialog,
+          useValue: fakeDialog,
+        },
+        {
+          provide: MatDialogRef<MapLayerSelectDialogComponent>,
+          useValue: fakeDialogRef,
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MapLayerSelectDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
