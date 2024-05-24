c4fbfeec203f840ac0fe4e05d67bafb399c26dd5
Pablo Lopez <pablo.lopez@splice.com>
2024-05-24 09:48:06 -0300

    table

--- src/interface/src/styleguide/table/table.component.ts
+++ src/interface/src/styleguide/table/table.component.ts
@@ -1,27 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { NgForOf } from '@angular/common';
 import { MatSortModule } from '@angular/material/sort';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
 @Component({
   selector: 'sg-table',
@@ -30,7 +10,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+export class TableComponent<T> {
+  @Input() dataSource: T[] = [];
+  @Input() displayedColumns: (keyof T)[] = [];
+  @Output() sortData = new EventEmitter<{
+    active: string;
+    direction: string;
+  }>();
 }
--- src/interface/src/styleguide/table/table.stories.ts
+++ src/interface/src/styleguide/table/table.stories.ts
@@ -1,9 +1,36 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { applicationConfig } from '@storybook/angular';
+import { applicationConfig, argsToTemplate } from '@storybook/angular';
 import { TableComponent } from './table.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
-const meta: Meta<TableComponent> = {
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
+
+const displayedColumns: (keyof PeriodicElement)[] = [
+  'name',
+  'position',
+  'weight',
+  'symbol',
+];
+
+const meta: Meta<TableComponent<PeriodicElement>> = {
   title: 'Components/Table',
   component: TableComponent,
   tags: ['autodocs'],
@@ -14,13 +41,16 @@ const meta: Meta<TableComponent> = {
   ],
   render: (args) => ({
     props: args,
-    template: `<sg-table></sg-table>`,
+    template: `<sg-table ${argsToTemplate(args)}></sg-table>`,
   }),
 };
 
 export default meta;
-type Story = StoryObj<TableComponent>;
+type Story = StoryObj<TableComponent<PeriodicElement>>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    dataSource: ELEMENT_DATA,
+    displayedColumns: displayedColumns,
+  },
 };
