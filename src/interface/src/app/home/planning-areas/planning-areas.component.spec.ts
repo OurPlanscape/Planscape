6a1af89f0d1dbdd21bc32e8de4452596cc7aaac7
Pablo Lopez <pablo.lopez@splice.com>
2024-05-28 17:11:23 -0300

    initial work, renaming and setup flag,etc

--- src/interface/src/app/home/planning-areas/planning-areas.component.spec.ts
+++ src/interface/src/app/home/planning-areas/planning-areas.component.spec.ts
@@ -1,8 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlanningAreasComponent } from './planning-areas.component';
-import { LegacyMaterialModule } from '../../material/legacy-material.module';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('PlanningAreasComponent', () => {
   let component: PlanningAreasComponent;
@@ -10,11 +8,10 @@ describe('PlanningAreasComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PlanningAreasComponent],
-      imports: [LegacyMaterialModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-
+      imports: [PlanningAreasComponent]
+    })
+    .compileComponents();
+    
     fixture = TestBed.createComponent(PlanningAreasComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/interface/src/app/home/planning-areas/planning-areas.component.ts
+++ src/interface/src/app/home/planning-areas/planning-areas.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '@styleguide';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-planning-areas',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    RouterLink,
+    ButtonComponent,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   templateUrl: './planning-areas.component.html',
-  styleUrls: ['./planning-areas.component.scss'],
+  styleUrl: './planning-areas.component.scss',
 })
 export class PlanningAreasComponent {}
