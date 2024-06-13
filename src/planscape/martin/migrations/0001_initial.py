db93887ef11552f8254175ebcb9e56d20d25fc85
George Silva <georger.silva@gmail.com>
2024-06-13 17:02:46 -0300

    add if exists to the drop clause

--- src/planscape/martin/migrations/0001_initial.py
+++ src/planscape/martin/migrations/0001_initial.py
@@ -3,7 +3,7 @@ from django.db import migrations
 from utils.file_utils import read_file
 
 UP_MIGRATION = read_file("martin/sql/martin_get_treatment_plan_prescriptions.sql")
-DOWN_MIGRATION = "DROP FUNCTION martin_get_treatment_plan_prescriptions;"
+DOWN_MIGRATION = "DROP FUNCTION IF EXISTS martin_get_treatment_plan_prescriptions;"
 
 
 class Migration(migrations.Migration):
