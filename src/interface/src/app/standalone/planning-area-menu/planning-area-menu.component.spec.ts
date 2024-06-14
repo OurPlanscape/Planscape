4e23ca27115f4c1bd5b9f28c25a7171cfc2b2cb7
Pablo Lopez <pablo.lopez@splice.com>
2024-06-14 12:03:27 -0300

    added tests to contextual menu permissisons

--- src/interface/src/app/standalone/planning-area-menu/planning-area-menu.component.spec.ts
+++ src/interface/src/app/standalone/planning-area-menu/planning-area-menu.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PlanningAreaMenuComponent } from './planning-area-menu.component';
 import { MockProvider } from 'ng-mocks';
 import { AuthService, PlanService } from '@services';
@@ -11,6 +10,7 @@ import { MOCK_PLAN } from '@services/mocks';
 describe('PlanningAreaMenuComponent', () => {
   let component: PlanningAreaMenuComponent;
   let fixture: ComponentFixture<PlanningAreaMenuComponent>;
+  let authService: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,10 +29,32 @@ describe('PlanningAreaMenuComponent', () => {
     fixture = TestBed.createComponent(PlanningAreaMenuComponent);
     component = fixture.componentInstance;
     component.plan = MOCK_PLAN;
+    authService = TestBed.inject(AuthService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable delete if user cannot delete the planning area', () => {
+    spyOn(authService, 'currentUser').and.returnValue({ id: 123 });
+
+    expect(component.canDeletePlanningArea).toBeFalse();
+  });
+
+  it('should enable delete if user can delete the planning area', () => {
+    spyOn(authService, 'currentUser').and.returnValue({ id: MOCK_PLAN.user });
+    expect(component.canDeletePlanningArea).toBeTrue();
+  });
+
+  it('should disable share if share is not enabled', () => {
+    expect(component.shareEnabled).toBeFalse();
+  });
+
+  it('should enable share if share is enabled', () => {
+    component.plan = { ...MOCK_PLAN, permissions: ['view_collaborator'] };
+    fixture.detectChanges();
+    expect(component.shareEnabled).toBeTrue();
   });
 });
