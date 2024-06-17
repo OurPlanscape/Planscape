b113e0b5a6b445b963f26ff515c43e619458dd6a
lastminutediorama <mkinsella@sig-gis.com>
2024-06-17 11:38:03 -0500

    disable screenshot saving

--- src/interface/cypress.config.ts
+++ src/interface/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: process.env['CYPRESS_TEST_BASE_URL'] ?? 'http://localhost:4200',
   },
-
+  screenshotOnRunFailure: false,
   component: {
     devServer: {
       framework: 'angular',
