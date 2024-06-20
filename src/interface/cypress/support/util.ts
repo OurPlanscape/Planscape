e511dfb61dd11b8b6742e88100c5c77fcab3ef36
Mark <100235124+lastminutediorama@users.noreply.github.com>
2024-06-20 13:09:59 -0500

    Some cypress adjustments (#1580)
    * rm acct creation test
    * look for component, not text, in tests

--- src/interface/cypress/support/util.ts
+++ src/interface/cypress/support/util.ts
@@ -19,11 +19,3 @@ export const urls = {
   SIGNUP: baseURL + '/signup',
   MAP: baseURL + '/map',
 };
-
-export function randString(strlen: number) {
-  let random_string = '';
-  for (let i = 0; i < strlen; i++) {
-    random_string += String.fromCharCode(Math.floor(Math.random() * 25 + 97));
-  }
-  return random_string;
-}
