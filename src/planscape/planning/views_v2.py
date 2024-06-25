e3551462384b4972a1aaaafb4917f1b121be6303
George Silva <863039+george-silva@users.noreply.github.com>
2024-06-25 16:02:01 -0300

    implements new endpoint and service layer for toggling the status of a scenario (#1591)

--- src/planscape/planning/views_v2.py
+++ src/planscape/planning/views_v2.py
@@ -17,6 +17,7 @@ from planning.services import (
     create_scenario,
     delete_planning_area,
     delete_scenario,
+    toggle_scenario_status,
 )
 
 logger = logging.getLogger(__name__)
@@ -111,13 +112,8 @@ class ScenarioViewSet(viewsets.ModelViewSet):
             return Scenario.objects.none()
 
     @action(methods=["post"], detail=True)
-    def toggle_status(self, request, pk=None):
+    def toggle_status(self, request, planningarea_pk, pk=None):
         scenario = self.get_object()
-        scenario.status = (
-            ScenarioStatus.ACTIVE
-            if scenario.status == ScenarioStatus.ARCHIVED
-            else ScenarioStatus.ARCHIVED
-        )
-        scenario.save()
+        toggle_scenario_status(scenario, self.request.user)
         serializer = ScenarioSerializer(instance=scenario)
         return Response(data=serializer.data)
