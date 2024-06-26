b5ece9f5bd6b866a9486c3ffbef69bb3847c3f03
Mark <100235124+lastminutediorama@users.noreply.github.com>
2024-06-25 20:55:52 -0500

    adds additional sorting criteria (#1562)
    * add custom filter
    * clean up misleading tests

--- src/planscape/planning/filters.py
+++ src/planscape/planning/filters.py
@@ -1,5 +1,9 @@
 from django_filters import rest_framework as filters
+from django.db.models import F, Func, Value, ExpressionWrapper, FloatField
+from django.contrib.gis.db.models.functions import Area, Transform
+from django.conf import settings
 from planning.models import PlanningArea, Scenario, RegionChoices
+from rest_framework.filters import OrderingFilter
 
 
 class MultipleValueFilter(filters.CharFilter):
@@ -29,6 +33,39 @@ class PlanningAreaFilter(filters.FilterSet):
     class Meta:
         model = PlanningArea
         fields = ["name", "region_name", "creator"]
+
+
+class PlanningAreaOrderingFilter(OrderingFilter):
+    def filter_queryset(self, request, queryset, view):
+        ordering = self.get_ordering(request, queryset, view)
+
+        if ordering:
+            for order in ordering:
+                reverse = order.startswith("-")
+                field_name = order.lstrip("-")
+
+                if field_name == "full_name":
+                    direction = "-" if reverse else ""
+                    queryset = queryset.annotate(
+                        full_name=Func(
+                            F("user__first_name"),
+                            Value(" "),
+                            F("user__last_name"),
+                            function="CONCAT",
+                        )
+                    ).order_by(f"{direction}full_name")
+
+                if field_name == "area_acres":
+                    direction = "-" if reverse else ""
+                    queryset = queryset.annotate(
+                        area_acres=ExpressionWrapper(
+                            Area(Transform(F("geometry"), settings.AREA_SRID)),
+                            output_field=FloatField(),
+                        )
+                        * settings.CONVERSION_SQM_ACRES
+                    ).order_by(f"{direction}area_acres")
+
+        return super().filter_queryset(request, queryset, view)
 
 
 class ScenarioFilter(filters.FilterSet):
