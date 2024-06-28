408a5b38bfbd946ffdd1aa2ea633eb4c4aa98f84
lastminutediorama <mkinsella@sig-gis.com>
2024-06-28 14:03:03 -0500

    simplify routes

--- src/planscape/planning/routers.py
+++ src/planscape/planning/routers.py
@@ -2,17 +2,16 @@ from planning.views_v2 import PlanningAreaViewSet, ScenarioViewSet, CreatorViewS
 from rest_framework import routers
 
 router = routers.SimpleRouter()
+router.register(
+    r"planningareas/creators",
+    CreatorViewSet,
+    basename="creators",
+)
 router.register(r"planningareas", PlanningAreaViewSet, basename="planningareas")
+
 
 router.register(
     r"planningareas/(?P<planningarea_pk>\d+)/scenarios",
     ScenarioViewSet,
     basename="scenarios",
-)
-
-creator_router = routers.SimpleRouter()
-creator_router.register(
-    r"creators",
-    CreatorViewSet,
-    basename="creators",
 )
