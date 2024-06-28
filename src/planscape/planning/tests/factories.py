5e5fff25a7797be124e9e7bb14f402bb94f35385
George Silva <863039+george-silva@users.noreply.github.com>
2024-06-28 12:58:02 -0300

    Create tx plan endpoint & service layer (#1597)
    implements creation tx plan endpoint & service layer

---
+++ src/planscape/planning/tests/factories.py
@@ -0,0 +1,43 @@
+import factory
+from django.contrib.gis.geos import Polygon, MultiPolygon
+from planning.models import (
+    PlanningArea,
+    Scenario,
+    ScenarioResult,
+    ScenarioResultStatus,
+    ScenarioStatus,
+)
+from planscape.tests.factories import UserFactory
+
+
+class PlanningAreaFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = PlanningArea
+
+    user = factory.SubFactory(UserFactory)
+    region_name = factory.Iterator(
+        [
+            "sierra-nevada",
+            "central-coast",
+        ],
+    )
+
+    name = factory.Sequence(lambda n: "planning area %s" % n)
+
+    geometry = MultiPolygon(Polygon(((1, 1), (1, 2), (2, 2), (1, 1))))
+
+
+class ScenarioFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Scenario
+
+    planning_area = factory.SubFactory(PlanningAreaFactory)
+
+    user = factory.SelfAttribute("planning_area.user")
+
+    name = factory.Sequence(lambda x: "scenario %s" % x)
+
+    uuid = factory.Faker("uuid4")
+
+    status = ScenarioStatus.ACTIVE
+    result_status = ScenarioResultStatus.PENDING
