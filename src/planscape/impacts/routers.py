3c6324f0425599de847cfbf6819eb7cc24eb9912
lastminutediorama <mkinsella@sig-gis.com>
2024-06-29 08:58:47 -0500

    add viewset and router

--- src/planscape/impacts/routers.py
+++ src/planscape/impacts/routers.py
@@ -1,10 +1,16 @@
 from rest_framework.routers import SimpleRouter
 
-from impacts.views import TreatmentPlanViewSet
+from impacts.views import TreatmentPlanViewSet, TreatmentPrescriptionViewSet
 
 router = SimpleRouter()
 router.register(
     "treatment_plans",
     TreatmentPlanViewSet,
     basename="tx-plans",
+)
+
+router.register(
+    "treatment_plans/(?P<tx_plan_pk>\d+)/treatment_prescriptions",
+    TreatmentPrescriptionViewSet,
+    basename="tx-prescriptions",
 )
--- src/planscape/impacts/serializers.py
+++ src/planscape/impacts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from core.fields import UUIDRelatedField
-from impacts.models import TreatmentPlan
+from impacts.models import TreatmentPlan, TreatmentPrescription
 from planning.models import Scenario
 
 
@@ -32,3 +32,25 @@ class TreatmentPlanSerializer(serializers.ModelSerializer):
 
 class TreatmentPlanListSerializer(serializers.ModelSerializer):
     pass
+
+
+class TreatmentPrescriptionSerializer(serializers.ModelSerializer):
+    pass
+
+
+class TreatmentPrescriptionListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TreatmentPrescription
+        fields = (
+            "created_at",
+            "updated_at",
+            "deleted_at",
+            "uuid",
+            "type",
+            "action",
+            "geometry",
+            "stand",
+            "created_by",
+            "project_area",
+            "treatment_plan",
+        )
