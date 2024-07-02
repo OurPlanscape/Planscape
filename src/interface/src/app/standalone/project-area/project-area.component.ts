3534d7e36c431b33cf52ce00fbec4f3064384e48
Pablo Lopez <pablo.lopez@splice.com>
2024-07-02 15:48:59 -0300

    start

---
+++ src/interface/src/app/standalone/project-area/project-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-area.component.html',
+  styleUrl: './project-area.component.scss'
+})
+export class ProjectAreaComponent {
+
+}
