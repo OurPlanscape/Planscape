33cee374d42c45241982f13369e0e7b13caca4a4
Mark <100235124+lastminutediorama@users.noreply.github.com>
2024-07-02 19:14:13 -0500

    Tx prescription viewset endpoint (#1603)
    * add prescription viewset and endpoint
    * add test factories, tests

--- src/planscape/impacts/serializers.py
+++ src/planscape/impacts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from core.fields import UUIDRelatedField
-from impacts.models import TreatmentPlan
+from impacts.models import TreatmentPlan, TreatmentPrescription
 from planning.models import Scenario
 
 
@@ -32,3 +32,22 @@ class TreatmentPlanSerializer(serializers.ModelSerializer):
 
 class TreatmentPlanListSerializer(serializers.ModelSerializer):
     pass
+
+
+class TreatmentPrescriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TreatmentPrescription
+
+
+class TreatmentPrescriptionListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TreatmentPrescription
+        fields = (
+            "created_at",
+            "updated_at",
+            "uuid",
+            "type",
+            "action",
+            "stand",
+            "created_by",
+        )
