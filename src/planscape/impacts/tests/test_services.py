a9a74d47f313e04e8034053b0f314fb43756395c
George Silva <georger.silva@gmail.com>
2024-07-04 16:24:56 -0300

    more tests

--- src/planscape/impacts/tests/test_services.py
+++ src/planscape/impacts/tests/test_services.py
@@ -54,5 +54,28 @@ class UpsertTreatmentPrescriptionTest(TransactionTestCase):
         self.assertEqual(TreatmentPrescription.objects.all().count(), 1)
         self.assertEqual(rx1.pk, results[0].pk)
 
-    # def test_upsert_creates_new_prescription(self):
-    #     pass
+    def test_upsert_creates_new_prescription(self):
+        user = self.treatment_plan.created_by
+        rx1 = TreatmentPrescription.objects.create(
+            created_by=user,
+            updated_by=user,
+            treatment_plan=self.treatment_plan,
+            project_area=self.project_area,
+            type=TreatmentPrescriptionType.SINGLE,
+            action=TreatmentPrescriptionAction.HEAVY_THINNING_BIOMASS,
+            stand=self.stand1,
+            geometry=self.stand1.geometry,
+        )
+
+        self.assertEqual(TreatmentPrescription.objects.all().count(), 1)
+
+        results = upsert_treatment_prescriptions(
+            treatment_plan=self.treatment_plan,
+            project_area=self.project_area,
+            stands=[self.stand2],
+            action_type=TreatmentPrescriptionAction.HEAVY_THINNING_BURN,
+            created_by=user,
+        )
+
+        self.assertEqual(TreatmentPrescription.objects.all().count(), 2)
+        self.assertNotEqual(rx1.pk, results[0].pk)
