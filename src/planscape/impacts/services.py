f1cf8e25b5940f4ec6e44d6d305d0944413390f4
George Silva <863039+george-silva@users.noreply.github.com>
2024-07-08 19:05:01 -0300

    PLAN-1373 (#1609)
    overrides creation methods to upsert treatment prescription

--- src/planscape/impacts/services.py
+++ src/planscape/impacts/services.py
@@ -1,13 +1,25 @@
-from typing import Type
+from typing import List, Type
 from django.db import transaction
-from impacts.models import TreatmentPlan, TreatmentPlanStatus
-from planning.models import Scenario
+from impacts.models import (
+    TreatmentPlan,
+    TreatmentPlanStatus,
+    TreatmentPrescription,
+    TreatmentPrescriptionType,
+    get_prescription_type,
+)
+from planning.models import ProjectArea, Scenario
 from django.contrib.auth.models import AbstractUser
-from actstream import action
+from actstream import action as actstream_action
+
+from stands.models import Stand
 
 TreatmentPlanType = Type[TreatmentPlan]
 ScenarioType = Type[Scenario]
 UserType = Type[AbstractUser]
+StandType = Type[Stand]
+ActionType = Type[TreatmentPrescriptionType]
+ProjectAreaType = Type[ProjectArea]
+TreatmentPrescriptionEntityType = Type[TreatmentPrescription]
 
 
 @transaction.atomic()
@@ -26,5 +38,46 @@ def create_treatment_plan(
         status=TreatmentPlanStatus.PENDING,
         name=name,
     )
-    action.send(created_by, verb="created", action_object=treatment_plan)
+    actstream_action.send(created_by, verb="created", action_object=treatment_plan)
     return treatment_plan
+
+
+@transaction.atomic()
+def upsert_treatment_prescriptions(
+    treatment_plan: TreatmentPlanType,
+    project_area: ProjectAreaType,
+    stands: List[StandType],
+    action_type: ActionType,
+    created_by: UserType,
+) -> List[TreatmentPrescriptionEntityType]:
+    def upsert(treatment_plan, project_area, stand, action_type, user):
+        upsert_defaults = {
+            "type": get_prescription_type(action_type),
+            "created_by": user,
+            "updated_by": user,
+            "geometry": stand.geometry,
+            "action": action_type,
+        }
+        instance, created = TreatmentPrescription.objects.update_or_create(
+            treatment_plan=treatment_plan,
+            project_area=project_area,
+            stand=stand,
+            defaults=upsert_defaults,
+        )
+        verb = "created" if created else "updated"
+        actstream_action.send(user, verb=verb, action_object=instance)
+        return instance
+
+    results = list(
+        map(
+            lambda stand: upsert(
+                treatment_plan=treatment_plan,
+                project_area=project_area,
+                action_type=action_type,
+                user=created_by,
+                stand=stand,
+            ),
+            stands,
+        )
+    )
+    return results
