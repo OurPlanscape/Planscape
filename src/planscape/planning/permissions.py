287a3342799ed2f0e634273ff1736352cf5b612f
George Silva <863039+george-silva@users.noreply.github.com>
2024-08-05 17:46:01 -0300

    fixes has_object_permission for scenario permission (#1659)

--- src/planscape/planning/permissions.py
+++ src/planscape/planning/permissions.py
@@ -31,4 +31,11 @@ class ScenarioViewPermission(PlanscapePermission):
 
     def has_object_permission(self, request, view, object):
         planning_area = object.planning_area
-        return PlanningAreaPermission.can_change(request.user, planning_area)
+        match view.action:
+            case "update" | "partial_update":
+                method = PlanningAreaPermission.can_change
+            case "destroy":
+                method = PlanningAreaPermission.can_remove
+            case _:
+                method = PlanningAreaPermission.can_view
+        return method(request.user, planning_area)
