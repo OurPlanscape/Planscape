248da488671aad59deb49e113d47f7c1733f62ec
Mark <100235124+lastminutediorama@users.noreply.github.com>
2024-08-15 00:36:27 +0000

    Modal confirmation dialog (PLAN-1535) (#1668)
    * add dialog section for modals
    * add scenario confirmation component

---
+++ src/interface/src/styleguide/modal-dialog-section/modal-confirmation-dialog.component.ts
@@ -0,0 +1,67 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+export type ModalDialogVariant = 'success' | 'error' | 'pending' | 'alert';
+
+/**
+ * Modal Confirmation Dialog
+ * A component that can be used within the modal body as a confirmation dialog
+ */
+@Component({
+  selector: 'sg-modal-confirmation-dialog',
+  standalone: true,
+  imports: [NgIf, MatIconModule, MatProgressSpinnerModule],
+  templateUrl: './modal-confirmation-dialog.component.html',
+  styleUrl: './modal-confirmation-dialog.component.scss',
+})
+export class ModalConfirmationDialogComponent {
+  /**
+   * The variant of the dialog
+   */
+  @Input() infoType: ModalDialogVariant = 'alert';
+  /**
+   * Icon to display, if not one of the defaults for this variant
+   */
+  @Input() dialogIcon?: string | null;
+  /**
+   * The content of the headling
+   */
+  @Input() headline?: string | null = '';
+  /**
+   * The message content to display
+   */
+  @Input() message?: string | null = '';
+  readonly IconByVariant: Record<ModalDialogVariant, string | null> = {
+    success: 'check_circle',
+    error: 'error',
+    pending: 'pending_actions',
+    alert: null,
+  };
+
+  // if there an icon specified, use the default for the dialog variant
+  displayIcon() {
+    if (!this.dialogIcon) {
+      return this.IconByVariant[this.infoType];
+    }
+    return this.dialogIcon;
+  }
+
+  @HostBinding('class.success')
+  get isInfo() {
+    return this.infoType === 'success';
+  }
+  @HostBinding('class.error')
+  get isError() {
+    return this.infoType === 'error';
+  }
+  @HostBinding('class.pending')
+  get isWarning() {
+    return this.infoType === 'pending';
+  }
+  @HostBinding('class.alert')
+  get isComplete() {
+    return this.infoType === 'alert';
+  }
+}
