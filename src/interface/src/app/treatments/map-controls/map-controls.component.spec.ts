f64e0fe212c39c6e7cd7a736bc67c8da4d709a45
Pablo Lopez <pablo.lopez@splice.com>
2024-08-14 21:31:09 -0300

    some cleanup, some mess

---
+++ src/interface/src/app/treatments/map-controls/map-controls.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MapControlsComponent } from './map-controls.component';
+
+describe('MapControlsComponent', () => {
+  let component: MapControlsComponent;
+  let fixture: ComponentFixture<MapControlsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MapControlsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MapControlsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/interface/src/app/treatments/treatment-map/treatment-map.component.ts
+++ src/interface/src/app/treatments/treatment-map/treatment-map.component.ts
@@ -13,20 +13,8 @@ import { Map as MapLibreMap, MapMouseEvent } from 'maplibre-gl';
 import { MapStandsComponent } from '../map-stands/map-stands.component';
 import { MapRectangleComponent } from '../map-rectangle/map-rectangle.component';
 import { SelectedStandsState } from './selected-stands.state';
-
-// possible assignment for  a stand.
-export type StandAssigment =
-  | 'selected'
-  | 'treatment-1'
-  | 'treatment-2'
-  | 'treatment-3';
-
-export const StandColors: Record<StandAssigment, string> = {
-  selected: '#0066ff',
-  'treatment-1': '#f8f802',
-  'treatment-2': '#d27601',
-  'treatment-3': '#ee0d0d',
-};
+import { MapControlsComponent } from '../map-controls/map-controls.component';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-treatment-map',
@@ -42,21 +30,21 @@ export const StandColors: Record<StandAssigment, string> = {
     GeoJSONSourceComponent,
     MapStandsComponent,
     MapRectangleComponent,
+    MapControlsComponent,
   ],
   providers: [SelectedStandsState],
   templateUrl: './treatment-map.component.html',
   styleUrl: './treatment-map.component.scss',
 })
 export class TreatmentMapComponent {
-  types: StandAssigment = 'selected';
-  maplibreMap!: MapLibreMap;
-  key = '52395617-45bd-4500-87d4-2159a35e3dcf';
+  mapLibreMap!: MapLibreMap;
+  readonly key = environment.stadiamaps_key;
 
   // ew, not sure if I want to do prop drilling here or set up something else (provider?)
   @Input() projectAreaId: number | null = null;
   @Input() treatmentPlanId = 0;
 
-  treatedStands: { id: number; assigment: StandAssigment }[] = [];
+  treatedStands: { id: number; assigment: string }[] = [];
 
   mapDragging = true;
 
@@ -64,7 +52,7 @@ export class TreatmentMapComponent {
   start: MapMouseEvent | null = null;
   end: MapMouseEvent | null = null;
 
-  constructor(private mapStandsService: SelectedStandsState) {}
+  constructor() {}
 
   // -----------------------------------------------------------------
   // Map events
@@ -76,7 +64,7 @@ export class TreatmentMapComponent {
     }
     this.isDragging = true;
     this.start = event;
-    this.maplibreMap.getCanvas().style.cursor = 'crosshair';
+    this.mapLibreMap.getCanvas().style.cursor = 'crosshair';
   }
 
   onMapMouseMove(event: MapMouseEvent): void {
@@ -87,35 +75,8 @@ export class TreatmentMapComponent {
   onMapMouseUp(event: MapMouseEvent): void {
     if (!this.isDragging) return;
     this.isDragging = false;
-    this.maplibreMap.getCanvas().style.cursor = '';
+    this.mapLibreMap.getCanvas().style.cursor = '';
     this.start = null;
     this.end = null;
-  }
-
-  // -----------------------------------------------------------------
-  // Page demo actions
-  // -----------------------------------------------------------------
-
-  toggleMapDrag() {
-    this.mapDragging = !this.mapDragging;
-  }
-
-  assignTreatment(treatment: StandAssigment) {
-    const selectedStands: number[] = this.mapStandsService.getSelectedStands();
-    this.treatedStands = [
-      ...this.treatedStands.filter(
-        (treatment) => !selectedStands.includes(treatment.id)
-      ),
-      ...selectedStands.map((id) => ({
-        id: id,
-        assigment: treatment,
-      })),
-    ];
-    this.mapStandsService.clearStands();
-  }
-
-  clearTreatments() {
-    this.mapStandsService.clearStands();
-    this.treatedStands = [];
   }
 }
