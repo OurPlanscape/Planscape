335aba4fca2b6495be20ef8f5d1f89757314436a
lastminutediorama <mkinsella@sig-gis.com>
2024-08-28 11:38:04 -0500

    add remaining icons

--- src/interface/src/styleguide/treatment-type-icon/treatment-type-icon.component.ts
+++ src/interface/src/styleguide/treatment-type-icon/treatment-type-icon.component.ts
@@ -1,6 +1,17 @@
 import { Component, Input } from '@angular/core';
-// import { TreatmentStatus } from '@types';
-// import filled_hex from '../../assets/svg/icons/treatment-types/treatment_filled.svg';
+
+// TODO: decide if we want to create a global list of treatment types that map to colors, instead...
+export type TreatmentIconColor =
+  | 'none'
+  | 'blue'
+  | 'purple'
+  | 'orange'
+  | 'yellow'
+  | 'junglegreen'
+  | 'limegreen'
+  | 'red'
+  | 'brown'
+  | 'pink';
 
 @Component({
   selector: 'sg-treatment-type-icon',
@@ -10,20 +21,41 @@ import { Component, Input } from '@angular/core';
   styleUrl: './treatment-type-icon.component.scss',
 })
 export class TreatmentTypeIconComponent {
-  @Input() type = '';
+  @Input() sequenceNum: number | null = null;
+  @Input() treatmentColor: TreatmentIconColor | null = null;
 
-  //   @HostBinding('class.disabled')
-  //   get isDisabled() {
-  //     return this.status === 'RUNNING';
-  //   }
+  //TODO: consider dynamically updating the fill for SVGs, instead
+  readonly treatmentIcons: Record<TreatmentIconColor, string> = {
+    none: 'treatment_none.svg',
+    blue: 'treatment_blue.svg',
+    purple: 'treatment_purple.svg',
+    orange: 'treatment_orange.svg',
+    yellow: 'treatment_yellow.svg',
+    junglegreen: 'treatment_junglegreen.svg',
+    limegreen: 'treatment_limegreen.svg',
+    red: 'treatment_red.svg',
+    brown: 'treatment_brown.svg',
+    pink: 'treatment_pink.svg',
+  };
 
-  //   @HostListener('click')
-  //   viewTreatment() {
-  //     this.view.emit();
-  //   }
+  readonly sequenceIcons: Record<number, string> = {
+    1: 'sequence_1.svg',
+    2: 'sequence_2.svg',
+    3: 'sequence_3.svg',
+    4: 'sequence_4.svg',
+    5: 'sequence_5.svg',
+    6: 'sequence_6.svg',
+    7: 'sequence_7.svg',
+    8: 'sequence_8.svg',
+  };
 
-  //   stopEventPropagation(event: Event) {
-  //     event.stopPropagation();
-  //     return false;
-  //   }
+  iconName() {
+    if (this.sequenceNum !== null) {
+      return this.sequenceIcons[this.sequenceNum];
+    } else if (this.treatmentColor !== null) {
+      return this.treatmentIcons[this.treatmentColor];
+    } else {
+      return 'treatment_none';
+    }
+  }
 }
--- src/interface/src/styleguide/treatment-type-icon/treatment-type-icon.stories.ts
+++ src/interface/src/styleguide/treatment-type-icon/treatment-type-icon.stories.ts
@@ -6,7 +6,6 @@ const meta: Meta<TreatmentTypeIconComponent> = {
   title: 'Components/Treatment Type Icon',
   component: TreatmentTypeIconComponent,
   tags: ['autodocs'],
-  //   decorators: [applicationConfig({})],
   render: (args) => ({
     props: args,
     template: `<sg-treatment-type-icon ${argsToTemplate(args)}></sg-treatment-type-icon>`,
@@ -18,6 +17,98 @@ type Story = StoryObj<TreatmentTypeIconComponent>;
 
 export const Default: Story = {
   args: {
-    type: 'Sequence',
+    sequenceNum: 1,
+  },
+};
+export const Sequence1: Story = {
+  args: {
+    sequenceNum: 1,
+  },
+};
+export const Sequence2: Story = {
+  args: {
+    sequenceNum: 2,
+  },
+};
+export const Sequence3: Story = {
+  args: {
+    sequenceNum: 3,
+  },
+};
+export const Sequence4: Story = {
+  args: {
+    sequenceNum: 4,
+  },
+};
+export const Sequence5: Story = {
+  args: {
+    sequenceNum: 5,
+  },
+};
+export const Sequence6: Story = {
+  args: {
+    sequenceNum: 6,
+  },
+};
+export const Sequence7: Story = {
+  args: {
+    sequenceNum: 7,
+  },
+};
+export const Sequence8: Story = {
+  args: {
+    sequenceNum: 8,
+  },
+};
+
+export const TreatmentNone: Story = {
+  args: {
+    treatmentColor: 'none',
+  },
+};
+export const TreatmentBlue: Story = {
+  args: {
+    treatmentColor: 'blue',
+  },
+};
+export const TreatmentPurple: Story = {
+  args: {
+    treatmentColor: 'purple',
+  },
+};
+export const TreatmentOrange: Story = {
+  args: {
+    treatmentColor: 'orange',
+  },
+};
+export const TreatmentYellow: Story = {
+  args: {
+    treatmentColor: 'yellow',
+  },
+};
+export const TreatmentJungleGreen: Story = {
+  args: {
+    treatmentColor: 'junglegreen',
+  },
+};
+export const TreatmentLimeGreen: Story = {
+  args: {
+    treatmentColor: 'limegreen',
+  },
+};
+export const TreatmentRed: Story = {
+  args: {
+    treatmentColor: 'red',
+  },
+};
+export const TreatmentBrown: Story = {
+  args: {
+    treatmentColor: 'brown',
+  },
+};
+
+export const TreatmentPink: Story = {
+  args: {
+    treatmentColor: 'pink',
   },
 };
