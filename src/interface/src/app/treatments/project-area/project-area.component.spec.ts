3d0d2eed5c3b652fb99af5d9ed8d95000ff4e382
lastminutediorama <mkinsella@sig-gis.com>
2024-08-30 10:55:46 -0500

    fix projectarea test

--- src/interface/src/app/treatments/project-area/project-area.component.spec.ts
+++ src/interface/src/app/treatments/project-area/project-area.component.spec.ts
@@ -1,39 +1,41 @@
-// REMOVE: FAILING TEST UNBLOCK WORKAROUND
-// We may just be missing some declarations/imports for this whole module
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ProjectAreaComponent } from './project-area.component';
-// import { MockProviders } from 'ng-mocks';
-// import { TreatmentsService } from '@services/treatments.service';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { LookupService } from '@services/lookup.service';
-// import { AuthService, NotesService, WINDOW_PROVIDERS } from '@services';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ProjectAreaComponent } from './project-area.component';
+import { MockProviders } from 'ng-mocks';
+import { TreatmentsService } from '@services/treatments.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LookupService } from '@services/lookup.service';
+import {
+  AuthService,
+  ProjectAreaNotesService,
+  WINDOW_PROVIDERS,
+} from '@services';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-// describe('ProjectAreaComponent', () => {
-//   let component: ProjectAreaComponent;
-//   let fixture: ComponentFixture<ProjectAreaComponent>;
+describe('ProjectAreaComponent', () => {
+  let component: ProjectAreaComponent;
+  let fixture: ComponentFixture<ProjectAreaComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [ProjectAreaComponent, MatSnackBarModule, RouterTestingModule],
-//       providers: [
-//         WINDOW_PROVIDERS,
-//         MockProviders(
-//           TreatmentsService,
-//           LookupService,
-//           AuthService,
-//           NotesService
-//         ),
-//       ],
-//     }).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProjectAreaComponent, MatSnackBarModule, RouterTestingModule],
+      providers: [
+        WINDOW_PROVIDERS,
+        MockProviders(
+          TreatmentsService,
+          LookupService,
+          AuthService,
+          ProjectAreaNotesService
+        ),
+      ],
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(ProjectAreaComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(ProjectAreaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
