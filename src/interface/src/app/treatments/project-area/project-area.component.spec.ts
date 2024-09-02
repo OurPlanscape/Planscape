ae37f7c769fcb03ca0c94bbb4409c8a48f8f3786
Pablo Lopez <pflopez@gmail.com>
2024-09-02 11:28:16 -0300

    Map baselayers (#1696)
    * cleaning things up
    
    * adding more sources
    
    * added all the options all the time
    
    * fixed dependencies on tests
    
    * removed other map base options
    
    * moving things to own files

--- src/interface/src/app/treatments/project-area/project-area.component.spec.ts
+++ src/interface/src/app/treatments/project-area/project-area.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectAreaComponent } from './project-area.component';
-import { MockProviders } from 'ng-mocks';
+import { MockDeclarations, MockProviders } from 'ng-mocks';
 import { TreatmentsService } from '@services/treatments.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LookupService } from '@services/lookup.service';
+import { TreatmentMapComponent } from '../treatment-map/treatment-map.component';
+import { PrescriptionActionsComponent } from '../prescription-actions/prescription-actions.component';
 
 describe('ProjectAreaComponent', () => {
   let component: ProjectAreaComponent;
@@ -14,6 +15,9 @@ describe('ProjectAreaComponent', () => {
     await TestBed.configureTestingModule({
       imports: [ProjectAreaComponent, RouterTestingModule],
       providers: [MockProviders(TreatmentsService, LookupService)],
+      declarations: [
+        MockDeclarations(TreatmentMapComponent, PrescriptionActionsComponent),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectAreaComponent);
