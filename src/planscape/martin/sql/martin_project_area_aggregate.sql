1364d6fbd26f774281435ebe42e9aeb37eb02fca
George Silva <georger.silva@gmail.com>
2024-09-10 17:35:28 -0300

    modify query

--- src/planscape/martin/sql/martin_project_area_aggregate.sql
+++ src/planscape/martin/sql/martin_project_area_aggregate.sql
@@ -32,23 +32,18 @@ BEGIN
         ss.size = p_stand_size
   );
 
-  SELECT INTO p_mvt (
-    SELECT ST_AsMVT(tile, 'project_area_aggregate', 4096, 'geom', 'id') FROM (
-      SELECT
-        (query_params->>'project_area_id')::int as "id",
-        ST_AsMVTGeom(
-            ST_Transform(p_stand_geometries, 3857),
-            ST_TileEnvelope(z, x, y),
-            4096, 64, true) AS geom
-      ) as tile WHERE geom IS NOT NULL ||
-      SELECT ST_AsMVT(tile, 'project_area_aggreagate_label', 4096, 'geom', 'id') FROM (
-        (query_params->>'project_area_id')::int as "id",
-        ST_AsMVTGeom(
-            ST_Transform(ST_PointOnSurface(p_stand_geometries), 3857),
-            ST_TileEnvelope(z, x, y),
-            4096, 64, true) AS geom
-      ) as tile WHERE geom IS NOT NULL
-    );
+  SELECT INTO p_mvt ST_AsMVT(compose.*) FROM (
+    SELECT
+      'project_area_aggregate' as "layer",
+      pa.id as "id",
+      pa.scenario_id::int as "scenario_id",
+      pa.name as "name",
+      COALESCE(pa.data, '{}'::jsonb) ->> 'treatment_rank' as "rank",
+      ST_AsMVTGeom(
+        ST_Transform(p_stand_geometries, 3857),
+        ST_TileEnvelope(z, x, y),
+        4096, 64, true) 
+      AS geom
 
     UNION
 
