53c670fbb85ac93b1690deb38857db4d675ff4ab
Roque Betiol Jr <jbetiol@sig-gis.com>
2024-09-11 16:15:56 -0300

    Metrics API descriptions

--- src/planscape/metrics/views.py
+++ src/planscape/metrics/views.py
@@ -1,12 +1,21 @@
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.pagination import LimitOffsetPagination
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from metrics.filters import MetricFilterSet
 from metrics.models import Metric
 from metrics.serializers import MetricSerializer
 from projects.models import ProjectVisibility
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description="List project's metrics that needs to be processed and will be used by the system"
+    ),
+    retrieve=extend_schema(
+        description="Detail project's metrics  that needs to be processed and will be used by the system"
+    )
+)
 class MetricsViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Metric.objects.filter(
         project__visibility=ProjectVisibility.PUBLIC
