ea24a26b00d3e7197f757b30585b1a930d40ca16
Roque Betiol Jr <jbetiol@sig-gis.com>
2024-09-12 09:56:28 -0300

    LookupViewSet API documentation

--- src/planscape/planscape/views.py
+++ src/planscape/planscape/views.py
@@ -21,6 +21,7 @@ class LookupViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
 
     @extend_schema(
         description="List lookup fields.",
+        filters=[],
         responses={
             200: LookupKeysSerializer,
         },
@@ -34,8 +35,30 @@ class LookupViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
         responses={200: TreatmentPrescriptionActionSerializer, 404: None},
         examples=[
             OpenApiExample(
-                "TPA description",
-                value={"SINGLE": {"foo": "bar"}, "SEQUENCE": {"foo": "bar"}},
+                "Treatment Prescription Action description",
+                value={
+                    "SINGLE": {
+                        "MODERATE_THINNING_BIOMASS": "Moderate Thinning & Biomass Removal",
+                        "HEAVY_THINNING_BIOMASS": "Heavy Thinning & Biomass Removal",
+                        "MODERATE_THINNING_BURN": "Moderate Thinning & Pile Burn",
+                        "HEAVY_THINNING_BURN": "Heavy Thinning & Pile Burn",
+                        "MODERATE_MASTICATION": "Moderate Mastication",
+                        "HEAVY_MASTICATION": "Heavy Mastication",
+                        "RX_FIRE": "Prescribed Fire",
+                        "HEAVY_THINNING_RX_FIRE": "Heavy Thinning & Prescribed Fire",
+                        "MASTICATION_RX_FIRE": "Mastication & Prescribed Fire",
+                    },
+                    "SEQUENCE": {
+                        "MODERATE_THINNING_BURN_PLUS_RX_FIRE": "Moderate Thinning & Pile Burn (year 0), Prescribed Burn (year 10)",
+                        "MODERATE_THINNING_BURN_PLUS_MODERATE_THINNING_BURN": "Moderate Thinning & Pile Burn (year 0), Moderate Thinning & Pile Burn (year 10)",
+                        "HEAVY_THINNING_BURN_PLUS_RX_FIRE": "Heavy Thinning & Pile Burn (year 0), Prescribed Burn (year 10)",
+                        "HEAVY_THINNING_BURN_PLUS_HEAVY_THINNING_BURN": "Heavy Thinning & Pile Burn (year 0), Heavy Thinning & Pile Burn (year 10)",
+                        "RX_FIRE_PLUS_RX_FIRE": "Prescribed Fire (year 0), Prescribed Fire (year 10)",
+                        "MODERATE_MASTICATION_PLUS_MODERATE_MASTICATION": "Moderate Mastication (year 0), Moderate Mastication (year 10)",
+                        "HEAVY_THINNING_BIOMASS_PLUS_RX_FIRE": "Heavy Thinning & Biomass Removal (year 0), Prescribed Fire (year 10)",
+                        "MODERATE_MASTICATION_PLUS_RX_FIRE": "Moderate Mastication (year 0), Prescribed Fire (year 10)",
+                    },
+                },
             )
         ],
     )
