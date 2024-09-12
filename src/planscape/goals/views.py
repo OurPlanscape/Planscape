eb326151a81cac3ec533403cd2372f9e8327ee4f
Roque Betiol Jr <jbetiol@sig-gis.com>
2024-09-12 17:17:57 -0300

    TratmentGoalViewSet API documentation improvement (#1740)

--- src/planscape/goals/views.py
+++ src/planscape/goals/views.py
@@ -1,12 +1,20 @@
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.pagination import LimitOffsetPagination
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from goals.filters import TreatmentGoalFilterSet
 from goals.models import TreatmentGoal
 from goals.serializers import TreatmentGoalSerializer
 from projects.models import ProjectVisibility
 
 
+@extend_schema_view(
+    list=extend_schema(description="List treatment goals."),
+    retrieve=extend_schema(
+        description="Detail a treatment goal.",
+        responses={200: TreatmentGoalSerializer, 404: None},
+    ),
+)
 class TreatmentGoalViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = TreatmentGoal.objects.filter(
         project__visibility=ProjectVisibility.PUBLIC
