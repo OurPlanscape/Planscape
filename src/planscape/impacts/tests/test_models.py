3cf114a145164b7f7f05bf332a83c7bbf5fb4b0a
George Silva <863039+george-silva@users.noreply.github.com>
2024-09-13 13:31:13 -0300

    PLAN-1376 (#1742)
    creates function to get zonal stats from raster, given a treatment plan, variable, action and year.

---
+++ src/planscape/impacts/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from impacts.models import (
+    ImpactVariable,
+    ImpactVariableAggregation,
+    TreatmentPrescriptionAction,
+)
+
+
+class TestImpactVariable(TestCase):
+    def test_s3_path_returns_correctly_with_action(self):
+        variable = ImpactVariable.CANOPY_BASE_HEIGHT
+        year = 2024
+        action = TreatmentPrescriptionAction.MASTICATION_RX_FIRE
+        s3_path = ImpactVariable.get_impact_raster(
+            impact_variable=variable,
+            action=action,
+            year=year,
+        )
+        self.assertEqual(
+            s3_path,
+            "s3://planscape-control-dev/rasters/impacts/Treatment_9_2024_cbh_3857_COG.tif",
+        )
+
+    def test_s3_path_returns_correctly_without_action_returns_baseline(self):
+        variable = ImpactVariable.CANOPY_BASE_HEIGHT
+        year = 2024
+        action = None
+        s3_path = ImpactVariable.get_impact_raster(
+            impact_variable=variable,
+            action=action,
+            year=year,
+        )
+        self.assertEqual(
+            s3_path,
+            "s3://planscape-control-dev/rasters/impacts/Baseline_2024_cbh_3857_COG.tif",
+        )
