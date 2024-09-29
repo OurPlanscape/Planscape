b4c4227d91fa0a22c08cb595b5a1a381db2eb6f8
lastminutediorama <mkinsella@sig-gis.com>
2024-09-29 16:05:40 -0500

    center height for no-treatments blurb

--- src/interface/src/app/treatments/treatments-tab/treatments-tab.component.ts
+++ src/interface/src/app/treatments/treatments-tab/treatments-tab.component.ts
@@ -10,45 +10,6 @@ import { InputFieldComponent } from '../../../styleguide/input/input-field.compo
 import { Prescription } from '@types';
 import { PRESCRIPTIONS, PrescriptionSingleAction } from '../prescriptions';
 
-// TODO: remove
-const examplePrescriptions: Prescription[] = [
-  {
-    action: 'HEAVY_THINNING_BURN',
-    type: 'SINGLE',
-    area_acres: 12000,
-    treated_stand_count: 34,
-    stand_ids: [],
-  },
-  {
-    action: 'MODERATE_THINNING_BURN',
-    type: 'SINGLE',
-    area_acres: 30,
-    treated_stand_count: 18,
-    stand_ids: [],
-  },
-  {
-    action: 'HEAVY_THINNING_BIOMASS',
-    type: 'SINGLE',
-    area_acres: 110,
-    treated_stand_count: 15,
-    stand_ids: [],
-  },
-  {
-    action: 'HEAVY_THINNING_RX_FIRE',
-    type: 'SINGLE',
-    area_acres: 0,
-    treated_stand_count: 265,
-    stand_ids: [],
-  },
-  {
-    action: 'MASTICATION_RX_FIRE',
-    type: 'SINGLE',
-    area_acres: 86,
-    treated_stand_count: 1111,
-    stand_ids: [1, 2, 4, 5],
-  },
-];
-
 @Component({
   selector: 'app-project-area-tx-tab',
   standalone: true,
@@ -66,12 +27,13 @@ const examplePrescriptions: Prescription[] = [
   styleUrl: './treatments-tab.component.scss',
 })
 export class ProjectAreaTreatmentsTabComponent {
-  @Input() treatments: Prescription[] = examplePrescriptions;
-  layerOpacity: number = 100; // TODO: raise this up to state?
+  //TODO: either get this from template input or directly from state?
+  @Input() prescriptions: Prescription[] = [];
+  layerOpacity: number = 100; // TODO: get this from state?
   searchString: string = '';
 
   hasTreatments() {
-    return this.treatments.length > 0;
+    return this.prescriptions.length > 0;
   }
 
   handleOpacityChange(opacity: number) {
@@ -81,22 +43,16 @@ export class ProjectAreaTreatmentsTabComponent {
   // TODO: only do this if an angular filter isn't an option
   displayedTreatments(): Prescription[] {
     const needle = this.searchString.toLowerCase();
-    return this.treatments.filter(
+    return this.prescriptions.filter(
       (p) =>
         p.action.toLowerCase().includes(needle) ||
-        PRESCRIPTIONS.SINGLE[p.action as PrescriptionSingleAction].includes(
-          needle
-        )
+        PRESCRIPTIONS.SINGLE[p.action as PrescriptionSingleAction]
+          .toLowerCase()
+          .includes(needle)
     );
   }
 
   setSearchString(searchString: string) {
     this.searchString = searchString;
-  }
-
-  // TODO: what does this data shape really look like?
-  // what daunting type mapping has to happen?
-  loadTreatments() {
-    // this.treatments =
   }
 }
