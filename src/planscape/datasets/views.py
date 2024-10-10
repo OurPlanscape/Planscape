021875a5bac886f91bad8f293928d190f5d23626
George Silva <863039+george-silva@users.noreply.github.com>
2024-10-10 17:19:39 -0300

    implements create datalayer (#1797)

--- src/planscape/datasets/views.py
+++ src/planscape/datasets/views.py
@@ -2,15 +2,23 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAdminUser
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework import status
 from core.serializers import MultiSerializerMixin
 from datasets.models import DataLayer
-from datasets.serializers import CreateDataLayerSerializer, DataLayerSerializer
+from datasets.serializers import (
+    CreateDataLayerSerializer,
+    DataLayerCreatedSerializer,
+    DataLayerSerializer,
+)
+from datasets.services import create_datalayer
 
 
 class AdminDataLayerViewSet(
     ListModelMixin,
     RetrieveModelMixin,
     MultiSerializerMixin,
+    CreateModelMixin,
     GenericViewSet,
 ):
     queryset = DataLayer.objects.all()
@@ -22,3 +30,13 @@ class AdminDataLayerViewSet(
         "retrieve": DataLayerSerializer,
     }
     pagination_class = LimitOffsetPagination
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        datalayer_created = create_datalayer(**serializer.validated_data)
+        out_serializer = DataLayerCreatedSerializer(instance=datalayer_created)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            out_serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
