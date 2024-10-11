cdcfcd9999467f18a66fe856d5ed8bf4fdb10519
George Silva <georger.silva@gmail.com>
2024-10-11 17:00:40 -0300

    uploads file

--- src/planscape/datasets/management/commands/datalayers.py
+++ src/planscape/datasets/management/commands/datalayers.py
@@ -119,6 +119,17 @@ class Command(PlanscapeCommand):
                 datalayer=datalayer,
                 upload_to=upload_to,
             )
+<<<<<<< HEAD
+=======
+            output_data = response.json()
+            datalayer = output_data.get("datalayer")
+            upload_to = output_data.get("upload_to")
+            self._upload_file(
+                rasters,
+                datalayer=datalayer,
+                upload_to=upload_to,
+            )
+>>>>>>> 8947e87c (uploads file)
             return output_data
         except Exception:
             self.stderr.write("Something went wrong while talking to Planscape.")
