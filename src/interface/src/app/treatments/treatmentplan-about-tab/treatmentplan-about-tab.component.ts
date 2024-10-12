aced7184fb52129ab75d5132e75aeafd1fd90485
lastminutediorama <mkinsella@sig-gis.com>
2024-10-12 16:24:02 -0500

    fetch scenario data for about tab

--- src/interface/src/app/treatments/treatmentplan-about-tab/treatmentplan-about-tab.component.ts
+++ src/interface/src/app/treatments/treatmentplan-about-tab/treatmentplan-about-tab.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { AsyncPipe, NgIf } from '@angular/common';
-import { DecimalPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe, NgIf, DecimalPipe } from '@angular/common';
 import { TreatmentsState } from '../treatments.state';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Scenario } from '@types';
+import { ScenarioService } from '@services';
 
+@UntilDestroy()
 @Component({
   selector: 'app-treatmentplan-about-tab',
   standalone: true,
@@ -10,9 +13,35 @@ import { TreatmentsState } from '../treatments.state';
   templateUrl: './treatmentplan-about-tab.component.html',
   styleUrl: './treatmentplan-about-tab.component.scss',
 })
-export class TreatmentplanAboutTabComponent {
-  constructor(private treatmentsState: TreatmentsState) {}
-
-  treatmentPlan$ = this.treatmentsState.treatmentPlan$;
+export class TreatmentplanAboutTabComponent implements OnInit {
+  constructor(
+    private treatmentsState: TreatmentsState,
+    private scenarioService: ScenarioService
+  ) {}
   summary$ = this.treatmentsState.summary$;
+  scenarioId = this.treatmentsState.getScenarioId();
+  areaAcres: number | undefined = undefined;
+  standSize: string | undefined = undefined;
+
+  ngOnInit(): void {
+    if (this.scenarioId) {
+      this.loadScenario(this.scenarioId);
+    }
+  }
+
+  loadScenario(scenarioId: number) {
+    this.scenarioService
+      .getScenario(scenarioId.toString())
+      .pipe(untilDestroyed(this))
+      .subscribe((scenario: Scenario) => {
+        this.standSize = scenario.configuration?.stand_size;
+        //TODO: confirm that this is actually the acres value we want to display- not planning area acres
+        // this is calculating from scenario_result, just as we do in the result table
+        this.areaAcres = scenario.scenario_result?.result.features
+          .map((featureCollection) => featureCollection.properties.area_acres)
+          .reduce((acc: number, acres) => {
+            return acc + acres;
+          });
+      });
+  }
 }
