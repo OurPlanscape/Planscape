ed88a39eae58c28024ba57a1b9940d6f37d5ab14
George Silva <863039+george-silva@users.noreply.github.com>
2024-10-21 17:08:03 -0300

    improvements to admin (#1824)

--- src/planscape/organizations/admin.py
+++ src/planscape/organizations/admin.py
@@ -1,9 +1,18 @@
+from typing import Any, Dict
 from django.contrib import admin
+from organizations.forms import OrganizationAdminForm
 from organizations.models import Organization
 
 
 class OrganizationAdmin(admin.ModelAdmin):
     search_fields = ["name"]
+    list_display = ("id", "name", "type")
+    list_display_links = ("id", "name")
+    list_filter = ("type",)
+    form = OrganizationAdminForm
+
+    def get_changeform_initial_data(self, request) -> Dict[str, Any]:
+        return {"created_by": request.user}
 
 
 admin.site.register(Organization, OrganizationAdmin)
