ee01b5af179f39307ce833b4efd2ac42c02b8d48
George Silva <georger.silva@gmail.com>
2024-10-21 16:13:09 -0300

    improvements to admin

--- src/planscape/organizations/admin.py
+++ src/planscape/organizations/admin.py
@@ -4,6 +4,9 @@ from organizations.models import Organization
 
 class OrganizationAdmin(admin.ModelAdmin):
     search_fields = ["name"]
+    list_display = ("id", "name", "type")
+    list_display_links = ("id", "name")
+    list_filter = ("type",)
 
 
 admin.site.register(Organization, OrganizationAdmin)
