6d6f7fe5cefe841aed39381798216dc6d9886fd2
George Silva <georger.silva@gmail.com>
2024-10-24 14:26:18 -0300

    fix circular import

--- src/planscape/gis/core.py
+++ src/planscape/gis/core.py
@@ -6,15 +6,9 @@ from rasterio.errors import RasterioIOError
 from fiona.errors import DriverError
 from datasets.models import DataLayerType, GeometryType
 from gis.errors import InvalidFileFormat, InvalidGeometryType
-from gis.info import info_raster, info_vector
+
 
 log = logging.getLogger(__name__)
-
-
-def get_layer_info(input_file: str) -> Dict[str, Any]:
-    layer_type = fetch_datalayer_type(input_file)
-    fn = info_raster if layer_type == DataLayerType.RASTER else info_vector
-    return fn(input_file=input_file)
 
 
 def is_vector(input_file: str) -> bool:
@@ -63,3 +57,11 @@ def fetch_geometry_type(
     if not geometry_type:
         raise InvalidGeometryType(f"Could not determine geometry type")
     return GeometryType[geometry_type.upper()]
+
+
+def get_layer_info(input_file: str) -> Dict[str, Any]:
+    from gis.info import info_raster, info_vector
+
+    layer_type = fetch_datalayer_type(input_file)
+    fn = info_raster if layer_type == DataLayerType.RASTER else info_vector
+    return fn(input_file=input_file)
