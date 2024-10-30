a0f71424c7caf881d4cfbfb1e4051730810d5b59
lastminutediorama <mkinsella@sig-gis.com>
2024-10-30 12:21:22 -0500

    revert default number vals

--- src/interface/src/styleguide/treatment-stands-progress-bar/treatment-stands-progress-bar.component.ts
+++ src/interface/src/styleguide/treatment-stands-progress-bar/treatment-stands-progress-bar.component.ts
@@ -20,20 +20,15 @@ export class TreatmentStandsProgressBarComponent {
   /**
    * The number of treated stands
    */
-  @Input() treatedStands?: number | null = 0;
+  @Input() treatedStands: number = 0;
   /**
    * The total number of stands, treated and not treated
    */
-  @Input() totalStands?: number | null = 0;
+  @Input() totalStands: number = 0;
 
   // convert to units for progress bar
   treatedAsPercent(): number {
-    if (
-      typeof this.treatedStands !== 'number' ||
-      typeof this.totalStands !== 'number' ||
-      this.treatedStands < 1 ||
-      this.totalStands < 1
-    ) {
+    if (this.treatedStands < 1 || this.totalStands < 1) {
       return 0;
     }
     return Math.round((this.treatedStands / this.totalStands) * 100);
