f56ae75a2e4e233e810520468a49c2b4931b8a26
Roque Betiol Jr <jbetiol@sig-gis.com>
2024-10-30 11:53:15 -0300

    lookups endpoint removal

--- src/planscape/planscape/urls_v2.py
+++ src/planscape/planscape/urls_v2.py
@@ -3,10 +3,8 @@ from rest_framework.routers import SimpleRouter
 from planning.routers import router as planning_router
 from impacts.routers import router as impacts_router
 from datasets.routers import router as datasets_router
-from planscape.views import LookupViewSet
 
 core_router = SimpleRouter()
-core_router.register("lookups", LookupViewSet, basename="lookups")
 
 app_name = "api"
 urlpatterns = [
