2bc725ff5716ded57245875b8488b80ce6c2b6e5
Lucho <33841664+LALuis@users.noreply.github.com>
2024-11-04 10:41:30 -0300

    PLAN-1770: Map base layer added to the tabs - Pending to solve some console errors

--- src/interface/src/app/treatments/treatment-plan-tabs/treatment-plan-tabs.component.ts
+++ src/interface/src/app/treatments/treatment-plan-tabs/treatment-plan-tabs.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { TreatmentPlanAboutTabComponent } from '../treatment-plan-about-tab/treatment-plan-about-tab.component';
 import { ProjectAreasTabComponent } from '../project-areas-tab/project-areas-tab.component';
+import { MapBaseLayerComponent } from '../map-base-layer/map-base-layer.component';
 
 @Component({
   selector: 'app-treatment-plan-tabs',
@@ -10,6 +11,7 @@ import { ProjectAreasTabComponent } from '../project-areas-tab/project-areas-tab
     MatTabsModule,
     TreatmentPlanAboutTabComponent,
     ProjectAreasTabComponent,
+    MapBaseLayerComponent
   ],
   templateUrl: './treatment-plan-tabs.component.html',
   styleUrl: './treatment-plan-tabs.component.scss',
