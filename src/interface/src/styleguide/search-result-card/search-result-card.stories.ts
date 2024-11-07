67d56735a4a23f012de1d66978538af4278c0273
Mark <100235124+lastminutediorama@users.noreply.github.com>
2024-11-07 11:36:48 -0600

    Search result card (#1882)
    * project area and treatment search result

---
+++ src/interface/src/styleguide/search-result-card/search-result-card.stories.ts
@@ -0,0 +1,81 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { applicationConfig, argsToTemplate } from '@storybook/angular';
+import { SearchResultCardComponent } from './search-result-card.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TreatmentProjectArea } from 'src/app/types';
+
+const containerStyle = `style="display: flex;
+  width: 100%;
+  height: 180px;
+  background-color: lightgray;
+  align-items: center;
+  justify-content: start;
+  padding: 10px;
+  flex-direction: column;"`;
+
+const meta: Meta<SearchResultCardComponent> = {
+  title: 'Components/Search Result Card',
+  component: SearchResultCardComponent,
+  decorators: [
+    applicationConfig({
+      providers: [provideAnimations()],
+    }),
+  ],
+  tags: ['autodocs'],
+  render: ({ ...args }) => ({
+    props: args,
+    template: `<div ${containerStyle}><div style="width:400px;"><sg-search-result-card ${argsToTemplate(args)}></sg-search-result-card></div>`,
+  }),
+};
+
+export default meta;
+type Story = StoryObj<SearchResultCardComponent>;
+
+const exampleProjectArea: TreatmentProjectArea = {
+  project_area_id: 20,
+  project_area_name: 'hello i am a project',
+  total_stand_count: 100,
+  prescriptions: [
+    {
+      action: 'RX_FIRE',
+      area_acres: 100,
+      treated_stand_count: 100,
+      type: 'SINGLE',
+      stand_ids: [],
+    },
+    {
+      action: 'HEAVY_THINNING_BURN_PLUS_RX_FIRE',
+      area_acres: 100,
+      treated_stand_count: 100,
+      type: 'SEQUENCE',
+      stand_ids: [],
+    },
+    {
+      action: 'MODERATE_THINNING_BURN_PLUS_RX_FIRE',
+      area_acres: 100,
+      treated_stand_count: 100,
+      type: 'SEQUENCE',
+      stand_ids: [],
+    },
+    {
+      action: 'HEAVY_THINNING_RX_FIRE',
+      area_acres: 100,
+      treated_stand_count: 100,
+      type: 'SINGLE',
+      stand_ids: [],
+    },
+  ],
+  extent: [0, 0, 0, 0],
+  centroid: {
+    type: 'Point',
+    coordinates: [],
+  },
+};
+
+export const Default: Story = {
+  args: {
+    projectArea: exampleProjectArea,
+    searchString: '',
+    wholeWordsOnly: false,
+  },
+};
