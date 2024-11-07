9d909c95d4fed5e9e5adf703a41ebc1168c21faa
Lucho <33841664+LALuis@users.noreply.github.com>
2024-11-07 12:39:31 -0300

    PLAN-1770: Fixing default base map layer issue

--- src/interface/src/app/treatments/map-base-layer/map-base-layer.component.ts
+++ src/interface/src/app/treatments/map-base-layer/map-base-layer.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
-import { NgForOf } from '@angular/common';
+import { AsyncPipe, NgForOf } from '@angular/common';
 import {
   baseLayerStyles,
   BaseLayerType,
   DEFAULT_BASE_MAP,
 } from '../treatment-map/map-base-layers';
 import { MapConfigState } from '../treatment-map/map-config.state';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-map-base-layer',
   standalone: true,
-  imports: [NgForOf],
+  imports: [AsyncPipe, NgForOf],
   templateUrl: './map-base-layer.component.html',
   styleUrl: './map-base-layer.component.scss',
 })
 export class MapBaseLayerComponent {
   baseLayers = Object.keys(baseLayerStyles) as BaseLayerType[];
-  readonly defaultLayer = DEFAULT_BASE_MAP;
+  readonly defaultLayer = this.mapConfigState.baseLayer$ || of(DEFAULT_BASE_MAP);
 
   constructor(private mapConfigState: MapConfigState) {}
 
