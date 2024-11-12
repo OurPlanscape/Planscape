623d171afe2dbd43723c1efa0b607523dd455fbe
Lucho <33841664+LALuis@users.noreply.github.com>
2024-11-12 16:17:35 -0300

    PLAN-1655: Adding unit test

--- src/interface/src/app/treatments/direct-impacts/metric-filters/metric-filters.component.spec.ts
+++ src/interface/src/app/treatments/direct-impacts/metric-filters/metric-filters.component.spec.ts
@@ -1,6 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MetricFiltersComponent } from './metric-filters.component';
+import { Metric, MetricFiltersComponent } from './metric-filters.component';
+
+export const MockMetrics = [
+  {
+    id: 'ID_1',
+    color: '',
+    label: 'Mock metric 1',
+    active: false,
+  },
+  {
+    id: 'ID_2',
+    color: '',
+    label: 'Mock metric 2',
+    active: false,
+  },
+  {
+    id: 'ID_3',
+    color: '',
+    label: 'Mock metric 2',
+    active: false,
+  },
+  {
+    id: 'ID_4',
+    color: '',
+    label: 'Mock metric 4',
+    active: false,
+  },
+  {
+    id: 'ID_5',
+    color: '',
+    label: 'Mock metric 5',
+    active: false,
+  },
+];
 
 describe('MetricFiltersComponent', () => {
   let component: MetricFiltersComponent;
@@ -13,10 +46,67 @@ describe('MetricFiltersComponent', () => {
 
     fixture = TestBed.createComponent(MetricFiltersComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // Configuración inicial de los datos
+    component.initialOptions = MockMetrics;
+
+    component.selectedOptions = [
+      MockMetrics[0].id,
+      MockMetrics[1].id,
+      MockMetrics[2].id,
+      MockMetrics[3].id,
+    ];
+    component.dropdownOptions = [
+      MockMetrics,
+      MockMetrics,
+      MockMetrics,
+      MockMetrics,
+    ];
+
+    spyOn(component, 'applyMetricColor').and.callFake((index, list) => list);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should update the dropdownOption lists', () => {
+    const updatedDropdownIndex = null;
+    component.updateDropdownOptions(updatedDropdownIndex);
+
+    // Verify applyMetricColor was called 4 times one for every dropdown
+    expect(component.applyMetricColor).toHaveBeenCalledTimes(4);
+    // The 1st dropdown should have the default option and the not selected one
+    expect(component.applyMetricColor).toHaveBeenCalledWith(0, [
+      MockMetrics[0],
+      MockMetrics[4],
+    ]);
+    // The 2nd dropdown should have the default option and the not selected one
+    expect(component.applyMetricColor).toHaveBeenCalledWith(1, [
+      MockMetrics[1],
+      MockMetrics[4],
+    ]);
+    // The 3rd dropdown should have the default option and the not selected one
+    expect(component.applyMetricColor).toHaveBeenCalledWith(2, [
+      MockMetrics[2],
+      MockMetrics[4],
+    ]);
+    // The 4th dropdown should have the default option and the not selected one
+    expect(component.applyMetricColor).toHaveBeenCalledWith(3, [
+      MockMetrics[3],
+      MockMetrics[4],
+    ]);
+
+    // We have 5 items, 4 of them are selected as default, we should have 2 options available per dropdown
+    expect(component.dropdownOptions[0].length).toEqual(2);
+    expect(component.dropdownOptions[1].length).toEqual(2);
+    expect(component.dropdownOptions[2].length).toEqual(2);
+    expect(component.dropdownOptions[3].length).toEqual(2);
+  });
+
+  it('should update the selected option and call updateDropdownOptions when optionSelected is called', () => {
+    spyOn(component, 'updateDropdownOptions').and.callFake(() => {});
+    const dropdownIndex = 1;
+    const metric: Metric = {id: 'Test', color: '', label: '', active: false};
+    component.optionSelected(dropdownIndex, metric);
+    // The selected option for this dropdown should be updated with the corresponding ID
+    expect(component.selectedOptions[dropdownIndex]).toEqual(metric.id)
+    // updateDropdownOptions should be called with the dropdown index we just selected
+    expect(component.updateDropdownOptions).toHaveBeenCalledWith(dropdownIndex);
   });
 });
