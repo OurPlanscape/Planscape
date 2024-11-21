41314caaf255004893c0e637c2e55bd1f87f05b3
lastminutediorama <mkinsella@sig-gis.com>
2024-11-20 20:13:40 -0600

    UI perms for disable/delete tx plan

--- src/interface/src/app/plan/create-scenarios/treatments-tab/treatments-tab.component.ts
+++ src/interface/src/app/plan/create-scenarios/treatments-tab/treatments-tab.component.ts
@@ -3,7 +3,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { TreatmentsService } from '@services/treatments.service';
 import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
 import { SNACK_ERROR_CONFIG, SNACK_NOTICE_CONFIG } from '@shared';
-import { TreatmentPlan, TreatmentStatus } from '@types';
+import { Plan, TreatmentPlan, TreatmentStatus } from '@types';
+import {
+  canDeleteTreatmentPlan,
+  canCloneTreatmentPlan,
+} from '../../permissions';
 
 @Component({
   selector: 'app-treatments-tab',
@@ -12,6 +16,7 @@ import { TreatmentPlan, TreatmentStatus } from '@types';
 })
 export class TreatmentsTabComponent implements OnInit {
   @Input() scenarioId!: string;
+  @Input() planningArea: Plan | null = null;
 
   state: 'loading' | 'empty' | 'loaded' = 'loading';
 
@@ -44,6 +49,41 @@ export class TreatmentsTabComponent implements OnInit {
     }
 
     this.router.navigate(route, { relativeTo: this.route });
+  }
+
+  userCanDelete(): boolean {
+    console.log('is planning area set for delete? ', this.planningArea);
+    if (this.planningArea) {
+      console.log(
+        'is permission set for delete? ',
+        canDeleteTreatmentPlan(this.planningArea)
+      );
+    }
+    console.log(
+      'so the result should be: ',
+      this.planningArea !== null && canDeleteTreatmentPlan(this.planningArea)
+    );
+    return (
+      this.planningArea !== null && canDeleteTreatmentPlan(this.planningArea)
+    );
+  }
+
+  userCanDuplicate(): boolean {
+    console.log('is planning area set? ', this.planningArea);
+    if (this.planningArea) {
+      console.log(
+        'is permission set? ',
+        canCloneTreatmentPlan(this.planningArea)
+      );
+    }
+    console.log(
+      'so the result should be: ',
+      this.planningArea !== null && canCloneTreatmentPlan(this.planningArea)
+    );
+
+    return (
+      this.planningArea !== null && canCloneTreatmentPlan(this.planningArea)
+    );
   }
 
   deleteTreatment(treatment: TreatmentPlan) {
--- src/interface/src/app/plan/permissions.ts
+++ src/interface/src/app/plan/permissions.ts
@@ -15,3 +15,16 @@ export function canDeletePlanningArea(plan: Plan | PreviewPlan, user: User) {
 export function canAddTreatmentPlan(plan: Plan | PreviewPlan) {
   return plan.permissions?.includes('add_tx_plan');
 }
+
+export function canCloneTreatmentPlan(plan: Plan | PreviewPlan) {
+  return plan.permissions?.includes('clone_tx_plan');
+}
+
+export function canDeleteTreatmentPlan(plan: Plan | PreviewPlan) {
+  //TODO: this should be remove_tx_plan, but we might not be getting a remove_tx_plan from the backend?
+  return plan.permissions?.includes('edit_tx_plan');
+}
+
+export function canRunTreatmentAnalysis(plan: Plan | PreviewPlan) {
+  return plan.permissions?.includes('run_tx');
+}
