39fb8c58c23eddfe468f061f08737fd22d7af365
lastminutediorama <mkinsella@sig-gis.com>
2024-11-24 20:45:16 -0600

    add legend and icons

--- src/interface/src/app/treatments/treatment-to-pdf.service.ts
+++ src/interface/src/app/treatments/treatment-to-pdf.service.ts
@@ -5,18 +5,44 @@ import {
   PRESCRIPTIONS,
   PrescriptionSequenceAction,
   nameForTypeAndAction,
+  nameForAction,
+  PrescriptionAction,
 } from './prescriptions';
 import { Map as MapLibreMap } from 'maplibre-gl';
 import { logoImg } from '../../assets/base64/icons';
 import { TreatmentSummary, Prescription, TreatmentProjectArea } from '@types';
+import { MapConfigState } from './treatment-map/map-config.state';
+
 import { TreatmentsState } from './treatments.state';
 import { TreatedStandsState } from './treatment-map/treated-stands.state';
+import * as txIcons from '../../assets/base64/stand_icons/treatments';
+
+const treatmentIcons: Record<PrescriptionAction, string> = {
+  MODERATE_THINNING_BIOMASS: txIcons.treatment_blue,
+  HEAVY_THINNING_BIOMASS: txIcons.treatment_purple,
+  MODERATE_THINNING_BURN: txIcons.treatment_orange,
+  HEAVY_THINNING_BURN: txIcons.treatment_yellow,
+  MODERATE_MASTICATION: txIcons.treatment_junglegreen,
+  HEAVY_MASTICATION: txIcons.treatment_limegreen,
+  RX_FIRE: txIcons.treatment_red,
+  HEAVY_THINNING_RX_FIRE: txIcons.treatment_brown,
+  MASTICATION_RX_FIRE: txIcons.treatment_pink,
+  MODERATE_THINNING_BURN_PLUS_RX_FIRE: txIcons.sequence_1,
+  MODERATE_THINNING_BURN_PLUS_MODERATE_THINNING_BURN: txIcons.sequence_2,
+  HEAVY_THINNING_BURN_PLUS_RX_FIRE: txIcons.sequence_3,
+  HEAVY_THINNING_BURN_PLUS_HEAVY_THINNING_BURN: txIcons.sequence_4,
+  RX_FIRE_PLUS_RX_FIRE: txIcons.sequence_5,
+  MODERATE_MASTICATION_PLUS_MODERATE_MASTICATION: txIcons.sequence_6,
+  HEAVY_THINNING_BIOMASS_PLUS_RX_FIRE: txIcons.sequence_7,
+  MODERATE_MASTICATION_PLUS_RX_FIRE: txIcons.sequence_8,
+};
 
 @Injectable()
 export class TreatmentToPDFService {
   constructor(
     private treatmentsState: TreatmentsState,
-    private treatedStandsState: TreatedStandsState
+    private treatedStandsState: TreatedStandsState,
+    private mapConfigState: MapConfigState
   ) {}
 
   activeMap: MapLibreMap | null = null;
@@ -45,11 +71,21 @@ export class TreatmentToPDFService {
       acc += p.total_stand_count;
       return acc;
     }, 0);
-
+    const treatmentsUsedSet = new Set(
+      this.treatedStandsState.getTreatedStands().map((s) => s.action)
+    );
     this.addLogo(this.leftMargin, 14);
 
-    const headerText = `${planningAreaName} / ${scenarioName} /  ${treatmentPlanName} - Treated Stands: ${treatedStandsCount} / ${totalStands}`;
-    this.addHeader(headerText, this.leftMargin, 24);
+    const headerText = `${planningAreaName} / ${scenarioName} /  ${treatmentPlanName}`;
+    this.addHeader(headerText, this.leftMargin, 24.5);
+
+    //add stands info:
+    const treatedStands = `Treated Stands: ${treatedStandsCount} / ${totalStands}`;
+
+    this.pdfDoc?.setFont('Helvetica', 'normal');
+    this.pdfDoc?.setFontSize(8);
+    const standsTextWidth = this.pdfDoc?.getTextWidth(treatedStands);
+    this.pdfDoc?.text(treatedStands, 190 - standsTextWidth, 24.5);
 
     const mapX = this.leftMargin + 10;
     const mapY = 28;
@@ -59,17 +95,112 @@ export class TreatmentToPDFService {
 
     this.addAttributions(attributions, mapX + mapWidth, mapHeight + mapY - 2);
 
-    const projectAreasX = this.leftMargin;
-    const projectAreasY = 132;
-    this.addProjectAreaTable(
-      this.tableRowsFromSummary(curSummary),
-      projectAreasX,
-      projectAreasY
-    );
+    // If we are showing the treatment stands, we change what's being rendered
+    if (this.mapConfigState.isTreatmentStandsVisible()) {
+      const nextY = 132;
+      this.drawTreatmentLegend(130, nextY, treatmentsUsedSet);
+      this.addProjectAreaTable(
+        this.tableRowsFromSummary(curSummary),
+        this.leftMargin + 10,
+        nextY,
+        80
+      );
+    } else {
+      const projectAreasX = this.leftMargin + 10;
+      const projectAreasY = 132;
+      this.addProjectAreaTable(
+        this.tableRowsFromSummary(curSummary),
+        projectAreasX,
+        projectAreasY,
+        140
+      );
+    }
 
     document.body.removeChild(mapContainer);
     const pdfName = `planscape-${encodeURI(treatmentPlanName.split(' ').join('_'))}.pdf`;
     this.pdfDoc.save(pdfName);
+  }
+
+  drawHexagon(
+    doc: any,
+    centerX: number,
+    centerY: number,
+    size: number,
+    fill: string
+  ) {
+    const points = this.calculateHexagonPoints(centerX, centerY, size);
+    doc.setLineWidth(0.1);
+    doc.setFillColor(fill);
+    doc.setDrawColor(0); // Black stroke
+    doc.moveTo(points[0].x, points[0].y);
+    for (let i = 1; i < points.length; i++) {
+      doc.lineTo(points[i].x, points[i].y);
+    }
+    doc.lineTo(points[0].x, points[0].y);
+    doc.fillStroke();
+  }
+
+  calculateHexagonPoints(centerX: number, centerY: number, size: number) {
+    const points = [];
+    for (let i = 0; i < 6; i++) {
+      const angle = (i * Math.PI) / 3;
+      const x = centerX + size * Math.cos(angle);
+      const y = centerY + size * Math.sin(angle);
+      points.push({ x, y });
+    }
+    return points;
+  }
+
+  drawTreatmentLegend(
+    startX: number,
+    startY: number,
+    treatmentsUsed: Set<string>
+  ) {
+    const treatments = Array.from(treatmentsUsed).map((t) => ({
+      name: nameForAction(t),
+      icon: treatmentIcons[t as PrescriptionAction],
+    }));
+
+    autoTable(this.pdfDoc, {
+      styles: {
+        fillColor: [255, 255, 255],
+        cellPadding: 2,
+        lineColor: '#000000',
+        lineWidth: 0.4,
+      },
+      headStyles: {
+        fontSize: 7,
+        lineWidth: 0,
+        font: 'Helvetica',
+        textColor: '#000000',
+      },
+      alternateRowStyles: { fillColor: [255, 255, 255] },
+      columnStyles: {
+        0: { fontSize: 7, cellWidth: 50, lineWidth: 0, cellPadding: 1 },
+      },
+      startY: startY,
+      tableLineWidth: 0.3,
+      tableLineColor: [0, 0, 0],
+      margin: {
+        left: startX,
+        right: 20,
+        top: 10,
+        bottom: 20,
+      },
+      tableWidth: 50,
+      horizontalPageBreak: true,
+      head: [['Treatment Legend']],
+      body: treatments,
+      didDrawCell: (data) => {
+        if (data.column.index === 0 && data.row.section === 'body') {
+          const idx = data.row.index;
+          const x = data.cell.x + 1; // Add some padding
+          const y = data.cell.y; // Add some padding
+          data.doc.addImage(treatments[idx].icon, 'PNG', x, y, 3, 3);
+          data.doc.text(treatments[idx].name, x + 4, y + 3);
+        }
+      },
+    });
   }
 
   tableRowsFromSummary(currentSummary: TreatmentSummary): string[][] {
@@ -90,21 +221,28 @@ export class TreatmentToPDFService {
         }
       });
       const treatedStands = `Treated stands: ${this.treatedStandCount(p)}/${p.total_stand_count}`;
-      tableRows.push([p.project_area_name, treatedStands, rxInfo]);
+      const projectInfo = `${p.project_area_name} \n ${treatedStands} \n\n ${rxInfo}`;
+      tableRows.push([projectInfo]);
     });
     return tableRows;
   }
 
-  addProjectAreaTable(bodyData: string[][], startX: number, startY: number) {
+  addProjectAreaTable(
+    bodyData: string[][],
+    startX: number,
+    startY: number,
+    tableWidth: number
+  ) {
     autoTable(this.pdfDoc, {
       styles: { fillColor: [255, 255, 255] },
       alternateRowStyles: { fillColor: [255, 255, 255] },
       columnStyles: {
-        0: { fontSize: 9, cellWidth: 30 },
-        1: { fontSize: 9, cellWidth: 40 },
-        2: { fontSize: 9 },
+        0: { fontSize: 8 },
       },
+      tableWidth: tableWidth,
       startY: startY,
+      tableLineWidth: 0.3,
+      tableLineColor: '#000000',
       theme: 'grid',
       margin: {
         left: startX,
@@ -112,7 +250,6 @@ export class TreatmentToPDFService {
         top: 10,
         bottom: 20,
       },
-      tableLineColor: '#000000',
       horizontalPageBreak: true,
       body: bodyData,
     });
@@ -161,7 +298,7 @@ export class TreatmentToPDFService {
   }
 
   addHeader(headerText: string, x: number, y: number) {
-    this.pdfDoc?.setFont('Helvetica');
+    this.pdfDoc?.setFont('Helvetica', 'bold');
     this.pdfDoc?.setFontSize(10);
     this.pdfDoc?.text(headerText, x, y);
   }
