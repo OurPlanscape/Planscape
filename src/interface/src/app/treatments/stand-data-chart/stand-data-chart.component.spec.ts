8e24c13e4fa3c084e8b7e9c025df2fbe6bbcb82f
Pablo Lopez <pablo.lopez@splice.com>
2024-12-02 10:59:30 -0300

    fixed tests

--- src/interface/src/app/treatments/stand-data-chart/stand-data-chart.component.spec.ts
+++ src/interface/src/app/treatments/stand-data-chart/stand-data-chart.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StandDataChartComponent } from './stand-data-chart.component';
+import { Chart } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 describe('StandDataChartComponent', () => {
   let component: StandDataChartComponent;
@@ -8,13 +10,17 @@ describe('StandDataChartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StandDataChartComponent]
-    })
-    .compileComponents();
-    
+      imports: [StandDataChartComponent],
+    }).compileComponents();
+    Chart.register(ChartDataLabels);
+
     fixture = TestBed.createComponent(StandDataChartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    Chart.unregister(ChartDataLabels);
   });
 
   it('should create', () => {
