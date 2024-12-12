2fad7fe9d860915aeb2ac519c05f10bfce2ca37a
Roque Betiol Jr <jbetiol@sig-gis.com>
2024-12-12 10:52:55 -0300

    Feature flags

--- src/planscape/analytics/services.py
+++ src/planscape/analytics/services.py
@@ -1,12 +1,20 @@
 from typing import AnyStr
+
+from django.conf import settings
 from django.utils.timezone import now
 
 from analytics.tasks import async_collect_metric
 
 
 def collect_metric(event_name: AnyStr, **kwargs) -> None:
+    if not settings.ANALYTICS_ENABLED:
+        return
+    
     event_params = {
         "timestamp": now(),
         **kwargs
     }
+    if settings.ANALYTICS_DEBUG_MODE:
+        event_params.update({"debug_mode": True})
+        
     async_collect_metric.delay(event_name, event_params)
