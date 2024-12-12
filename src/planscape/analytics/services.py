5e50f9c63dab1a58ac90cc910f181f64ac9371b2
Roque Betiol Jr <jbetiol@sig-gis.com>
2024-12-12 09:59:15 -0300

    Build metric parameters on service layer

--- src/planscape/analytics/services.py
+++ src/planscape/analytics/services.py
@@ -1,7 +1,12 @@
-from typing import Dict, AnyStr, Any
+from typing import AnyStr
+from django.utils.timezone import now
 
 from analytics.tasks import async_collect_metric
 
 
-def collect_metric(event_name: AnyStr, event_params: Dict[str, Any]) -> None:
+def collect_metric(event_name: AnyStr, **kwargs) -> None:
+    event_params = {
+        "timestamp": now(),
+        **kwargs
+    }
     async_collect_metric.delay(event_name, event_params)
