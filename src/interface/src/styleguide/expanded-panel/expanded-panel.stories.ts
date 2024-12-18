dd30ac851a80b8799b061530376cd40f99c898e9
Pablo Lopez <plopez@sig-gis.com>
2024-12-18 13:29:57 -0300

    expanded panel modal

---
+++ src/interface/src/styleguide/expanded-panel/expanded-panel.stories.ts
@@ -0,0 +1,53 @@
+import {
+  argsToTemplate,
+  Meta,
+  moduleMetadata,
+  StoryObj,
+} from '@storybook/angular';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalInfoComponent } from '../modal-info-box/modal-info.component';
+import { ExpandedPanelComponent } from './expanded-panel.component';
+
+const meta: Meta<ExpandedPanelComponent> = {
+  title: 'Components/Expanded Panel',
+  component: ExpandedPanelComponent,
+  tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [MatDialogModule, BrowserAnimationsModule, ModalInfoComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+      ],
+    }),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<ExpandedPanelComponent>;
+
+const containerStyle = `style="background-color: gray;
+    height: 400px;
+    align-content: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;"`;
+
+export const Default: Story = {
+  args: {},
+  render: (args) => ({
+    props: args,
+    template: `<div ${containerStyle}>
+      <sg-expanded-panel ${argsToTemplate(args)}>
+        <div modalBodyTitle>The title</div>
+        <div modalBodyContent>Just a basic expaded panel</div>
+      </sg-expanded-panel><div>`,
+  }),
+};
