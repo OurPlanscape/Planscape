1d391c4e9805ff63264a020a583a0483c29d43b5
Pablo Lopez <plopez@sig-gis.com>
2024-12-26 09:07:40 -0300

    Expanded panel (#2029)
    * expanded panel modal
    
    * styles
    
    * added height variants

---
+++ src/interface/src/styleguide/expanded-panel/expanded-panel.component.ts
@@ -0,0 +1,46 @@
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output,
+} from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ButtonComponent } from '@styleguide';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'sg-expanded-panel',
+  templateUrl: './expanded-panel.component.html',
+  styleUrls: ['./expanded-panel.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgIf,
+    MatDialogModule,
+    ButtonComponent,
+    MatIconModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
+})
+export class ExpandedPanelComponent {
+  @Output() clickedClose = new EventEmitter<any>();
+  @Input() height: 'full' | 'narrow' = 'full';
+
+  constructor() {}
+
+  handleCloseButton() {
+    this.clickedClose.emit();
+  }
+
+  @HostBinding('class.narrow')
+  get isNarrow() {
+    return this.height === 'narrow';
+  }
+}
