9f8a95c5f78c25196b63ebce6a298990a67d179b
Lucho <llopez@sig-gis.com>
2025-01-13 10:54:39 -0300

    Cleaning stand on treatment selection

--- src/interface/src/app/treatments/map-stands-tx-result/map-stands-tx-result.component.ts
+++ src/interface/src/app/treatments/map-stands-tx-result/map-stands-tx-result.component.ts
@@ -16,7 +16,7 @@ import {
 import { SINGLE_STAND_SELECTED, STANDS_CELL_PAINT } from '../map.styles';
 import { environment } from '../../../environments/environment';
 import { DEFAULT_SLOT, ImpactsMetricSlot, SLOT_PALETTES } from '../metrics';
-import { map, switchMap, take } from 'rxjs';
+import { combineLatest, map, switchMap, take } from 'rxjs';
 import { DirectImpactsStateService } from '../direct-impacts.state.service';
 import { TreatmentsState } from '../treatments.state';
 import { descriptionForAction } from '../prescriptions';
@@ -94,6 +94,7 @@ export class MapStandsTxResultComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.updateStandOnTreatmentChanges();
     this.paint = this.generatePaint(DEFAULT_SLOT);
     this.directImpactsStateService.standsTxSourceLoaded$
       .pipe(
@@ -112,6 +113,22 @@ export class MapStandsTxResultComponent implements OnInit {
           if (sourceFeatures[0]) {
             this.directImpactsStateService.setActiveStand(sourceFeatures[0]);
           }
+        }
+      });
+  }
+
+  updateStandOnTreatmentChanges() {
+    combineLatest([
+      this.directImpactsStateService.filteredTreatmentTypes$,
+      this.directImpactsStateService.activeStand$,
+    ])
+      .pipe(untilDestroyed(this))
+      .subscribe(([treatments, stand]) => {
+        if (
+          treatments?.length &&
+          !treatments.includes(stand?.properties?.['action'])
+        ) {
+          this.directImpactsStateService.resetActiveStand();
         }
       });
   }
