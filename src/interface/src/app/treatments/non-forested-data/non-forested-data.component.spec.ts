333356ca56025fb8eba08e29209e77b3061350ee
Pablo Lopez <plopez@sig-gis.com>
2025-01-13 15:38:26 -0300

    Non forested stands (#2096)
    * non forested
    
    * non-forested data
    
    * style loader

--- src/interface/src/app/treatments/non-forested-data/non-forested-data.component.spec.ts
+++ src/interface/src/app/treatments/non-forested-data/non-forested-data.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NonForestedDataComponent } from './non-forested-data.component';
+import { MockProvider, MockProviders } from 'ng-mocks';
+import { DirectImpactsStateService } from '../direct-impacts.state.service';
+import { TreatmentsState } from '../treatments.state';
+import { TreatmentsService } from '@services/treatments.service';
+import { of } from 'rxjs';
 
 describe('NonForestedDataComponent', () => {
   let component: NonForestedDataComponent;
@@ -9,6 +14,12 @@ describe('NonForestedDataComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NonForestedDataComponent],
+      providers: [
+        MockProvider(DirectImpactsStateService, {
+          activeStand$: of(null),
+        }),
+        MockProviders(TreatmentsState, TreatmentsService),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NonForestedDataComponent);
--- src/interface/src/app/treatments/non-forested-data/non-forested-data.component.ts
+++ src/interface/src/app/treatments/non-forested-data/non-forested-data.component.ts
@@ -1,20 +1,58 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { DirectImpactsStateService } from '../direct-impacts.state.service';
+import { distinctUntilChanged, switchMap, tap } from 'rxjs';
 
+import { TreatmentsState } from '../treatments.state';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { NgIf } from '@angular/common';
+import { TreatmentsService } from '@services/treatments.service';
+
+@UntilDestroy()
 @Component({
   selector: 'app-non-forested-data',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatProgressSpinnerModule, NgIf],
   templateUrl: './non-forested-data.component.html',
   styleUrl: './non-forested-data.component.scss',
 })
 export class NonForestedDataComponent {
-  dataSource = [
-    { time_step: 0, rate_of_spread: 'High', flame_length: 'Placeholder' },
-    { time_step: 5, rate_of_spread: 'Placeholder', flame_length: 'High' },
-    { time_step: 10, rate_of_spread: 'High', flame_length: 'Placeholder' },
-    { time_step: 15, rate_of_spread: 'Moderate', flame_length: 'Moderate' },
-    { time_step: 20, rate_of_spread: 'Placeholder', flame_length: 'Moderate' },
-  ];
+  loading = true;
+
+  constructor(
+    private directImpactsStateService: DirectImpactsStateService,
+    private treatmentsState: TreatmentsState,
+    private treatmentsService: TreatmentsService
+  ) {
+    this.directImpactsStateService.activeStand$
+      .pipe(
+        untilDestroyed(this),
+        distinctUntilChanged((prev, curr) => prev?.id === curr?.id),
+        tap((_) => (this.loading = true)),
+        switchMap((s) =>
+          this.treatmentsService.getStandResult(
+            this.treatmentsState.getTreatmentPlanId(),
+            (s?.id as number) || 0
+          )
+        )
+      )
+      .subscribe((dataset) => {
+        this.dataSource = dataset.map((data, i) => {
+          return {
+            time_step: i * 5,
+            rate_of_spread: data.ROS.category,
+            flame_length: data.FL.category,
+          };
+        });
+        this.loading = false;
+      });
+  }
+
+  dataSource: {
+    time_step: number;
+    rate_of_spread: string;
+    flame_length: string;
+  }[] = [];
   displayedColumns: string[] = ['time_step', 'rate_of_spread', 'flame_length'];
 }
