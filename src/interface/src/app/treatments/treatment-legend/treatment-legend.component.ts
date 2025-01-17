1c06f0d5ff0499b584d4e888f5ac7d37d62ffa47
lastminutediorama <mkinsella@sig-gis.com>
2025-01-17 13:55:14 -0600

    check if tx is in a given list

--- src/interface/src/app/treatments/treatment-legend/treatment-legend.component.ts
+++ src/interface/src/app/treatments/treatment-legend/treatment-legend.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { KeyValuePipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { TreatmentTypeIconComponent } from '../../../styleguide/treatment-type-icon/treatment-type-icon.component';
 import { SequenceIconComponent } from '../../../styleguide/sequence-icon/sequence-icon.component';
 import { ModalComponent } from '../../../styleguide/modal/modal.component';
-import { PRESCRIPTIONS } from '../prescriptions';
+import { PrescriptionAction, PRESCRIPTIONS } from '../prescriptions';
 import { MatDividerModule } from '@angular/material/divider';
 import { MapConfigState } from '../treatment-map/map-config.state';
 
@@ -17,12 +17,10 @@ import { MapConfigState } from '../treatment-map/map-config.state';
   selector: 'app-treatment-legend',
   standalone: true,
   imports: [
-    KeyValuePipe,
     MatDividerModule,
     MatExpansionModule,
     MatIconModule,
     ModalComponent,
-    NgClass,
     NgIf,
     NgFor,
     SequenceIconComponent,
@@ -34,7 +32,22 @@ import { MapConfigState } from '../treatment-map/map-config.state';
 export class TreatmentLegendComponent {
   readonly singlePrescriptions = PRESCRIPTIONS.SINGLE;
   readonly sequencePrescriptions = PRESCRIPTIONS.SEQUENCE;
+
+  singleRxTypes: { type: PrescriptionAction; value: string }[] = Object.entries(
+    PRESCRIPTIONS.SINGLE
+  ).map(([key, value]) => ({
+    type: key as PrescriptionAction,
+    value,
+  }));
+  sequenceRxTypes = Object.entries(PRESCRIPTIONS.SEQUENCE).map(
+    ([key, value]) => ({
+      type: key as PrescriptionAction,
+      value,
+    })
+  );
+
   @Input() defaultExpanded = true;
+  @Input() displayedTreatments: PrescriptionAction[] | null = null;
 
   constructor(private mapConfigState: MapConfigState) {}
 
@@ -44,5 +57,17 @@ export class TreatmentLegendComponent {
 
   handleClose() {
     this.mapConfigState.setTreatmentLegendVisible(false);
+  }
+
+  inDisplayedTreatments(pa: PrescriptionAction) {
+    // if the displayedTreatments list is null, return true
+    if (!this.displayedTreatments) {
+      return true;
+    }
+    //otherwise check if the item is in the list first
+    if (this.displayedTreatments.includes(pa)) {
+      return true;
+    }
+    return false;
   }
 }
