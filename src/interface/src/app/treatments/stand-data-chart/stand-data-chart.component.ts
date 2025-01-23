aed8520338f60ee1d12ce6cb9af23d3d2fe40cde
lastminutediorama <mkinsella@sig-gis.com>
2025-01-23 12:14:26 -0600

    add inner chart title

--- src/interface/src/app/treatments/stand-data-chart/stand-data-chart.component.ts
+++ src/interface/src/app/treatments/stand-data-chart/stand-data-chart.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
 import { NgChartsModule } from 'ng2-charts';
-import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { DirectImpactsStateService } from '../direct-impacts.state.service';
 import { map, Observable } from 'rxjs';
 import { SLOT_COLORS, YEAR_INTERVAL_PROPERTY } from '../metrics';
@@ -26,7 +26,6 @@ const baseFont = {
     NgChartsModule,
     NgIf,
     AsyncPipe,
-    JsonPipe,
     TreatmentTypeIconComponent,
     MatTableModule,
     NonForestedDataComponent,
@@ -42,6 +41,13 @@ export class StandDataChartComponent {
   activeStandIsForested$ = this.activeStand$.pipe(
     map((d) => standIsForested(d))
   );
+
+  chartTitle(s: MapGeoJSONFeature) {
+    if (!s.properties['project_area_name'] || !s.properties['id']) {
+      return '';
+    }
+    return `${s.properties['project_area_name']}, Stand ${s.properties['id']}`;
+  }
 
   activeStandValues$: Observable<number[]> =
     this.directImpactsStateService.activeStand$.pipe(
