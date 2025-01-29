d37d739e291e27219ce65fa59e0cc9815d0847ac
Lucho <llopez@sig-gis.com>
2025-01-29 11:49:58 -0300

    Fixing some unit  tests

--- src/interface/src/app/treatments/treatment-filter/treatment-filter.component.spec.ts
+++ src/interface/src/app/treatments/treatment-filter/treatment-filter.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TreatmentFilterComponent } from './treatment-filter.component';
+import { TreatmentsState } from '../treatments.state';
+import { DirectImpactsStateService } from '../direct-impacts.state.service';
+import { MockProvider } from 'ng-mocks';
+import { BehaviorSubject, of } from 'rxjs';
 
 describe('TreatmentFilterComponent', () => {
   let component: TreatmentFilterComponent;
@@ -8,10 +12,20 @@ describe('TreatmentFilterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TreatmentFilterComponent]
-    })
-    .compileComponents();
-    
+      imports: [TreatmentFilterComponent],
+      providers: [
+        MockProvider(TreatmentsState, {
+          treatmentTypeOptions$: new BehaviorSubject([]),
+        }),
+        MockProvider(DirectImpactsStateService, {
+          filteredTreatmentTypes$: new BehaviorSubject([]),
+          activeStand$: new BehaviorSubject(null),
+          selectedProjectArea$: of('All' as any),
+          reportMetrics$: of(null as any),
+        }),
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TreatmentFilterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
