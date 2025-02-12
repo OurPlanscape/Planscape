638ef0efa8346b85fe6cd5d1020afbcf24c7721d
Lucho <llopez@sig-gis.com>
2025-02-12 14:13:36 -0300

    PLAN-2068: Treatment summary to direct impacts

--- src/interface/src/app/treatments/direct-impacts/direct-impacts.component.ts
+++ src/interface/src/app/treatments/direct-impacts/direct-impacts.component.ts
@@ -54,6 +54,7 @@ import { standIsForested } from '../stands';
 import { MapGeoJSONFeature } from 'maplibre-gl';
 import { MetricSelectorComponent } from '../metric-selector/metric-selector.component';
 import { TreatmentFilterComponent } from '../treatment-filter/treatment-filter.component';
+import { TreatmentSummaryComponent } from '../treatment-summary/treatment-summary.component';
 
 @Component({
   selector: 'app-direct-impacts',
@@ -90,6 +91,7 @@ import { TreatmentFilterComponent } from '../treatment-filter/treatment-filter.c
     MetricSelectorComponent,
     FilterDropdownComponent,
     TreatmentFilterComponent,
+    TreatmentSummaryComponent,
   ],
   providers: [
     DirectImpactsStateService,
