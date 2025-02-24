ac0719c2360845025812aeb481f66764afa6f9b1
lastminutediorama <mkinsella@sig-gis.com>
2025-02-24 15:37:47 -0600

    notes drawer overlay

--- src/interface/src/app/treatments/treatment-overview/treatment-overview.component.ts
+++ src/interface/src/app/treatments/treatment-overview/treatment-overview.component.ts
@@ -10,6 +10,11 @@ import { TreatmentsState } from '../treatments.state';
 import { AcresTreatedComponent } from '../acres-treated/acres-treated.component';
 import { canEditTreatmentPlan } from 'src/app/plan/permissions';
 import { TreatmentSummaryButtonComponent } from '../treatment-summary-button/treatment-summary-button.component';
+import { ButtonComponent } from '@styleguide';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { TreatmentPlanNotesComponent } from '../treatment-plan-notes/treatment-plan-notes.component';
+import { FeaturesModule } from 'src/app/features/features.module';
 
 @Component({
   selector: 'app-treatment-overview',
@@ -17,11 +22,15 @@ import { TreatmentSummaryButtonComponent } from '../treatment-summary-button/tre
   imports: [
     AsyncPipe,
     TreatmentMapComponent,
+    TreatmentPlanNotesComponent,
+    MatCheckboxModule,
+    MatSidenavModule,
     TreatmentPlanTabsComponent,
-
+    ButtonComponent,
     MapBaseLayerComponent,
     DebounceInputComponent,
     NgIf,
+    FeaturesModule,
     JsonPipe,
     DecimalPipe,
     AcresTreatedComponent,
@@ -34,7 +43,6 @@ export class TreatmentOverviewComponent {
   constructor(private treatmentsState: TreatmentsState) {}
 
   nameFieldStatus$ = new BehaviorSubject<DebounceEditState>('INITIAL');
-
   errorSavingName: string | null = null;
   summary$ = this.treatmentsState.summary$;
   projectAreas$ = this.summary$?.pipe(
