e236f4a43f5f64a754c588c6eca0c2e127ee4539
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-03-06 11:36:25 -0300

    Factories for Style and DataLayerHasStyle Style post generation added to DataLayerFactory Test case

--- src/planscape/datasets/tests/factories.py
+++ src/planscape/datasets/tests/factories.py
@@ -1,15 +1,17 @@
 import factory
-from django.contrib.gis.geos import Polygon
 from datasets.models import (
     Dataset,
     DataLayer,
     Category,
+    DataLayerHasStyle,
     DataLayerStatus,
     DataLayerType,
     VisibilityOptions,
     GeometryType,
+    Style,
 )
 from organizations.tests.factories import OrganizationFactory
+from planscape.tests.factories import UserFactory
 
 
 class DatasetFactory(factory.django.DjangoModelFactory):
@@ -32,6 +34,16 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda x: f"Category {x}")
 
 
+class StyleFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Style
+
+    created_by = factory.SubFactory(UserFactory)
+    organization = factory.SubFactory(OrganizationFactory)
+    name = factory.Sequence(lambda x: f"Style {x}")
+    data = {"foo": "bar"}
+
+
 class DataLayerFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = DataLayer
@@ -45,3 +57,18 @@ class DataLayerFactory(factory.django.DjangoModelFactory):
     status = factory.Iterator(DataLayerStatus.values)
     metadata = {}
     url = None
+
+    @factory.post_generation
+    def style(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        DataLayerHasStyleFactory.create(datalayer=self, style=extracted)
+        return extracted
+
+
+class DataLayerHasStyleFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = DataLayerHasStyle
+
+    datalayer = factory.SubFactory(DataLayerFactory)
+    style = factory.SubFactory(StyleFactory)
