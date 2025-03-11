72f21b74fc0659c05307cb6f65af51dc15fd5326
Pablo Lopez <plopez@sig-gis.com>
2025-03-11 18:10:48 -0300

    updating styles

---
+++ src/interface/src/app/data-layers/data-set/data-set.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { DataSetSearchResult } from '@types';
+import { ButtonComponent } from '@styleguide';
+
+@Component({
+  selector: 'app-data-set',
+  standalone: true,
+  imports: [NgForOf, MatIconModule, ButtonComponent, NgIf],
+  templateUrl: './data-set.component.html',
+  styleUrl: './data-set.component.scss',
+})
+export class DataSetComponent {
+  @Input() name = '';
+  @Input() organizationName = '';
+  @Input() layers?: DataSetSearchResult[];
+  @Input() path?: string[];
+}
