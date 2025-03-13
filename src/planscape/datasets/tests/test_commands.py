dbe26ac4bb3d2a137e2ed75f7b25f34eb06fbe5d
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-03-13 18:42:19 -0300

    iso 19139 file test

--- src/planscape/datasets/tests/test_commands.py
+++ src/planscape/datasets/tests/test_commands.py
@@ -9,3 +9,56 @@ class DataLayerCreateTest(TestCase):
         path = Path("datasets/tests/assets/qgis_metadata.tif")
         metadata = get_datalayer_metadata(path)
         self.assertIsNotNone(metadata)
+
+        metadata_obj = metadata.get("metadata")
+        self.assertIsNotNone(metadata_obj)
+        self.assertEqual(
+            metadata_obj.keys(),
+            {"hierarchylevel", "identifier", "language", "parentidentifier"},
+        )
+
+        identification = metadata.get("identification")
+        self.assertIsNotNone(identification)
+        self.assertEqual(
+            identification.keys(),
+            {
+                "language",
+                "title",
+                "abstract",
+                "fees",
+                "accessconstraints",
+                "license",
+                "keywords",
+                "extents",
+            },
+        )
+
+        distribution = metadata.get("distribution")
+        self.assertIsNotNone(distribution)
+        self.assertEqual(distribution.keys(), {"planscape"})
+
+        contact = metadata.get("contact")
+        self.assertIsNotNone(contact)
+        self.assertEqual(
+            contact.keys(),
+            {
+                "address",
+                "city",
+                "country",
+                "postalcode",
+                "email",
+                "fax",
+                "voice",
+                "name",
+                "organization",
+                "position",
+                "role",
+            },
+        )
+
+        self.assertIsNotNone(metadata.get("crs"))
+
+    def test_file_metadata_iso_19139_xml(self):
+        path = Path("datasets/tests/assets/iso19139_metadata.tif")
+        metadata = get_datalayer_metadata(path)
+        self.assertIsNotNone(metadata)
