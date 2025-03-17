246b34b428c63dd7c714a15233cfca002783064b
lastminutediorama <mkinsella@sig-gis.com>
2025-03-16 20:34:37 -0500

    continue after fail

--- src/interface/testrununtilfail.sh
+++ src/interface/testrununtilfail.sh
@@ -6,7 +6,10 @@ success_count=0
 while true; do
   # Run tests and capture output
   ng test --watch=false > temp-output.log 2>&1
+  test_run_count=$((test_run_count + 1))
+
   if [ $? -ne 0 ]; then
+    echo "**** TEST FAILED - run #: $test_run_count  at $(date) ****" >> test-runs.log
     # If tests fail, append the output to failed-tests.log
     echo "====================" >> failed-tests.log
     echo "Failure time: $(date)" >> failed-tests.log
@@ -14,11 +17,9 @@ while true; do
     cat temp-output.log >> failed-tests.log
     echo "====================" >> failed-tests.log
     echo "Tests failed. Check failed-tests.log for details."
-    exit 1
   else
     # If tests pass, increment the success counter
-    success_count=$((success_count + 1))
-    echo "==== Ran test $success_count ==== passed at $(date)" >> failed-tests.log
+    echo "---- test passed - run #: $test_run_count -- run at $(date) ---- " >> test-runs.log
     # Remove the temporary log file
     rm temp-output.log
   fi
