55e229ba480daeea804c47c933c52fb59a7bfb70
lastminutediorama <mkinsella@sig-gis.com>
2025-03-24 10:47:43 -0500

    fix test

--- src/interface/src/app/maplibre-map/map-layer-color-legend/map-layer-color-legend.component.spec.ts
+++ src/interface/src/app/maplibre-map/map-layer-color-legend/map-layer-color-legend.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MapService } from '@maplibre/ngx-maplibre-gl';
 import { MapLayerColorLegendComponent } from './map-layer-color-legend.component';
 
 describe('MapLayerColorLegendComponent', () => {
@@ -9,6 +9,7 @@ describe('MapLayerColorLegendComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MapLayerColorLegendComponent],
+      providers: [MapService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MapLayerColorLegendComponent);
