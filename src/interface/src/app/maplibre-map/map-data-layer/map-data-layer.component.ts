4c9d9ea33b49b754ec19013d4af392c6d6dacb5b
lastminutediorama <mkinsella@sig-gis.com>
2025-03-26 10:51:39 -0500

    address some comments

--- src/interface/src/app/maplibre-map/map-data-layer/map-data-layer.component.ts
+++ src/interface/src/app/maplibre-map/map-data-layer/map-data-layer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { makeColorFunction } from '../../data-layers/utilities';
 import { setColorFunction } from '@geomatico/maplibre-cog-protocol';
 import {
@@ -6,7 +6,10 @@ import {
   RasterSourceComponent,
 } from '@maplibre/ngx-maplibre-gl';
 import { DataLayer } from '@types';
+import { DataLayersStateService } from 'src/app/data-layers/data-layers.state.service';
+import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'app-map-data-layer',
   standalone: true,
@@ -14,18 +17,18 @@ import { DataLayer } from '@types';
   templateUrl: './map-data-layer.component.html',
   styleUrl: './map-data-layer.component.scss',
 })
-export class MapDataLayerComponent implements OnChanges {
+export class MapDataLayerComponent {
   OPACITY = 0.75;
-  @Input() dataLayer!: DataLayer | null;
   cogUrl = '';
 
-  constructor() {}
-
-  ngOnChanges() {
-    if (this.dataLayer?.public_url) {
-      this.cogUrl = `cog://${this.dataLayer?.public_url}`;
-      const colorFn = makeColorFunction(this.dataLayer?.styles as any);
-      setColorFunction(this.dataLayer?.public_url ?? '', colorFn);
-    }
+  constructor(dataLayersStateService: DataLayersStateService) {
+    dataLayersStateService.selectedDataLayer$.pipe(untilDestroyed(this)).subscribe((dataLayer: DataLayer | null) => {
+      if (dataLayer?.public_url) {
+        this.cogUrl = `cog://${dataLayer?.public_url}`;
+        const colorFn = makeColorFunction(dataLayer?.styles as any);
+        setColorFunction(dataLayer?.public_url ?? '', colorFn);
+      }
+    });
   }
+
 }
