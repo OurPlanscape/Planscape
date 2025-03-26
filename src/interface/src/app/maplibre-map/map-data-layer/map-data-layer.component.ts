651cd0e7ba9932d12c0e461bdb04d23621c512e5
lastminutediorama <mkinsella@sig-gis.com>
2025-03-26 12:24:26 -0500

    add background as bottom layer

--- src/interface/src/app/maplibre-map/map-data-layer/map-data-layer.component.ts
+++ src/interface/src/app/maplibre-map/map-data-layer/map-data-layer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { makeColorFunction } from '../../data-layers/utilities';
 import { setColorFunction } from '@geomatico/maplibre-cog-protocol';
 import {
@@ -13,22 +14,25 @@ import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
 @Component({
   selector: 'app-map-data-layer',
   standalone: true,
-  imports: [LayerComponent, RasterSourceComponent],
+  imports: [NgIf, LayerComponent, RasterSourceComponent],
   templateUrl: './map-data-layer.component.html',
   styleUrl: './map-data-layer.component.scss',
 })
 export class MapDataLayerComponent {
   OPACITY = 0.75;
   cogUrl = '';
+  dataLayer: DataLayer | null = null;
 
   constructor(dataLayersStateService: DataLayersStateService) {
-    dataLayersStateService.selectedDataLayer$.pipe(untilDestroyed(this)).subscribe((dataLayer: DataLayer | null) => {
-      if (dataLayer?.public_url) {
-        this.cogUrl = `cog://${dataLayer?.public_url}`;
-        const colorFn = makeColorFunction(dataLayer?.styles as any);
-        setColorFunction(dataLayer?.public_url ?? '', colorFn);
-      }
-    });
+    dataLayersStateService.selectedDataLayer$
+      .pipe(untilDestroyed(this))
+      .subscribe((dataLayer: DataLayer | null) => {
+        if (dataLayer?.public_url) {
+          this.dataLayer = dataLayer;
+          this.cogUrl = `cog://${dataLayer?.public_url}`;
+          const colorFn = makeColorFunction(dataLayer?.styles as any);
+          setColorFunction(dataLayer?.public_url ?? '', colorFn);
+        }
+      });
   }
-
 }
