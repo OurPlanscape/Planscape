212df10ef14c8d20e81b37f4edb46e99e40ff190
George Silva <863039+george-silva@users.noreply.github.com>
2025-03-27 10:30:23 -0300

    fix cached result (#2355)

--- src/planscape/datasets/views.py
+++ src/planscape/datasets/views.py
@@ -55,10 +55,10 @@ class DatasetViewSet(ListModelMixin, MultiSerializerMixin, GenericViewSet):
     )
     @action(detail=True, methods=["get"])
     def browse(self, request, pk=None):
-        data = self._get_browse_result(request.query_params)
-
+        results = self._get_browse_result(request.query_params)
+        serializer = BrowseDataLayerSerializer(results, many=True)
         return Response(
-            data,
+            serializer.data,
             status=status.HTTP_200_OK,
         )
 
@@ -71,11 +71,8 @@ class DatasetViewSet(ListModelMixin, MultiSerializerMixin, GenericViewSet):
             .prefetch_related("styles")
             .filter(status=DataLayerStatus.READY)
         )
-
         filterset = BrowseDataLayerFilterSet(queryset=datalayers, request=query_params)
-        serializer = BrowseDataLayerSerializer(filterset.qs, many=True)
-
-        return serializer.data
+        return list(filterset.qs)
 
 
 class DataLayerViewSet(ListModelMixin, MultiSerializerMixin, GenericViewSet):
