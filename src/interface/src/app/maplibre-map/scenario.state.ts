763f2260f14f7d8052890cbb77575be65f0104a2
Lucho <llopez@sig-gis.com>
2025-04-02 15:36:48 -0300

    PR comments

--- src/interface/src/app/maplibre-map/scenario.state.ts
+++ src/interface/src/app/maplibre-map/scenario.state.ts
@@ -4,6 +4,7 @@ import { LoadedResult, Resource, Scenario } from '@types';
 import {
   BehaviorSubject,
   catchError,
+  combineLatest,
   concat,
   distinctUntilChanged,
   filter,
@@ -22,30 +23,34 @@ export class ScenarioState {
 
   // The ID of the current scenario
   private _currentScenarioId$ = new BehaviorSubject<number | null>(null);
+  public currentScenarioId$ = this._currentScenarioId$.asObservable();
+
+  // BehaviorSubject that we are going to use to manually reload the scenario
+  private _reloadScenario$ = new BehaviorSubject<void>(undefined);
 
   // Listen to ID changes and trigger network calls, returning typed results.
-  currentScenarioResource$: Observable<Resource<Scenario>> =
+  currentScenarioResource$: Observable<Resource<Scenario>> = combineLatest([
     this._currentScenarioId$.pipe(
-      // we might need to tweak this for reloading scenarios / etc.
       distinctUntilChanged(),
-      filter((id): id is number => !!id),
-      switchMap((id) => {
-        return concat(
-          // when loading emit object with loading
-          of({ isLoading: true }),
-          this.scenarioService.getScenario(id.toString()).pipe(
-            // when done, emit object with loading false and data
-            map(
-              (data) => ({ data, isLoading: false }) as LoadedResult<Scenario>
-            ),
-            // when we have errors, emit object with loading false and error
-            catchError((error) => of({ isLoading: false, error: error }))
-          )
-        );
-      }),
-      // ensure each new subscriber gets the cached result immediately without re-fetching
-      shareReplay(1)
-    );
+      filter((id): id is number => !!id)
+    ),
+    this._reloadScenario$,
+  ]).pipe(
+    switchMap(([id]) =>
+      concat(
+        // when loading emit object with loading
+        of({ isLoading: true }),
+        this.scenarioService.getScenario(id.toString()).pipe(
+          // when done, emit object with loading false and data
+          map((data) => ({ data, isLoading: false }) as LoadedResult<Scenario>),
+          // when we have errors, emit object with loading false and error
+          catchError((error) => of({ isLoading: false, error }))
+        )
+      )
+    ),
+    // ensure each new subscriber gets the cached result immediately without re-fetching
+    shareReplay(1)
+  );
 
   /**
    * This observable filter currentScenarioResource$ to only emit when we have a scenario,
@@ -57,13 +62,6 @@ export class ScenarioState {
   );
 
   /**
-   * This observable returns the current scenario id
-   */
-  public currentScenarioId$ = this.currentScenario$.pipe(
-    map((scenario) => Number(scenario.id))
-  );
-
-  /**
    * Observable that maps only to loading status.
    */
   public isScenarioLoading$ = this.currentScenarioResource$.pipe(
@@ -72,5 +70,10 @@ export class ScenarioState {
 
   setScenarioId(id: number) {
     this._currentScenarioId$.next(id);
+  }
+
+  // Reload the current scenario manually
+  reloadScenario() {
+    this._reloadScenario$.next();
   }
 }
