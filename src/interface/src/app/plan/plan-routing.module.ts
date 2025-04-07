9e26c4b5f2ad134123ad85487eb20e24c7bcf261
Luis Lopez <33841664+LALuis@users.noreply.github.com>
2025-04-07 12:13:48 -0300

    Plan 2319 and PLAN 2333 (#2369)
    * Creating scenario state

--- src/interface/src/app/plan/plan-routing.module.ts
+++ src/interface/src/app/plan/plan-routing.module.ts
@@ -6,6 +6,7 @@ import { CreateScenariosComponent } from './create-scenarios/create-scenarios.co
 import { ExploreComponent } from './explore/explore/explore.component';
 import { ScenarioRoutePlaceholderComponent } from './scenario-route-placeholder/scenario-route-placeholder';
 import { planLoaderResolver } from '../resolvers/plan-loader.resolver';
+import { scenarioLoaderResolver } from '../resolvers/scenario-loader.resolver';
 
 const routes: Routes = [
   {
@@ -22,13 +23,25 @@ const routes: Routes = [
         path: 'config/',
         title: 'Scenario Configuration',
         component: CreateScenariosComponent,
-        data: { showOverview: false },
+        data: {
+          showOverview: false,
+          showProjectAreas: false,
+        },
+        resolve: {
+          scenarioInit: scenarioLoaderResolver,
+        },
       },
       {
         path: 'config/:id',
         title: 'Scenario Configuration',
         component: ScenarioRoutePlaceholderComponent,
-        data: { showOverview: false },
+        resolve: {
+          scenarioInit: scenarioLoaderResolver,
+        },
+        data: {
+          showOverview: false,
+          showProjectAreas: true,
+        },
       },
       {
         path: 'explore',
