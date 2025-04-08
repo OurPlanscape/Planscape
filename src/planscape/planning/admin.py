b5fdfa8527c87f0e167397c7643e10cbb577528a
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-04-08 17:43:37 -0300

    Treatment Goal on Admin panel

---
+++ src/planscape/planning/admin.py
@@ -0,0 +1,20 @@
+
+from django.contrib import admin
+
+from planning.models import TreatmentGoal
+from planning.forms import TreatmentGoalAdminForm
+
+class TreatmentGoalAdmin(admin.ModelAdmin):
+    """
+    Admin interface for TreatmentGoal model.
+    """
+
+    list_display = ("id", "name", "description", "priorities", "active")
+    list_display_links = ("id", "name")
+    form = TreatmentGoalAdminForm
+    search_fields = ["name"]
+    list_filter = ["priorities"]
+    ordering = ["name"]
+
+
+admin.site.register(TreatmentGoal, TreatmentGoalAdmin)
