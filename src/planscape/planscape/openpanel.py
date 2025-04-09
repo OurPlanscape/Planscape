318a5479074598e165f76f0f28e7d77e3444f4cd
George Silva <georger.silva@gmail.com>
2025-04-09 17:19:39 -0300

    make openpanel dynamic

--- src/planscape/planscape/openpanel.py
+++ src/planscape/planscape/openpanel.py
@@ -12,18 +12,21 @@ class SingleOpenPanel(metaclass=Singleton):
     """
 
     def __init__(self):
-        self.op = OpenPanel(
-            client_id=settings.OPENPANEL_CLIENT_ID,
-            client_secret=settings.OPENPANEL_CLIENT_SECRET,
-            api_url=settings.OPENPANEL_URL,
-            disabled=settings.TESTING_MODE is True,
-        )
-        self.op.set_global_properties(
-            {"environment": settings.ENV, "testing": settings.TESTING_MODE}
-        )
+        if settings.OPENPANEL_URL:
+            self.op = OpenPanel(
+                client_id=settings.OPENPANEL_CLIENT_ID,
+                client_secret=settings.OPENPANEL_CLIENT_SECRET,
+                api_url=settings.OPENPANEL_URL,
+                disabled=settings.TESTING_MODE is True,
+            )
+            self.op.set_global_properties(
+                {"environment": settings.ENV, "testing": settings.TESTING_MODE}
+            )
 
     def track(self, name: str, properties: Optional[Dict[str, Any]] = None):
-        self.op.track(name=name, properties=properties)
+        if self.op:
+            self.op.track(name=name, properties=properties)
 
     def identify(self, profile_id: str, traits: Optional[Dict[str, Any]] = None):
-        self.op.identify(profile_id=profile_id, traits=traits)  # type: ignore
+        if self.op:
+            self.op.identify(profile_id=profile_id, traits=traits)  # type: ignore
