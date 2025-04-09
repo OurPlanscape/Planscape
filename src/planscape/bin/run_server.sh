f617aa45cdc52d5ba63f98bab24fdc526d5d74d9
George Silva <georger.silva@gmail.com>
2025-04-09 17:15:04 -0300

    customize the uwsgi start up without otel

--- src/planscape/bin/run_server.sh
+++ src/planscape/bin/run_server.sh
@@ -7,21 +7,9 @@ CORN_WORKERS=4
 OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
 
 app_name="planscape"
-echo $(pwd)
 echo "Running migrations & collectstatic"
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
-echo "migrated"
-echo 
-if [[ "$ENV" == "production" ]]; then
-  echo "Starting gunicorn for production"
-  opentelemetry-instrument \
-    --service_name planscape \
-    uwsgi --ini planscape/uwsgi.ini
-
-else
-  echo "Starting gunicorn locally"
-  opentelemetry-instrument \
-    --service_name planscape \
-    uwsgi --ini planscape/uwsgi.ini
-fi
+echo "----------------------------------------"
+echo "Starting uwsgi locally"
+uwsgi --ini planscape/uwsgi.ini
