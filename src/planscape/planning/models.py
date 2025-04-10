f13fcfa7b5d9e2a21645a7f6826fd8e86b252552
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-04-10 17:58:24 -0300

    Treatment Goal admin page improvements * Object names * Treatment Goal and Datalayer search on Relational table creation

--- src/planscape/planning/models.py
+++ src/planscape/planning/models.py
@@ -213,6 +213,9 @@ class TreatmentGoal(CreatedAtMixin, UpdatedAtMixin, DeletedAtMixin, models.Model
         null=True,
     )
 
+    def __str__(self):
+        return f"{self.name} - {self.category}"
+
 
 class TreatmentGoalUsageType(models.TextChoices):
     PRIORITY = "PRIORITY", "Priority"
@@ -258,6 +261,9 @@ class TreatmentGoalUsesDataLayer(
     @property
     def datalayer_name(self):
         return self.datalayer.name
+
+    def __str__(self):
+        return f"{self.usage_type} ({self.treatment_goal} - {self.datalayer})"
 
     class Meta:
         constraints = [
