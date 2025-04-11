e869800a07f0ea0f1fad45216e81c5ef392040f2
Lucho <llopez@sig-gis.com>
2025-04-11 11:42:51 -0300

    Fixing unit tests

--- src/interface/src/app/services/file-saver.service.spec.ts
+++ src/interface/src/app/services/file-saver.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { FileSaverService } from './file-saver.service';
 
-fdescribe('FileSaverService', () => {
+describe('FileSaverService', () => {
   let service: FileSaverService;
 
   beforeEach(() => {
