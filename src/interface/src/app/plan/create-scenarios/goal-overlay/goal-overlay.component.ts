020d7d1d7eb0a888297b0212c6957af1e899ee9a
Pablo Lopez <plopez@sig-gis.com>
2025-04-14 13:06:22 -0300

    Statewide scenario goals (#2412)
    * working on new scenario goals, wip
    
    * wip
    
    * revert some changes and show new treatment goals
    
    * showing new goal descriptions
    
    * fix goal descriptions overlay
    
    * fixing test and updating category grouping field
    
    * cleanup and adding minimal styles

--- src/interface/src/app/plan/create-scenarios/goal-overlay/goal-overlay.component.ts
+++ src/interface/src/app/plan/create-scenarios/goal-overlay/goal-overlay.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { GoalOverlayService } from './goal-overlay.service';
+import { FeatureService } from '../../../features/feature.service';
+import { map } from 'rxjs';
+import { ScenarioGoal } from '@types';
+import { filter } from 'rxjs/operators';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-goal-overlay',
   templateUrl: './goal-overlay.component.html',
   styleUrls: ['./goal-overlay.component.scss'],
 })
-export class GoalOverlayComponent {
-  goal$ = this.service.selectedQuestion$;
+export class GoalOverlayComponent implements OnDestroy {
+  goal$ = this.goalOverlayService.selectedQuestion$;
+  stateWideGoal$ = this.goalOverlayService.selectedStateWideGoal$;
 
-  constructor(private service: GoalOverlayService) {}
+  safeDescription$ = this.stateWideGoal$.pipe(
+    filter((goal): goal is ScenarioGoal => !!goal),
+    map((goal) =>
+      this.sanitizer.bypassSecurityTrustHtml(
+        this.sanitizeLinks(goal.description)
+      )
+    )
+  );
+
+  constructor(
+    private goalOverlayService: GoalOverlayService,
+    private featureService: FeatureService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   close() {
-    this.service.close();
+    this.goalOverlayService.close();
+  }
+
+  get isStatewideScenariosEnabled() {
+    return this.featureService.isFeatureEnabled('statewide_scenarios');
+  }
+
+  sanitizeLinks(html: string): string {
+    return html.replace(
+      /<a /g,
+      '<a target="_blank" rel="noopener noreferrer" '
+    );
+  }
+
+  ngOnDestroy() {
+    this.close();
   }
 }
