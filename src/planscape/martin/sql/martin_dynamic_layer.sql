6335efd462f8cd93e30d6c6400af8a8f79239833
Zalo <gcrivelli@sig-gis.com>
2025-04-18 15:10:30 -0400

    function-for-Martin v2

--- src/planscape/martin/sql/martin_dynamic_layer.sql
+++ src/planscape/martin/sql/martin_dynamic_layer.sql
@@ -7,37 +7,54 @@ CREATE OR REPLACE FUNCTION martin_dynamic_layer(
 RETURNS bytea AS $$
 
 DECLARE
-    layer_name text := query_params->>'layer';
-    sql         text;
+    layer_id integer := (query_params->>'layer')::int;
+    dyn_table    text;
     tile        bytea;
 BEGIN
-    IF layer_name !~ '^[a-zA-Z0-9_\.]+$' THEN
-        RAISE EXCEPTION 'Invalid layer name %', layer_name;
+    SELECT "table"
+        INTO dyn_table
+        FROM public.datasets_datalayer
+    WHERE id = layer_id;
+    
+    IF dyn_table IS NULL THEN    
+        RAISE EXCEPTION 'Datalayer with id % not found or has no table defined', layer_id;
     END IF;
 
-    sql := format($f$
+    EXECUTE format($f$
         WITH
         bbox AS (
-          SELECT ST_TileEnvelope(%1$s, %2$s, %3$s, margin => (64.0/4096)) AS geom
+          SELECT ST_TileEnvelope($1, $2, $3, margin => (64.0/4096)) AS geom
         ),
         mvtgeom AS (
           SELECT
             t.id AS id,  
             ST_AsMVTGeom(
               ST_Transform(t.geometry, 3857),
-              ST_TileEnvelope(%1$s, %2$s, %3$s),
+              ST_TileEnvelope($1, $2, $3),
               4096, 64, true
             ) AS geom
-          FROM %4$I AS t, bbox
-          WHERE
-            t.geometry && ST_Transform(bbox.geom, ST_SRID(t.geometry))
+          FROM %I AS t, bbox
+          WHERE t.geometry && ST_Transform(bbox.geom, ST_SRID(t.geometry))
         )
-        SELECT ST_AsMVT(mvtgeom.*, 'dynamic_layer', 4096, 'geom')
+        SELECT ST_AsMVT(mvtgeom.*, %L, 4096, 'geom')
         FROM mvtgeom;
-    $f$, z, x, y, layer_name);
-
-    EXECUTE sql INTO tile;
+    $f$, dyn_table, format('dynamic_%s', layer_id))
+    INTO tile
+    USING z, x, y;
     RETURN tile;
+
+EXCEPTION WHEN OTHERS THEN
+    RAISE NOTICE 'martin_dynamic_layer(%): %', layer_id, SQLERRM;
+
+    RETURN (
+      WITH empty AS (
+        SELECT NULL::integer AS id, NULL::geometry AS geom
+        WHERE FALSE
+      )
+      SELECT ST_AsMVT(empty, format('dynamic_%s', layer_id), 4096, 'geom')
+      FROM empty
+    );
+
 END; 
 
 $$ LANGUAGE plpgsql
