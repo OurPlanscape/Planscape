b5ba82623c258af223d6996dab5d355dfb69201a
George Silva <georger.silva@gmail.com>
2025-04-23 13:27:38 -0300

    adds migration dependencies for proper sorting

--- src/planscape/datasets/migrations/0013_auto_20250423_1620.py
+++ src/planscape/datasets/migrations/0013_auto_20250423_1620.py
@@ -30,6 +30,8 @@ def delete_vector_dataset(apps, schema_editor):
 class Migration(migrations.Migration):
     dependencies = [
         ("datasets", "0012_auto_20250418_1307"),
+        ("organizations", "0004_alter_organization_options"),
+        ("auth", "0012_alter_user_first_name_max_length"),
     ]
 
     operations = [migrations.RunPython(create_vector_dataset, delete_vector_dataset)]
