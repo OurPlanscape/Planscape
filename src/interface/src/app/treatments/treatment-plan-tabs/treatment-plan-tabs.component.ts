8eff98b9f34f5ad06e959da74ad5816f3b8c118f
Pablo Lopez <plopez@sig-gis.com>
2025-04-23 15:27:07 -0300

    change tab when clicking data layer name on map

--- src/interface/src/app/treatments/treatment-plan-tabs/treatment-plan-tabs.component.ts
+++ src/interface/src/app/treatments/treatment-plan-tabs/treatment-plan-tabs.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
+import { Component, ViewChild } from '@angular/core';
+import { MatTabGroup, MatTabsModule } from '@angular/material/tabs';
 import { TreatmentPlanNotesComponent } from '../treatment-plan-notes/treatment-plan-notes.component';
 import { ProjectAreasTabComponent } from '../project-areas-tab/project-areas-tab.component';
 import { MapBaseLayerComponent } from '../map-base-layer/map-base-layer.component';
 import { FeaturesModule } from 'src/app/features/features.module';
 import { DataLayersComponent } from '../../data-layers/data-layers/data-layers.component';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { DataLayersStateService } from '../../data-layers/data-layers.state.service';
+import { skip } from 'rxjs';
 
+@UntilDestroy()
 @Component({
   selector: 'app-treatment-plan-tabs',
   standalone: true,
@@ -20,4 +24,16 @@ import { DataLayersComponent } from '../../data-layers/data-layers/data-layers.c
   templateUrl: './treatment-plan-tabs.component.html',
   styleUrl: './treatment-plan-tabs.component.scss',
 })
-export class TreatmentPlanTabsComponent {}
+export class TreatmentPlanTabsComponent {
+  @ViewChild('tabGroup') tabGroup!: MatTabGroup;
+
+  constructor(private dataLayersStateService: DataLayersStateService) {
+    this.dataLayersStateService.paths$
+      .pipe(untilDestroyed(this), skip(1))
+      .subscribe((path) => {
+        if (path.length > 0) {
+          this.tabGroup.selectedIndex = 2;
+        }
+      });
+  }
+}
