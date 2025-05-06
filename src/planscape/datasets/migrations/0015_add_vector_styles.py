62aea0046b4445dfa01a5a2b5f0c8b48451deb32
Pablo Lopez <plopez@sig-gis.com>
2025-05-06 12:10:02 -0300

    update dependencies

--- src/planscape/datasets/migrations/0015_add_vector_styles.py
+++ src/planscape/datasets/migrations/0015_add_vector_styles.py
@@ -70,9 +70,9 @@ def remove_styles(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("datasets", "0012_auto_20250418_1307"), 
         ("organizations", "0004_alter_organization_options"), 
         ("auth", "0012_alter_user_first_name_max_length"), 
+        ("datasets", "0014_remove_category_deleted_at")
     ]
 
     operations = [
