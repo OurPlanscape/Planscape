780ed363839d759ae24f6cc8c38faa2361472d70
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-05-07 17:12:41 -0300

    Check if datastore table was created once VECTOR datalayer is uploaded

--- src/planscape/datasets/tasks.py
+++ src/planscape/datasets/tasks.py
@@ -1,10 +1,11 @@
 import logging
 
+from django.db import connection
+
 from core.s3 import get_s3_hash
 from datasets.models import DataLayer, DataLayerStatus, DataLayerType
 from django.conf import settings
 from gis.vectors import ogr2ogr
-from utils.frontend import get_base_url
 
 from planscape.celery import app
 
@@ -26,7 +27,9 @@ def datalayer_uploaded(
         datalayer.hash = get_s3_hash(datalayer.url, bucket=settings.S3_BUCKET)
         datalayer.status = status
         if datalayer.type == DataLayerType.VECTOR:
-            datalayer.table = ogr2ogr(datalayer.url)
+            datastore_table = ogr2ogr(datalayer.url)
+            check_datastore_table(datastore_table)
+            datalayer.table = datastore_table
     except Exception:
         logger.exception(
             "Something went wrong while ingesting and processing datalayer %s",
@@ -35,3 +38,27 @@ def datalayer_uploaded(
         datalayer.status = DataLayerStatus.FAILED
     finally:
         datalayer.save()
+
+
+def check_datastore_table(datastore_table_name: str) -> bool:
+    """
+    Check if the datastore table exists in the database.
+    :param datastore_table_name: The name of the datastore table to check.
+    :return: True if the table exists, False otherwise.
+    """
+    table_schema = datastore_table_name.split(".")[0]
+    table_name = datastore_table_name.split(".")[1]
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = %s AND table_name = %s)",
+            [table_schema, table_name],
+        )
+        exists = cursor.fetchone()[0]
+    if not exists:
+        logger.exception(
+            "Datastore table %s does not exist in the database.", datastore_table_name
+        )
+        raise ValueError(
+            f"Datastore table {datastore_table_name} does not exist in the database."
+        )
+    return exists
