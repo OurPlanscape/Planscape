a0307e15b223353af17f7195b79932b196b4741c
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-05-08 16:11:05 -0300

    Backfill of excluded areas

--- src/planscape/planning/migrations/0035_auto_20250505_2006.py
+++ src/planscape/planning/migrations/0035_auto_20250505_2006.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.db import migrations
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -31,9 +32,40 @@ def backfill_scenario_configuration(apps, schema_editor):
         scenario.save()
 
 
+def backfill_excluded_areas(apps, schema_editor):
+    DataLayer = apps.get_model("datasets", "DataLayer")
+    Dataset = apps.get_model("datasets", "Dataset")
+    Scenario = apps.get_model("planning", "Scenario")
+
+    legacy_excluded_areas_dataset = Dataset.objects.filter(
+        name="Legacy Excluded Areas",
+        organization__name=settings.DEFAULT_ORGANIZATION_NAME,
+    ).first()
+
+    legacy_excluded_areas = DataLayer.objects.filter(
+        dataset=legacy_excluded_areas_dataset,
+    ).values_list("name", "id")
+
+    lookup_dict = {name: id for name, id in legacy_excluded_areas}
+
+    for scenario in Scenario.objects.iterator(chunk_size=100):
+        legacy_excluded_areas = scenario.configuration.get("excluded_areas", [])
+        if not legacy_excluded_areas:
+            logger.warning(f"Skipping scenario {scenario.id} as it has no excluded areas")
+            continue
+        new_excluded_areas = [
+            lookup_dict.get(area) for area in legacy_excluded_areas
+        ]
+        scenario.configuration["excluded_areas"] = new_excluded_areas
+        scenario.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("planning", "0034_treatmentgoal_datalayers_and_more"),
     ]
 
-    operations = [migrations.RunPython(backfill_scenario_configuration)]
+    operations = [
+        migrations.RunPython(backfill_scenario_configuration), 
+        migrations.RunPython(backfill_excluded_areas),
+    ]
