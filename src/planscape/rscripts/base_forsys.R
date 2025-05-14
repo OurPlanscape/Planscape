6725a869227d3113c0d6ecb4f04d1cea7922cdc4
George Silva <georger.silva@gmail.com>
2025-05-14 10:18:11 -0300

    split more imports

--- src/planscape/rscripts/base_forsys.R
+++ src/planscape/rscripts/base_forsys.R
@@ -1,25 +1,7 @@
-import::from(queries.R, .all=TRUE)
-import::from(constants.R, .all=TRUE)
 
 FORSYS_V2 <- as.logical(Sys.getenv("USE_SCENARIO_V2", "False"))
 
-average_per_stand <- function(value, stand_count, stand_size = NA, metric = NA) {
-  return(round(value / stand_count, digits = 2))
-}
 
-average_and_clamp <- function(value, stand_count, stand_size = NA, metric = NA) {
-  categories <- CATEGORIES_PER_METRIC[[metric]]
-  average <- value / stand_count
-  result <- abs(categories - average) %>%
-    which.min() %>%
-    categories[.]
-  return(result)
-}
-
-total_acres_per_project <- function(value, stand_count, stand_size, metric = NA) {
-  stand_size_in_acres <- STAND_AREAS_ACRES[[stand_size]]
-  return(round(value * stand_size_in_acres, digits = 2))
-}
 
 get_sdw <- function() {
   return(as.numeric(Sys.getenv("FORSYS_SDW", "0.5")))
