1ac43e4cc0185a2520f5ce28664904b5e0c0a9d4
lastminutediorama <mkinsella@sig-gis.com>
2025-05-23 17:55:35 -0500

    initial functions to collect mapwidth

--- src/interface/src/app/maplibre-map/map-base-layer-tooltip/map-base-layer-tooltip.component.ts
+++ src/interface/src/app/maplibre-map/map-base-layer-tooltip/map-base-layer-tooltip.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { PopupComponent } from '@maplibre/ngx-maplibre-gl';
 import { NgIf } from '@angular/common';
 import { LngLat } from 'maplibre-gl';
+import {
+  Map as MapLibreMap,
+} from 'maplibre-gl';
 
 @Component({
   selector: 'app-map-base-layer-tooltip',
@@ -13,4 +16,25 @@ import { LngLat } from 'maplibre-gl';
 export class MapBaseLayerTooltipComponent {
   @Input() content: string | null = null;
   @Input() lngLat: LngLat | null = null;
+  @Input() mapContainer!: MapLibreMap;
+
+  popupAnchor: 'left' | 'right' = 'left'; // Default anchor
+  popupOffset: number = 20; // Default offset
+  mouseX: number = 0;
+  mouseY: number = 0;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['lngLat']) {
+      this.updatePopupPosition();
+    }
+
+  }
+
+  updatePopupPosition() {
+    if (this.mapContainer && this.lngLat) {
+      const mapCanvas = this.mapContainer.getCanvasContainer()
+      const mapWidth = mapCanvas.clientWidth;
+      console.log('mapwidth:', mapWidth);
+    }
+  }
 }
