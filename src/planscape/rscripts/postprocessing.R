1a3f2c994b33ec7fcd153deef2e467974f814cf9
George Silva <georger.silva@gmail.com>
2025-06-11 15:57:48 -0300

    change variable name

--- src/planscape/rscripts/postprocessing.R
+++ src/planscape/rscripts/postprocessing.R
@@ -5,7 +5,6 @@ summarize_metrics <- function(forsys_output, stand_data, datalayers) {
     mutate(stand_id = as.integer(stand_id)) |> 
     left_join(stand_data, by="stand_id")
 
-  foo <- stand_output |> group_by(proj_id) |> summarize(across(all_of(fields), sum, .names = "sum_{col}"))
-  foo
+  summarized_metrics <- stand_output |> group_by(proj_id) |> summarize(across(all_of(fields), sum, .names = "sum_{col}"))
+  summarized_metrics
 }
-  
