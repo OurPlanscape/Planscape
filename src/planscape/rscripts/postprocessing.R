fcd3442534389259c18b3de48b67eec35041a66f
George Silva <georger.silva@gmail.com>
2025-06-11 15:53:15 -0300

    fix script

--- src/planscape/rscripts/postprocessing.R
+++ src/planscape/rscripts/postprocessing.R
@@ -2,8 +2,10 @@ summarize_metrics <- function(forsys_output, stand_data, datalayers) {
   fields <- paste0("datalayer_", datalayers[["id"]])
   stand_output <- forsys_output$stand_output |> 
     select(stand_id, proj_id, DoTreat, weightedPriority) |>
+    mutate(stand_id = as.integer(stand_id)) |> 
     left_join(stand_data, by="stand_id")
 
   foo <- stand_output |> group_by(proj_id) |> summarize(across(all_of(fields), sum, .names = "sum_{col}"))
   foo
 }
+  
