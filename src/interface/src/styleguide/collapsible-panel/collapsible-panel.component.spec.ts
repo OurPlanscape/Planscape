4f3a6403d721415c59985de1a3f0a2eb18a78d5e
Pablo Lopez <plopez@sig-gis.com>
2025-07-08 00:38:34 -0300

    rework to use mat-expansion-panel

--- src/interface/src/styleguide/collapsible-panel/collapsible-panel.component.spec.ts
+++ src/interface/src/styleguide/collapsible-panel/collapsible-panel.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CollapsiblePanelComponent } from './collapsible-panel.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CollapsiblePanelComponent', () => {
   let component: CollapsiblePanelComponent;
@@ -8,7 +9,7 @@ describe('CollapsiblePanelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CollapsiblePanelComponent],
+      imports: [CollapsiblePanelComponent, NoopAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CollapsiblePanelComponent);
--- src/interface/src/styleguide/collapsible-panel/collapsible-panel.component.ts
+++ src/interface/src/styleguide/collapsible-panel/collapsible-panel.component.ts
@@ -1,39 +1,28 @@
 import { Component, Input } from '@angular/core';
-import { NgClass, NgIf } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from '@styleguide';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'sg-collapsible-panel',
   standalone: true,
   imports: [
     NgIf,
-    NgClass,
-    MatIconModule,
     MatMenuModule,
     MatButtonModule,
     ButtonComponent,
+    MatExpansionModule,
   ],
   templateUrl: './collapsible-panel.component.html',
   styleUrl: './collapsible-panel.component.scss',
 })
 export class CollapsiblePanelComponent {
-  isOpen = true;
   @Input() title = '';
   @Input() tooltipContent = '';
 
-  // remove after turning on features
   @Input() isCollapsible = true;
-
-  togglePanel(e: Event) {
-    if (this.isCollapsible) {
-      this.isOpen = !this.isOpen;
-    }
-
-    e.preventDefault();
-  }
 
   stopPropagation(e: Event) {
     e.stopPropagation();
