b33ce3880a2340e72bf4e5a603be70aa3a9a0acb
lastminutediorama <mkinsella@sig-gis.com>
2025-07-18 13:27:47 -0500

    fix tests

--- src/interface/src/app/plan/scenario-results/scenario-metrics-legend/scenario-metrics-legend.component.spec.ts
+++ src/interface/src/app/plan/scenario-results/scenario-metrics-legend/scenario-metrics-legend.component.spec.ts
@@ -1,10 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ScenarioResult } from '@types';
 import { ScenarioMetricsLegendComponent } from './scenario-metrics-legend.component';
 
 describe('ScenarioMetricsLegendComponent', () => {
   let component: ScenarioMetricsLegendComponent;
   let fixture: ComponentFixture<ScenarioMetricsLegendComponent>;
+
+  const mockScenarioResult: ScenarioResult = {
+    status: 'PENDING',
+    completed_at: '0',
+    result: {
+      features: [
+        {
+          properties: {
+            area_acres: 10,
+            attainment: { X: 10, Y: 5 },
+          },
+        },
+        {
+          properties: {
+            area_acres: 20,
+            attainment: { X: 20, Y: 10 },
+          },
+        },
+      ] as any,
+      type: 'polygon',
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +35,8 @@ describe('ScenarioMetricsLegendComponent', () => {
 
     fixture = TestBed.createComponent(ScenarioMetricsLegendComponent);
     component = fixture.componentInstance;
+    component.scenarioResult = mockScenarioResult;
+
     fixture.detectChanges();
   });
 
