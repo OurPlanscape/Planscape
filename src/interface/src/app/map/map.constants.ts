bfc068414a300802c6f9d521d1493c49c576956d
lastminutediorama <mkinsella@sig-gis.com>
2025-07-21 12:46:46 -0500

    add bbox for west of mississippi river

--- src/interface/src/app/map/map.constants.ts
+++ src/interface/src/app/map/map.constants.ts
@@ -62,6 +62,21 @@ export const BOUNDARY_LAYER_HOVER_STYLES: L.PathOptions = {
   fillOpacity: 0.5,
   fill: true,
 };
+
+export const MAP_CA_BOUNDS: Extent = [
+  -124.409591, // minLng (west)
+  32.534156, // minLat (south)
+  -114.131211, // maxLng (east)
+  42.009518, // maxLat (north)
+];
+
+export const MAP_WEST_CONUS_BOUNDS: Extent =
+  [-126.0,  // West Lng
+    31.0, // Southern Lat
+  -110.0, // East Lng
+    47.0, // Northernmost Lat
+  ];
+
 export const FrontendConstants = {
   LEAFLET_MAP_INITIAL_ZOOM: 9,
   LEAFLET_MAP_MIN_ZOOM: 7,
@@ -73,12 +88,8 @@ export const FrontendConstants = {
   MAPLIBRE_MAP_MAX_ZOOM: 17,
   MAPLIBRE_MAP_DATA_LAYER_OPACITY: 0.75,
   MAPLIBRE_MAP_DATA_LAYER_TILESIZE: 512,
-  MAPLIBRE_DEFAULT_BOUNDS: [
-    -124.409591, // minLng (west)
-    32.534156, // minLat (south)
-    -114.131211, // maxLng (east)
-    42.009518, // maxLat (north)
-  ] as Extent,
+  MAPLIBRE_DEFAULT_BOUNDS: MAP_WEST_CONUS_BOUNDS as Extent,
+
   MAPLIBRE_BOUND_OPTIONS: {
     padding: { top: 80, bottom: 60, left: 20, right: 20 },
   },
