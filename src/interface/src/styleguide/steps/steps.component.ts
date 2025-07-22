02388f3c0f61e7e047d30632fbcc5436547617fb
Pablo Lopez <plopez@sig-gis.com>
2025-07-22 15:34:10 -0300

    Using CDK versions for steps, as well as a demo of a wizard without using cdk

--- src/interface/src/styleguide/steps/steps.component.ts
+++ src/interface/src/styleguide/steps/steps.component.ts
@@ -1,45 +1,89 @@
-import {
-  Component,
-  ContentChildren,
-  QueryList,
-  TemplateRef,
-  ViewChild,
-} from '@angular/core';
-import { MatStepper, MatStepperModule } from '@angular/material/stepper';
-import { CommonModule, NgComponentOutlet } from '@angular/common';
-import { StepTemplateDirective } from './step.directive';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '@styleguide';
+import { CdkStepper, CdkStepperModule } from '@angular/cdk/stepper';
+import { Observable } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 
+export interface Step {
+  form: FormGroup;
+}
+
+/**
+ * Steps component implementing [CDKStepper](https://v16.material.angular.dev/cdk/stepper/overview).
+ * Similar to the Angular Material component, it manages flow based on individual
+ * [forms for each step](https://v16.material.angular.dev/components/stepper/overview#using-a-different-form-for-each-step).
+ *
+ * If you provide a step with a form, it checks its validity to figure out if we can go to the next step.
+ * You can make steps optional by setting your own form validity.
+ *
+ * ```
+ * <sg-steps>
+ *   <cdk-step>Simple step, no form</cdk-step>
+ *   <cdk-step [stepControl]='step1.form'><step-1 #step1></step-1></cdk-step>
+ *   <cdk-step [stepControl]='step2.form'><step-2 #step2></step-1></cdk-step>
+ * </sg-steps>
+ *```
+ */
 @Component({
   selector: 'sg-steps',
   standalone: true,
-  imports: [MatStepperModule, NgComponentOutlet, CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, CdkStepperModule],
+  providers: [{ provide: CdkStepper, useExisting: StepsComponent }],
   templateUrl: './steps.component.html',
   styleUrl: './steps.component.scss',
 })
-export class StepsComponent {
-  @ContentChildren(StepTemplateDirective)
-  stepDirs!: QueryList<StepTemplateDirective>;
-  @ViewChild(MatStepper) stepper!: MatStepper;
+export class StepsComponent<T> extends CdkStepper {
+  @Input() backLabel = 'Back';
+  @Input() continueLabel = 'Save & Continue';
+  @Input() finishLabel = 'finish';
+  @Output() saveData = new EventEmitter<Partial<T>>();
 
-  steps: { label: string; template: TemplateRef<any> }[] = [];
+  savingStep = false;
+  // not sure if we need this, added just to demo
+  data!: Partial<T>;
 
-  ngAfterContentInit() {
-    this.steps = this.stepDirs.map((dir) => ({
-      label: dir.label,
-      template: dir.template,
-    }));
+  // I don't love this...
+  // alt would be to use a service but how do I provide the right
+  // dependencies on the constructor to extend CdkStepper?
+  @Input() save?: (data: Partial<T>) => Observable<boolean>;
+
+  goNext(): void {
+    // this would be the key to interact - use the forms
+    const control = this.selected?.stepControl;
+
+    // if no control go ahead
+    if (!control) {
+      this.next();
+      return;
+    }
+
+    if (control.valid) {
+      // either save the data here...
+      this.data = { ...this.data, ...control.value };
+      // what if saving needs to be async?
+      if (this.save) {
+        this.savingStep = true;
+        this.save(control.value).subscribe((result) => {
+          if (result) {
+            this.next();
+            this.savingStep = false;
+          } else {
+            // TODO - provide errors back to the step itself
+            // control.setErrors({name...:result...})
+          }
+        });
+      } else {
+        // or just push it on an output ?
+        this.saveData.emit(control.value);
+        this.next();
+      }
+    } else {
+      this.selected?.stepControl.markAsDirty();
+    }
   }
 
-  goNext() {
-    this.stepper.next();
-  }
-
-  goBack() {
-    this.stepper.previous();
-  }
-
-  get currentStep() {
-    return this.stepper?.selectedIndex + 1;
+  goBack(): void {
+    this.previous();
   }
 }
--- src/interface/src/styleguide/steps/steps.stories.ts
+++ src/interface/src/styleguide/steps/steps.stories.ts
@@ -6,12 +6,72 @@ import {
 } from '@storybook/angular';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { StepsComponent } from './steps.component';
-import { MatStepperModule } from '@angular/material/stepper';
+import { Step, StepsComponent } from './steps.component';
 import { CommonModule } from '@angular/common';
-import { StepTemplateDirective } from './step.directive';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { delay, of } from 'rxjs';
 
-const meta: Meta<StepsComponent> = {
+// interface just for testing
+interface Person {
+  name: string;
+  age: number;
+  email?: string;
+}
+
+@Component({
+  selector: 'sg-step-demo-1',
+  template: `
+    <form [formGroup]="form">
+      Name : <input type="text" id="name" formControlName="name" />
+      <div *ngIf="!form.valid && form.dirty">not valid</div>
+    </form>
+  `,
+})
+export class MyStep1Component implements Step {
+  form: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+  });
+}
+
+@Component({
+  selector: 'sg-step-demo-2',
+  template: `
+    <form [formGroup]="form">
+      Age : <input type="text" id="name" formControlName="age" />
+      <div *ngIf="!form.valid && form.dirty">not valid</div>
+    </form>
+  `,
+})
+export class MyStep2Component implements Step {
+  form: FormGroup = new FormGroup({
+    age: new FormControl('', Validators.required),
+  });
+}
+
+@Component({
+  selector: 'sg-step-demo-3',
+  template: `
+    <form [formGroup]="form">
+      email (optional) :
+      <input type="text" id="email" formControlName="email" />
+      <div *ngIf="!form.valid && form.dirty">not valid</div>
+    </form>
+  `,
+})
+export class MyStep3Component implements Step {
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.email),
+  });
+}
+
+const meta: Meta<StepsComponent<Person>> = {
   title: 'Components/Steps',
   component: StepsComponent,
   decorators: [
@@ -19,23 +79,39 @@ const meta: Meta<StepsComponent> = {
       providers: [provideAnimations()],
     }),
     moduleMetadata({
-      imports: [MatStepperModule, CommonModule, StepTemplateDirective],
+      declarations: [MyStep1Component, MyStep2Component, MyStep3Component],
+      imports: [CommonModule, CdkStepperModule, ReactiveFormsModule],
     }),
   ],
   tags: ['autodocs'],
   render: ({ ...args }) => ({
-    props: args,
-    template: `<sg-steps ${argsToTemplate(args)}>
-     <ng-template sgStep='Step 1'>Step 1 content</ng-template>
-      <ng-template sgStep='Step 2'>Step 2 content</ng-template>
-      <ng-template sgStep='Step 3'>Step 2 content</ng-template>
-      <ng-template sgStep='Step 4'>Step 2 content</ng-template>
-</sg-steps>`,
+    props: {
+      ...args,
+      index: 0,
+      saveData: (data: Partial<Person>) => {
+        return of(true).pipe(delay(1000)); // simulate a successful save
+      },
+    },
+    template: `
+<section style='height: 200px; background-color: #d0d0d0'>
+  <div>Index is {{index || 0}}</div>
+  <sg-steps ${argsToTemplate(args)} linear (selectedIndexChange)='index = $event'>
+    <cdk-step>
+      <div style='line-height: 40px;'>
+        Some <br> long <br>  step <br>  that <br>  does <br>  not <br>  have a  <br> form
+      </div>
+    </cdk-step>
+    <cdk-step [stepControl]='step1.form'><sg-step-demo-1 #step1></sg-step-demo-1></cdk-step>
+    <cdk-step [stepControl]='step3.form' > <sg-step-demo-3 #step3></sg-step-demo-3></cdk-step>
+    <cdk-step [stepControl]='step2.form'><sg-step-demo-2 #step2></sg-step-demo-2></cdk-step>
+    <cdk-step>Bye</cdk-step>
+  </sg-steps>
+</section>`,
   }),
 };
 
 export default meta;
-type Story = StoryObj<StepsComponent>;
+type Story = StoryObj<StepsComponent<Person>>;
 
 export const Default: Story = {
   args: {},
