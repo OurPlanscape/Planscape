a177ac12c8d7a4d0c7186aa0b311c5ebcb882d17
Pablo Lopez <plopez@sig-gis.com>
2025-07-28 15:58:30 -0300

    add steps and tabs to new scenario

--- src/interface/src/app/scenario/scenario-creation/scenario-creation.component.spec.ts
+++ src/interface/src/app/scenario/scenario-creation/scenario-creation.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScenarioCreationComponent } from './scenario-creation.component';
+import { MockDeclarations } from 'ng-mocks';
+import { DataLayersComponent } from '../../data-layers/data-layers/data-layers.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ScenarioCreationComponent', () => {
   let component: ScenarioCreationComponent;
@@ -8,7 +11,8 @@ describe('ScenarioCreationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ScenarioCreationComponent],
+      imports: [ScenarioCreationComponent, NoopAnimationsModule],
+      declarations: [MockDeclarations(DataLayersComponent)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ScenarioCreationComponent);
--- src/interface/src/app/scenario/scenario-creation/scenario-creation.component.ts
+++ src/interface/src/app/scenario/scenario-creation/scenario-creation.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { MatLegacyButtonModule } from '@angular/material/legacy-button';
+import { DataLayersComponent } from '../../data-layers/data-layers/data-layers.component';
+import { StepsComponent } from '../../../styleguide/steps/steps.component';
+import { CdkStepperModule } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-scenario-creation',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule,
+    AsyncPipe,
+    MatLegacyButtonModule,
+    NgIf,
+    DataLayersComponent,
+    StepsComponent,
+    CdkStepperModule,
+  ],
   templateUrl: './scenario-creation.component.html',
   styleUrl: './scenario-creation.component.scss',
 })
