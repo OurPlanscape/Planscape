ce52b36e2f4a61e6a91f1dc155552ea918b99dec
lastminutediorama <mkinsella@sig-gis.com>
2025-08-01 10:01:06 -0500

    fix tests

--- src/interface/src/app/scenario/scenario-creation/scenario-creation.component.spec.ts
+++ src/interface/src/app/scenario/scenario-creation/scenario-creation.component.spec.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 import { FormControl, ValidationErrors } from '@angular/forms';
 import { TreatmentGoalsComponent } from '../treatment-goals/treatment-goals.component';
 import { StandSizeComponent } from '../stand-size/stand-size.component';
+import { ScenarioState } from 'src/app/maplibre-map/scenario.state';
 
 describe('ScenarioCreationComponent', () => {
   let component: ScenarioCreationComponent;
@@ -28,6 +29,9 @@ describe('ScenarioCreationComponent', () => {
           } as any,
         }),
         MockProvider(ScenarioService),
+        MockProvider(ScenarioState, {
+          excludedAreas$: of([]),
+        }),
         MockProvider(DataLayersStateService, {
           paths$: of([]),
         }),
