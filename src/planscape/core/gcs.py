172ce23b9453fd87c944ee6351904c77d6860e70
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-08-01 18:38:52 -0300

    doc updated

--- src/planscape/core/gcs.py
+++ src/planscape/core/gcs.py
@@ -123,8 +123,6 @@ def upload_file_via_cli(object_name: str, input_file: str):
     Args:
         object_name (str): The name of the object in GCS.
         input_file (str): The path to the local file to upload.
-        url (str): The URL to upload the file to.
-        chunk_size (int): The size of each chunk to upload.
     """
     logger.info(f"Uploading file {object_name} via CLI.")
     storage_client = storage.Client()
