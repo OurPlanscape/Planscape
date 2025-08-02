f9087746d86c308034027b45603ad0dda4ae2251
lastminutediorama <mkinsella@sig-gis.com>
2025-08-02 17:03:33 -0500

    slightly different form approach

--- src/interface/src/app/scenario/excluded-areas/excluded-areas.component.ts
+++ src/interface/src/app/scenario/excluded-areas/excluded-areas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SectionComponent } from 'src/styleguide/collapsible-panel/section.component';
 import { CommonModule } from '@angular/common';
 import {
@@ -10,6 +10,12 @@ import {
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ScenarioState } from 'src/app/maplibre-map/scenario.state';
 import { tap } from 'rxjs';
+
+interface ExcludedArea {
+  key: number;
+  label: string;
+  id: number;
+}
 
 @Component({
   selector: 'app-excluded-areas',
@@ -23,17 +29,48 @@ import { tap } from 'rxjs';
   templateUrl: './excluded-areas.component.html',
   styleUrl: './excluded-areas.component.scss',
 })
-export class ExcludedAreasComponent {
+export class ExcludedAreasComponent implements OnInit {
   constructor(private scenarioState: ScenarioState) {}
+
+  excludedAreas: ExcludedArea[] = [];
+
   excludedAreas$ = this.scenarioState.excludedAreas$.pipe(
     tap((areas) => (this.excludedAreas = areas))
   );
-  excludedAreas: { key: number; label: string; id: number }[] = [];
 
   form: FormGroup = new FormGroup({
-    // TODO: use types for this form
-    selectedQuestion: new FormControl<string[] | null>(null, [
+    excludedAreas: new FormControl<string[] | null>(null, [
       Validators.required,
     ]),
   });
+
+  ngOnInit() {
+    this.scenarioState.excludedAreas$.subscribe((areas) => {
+      this.excludedAreas = areas;
+      this.createForm();
+    });
+  }
+
+  private createForm() {
+    this.form = new FormGroup({
+      excluded_areas: new FormControl([]),
+    });
+  }
+
+  isChecked(key: number): boolean {
+    const excludedAreas = this.form.get('excluded_areas')?.value || [];
+    return excludedAreas.includes(key);
+  }
+
+  onCheckboxChange(key: number, event: any) {
+    const excludedAreas = this.form.get('excluded_areas')?.value || [];
+
+    if (event.checked) {
+      this.form.get('excluded_areas')?.setValue([...excludedAreas, key]);
+    } else {
+      this.form
+        .get('excluded_areas')
+        ?.setValue(excludedAreas.filter((k: number) => k !== key));
+    }
+  }
 }
