4b8dacfd9be862bd5a10ca5b15150047c5adb7af
George Silva <863039+george-silva@users.noreply.github.com>
2025-08-13 18:03:34 -0300

    commit functions (#2858)

--- src/planscape/planning/migrations/0046_alter_treatmentgoal_geometry.py
+++ src/planscape/planning/migrations/0046_alter_treatmentgoal_geometry.py
@@ -2,12 +2,31 @@ import django.contrib.gis.db.models.fields
 from django.db import migrations
 
 
+def noop(apps, schema_editor):
+    pass
+
+
+def set_none(apps, schema_editor):
+    from planning.models import TreatmentGoal
+
+    TreatmentGoal.objects.update(geometry=None)
+
+
+def set_geometry(apps, schema_editor):
+    from planning.models import TreatmentGoal
+
+    for t in TreatmentGoal.objects.filter(active=True):
+        t.geometry = t.get_coverage()
+        t.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("planning", "0045_auto_20250811_1507"),
     ]
 
     operations = [
+        migrations.RunPython(set_none, noop),
         migrations.AlterField(
             model_name="treatmentgoal",
             name="geometry",
@@ -17,4 +36,5 @@ class Migration(migrations.Migration):
                 srid=4269,
             ),
         ),
+        migrations.RunPython(set_geometry, noop),
     ]
