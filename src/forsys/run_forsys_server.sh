5718f082621db58d049712420e73f3839c0ccaca
Gunjan Dayani <dgh007786007@gmail.com>
2025-08-13 16:22:12 -0400

    undo changed in script

--- src/forsys/run_forsys_server.sh
+++ src/forsys/run_forsys_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-Rscript --vanilla -e "library(plumber); plumb('server.R') %>% pr_run(host='0.0.0.0', port=$1)"
+Rscript --vanilla -e "library(plumber); pr('server.R') %>% pr_run(port=$1)"
 
 exit 0
