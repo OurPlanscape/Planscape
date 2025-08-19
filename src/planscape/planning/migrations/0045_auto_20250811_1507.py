f5e92f557d2bf0b27a796a3a614d055e81807688
Zalo <gcrivelli@sig-gis.com>
2025-08-19 10:55:38 -0400

    plan-bug-pass-migration-0045

--- src/planscape/planning/migrations/0045_auto_20250811_1507.py
+++ src/planscape/planning/migrations/0045_auto_20250811_1507.py
@@ -2,17 +2,7 @@ from django.db import migrations
 
 
 def forward(apps, schema_editor):
-    TreatmentGoal = apps.get_model("planning", "TreatmentGoal")
-    from django.contrib.gis.geos import MultiPolygon, Polygon
-
-    for t in TreatmentGoal.objects.all():
-        geom = t.datalayers.all().geometric_intersection()
-        if not geom:
-            continue
-        if isinstance(geom, Polygon):
-            geom = MultiPolygon(geom)
-        t.geometry = geom
-        t.save(update_fields=["geometry"])
+    pass
 
 
 def backward(apps, schema_editor):
