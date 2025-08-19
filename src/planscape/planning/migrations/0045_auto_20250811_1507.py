1cf7b9f7f9b844c05c09c4e9e08cf75f46e85c8b
Zalo <gcrivelli@sig-gis.com>
2025-08-19 10:30:01 -0400

    0045 coerce single Polygon intersections to MultiPolygon

--- src/planscape/planning/migrations/0045_auto_20250811_1507.py
+++ src/planscape/planning/migrations/0045_auto_20250811_1507.py
@@ -2,11 +2,17 @@ from django.db import migrations
 
 
 def forward(apps, schema_editor):
-    from planning.models import TreatmentGoal
+    TreatmentGoal = apps.get_model("planning", "TreatmentGoal")
+    from django.contrib.gis.geos import MultiPolygon, Polygon
 
     for t in TreatmentGoal.objects.all():
-        t.geometry = t.datalayers.all().geometric_intersection()  # type: ignore
-        t.save()
+        geom = t.datalayers.all().geometric_intersection()
+        if not geom:
+            continue
+        if isinstance(geom, Polygon):
+            geom = MultiPolygon(geom)
+        t.geometry = geom
+        t.save(update_fields=["geometry"])
 
 
 def backward(apps, schema_editor):
@@ -14,8 +20,5 @@ def backward(apps, schema_editor):
 
 
 class Migration(migrations.Migration):
-    dependencies = [
-        ("planning", "0044_add_secondary_metrics"),
-    ]
-
+    dependencies = [("planning", "0044_add_secondary_metrics")]
     operations = [migrations.RunPython(forward, backward)]
