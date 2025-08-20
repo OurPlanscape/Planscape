e466171afbc146af14bd4d6ac088699ff14b60fb
lastminutediorama <mkinsella@sig-gis.com>
2025-08-20 13:06:38 -0500

    initial updates

--- src/interface/cypress/e2e/map.spec.cy.ts
+++ src/interface/cypress/e2e/map.spec.cy.ts
@@ -3,40 +3,53 @@ import { urls } from '../support/util';
 describe('Map', () => {
   it('Shows welcome page', () => {
     cy.visit(urls.MAP);
-    cy.url().should('include', '/map');
-    cy.get('app-map-control-panel').should('exist');
+    cy.url().should('include', '/explore');
+    cy.get('app-multi-map-control').should('exist');
   });
   it('Switches number of maps', () => {
     cy.visit(urls.MAP);
-    cy.get('.map').should('be.visible');
+    cy.get('.maps').should('be.visible');
     cy.get('[aria-label="Show 1 map"]').click();
-    let maps = cy.get('.map:visible');
-    maps.should('have.length', 1);
+    cy.get('.maps app-explore-map').should('have.length', 1);
 
     cy.get('[aria-label="Show 2 maps"]').click();
-    maps = cy.get('.map:visible');
-    maps.should('have.length', 2);
+    cy.get('.maps app-explore-map').should('have.length', 2);
 
     cy.get('[aria-label="Show 4 maps"]').click();
-    maps = cy.get('.map:visible');
-    maps.should('have.length', 4);
+    cy.get('.maps app-explore-map').should('have.length', 4);
   });
 
   it('should show the correct basemap map layer when changing basemap', () => {
     cy.visit(urls.MAP);
-    cy.get('.mat-radio-button').contains('Road').click();
-    cy.get('img.leaflet-tile')
-      .invoke('attr', 'src')
-      .should('contain', 'tiles.stadiamaps.com/tiles/alidade_smooth/');
+    cy.intercept('GET', '**/tiles.mapbox.com/**').as('tiles');
+    cy.intercept('GET', '**mapbox**', (req: any) => {
+      if (req.url.includes('mapbox')) {
+        console.log('Mapbox request:', req.url);
+      }
+    }).as('any');
+    cy.get('app-map-base-dropdown').click();
+    cy.get('[aria-label="Select road base map"]').should('be.visible');
+    cy.get('[aria-label="Select road base map"]').click();
+    cy.wait('@tiles').then((interception: any) => {
+      // Check if the tile URL indicates road base map
+      expect(interception.request.url).to.contain('road') // or whatever identifier
+    });
 
-    cy.get('.mat-radio-button').contains('Terrain').click();
-    cy.get('img.leaflet-tile')
-      .invoke('attr', 'src')
-      .should('contain', 'World_Terrain_Base/MapServer');
+    // cy.get('[aria-label="Select road base map"]').click();
 
-    cy.get('.mat-radio-button').contains('Satellite').click();
-    cy.get('img.leaflet-tile')
-      .invoke('attr', 'src')
-      .should('contain', 'World_Imagery/MapServer');
+
+    // cy.get('img.leaflet-tile')
+    //   .invoke('attr', 'src')
+    //   .should('contain', 'tiles.stadiamaps.com/tiles/alidade_smooth/');
+
+    // cy.get('[aria-label="Select terrain base map"]').click();
+    // cy.get('img.leaflet-tile')
+    //   .invoke('attr', 'src')
+    //   .should('contain', 'World_Terrain_Base/MapServer');
+
+    // cy.get('[aria-label="Select satellite base map"]').click();
+    // cy.get('img.leaflet-tile')
+    //   .invoke('attr', 'src')
+    //   .should('contain', 'World_Imagery/MapServer');
   });
 });
