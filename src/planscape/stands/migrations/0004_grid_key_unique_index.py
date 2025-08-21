540f9fd0f077b916e13952a2a7b6282066c875e4
Zalo <gcrivelli@sig-gis.com>
2025-08-21 14:55:46 -0400

    plan-2678-pr-1-of-3-add-grid-key-and-backfill

---
+++ src/planscape/stands/migrations/0004_grid_key_unique_index.py
@@ -0,0 +1,22 @@
+# src/planscape/stands/migrations/0004_grid_key_unique_index.py
+
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    atomic = False
+
+    dependencies = [("stands", "0003_stand_grid_key")]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS
+                stands_stand_grid_key_index
+                ON public.stands_stand(grid_key);
+            """,
+            reverse_sql="""
+                DROP INDEX CONCURRENTLY IF EXISTS
+                stands_stand_grid_key_index;
+            """,
+        ),
+    ]
