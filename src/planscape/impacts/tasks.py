af30749971551810e5b2d969a9b1f98602cae735
George Silva <georger.silva@gmail.com>
2025-08-21 16:45:02 -0300

    adds email to tracking

--- src/planscape/impacts/tasks.py
+++ src/planscape/impacts/tasks.py
@@ -4,10 +4,14 @@ from urllib.parse import urljoin
 
 from celery import chain, chord
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.db import transaction
 from django.template.loader import render_to_string
 from django.utils import timezone
+from rasterio.errors import RasterioIOError
+
+User = get_user_model()
 from impacts.models import (
     AVAILABLE_YEARS,
     ImpactVariable,
@@ -21,8 +25,6 @@ from impacts.services import (
     get_calculation_matrix,
     get_calculation_matrix_wo_action,
 )
-from rasterio.errors import RasterioIOError
-
 from planscape.celery import app
 from planscape.openpanel import track_openpanel
 
@@ -138,6 +140,7 @@ def async_set_status(
     this is used as a callback in celery canvas.
     """
     with transaction.atomic():
+        user = User.objects.get(pk=user_id)
         try:
             treatment_plan = TreatmentPlan.objects.select_for_update().get(
                 pk=treatment_plan_pk
@@ -149,7 +152,11 @@ def async_set_status(
             log.info(f"Treatment plan {treatment_plan_pk} changed status to {status}.")
             track_openpanel(
                 name="impacts.treatment_plan.status_changed",
-                properties={"treatment_plan": treatment_plan_pk, "status": status},
+                properties={
+                    "treatment_plan": treatment_plan_pk,
+                    "status": status,
+                    "email": user.email if user.email else None,
+                },
                 user_id=user_id,
             )
         except TreatmentPlan.DoesNotExist:
