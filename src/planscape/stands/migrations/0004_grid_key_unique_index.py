ec785dd50fb91174b367666fb8c4acc492bd4632
Zalo <gcrivelli@sig-gis.com>
2025-08-28 16:52:22 -0400

    migrations-sequential-order

--- src/planscape/stands/migrations/0004_grid_key_unique_index.py
+++ src/planscape/stands/migrations/0004_grid_key_unique_index.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
     atomic = False
 
-    dependencies = [("stands", "0005_backfill_grid_key_batches")]
+    dependencies = [("stands", "0003_stand_grid_key")]
 
     operations = [
         migrations.RunSQL(
