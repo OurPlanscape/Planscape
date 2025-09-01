92c4b1cb93ab5dcbdf4a7f35aceb7c870b88c54e
George Silva <georger.silva@gmail.com>
2025-09-01 14:37:22 -0300

    creates index conc

--- src/planscape/stands/migrations/0008_auto_20250901_1722.py
+++ src/planscape/stands/migrations/0008_auto_20250901_1722.py
@@ -1,13 +1,15 @@
+from django.contrib.postgres.operations import AddIndexConcurrently
 from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
+    atomic = False
     dependencies = [
         ("stands", "0007_auto_20250901_1639"),
     ]
 
     operations = [
-        migrations.AddIndex(
+        AddIndexConcurrently(
             model_name="standmetric",
             index=models.Index(
                 name="standmetrics_majority_partial_index",
