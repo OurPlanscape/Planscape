5bfdc51acf0ec4c1df07a65025706571f463cb38
George Silva <863039+george-silva@users.noreply.github.com>
2025-09-05 16:28:43 -0300

    module endpoint (#3006)

--- src/planscape/modules/views.py
+++ src/planscape/modules/views.py
@@ -3,7 +3,6 @@ from typing import Any, Dict
 from rest_framework.exceptions import NotFound
 from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.response import Response
-from rest_framework.serializers import BaseSerializer
 from rest_framework.viewsets import GenericViewSet
 
 from modules.serializers import ModuleSerializer
@@ -14,10 +13,6 @@ class ModuleViewSet(RetrieveModelMixin, GenericViewSet):
     lookup_field = "pk"
     serializer_class = ModuleSerializer
 
-    def get_serializer(self, *args: Any, **kwargs: Any) -> BaseSerializer:
-        module_name = str(self.kwargs.get(self.lookup_field))
-        return ModuleSerializer(module_name=module_name)
-
     def get_object(self) -> Dict[str, Any]:
         pk = str(self.kwargs.get(self.lookup_field))
         try:
@@ -26,7 +21,8 @@ class ModuleViewSet(RetrieveModelMixin, GenericViewSet):
             raise NotFound(f"No item with key '{pk}'")
 
     def retrieve(self, request, *args, **kwargs):
+        pk = str(self.kwargs.get(self.lookup_field))
         payload = self.get_object()
         # this is passed as SerializerClass(data=payload)
-        serializer = self.get_serializer(data=payload)
+        serializer = ModuleSerializer(data=payload, module_name=pk)
         return Response(serializer.data)
