aa6af671a1e23ad4eb2c4f50044e81b4460f58dd
George Silva <863039+george-silva@users.noreply.github.com>
2025-09-05 15:14:42 -0300

    creates serializers, viewset and urls for modules (#2997)

---
+++ src/planscape/modules/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+
+class OptionDataLayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            "id",
+            "name",
+            "info",
+        )
+
+
+class OptionThresholdsSerializer(serializers.Serializer):
+    slope = OptionDataLayerSerializer()
+
+    distance_from_roads = OptionDataLayerSerializer()
+
+
+class ForsysOptionsSerializer(serializers.Serializer):
+    inclusions = serializers.ListField(child=OptionDataLayerSerializer())
+    exclusions = serializers.ListField(child=OptionDataLayerSerializer())
+    thresholds = OptionThresholdsSerializer()
+
+
+OPTIONS_SERIALIZERS = {
+    "forsys": ForsysOptionsSerializer,
+}
+
+
+class ModuleSerializer(serializers.Serializer):
+    def get_options_serializer(self, module_name: str) -> serializers.Serializer:
+        return OPTIONS_SERIALIZERS[module_name]()
+
+    def __init__(self, *args, module_name: str, **kwargs):
+        self.fields["options"] = self.get_options_serializer(module_name)
+
+    name = serializers.CharField()
---
+++ src/planscape/modules/views.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict
+
+from rest_framework.exceptions import NotFound
+from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.response import Response
+from rest_framework.serializers import BaseSerializer
+from rest_framework.viewsets import GenericViewSet
+
+from modules.serializers import ModuleSerializer
+from modules.services import get_module
+
+
+class ModuleViewSet(RetrieveModelMixin, GenericViewSet):
+    lookup_field = "pk"
+    serializer_class = ModuleSerializer
+
+    def get_serializer_context(self):
+        base = super().get_serializer_context()
+        return {**base, "module_name": self.kwargs.get(self.lookup_field)}
+
+    def get_serializer(self, *args: Any, **kwargs: Any) -> BaseSerializer:
+        return super().get_serializer(*args, **kwargs)
+
+    def get_object(self) -> Dict[str, Any]:
+        pk = str(self.kwargs.get(self.lookup_field))
+        try:
+            return get_module(module_name=pk)
+        except KeyError:
+            raise NotFound(f"No item with key '{pk}'")
+
+    def retrieve(self, request, *args, **kwargs):
+        payload = self.get_object()
+        # this is passed as SerializerClass(data=payload)
+        serializer = self.get_serializer(data=payload)
+        return Response(serializer.data)
