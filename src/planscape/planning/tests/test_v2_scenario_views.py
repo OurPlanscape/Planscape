c61258b76a3a79d63a089c9b533f794088f365c9
lastminutediorama <mkinsella@sig-gis.com>
2025-09-10 09:20:40 -0500

    add usage-types to scenariov2

--- src/planscape/planning/tests/test_v2_scenario_views.py
+++ src/planscape/planning/tests/test_v2_scenario_views.py
@@ -3,11 +3,17 @@ from unittest import mock
 
 from datasets.models import DataLayerType, GeometryType
 from datasets.tests.factories import DataLayerFactory
+from django.contrib.gis.geos import GEOSGeometry, MultiPolygon
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APITransactionTestCase
 
-from planning.models import Scenario, ScenarioResult, ScenarioVersion
+from planning.models import (
+    Scenario,
+    ScenarioResult,
+    ScenarioVersion,
+    TreatmentGoalUsesDataLayer,
+)
 from planning.tests.factories import (
     PlanningAreaFactory,
     ProjectAreaFactory,
@@ -658,6 +664,76 @@ class ScenarioDetailTest(APITestCase):
         data = response.json()
         self.assertEqual(response.status_code, 200)
         self.assertIsNone(data.get("geopackage_url"))
+
+    def test_detail_scenario_v2_no_usage_types(self):
+        self.scenario.save()
+        self.client.force_authenticate(self.owner_user)
+        response = self.client.get(
+            reverse(
+                "api:planning:scenarios-detail",
+                kwargs={
+                    "pk": self.scenario.pk,
+                },
+            ),
+            content_type="application/json",
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data.get("usage_types"), [])
+
+    def test_detail_scenario_v2_with_usage_types(self):
+        self.poly1 = GEOSGeometry("POLYGON((0 0, 1 0, 1 1, 0 1, 0 0))", srid=4269)
+        self.poly2 = GEOSGeometry(
+            "POLYGON((0.5 0.5, 1.5 0.5, 1.5 1.5, 0.5 1.5, 0.5 0.5))", srid=4269
+        )
+        self.mpoly1 = GEOSGeometry(
+            "MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)))", srid=4269
+        )
+        self.mpoly2 = GEOSGeometry(
+            "MULTIPOLYGON(((0.5 0.5, 1.5 0.5, 1.5 1.5, 0.5 1.5, 0.5 0.5)))", srid=4269
+        )
+        self.dl1 = DataLayerFactory.create(
+            name="Layer 1",
+            geometry=self.poly1,
+            outline=self.mpoly1,
+        )
+        self.dl2 = DataLayerFactory.create(
+            name="Layer 2",
+            geometry=self.poly2,
+            outline=self.mpoly2,
+        )
+        self.usage1 = TreatmentGoalUsesDataLayer.objects.create(
+            usage_type="PRIORITY",
+            datalayer=self.dl1,
+            treatment_goal=self.treatment_goal,
+        )
+        self.usage2 = TreatmentGoalUsesDataLayer.objects.create(
+            usage_type="THRESHOLD",
+            datalayer=self.dl2,
+            treatment_goal=self.treatment_goal,
+        )
+        self.scenario.treatment_goal = self.treatment_goal
+        self.scenario.save()
+
+        self.client.force_authenticate(self.owner_user)
+        response = self.client.get(
+            reverse(
+                "api:planning:scenarios-detail",
+                kwargs={
+                    "pk": self.scenario.pk,
+                },
+            ),
+            content_type="application/json",
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertCountEqual(
+            data.get("usage_types"),
+            [
+                {"usage_type": "PRIORITY", "datalayer": "Layer 1"},
+                {"usage_type": "THRESHOLD", "datalayer": "Layer 2"},
+            ],
+        )
 
 
 class PatchScenarioConfigurationTest(APITransactionTestCase):
