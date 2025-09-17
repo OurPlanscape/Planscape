e345aeec21b023a5f6032ec3bfad40a4fd17d3f6
George Silva <georger.silva@gmail.com>
2025-09-17 09:45:49 -0300

    remove openpanel client

--- src/planscape/planscape/openpanel.py
+++ src/planscape/planscape/openpanel.py
@@ -4,19 +4,8 @@ from typing import Any, Dict, Optional, Union
 from core.tasks import track
 from django.conf import settings
 from django.contrib.auth.models import User
-from openpanel import OpenPanel
 
 log = logging.getLogger(__name__)
-
-
-def get_openpanel(user_id: Optional[Union[str, int]] = None) -> Optional[OpenPanel]:
-    from django.conf import settings
-
-    op = settings.OPENPANEL_CLIENT
-    log.info(f"We have OpenPanel client {op}")
-    if user_id is not None:
-        op.profile_id = str(user_id)
-    return op
 
 
 def get_domain(email: str) -> str:
@@ -49,17 +38,20 @@ def track_openpanel(
 
 
 def identify_openpanel(user: User) -> None:
-    profile_id = str(user.pk)
-    op = get_openpanel()
-    if op:
-        log.info("openpanel client available for identify.")
-        traits = {
-            "firstName": user.first_name,
-            "lastName": user.last_name,
-            "email": user.email,
+    payload = {
+        "type": "identify",
+        "payload": {
+            "profileId": str(user.pk),
+            "traits": {
+                "firstName": user.first_name,
+                "lastName": user.last_name,
+                "email": user.email,
+            },
             "properties": {
-                "organization": None,
+                "organizatio": None,
                 "last_login": user.last_login,
             },
-        }
-        op.identify(profile_id=profile_id, traits=traits)
+        },
+    }
+    if not settings.TESTING_MODE:
+        track.delay(payload=payload)
