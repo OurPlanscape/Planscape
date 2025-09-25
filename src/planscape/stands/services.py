c0aa58a1c44496ae7578274b10f02623c357ac1f
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-09-25 12:18:40 -0300

    Fix stand_vector_stats3 query

--- src/planscape/stands/services.py
+++ src/planscape/stands/services.py
@@ -102,7 +102,7 @@ def calculate_stand_vector_stats3(
     datalayer: DataLayer,
     planning_area_geometry: GEOSGeometry,
     stand_size: StandSizeChoices,
-    grid_key_start: str = "",
+    grid_key_start: str,
 ):
     stands = Stand.objects.all().within_polygon(planning_area_geometry, stand_size)
     stand_ids = set(stands.all().values_list("id", flat=True))
@@ -111,6 +111,8 @@ def calculate_stand_vector_stats3(
     )
     existing_stand_ids = set(existing_metrics.all().values_list("id", flat=True))
     missing_stand_ids = stand_ids - existing_stand_ids
+
+    grid_key_start = f"{grid_key_start}%" if grid_key_start else "%"
 
     if len(missing_stand_ids) <= 0:
         log.info("There are no missing stands. Early return.")
@@ -127,7 +129,7 @@ def calculate_stand_vector_stats3(
         FROM stands_stand s
         WHERE
             s.id IN %s
-            AND grid_key LIKE %s%
+            AND grid_key LIKE %s
     )
     INSERT INTO stands_standmetric (created_at, stand_id, datalayer_id, majority)
     SELECT
