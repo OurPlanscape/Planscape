ea076eb6984a027b4cb8819b1f241f2009413e68
George Silva <georger.silva@gmail.com>
2025-10-07 11:02:28 -0300

    sleep per planning_area

--- src/planscape/planning/management/commands/prepare_planning_areas.py
+++ src/planscape/planning/management/commands/prepare_planning_areas.py
@@ -1,30 +1,40 @@
 import logging
+import time
 from typing import Any
 
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 
 from planning.models import PlanningArea, PlanningAreaMapStatus
+from planning.services import get_acreage
 from planning.tasks import prepare_planning_area
 
 log = logging.getLogger(__name__)
 
+ACRES_PER_SECOND = 6_000
+
 
 class Command(BaseCommand):
+    def get_sleep_timer(self, planning_area) -> float:
+        acreage = get_acreage(planning_area.geometry)
+        return acreage / ACRES_PER_SECOND
+
     def handle(self, *args: Any, **options: Any) -> str | None:
         planning_areas = PlanningArea.objects.exclude(
             map_status=PlanningAreaMapStatus.DONE
         )
+        total_pas = planning_areas.objects.count()
+        self.stdout.write(f"Calculating stand metrics for {total_pas}.")
         for planning_area in planning_areas:
-            start = timezone.now()
-            self.stdout.write(f"Preparing Planning Area {planning_area.pk} - {start}")
+            self.stdout.write(f"Preparing Planning Area {planning_area.pk}")
+            sleep = self.get_sleep_timer(planning_area)
             try:
                 prepare_planning_area(planning_area_id=planning_area.pk)
-                end = timezone.now()
-                total_time = end - start
                 self.stdout.write(
-                    f"[OK] Planning Area Prepared {planning_area.pk} - {end}\nTotal Time: {total_time.total_seconds()/60} minutes."
+                    f"[OK] Queued prepare_planning_area {planning_area.pk}. Waiting {sleep}."
                 )
+                total_pas -= 1
+                time.sleep(sleep)
+                self.stdout.write(f"We have {total_pas} remaining.")
             except Exception:
                 self.stderr.write(
                     f"[FAIL] Planning Area failed preparing {planning_area.pk}"
