01b52775a47b99d979de41a6e821a98da702d441
lastminutediorama <mkinsella@sig-gis.com>
2025-10-13 15:55:45 -0500

    add v3 config items to overlay

--- src/interface/src/app/scenario/scenario-config-overlay/scenario-config-overlay.component.ts
+++ src/interface/src/app/scenario/scenario-config-overlay/scenario-config-overlay.component.ts
@@ -8,6 +8,9 @@ import { STAND_OPTIONS } from 'src/app/plan/plan-helpers';
 import { catchError, combineLatest, map } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { ForsysService } from '@services/forsys.service';
+import { Constraint } from '@types';
+import { ForsysData } from 'src/app/types/module.types';
+import { AccountRoutingModule } from 'src/app/account/account-routing.module';
 
 @UntilDestroy()
 @Component({
@@ -19,17 +22,28 @@ import { ForsysService } from '@services/forsys.service';
     MatIconModule,
     ButtonComponent,
     DecimalPipe,
+    AccountRoutingModule,
   ],
   templateUrl: './scenario-config-overlay.component.html',
   styleUrl: './scenario-config-overlay.component.scss',
 })
+@UntilDestroy()
 export class ScenarioConfigOverlayComponent implements OnDestroy {
   private scenarioState: ScenarioState = inject(ScenarioState);
   private forsysService: ForsysService = inject(ForsysService);
 
+  constructor() {
+    this.forsysService.forsysData$
+      .pipe(untilDestroyed(this))
+      .subscribe((forsys) => {
+        this.knownConstraintsMap = forsys;
+      });
+  }
+
   displayScenarioConfigOverlay$ = this.scenarioState.displayConfigOverlay$;
   currentScenario$ = this.scenarioState.currentScenario$;
   excludedAreas$ = this.forsysService.excludedAreas$;
+  knownConstraintsMap: ForsysData | null = null;
 
   readonly standSizeOptions = STAND_OPTIONS;
 
@@ -53,6 +67,23 @@ export class ScenarioConfigOverlayComponent implements OnDestroy {
   scenarioGoal$ = this.scenarioState.currentScenario$.pipe(
     map((s) => s.treatment_goal?.name || '')
   );
+
+  constraintsToStrings(constraints: Constraint[]) {
+    const toDisplay: string[] = [];
+    this.knownConstraintsMap?.thresholds.distance_from_roads.id;
+    constraints.map((c) => {
+      if (
+        c.datalayer ===
+        this.knownConstraintsMap?.thresholds.distance_from_roads.id
+      ) {
+        toDisplay.push(`Distance from roads less than (<) ${c.value} %`);
+      }
+      if (c.datalayer === this.knownConstraintsMap?.thresholds.slope.id) {
+        toDisplay.push(`Slope less or equal than (<=) ${c.value} %`);
+      }
+    });
+    return toDisplay;
+  }
 
   close() {
     this.scenarioState.setDisplayOverlay(false);
