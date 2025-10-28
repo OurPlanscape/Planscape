31671bee3b906638546b1f1818ae51eebbb65785
Pablo <plopez@sig-gis.com>
2025-10-28 09:18:41 -0300

    static sidebar widths for scenario config and results

--- src/interface/src/app/scenario/scenario.component.spec.ts
+++ src/interface/src/app/scenario/scenario.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScenarioComponent } from './scenario.component';
-import { MockDeclarations } from 'ng-mocks';
+import { MockDeclarations, MockProvider } from 'ng-mocks';
 import { ScenarioMapComponent } from '../maplibre-map/scenario-map/scenario-map.component';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from '@shared';
 import { GoalOverlayComponent } from '../plan/goal-overlay/goal-overlay.component';
+import { ScenarioState } from './scenario.state';
 
 describe('ScenarioComponent', () => {
   let component: ScenarioComponent;
@@ -22,6 +23,7 @@ describe('ScenarioComponent', () => {
         ),
       ],
       imports: [RouterModule],
+      providers: [MockProvider(ScenarioState)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ScenarioComponent);
