1e5a2f656c5ae3a449f56a9959e258fdbdb267fc
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-10-28 18:40:25 -0300

    revert DoTreat == 0 stand count and filtering DoTreat == 1 stands on summare metrics

--- src/planscape/rscripts/postprocessing.R
+++ src/planscape/rscripts/postprocessing.R
@@ -6,7 +6,13 @@ summarize_metrics <- function(forsys_output, stand_data, datalayers) {
   output_fields <- paste0("attain_", datalayers[["name"]])
   lookup <- setNames(pcp_sum_fields, output_fields)
   stand_output <- forsys_output$stand_output |>
-    select(stand_id, proj_id, DoTreat, weightedPriority) |>
+    select(
+      filter(DoTreat == 1), 
+      stand_id, 
+      proj_id, 
+      DoTreat, 
+      weightedPriority
+    ) |>
     mutate(stand_id = as.integer(stand_id)) |>
     left_join(stand_data, by = "stand_id")
 
