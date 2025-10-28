fadba690bfbbc4aec489a7ef4df3bcb8068ea7ca
Andrew Giangrant <andrew.giangrant@outlook.com>
2025-10-28 05:43:11 -0700

    update serializers for input datalayer upserts and deletes

--- src/planscape/climate_foresight/serializers.py
+++ src/planscape/climate_foresight/serializers.py
@@ -3,16 +3,34 @@ from climate_foresight.models import (
     ClimateForesightRun,
     ClimateForesightRunInputDataLayer,
 )
+from climate_foresight.tasks import calculate_climate_foresight_layer_statistics
 from planning.models import PlanningArea
 
 
 class ClimateForesightRunInputDataLayerSerializer(serializers.ModelSerializer):
     """Serializer for ClimateForesightRunInputDataLayer model."""
 
+    normalized_datalayer_id = serializers.IntegerField(
+        source="normalized_datalayer.id", read_only=True, allow_null=True
+    )
+
     class Meta:
         model = ClimateForesightRunInputDataLayer
-        fields = ["id", "datalayer", "favor_high", "pillar"]
-        read_only_fields = ["id"]
+        fields = [
+            "id",
+            "datalayer",
+            "favor_high",
+            "pillar",
+            "normalized_datalayer_id",
+            "outlier_thresholds",
+            "statistics_calculated",
+        ]
+        read_only_fields = [
+            "id",
+            "normalized_datalayer_id",
+            "outlier_thresholds",
+            "statistics_calculated",
+        ]
 
 
 class ClimateForesightRunSerializer(serializers.ModelSerializer):
@@ -38,6 +56,8 @@ class ClimateForesightRunSerializer(serializers.ModelSerializer):
             "created_by",
             "creator",
             "status",
+            "current_step",
+            "furthest_step",
             "created_at",
             "input_datalayers",
         ]
@@ -61,8 +81,17 @@ class ClimateForesightRunSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         input_datalayers_data = validated_data.pop("input_datalayers", [])
         run = ClimateForesightRun.objects.create(**validated_data)
+
         for datalayer_data in input_datalayers_data:
-            ClimateForesightRunInputDataLayer.objects.create(run=run, **datalayer_data)
+            input_dl = ClimateForesightRunInputDataLayer.objects.create(
+                run=run, **datalayer_data
+            )
+            calculate_climate_foresight_layer_statistics.delay(input_dl.id)
+
+        if input_datalayers_data:
+            run.furthest_step = max(run.furthest_step, 1)
+            run.save()
+
         return run
 
     def update(self, instance, validated_data):
@@ -70,15 +99,40 @@ class ClimateForesightRunSerializer(serializers.ModelSerializer):
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-        instance.save()
 
         if input_datalayers_data is not None:
-            instance.input_datalayers.all().delete()
-            for datalayer_data in input_datalayers_data:
-                ClimateForesightRunInputDataLayer.objects.create(
-                    run=instance, **datalayer_data
-                )
+            existing_layers = {
+                layer.datalayer: layer for layer in instance.input_datalayers.all()
+            }
 
+            incoming_datalayer_ids = {
+                datalayer_data.get("datalayer")
+                for datalayer_data in input_datalayers_data
+            }
+
+            for datalayer_data in input_datalayers_data:
+                datalayer_id = datalayer_data.get("datalayer")
+                existing_layer = existing_layers.get(datalayer_id)
+
+                if existing_layer:
+                    for attr, value in datalayer_data.items():
+                        if attr != "datalayer":
+                            setattr(existing_layer, attr, value)
+                    existing_layer.save()
+                else:
+                    input_dl = ClimateForesightRunInputDataLayer.objects.create(
+                        run=instance, **datalayer_data
+                    )
+                    calculate_climate_foresight_layer_statistics.delay(input_dl.id)
+
+            for datalayer_id, layer in existing_layers.items():
+                if datalayer_id not in incoming_datalayer_ids:
+                    layer.delete()
+
+            if "furthest_step" not in validated_data:
+                instance.furthest_step = max(instance.furthest_step, 1)
+
+        instance.save()
         return instance
 
 
