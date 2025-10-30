5666bc422d72a2427b6482daf28f7f13065dc39c
Pablo <plopez@sig-gis.com>
2025-10-30 17:42:07 -0300

    added process overview styles

---
+++ src/interface/src/styleguide/process-overview/process-overview.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProcessOverviewComponent } from './process-overview.component';
+
+describe('ProcessOverviewComponent', () => {
+  let component: ProcessOverviewComponent;
+  let fixture: ComponentFixture<ProcessOverviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProcessOverviewComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ProcessOverviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
