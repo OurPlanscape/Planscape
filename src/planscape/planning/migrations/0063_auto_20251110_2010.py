26afc6e220ee8b1635f6e8a1a3f20c4943555bd4
George Silva <georger.silva@gmail.com>
2025-11-10 17:28:22 -0300

    fix migration

--- src/planscape/planning/migrations/0063_auto_20251110_2010.py
+++ src/planscape/planning/migrations/0063_auto_20251110_2010.py
@@ -3,7 +3,7 @@ from django.db import migrations
 
 def handle(apps, schema_editor):
     PlanningArea = apps.get_model("planning", "PlanningArea")
-    for pa in PlanningArea.objects.dead_or_alive():
+    for pa in PlanningArea.objects.all():
         pa.scenario_count = pa.scenarios.all().count()
         pa.save(update_fields=["updated_at", "scenario_count"])
 
