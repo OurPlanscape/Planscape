735382bf83accbe233f895623b9133e4430503b7
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-11-21 16:39:40 -0300

    Handling ValidationError exception in order to add details key to payload

--- src/planscape/core/exception_handler.py
+++ src/planscape/core/exception_handler.py
@@ -1,13 +1,23 @@
 from rest_framework.views import exception_handler
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 def planscape_api_exception_handler(exc, context):
+    if isinstance(exc, ValidationError):
+        # Customize ValidationError response structure
+        if isinstance(exc.detail, dict):
+            customized_detail = {
+                "detail": exc.detail.get("detail", "Validation error."),
+                "errors": exc.detail,
+            }
+        else:
+            customized_detail = {
+                "detail": "Validation error.",
+                "errors": exc.detail,
+            }
+        exc.detail = customized_detail
+        response = Response(exc.detail, status=exc.status_code)
+        return response
+
     response = exception_handler(exc, context)
-
-    if response is not None and isinstance(response.data, dict):
-        payload = {
-            "detail": response.data.get("detail", "An error occurred."),
-            "errors": response.data if response.data != {} else [],
-        }
-        response.data = payload
-
     return response
