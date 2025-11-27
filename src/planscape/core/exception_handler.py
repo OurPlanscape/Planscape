bb0fd5354d3c5068892a795233db9f3e768bc12f
Roque Betiol Jr <jbetiol@sig-gis.com>
2025-11-27 14:45:47 -0300

    Feature flag added to custom API exception handler

--- src/planscape/core/exception_handler.py
+++ src/planscape/core/exception_handler.py
@@ -2,21 +2,24 @@ from rest_framework.views import exception_handler
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
+from core.flags import feature_enabled
+
 def planscape_api_exception_handler(exc, context):
-    if isinstance(exc, ValidationError):
-        if isinstance(exc.detail, dict):
-            customized_detail = {
-                "detail": exc.detail.get("detail", "Validation error."),
-                "errors": exc.detail,
-            }
-        else:
-            customized_detail = {
-                "detail": "Validation error.",
-                "errors": exc.detail,
-            }
-        exc.detail = customized_detail
-        response = Response(exc.detail, status=exc.status_code)
-        return response
+    if feature_enabled("CUSTOM_EXCEPTION_HANDLER"):
+        if isinstance(exc, ValidationError):
+            if isinstance(exc.detail, dict):
+                customized_detail = {
+                    "detail": exc.detail.get("detail", "Validation error."),
+                    "errors": exc.detail,
+                }
+            else:
+                customized_detail = {
+                    "detail": "Validation error.",
+                    "errors": exc.detail,
+                }
+            exc.detail = customized_detail
+            response = Response(exc.detail, status=exc.status_code)
+            return response
 
     response = exception_handler(exc, context)
     return response
