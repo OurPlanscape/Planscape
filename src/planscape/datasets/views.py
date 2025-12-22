f4eb5b745b1aea59f8a6dbd5a95ca652c1456442
George Silva <georger.silva@gmail.com>
2025-12-22 15:16:17 -0300

    change how we select query_params or data based on method

--- src/planscape/datasets/views.py
+++ src/planscape/datasets/views.py
@@ -121,7 +121,7 @@ class DataLayerViewSet(ListModelMixin, MultiSerializerMixin, GenericViewSet):
     )
     @action(detail=False, methods=["get", "post"])
     def find_anything(self, request):
-        params = request.query_params if request.query_params else request.data
+        params = request.query_params if request.method == "GET" else request.data
         serializer = FindAnythingSerializer(data=params)
         serializer.is_valid(raise_exception=True)
 
