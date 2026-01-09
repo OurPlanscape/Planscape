f541b7f2180562e8b395f7ac0f384e848344cc0d
George Silva <863039+george-silva@users.noreply.github.com>
2026-01-09 13:27:17 -0300

    fix query (#3521)

--- src/planscape/modules/base.py
+++ src/planscape/modules/base.py
@@ -160,8 +160,10 @@ class ClimateForesightModule(BaseModule):
     def get_datasets(self, **kwargs) -> QuerySet[Dataset]:
         return Dataset.objects.filter(
             Q(modules__contains=[self.name])
-            & Q(preferred_display_type=PreferredDisplayType.MAIN_DATALAYERS)
-            | Q(preferred_display_type=PreferredDisplayType.BASE_DATALAYERS)
+            & (
+                Q(preferred_display_type=PreferredDisplayType.MAIN_DATALAYERS)
+                | Q(preferred_display_type=PreferredDisplayType.BASE_DATALAYERS)
+            )
         ).select_related("organization")
 
 
