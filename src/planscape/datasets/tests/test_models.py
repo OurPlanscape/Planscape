6becdc539768becd7f8d283783535ffaa07ea22e
George Silva <georger.silva@gmail.com>
2026-01-09 10:08:18 -0300

    readd tests

--- src/planscape/datasets/tests/test_models.py
+++ src/planscape/datasets/tests/test_models.py
@@ -1,8 +1,34 @@
+from django.contrib.gis.geos import GEOSGeometry
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-
-from datasets.models import validate_dataset_modules
 from modules.base import MODULE_HANDLERS
+
+from datasets.models import DataLayer, validate_dataset_modules
+from datasets.tests.factories import DataLayerFactory
+
+
+class GeometricIntersectionTest(TestCase):
+    def setUp(self):
+        # Create two overlapping polygons
+        self.poly1 = GEOSGeometry("POLYGON((0 0, 1 0, 1 1, 0 1, 0 0))")
+        self.poly2 = GEOSGeometry(
+            "POLYGON((0.5 0.5, 1.5 0.5, 1.5 1.5, 0.5 1.5, 0.5 0.5))"
+        )
+        self.poly3 = GEOSGeometry("POLYGON((2 2, 3 2, 3 3, 2 3, 2 2))")
+        self.dl1 = DataLayerFactory.create(name="Layer 1", geometry=self.poly1)
+        self.dl2 = DataLayerFactory.create(name="Layer 2", geometry=self.poly2)
+        self.dl3 = DataLayerFactory.create(name="Layer 3", geometry=self.poly3)
+
+    def test_geometric_intersection1(self):
+        qs = DataLayer.objects.filter(id__in=[self.dl1.id, self.dl2.id])
+        intersection = qs.geometric_intersection()
+        expected = self.poly1.intersection(self.poly2)
+        self.assertEqual(intersection, expected)
+
+    def test_geometric_intersection_with_empty_intersection_returns_none(self):
+        qs = DataLayer.objects.all()
+        output = qs.geometric_intersection()
+        self.assertIsNone(output)
 
 
 class ValidateDatasetModulesTest(TestCase):
