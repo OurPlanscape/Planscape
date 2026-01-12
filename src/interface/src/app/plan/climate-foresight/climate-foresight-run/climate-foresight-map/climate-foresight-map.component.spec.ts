86e396dea5892e899f9eb3d963c980019ba1a1ad
Luis Lopez <33841664+LALuis@users.noreply.github.com>
2026-01-12 10:08:11 -0300

    PROM-45: Datalayers selection (#3377)

---
+++ src/interface/src/app/plan/climate-foresight/climate-foresight-run/climate-foresight-map/climate-foresight-map.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClimateForesightMapComponent } from './climate-foresight-map.component';
+import { MapConfigState } from 'src/app/maplibre-map/map-config.state';
+import { MockProvider, MockProviders } from 'ng-mocks';
+import { AuthService } from '@services';
+import { PlanState } from 'src/app/plan/plan.state';
+import { MapConfigService } from 'src/app/maplibre-map/map-config.service';
+import { of } from 'rxjs';
+
+describe('ClimateForesightMapComponent', () => {
+  let component: ClimateForesightMapComponent;
+  let fixture: ComponentFixture<ClimateForesightMapComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ClimateForesightMapComponent],
+      providers: [
+        MockProviders(MapConfigState, AuthService, MapConfigService),
+        MockProvider(PlanState, {
+          planningAreaGeometry$: of({} as any),
+        }),
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ClimateForesightMapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
