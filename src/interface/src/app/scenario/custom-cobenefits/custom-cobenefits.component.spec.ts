ca6493e59b99de2025507f8df5bc1aaca142a32c
lastminutediorama <mkinsella@sig-gis.com>
2026-01-23 11:45:23 -0600

    fix test

--- src/interface/src/app/scenario/custom-cobenefits/custom-cobenefits.component.spec.ts
+++ src/interface/src/app/scenario/custom-cobenefits/custom-cobenefits.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
+import { DataLayersStateService } from 'src/app/data-layers/data-layers.state.service';
 import { CustomCobenefitsComponent } from './custom-cobenefits.component';
 
 describe('CustomCobenefitsComponent', () => {
@@ -15,6 +15,7 @@ describe('CustomCobenefitsComponent', () => {
         HttpClientTestingModule,
         NoopAnimationsModule,
       ],
+      providers: [DataLayersStateService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CustomCobenefitsComponent);
