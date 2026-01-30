5d21e2c3076ca1f0580d240deebe8a3ab208f0f2
Pablo <plopez@sig-gis.com>
2026-01-30 18:11:30 -0300

    cleanup unused code

--- src/interface/src/app/services/scenario.service.spec.ts
+++ src/interface/src/app/services/scenario.service.spec.ts
@@ -51,7 +51,6 @@ describe('ScenarioService', () => {
         min_distance_from_road: undefined,
         max_slope: undefined,
         max_area: undefined,
-        project_areas: [],
         excluded_areas: undefined,
         stand_size: 'MEDIUM',
       };
