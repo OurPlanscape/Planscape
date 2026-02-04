fcbb9651ad2938112e14a35a56ffced53f4a6b9b
lastminutediorama <mkinsella@sig-gis.com>
2026-02-04 15:35:19 -0600

    grab objs from endpoint

--- src/interface/src/app/scenario-creation/scenario-summary/scenario-summary.component.ts
+++ src/interface/src/app/scenario-creation/scenario-summary/scenario-summary.component.ts
@@ -2,7 +2,14 @@ import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { SectionComponent } from '@styleguide';
 import { STAND_OPTIONS, STAND_SIZE } from '../../plan/plan-helpers';
+import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
+import { DataLayer, ScenarioConfig } from '@types';
+import { NewScenarioState } from '../new-scenario.state';
+import { catchError, distinctUntilChanged, of, filter, switchMap, map } from 'rxjs';
+import { DataLayersService } from '@services';
 
+
+@UntilDestroy()
 @Component({
   selector: 'app-scenario-summary',
   standalone: true,
@@ -14,9 +21,36 @@ export class ScenarioSummaryComponent {
   @Input() title: string = '';
 
   @Input() treatmentGoal?: string;
-  @Input() priorityObjective?: string;
+  @Input() priorityObjectives?: string;
 
   @Input() standSize: STAND_SIZE | null = null;
 
   standSizeOptions = STAND_OPTIONS;
+
+  constructor(private newScenarioState: NewScenarioState,
+    private dataLayersService: DataLayersService,
+
+  ) {
+    this.newScenarioState.scenarioConfig$
+      .pipe(
+        untilDestroyed(this),
+        map((config: ScenarioConfig) => config.priority_objectives),
+        filter((ids): ids is number[] => Array.isArray(ids) && ids.length > 0),
+        distinctUntilChanged((prev, curr) =>
+          JSON.stringify(prev) === JSON.stringify(curr)
+        ),
+        switchMap((ids: number[]) =>
+          this.dataLayersService.getDataLayersByIds(ids).pipe(
+            map(layers => layers ?? [] as DataLayer[]),
+            catchError((error) => {
+              console.error('Error fetching data layers:', error);
+              return of<DataLayer[]>([]);
+            })
+          )
+        )
+      )
+      .subscribe((layers: DataLayer[]) => {
+        this.priorityObjectives = layers.map(layer => layer.name).join(', ');
+      });
+  }
 }
