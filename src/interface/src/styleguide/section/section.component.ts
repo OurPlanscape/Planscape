1cbbe98e2e6e64feec3e8382389cf70ee0e39db0
Pablo Lopez <plopez@sig-gis.com>
2026-02-05 14:23:22 -0300

    adding no-restricted-imports rule to eslint and updating codebase (#3636)

--- src/interface/src/styleguide/section/section.component.ts
+++ src/interface/src/styleguide/section/section.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { ButtonComponent } from '@styleguide/button/button.component';
 import { PopoverComponent } from '@styleguide/popover/popover.component';
 
 /**
@@ -18,9 +17,9 @@ import { PopoverComponent } from '@styleguide/popover/popover.component';
     NgIf,
     MatMenuModule,
     MatButtonModule,
-    ButtonComponent,
     MatExpansionModule,
     PopoverComponent,
+    NgClass,
   ],
   templateUrl: './section.component.html',
   styleUrl: './section.component.scss',
