ffcaa3abb2f8bfc3f79a78026e57724f56334137
George Silva <georger.silva@gmail.com>
2026-02-26 11:36:04 -0300

    readd permissions

---
+++ src/planscape/planscape/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import BasePermission
+
+
+class PlanscapePermission(BasePermission):
+    permission_set = None
+
+    def has_permission(self, request, view):
+        return self.is_authenticated(request)
+
+    def is_authenticated(self, request):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if not self.permission_set:
+            return False
+
+        match view.action:
+            case "update" | "partial_update":
+                return self.permission_set.can_change(request.user, obj)
+            case "destroy":
+                return self.permission_set.can_remove(request.user, obj)
+            case _:
+                return self.permission_set.can_view(request.user, obj)
