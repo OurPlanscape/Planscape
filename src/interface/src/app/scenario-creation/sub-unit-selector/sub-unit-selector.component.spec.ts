c0a38a9abd04a0d93e04a2376b2be4c5e15b6b8c
lastminutediorama <mkinsella@sig-gis.com>
2026-02-26 12:54:03 -0600

    update test

--- src/interface/src/app/scenario-creation/sub-unit-selector/sub-unit-selector.component.spec.ts
+++ src/interface/src/app/scenario-creation/sub-unit-selector/sub-unit-selector.component.spec.ts
@@ -2,6 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SubUnitSelectorComponent } from './sub-unit-selector.component';
 import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NewScenarioState } from '../new-scenario.state';
+import { MockProvider } from 'ng-mocks';
+import { of } from 'rxjs';
 
 describe('SubUnitSelectorComponent', () => {
   let component: SubUnitSelectorComponent;
@@ -9,13 +13,23 @@ describe('SubUnitSelectorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, SubUnitSelectorComponent],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientModule,
+        SubUnitSelectorComponent,
+      ],
+      providers: [
+        MockProvider(NewScenarioState, {
+          scenarioConfig$: of({}),
+          priorityObjectivesDetails$: of([]),
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SubUnitSelectorComponent);
     component = fixture.componentInstance;
     component.form = new FormGroup({
-      subunit: new FormControl<number | undefined>(undefined),
+      sub_units_layer: new FormControl<number | undefined>(undefined),
     });
 
     fixture.detectChanges();
