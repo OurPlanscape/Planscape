54490c24b5a736fc0b481fe91bc3105ea119b9c3
lastminutediorama <mkinsella@sig-gis.com>
2026-03-04 16:22:51 -0600

    initial map viewer card

---
+++ src/interface/src/app/plan/map-viewer-card/map-viewer-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MapViewerCardComponent } from './map-viewer-card.component';
+
+describe('MapViewerCardComponent', () => {
+  let component: MapViewerCardComponent;
+  let fixture: ComponentFixture<MapViewerCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MapViewerCardComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MapViewerCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/interface/src/app/plan/map-viewer-card/map-viewer-card.component.ts
@@ -0,0 +1,24 @@
+import { AsyncPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { PlanningAreaLayerComponent } from '@app/maplibre-map/planning-area-layer/planning-area-layer.component';
+import { MapComponent } from "@maplibre/ngx-maplibre-gl";
+import {
+  Map as MapLibreMap,
+} from 'maplibre-gl';
+
+
+@Component({
+  selector: 'app-map-viewer-card',
+  standalone: true,
+  imports: [AsyncPipe, MapComponent, MatIconModule, PlanningAreaLayerComponent
+],
+  templateUrl: './map-viewer-card.component.html',
+  styleUrl: './map-viewer-card.component.scss'
+})
+export class MapViewerCardComponent {
+
+  mapLibreMap!: MapLibreMap;
+
+
+}
