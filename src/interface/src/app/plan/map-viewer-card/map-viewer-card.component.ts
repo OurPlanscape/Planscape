28c9a8942bb260ba3be18629273a4d1f513a1c3b
lastminutediorama <mkinsella@sig-gis.com>
2026-03-06 12:20:26 -0600

    add config state

--- src/interface/src/app/plan/map-viewer-card/map-viewer-card.component.ts
+++ src/interface/src/app/plan/map-viewer-card/map-viewer-card.component.ts
@@ -1,17 +1,40 @@
-import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { PlanningAreaLayerComponent } from '@app/maplibre-map/planning-area-layer/planning-area-layer.component';
 import { MapComponent } from '@maplibre/ngx-maplibre-gl';
 import { Map as MapLibreMap } from 'maplibre-gl';
+import { DEFAULT_BASE_MAP } from '@app/types';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { MapConfigState } from '@app/maplibre-map/map-config.state';
+import { MapConfigService } from '@app/maplibre-map/map-config.service';
+import { PlanState } from '../plan.state';
 
 @Component({
   selector: 'app-map-viewer-card',
   standalone: true,
-  imports: [AsyncPipe, MapComponent, MatIconModule, PlanningAreaLayerComponent],
+  imports: [
+    AsyncPipe,
+    MapComponent,
+    MatIconModule,
+    NgIf,
+    PlanningAreaLayerComponent,
+  ],
+  providers: [MapConfigState],
   templateUrl: './map-viewer-card.component.html',
   styleUrl: './map-viewer-card.component.scss',
 })
 export class MapViewerCardComponent {
   mapLibreMap!: MapLibreMap;
+  baseMapUrl$ = this.mapConfigState.baseMapUrl$;
+  currentPlan$ = this.planState.currentPlan$;
+
+  constructor(
+    private mapConfigState: MapConfigState,
+    private mapConfigService: MapConfigService,
+    private planState: PlanState
+  ) {
+    this.mapConfigService.initialize();
+    this.mapConfigState.setShowMapControls(false);
+    this.mapConfigState.updateBaseMap(DEFAULT_BASE_MAP);
+  }
 }
