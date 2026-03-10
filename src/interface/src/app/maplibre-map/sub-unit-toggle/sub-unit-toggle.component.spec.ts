d58a84060d31c2374c9e984bf6fab041a5822a71
Pablo <plopez@sig-gis.com>
2026-03-10 11:06:01 -0300

    add sub units for scenario results

--- src/interface/src/app/maplibre-map/sub-unit-toggle/sub-unit-toggle.component.spec.ts
+++ src/interface/src/app/maplibre-map/sub-unit-toggle/sub-unit-toggle.component.spec.ts
@@ -1,39 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-
 import { SubUnitToggleComponent } from './sub-unit-toggle.component';
-import { NewScenarioState } from '@scenario-creation/new-scenario.state';
 import { BaseLayersStateService } from '@base-layers/base-layers.state.service';
+import { BaseLayer } from '@types';
 
 describe('SubUnitToggleComponent', () => {
   let component: SubUnitToggleComponent;
   let fixture: ComponentFixture<SubUnitToggleComponent>;
+  let baseLayersServiceSpy: jasmine.SpyObj<BaseLayersStateService>;
+
+  const mockLayer = { id: 1, name: 'Subfireshed' } as BaseLayer;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('BaseLayersStateService', [
+      'addBaseLayer',
+      'removeBaseLayer',
+      'clearBaseLayer',
+    ]);
+
     await TestBed.configureTestingModule({
       imports: [SubUnitToggleComponent],
-      providers: [
-        {
-          provide: NewScenarioState,
-          useValue: { selectedSubUnitLayer$: of(null) },
-        },
-        {
-          provide: BaseLayersStateService,
-          useValue: {
-            addBaseLayer: () => {},
-            clearBaseLayer: () => {},
-            removeBaseLayer: () => {},
-          },
-        },
-      ],
+      providers: [{ provide: BaseLayersStateService, useValue: spy }],
     }).compileComponents();
+
+    baseLayersServiceSpy = TestBed.inject(
+      BaseLayersStateService
+    ) as jasmine.SpyObj<BaseLayersStateService>;
 
     fixture = TestBed.createComponent(SubUnitToggleComponent);
     component = fixture.componentInstance;
+    component.layer = mockLayer;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the layer name', () => {
+    const el: HTMLElement = fixture.nativeElement;
+    expect(el.textContent).toContain('Subfireshed');
+  });
+
+  it('should call addBaseLayer when toggled on', () => {
+    component.showBaseLayer = true;
+    component.toggleBaseLayer(mockLayer);
+    expect(baseLayersServiceSpy.addBaseLayer).toHaveBeenCalledWith(mockLayer);
+  });
+
+  it('should call removeBaseLayer when toggled off', () => {
+    component.showBaseLayer = false;
+    component.toggleBaseLayer(mockLayer);
+    expect(baseLayersServiceSpy.removeBaseLayer).toHaveBeenCalledWith(
+      mockLayer
+    );
   });
 });
