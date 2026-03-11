d2c6ae2b9e21e5779d7ee2ba0703f26ca8ebe512
Roque Betiol Jr <jbetiol@sig-gis.com>
2026-03-11 16:07:10 -0300

    Planning Approach check to allow/forbid execution of Impacts module * Rule to prevent scenarios with planning_approach as PRIORITIZE_SUB_UNITS to have access to Impacts module (hide Treatment Plans)

--- src/planscape/modules/tests/test_modules.py
+++ src/planscape/modules/tests/test_modules.py
@@ -9,6 +9,8 @@ from django.contrib.gis.geos import GEOSGeometry
 
 from modules.base import get_module
 
+from planning.tests.factories import PlanningAreaFactory, ScenarioFactory
+from planning.models import ScenarioPlanningApproach
 
 class MapModuleTest(TestCase):
     def test_returns_options_correctly(self):
@@ -166,3 +168,52 @@ class PrioritizeSubUnitsModuleTest(TestCase):
         self.assertEqual(len(main), 1)
         self.assertEqual(len(base), 1)
         self.assertEqual(len(sub_units), 1)
+
+
+class ImpactsModulesTest(TestCase):
+    def setUp(self):
+        inside_california_goemtry = {
+            "type": "MultiPolygon", 
+            "coordinates": [ [ [ [ -121.366892199, 36.329257174 ], [ -121.361359175, 36.329425383 ], [ -121.364386679, 36.324967718 ], [ -121.366892199, 36.329257174 ] ] ] ] 
+        }
+        outside_california_geometry = {
+            "type": "MultiPolygon",
+            "coordinates": [ [ [ [ -77.906814571199931, 39.040366839373661 ], [ -77.901871799384352, 39.039805013389277 ], [ -77.905187059969236, 39.037089508161827 ], [ -77.906814571199931, 39.040366839373661 ] ] ] ] 
+        }
+        self.ca_planning_area = PlanningAreaFactory.create(
+            geometry=GEOSGeometry(json.dumps(inside_california_goemtry))
+        )
+        self.non_ca_planning_area = PlanningAreaFactory.create(
+            geometry=GEOSGeometry(json.dumps(outside_california_geometry))
+        )
+
+        self.ca_scenario = ScenarioFactory.create(planning_area=self.ca_planning_area)
+        self.non_ca_scenario = ScenarioFactory.create(planning_area=self.non_ca_planning_area)
+
+        return super().setUp()
+    
+    def test_can_run_planning_area(self):
+        module = get_module("impacts")
+
+        self.assertTrue(module.can_run(self.ca_planning_area))
+        self.assertTrue(module.can_run(self.non_ca_planning_area))
+
+    def test_can_run_scenario_based_on_geometry(self):
+        module = get_module("impacts")
+
+        self.assertTrue(module.can_run(self.ca_scenario))
+        self.assertFalse(module.can_run(self.non_ca_scenario))
+
+    def test_can_run_scenario_planning_approach(self):
+        module = get_module("impacts")
+
+        self.ca_scenario.planning_approach = ScenarioPlanningApproach.OPTIMIZE_PROJECT_AREAS
+        self.ca_scenario.save()
+
+        self.assertTrue(module.can_run(self.ca_scenario))
+
+        self.ca_scenario.planning_approach = ScenarioPlanningApproach.PRIORITIZE_SUB_UNITS
+        self.ca_scenario.save()
+
+        self.assertFalse(module.can_run(self.ca_scenario))
+
