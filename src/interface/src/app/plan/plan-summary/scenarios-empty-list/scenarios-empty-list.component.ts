e5a47c58c270af46bc992220a7ef4b4c8649a09b
lastminutediorama <mkinsella@sig-gis.com>
2026-03-12 09:14:28 -0500

    handle clicks

--- src/interface/src/app/plan/plan-summary/scenarios-empty-list/scenarios-empty-list.component.ts
+++ src/interface/src/app/plan/plan-summary/scenarios-empty-list/scenarios-empty-list.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ScenarioSetupModalComponent } from '@app/scenario/scenario-setup-modal/scenario-setup-modal.component';
+import { Plan, SCENARIO_TYPE } from '@app/types';
 import { ActionCardComponent } from '@styleguide';
+import { MatDialog } from '@angular/material/dialog';
+import { UploadProjectAreasModalComponent } from '@app/plan/upload-project-areas-modal/upload-project-areas-modal.component';
 
 @Component({
   selector: 'app-scenarios-empty-list',
@@ -8,4 +12,25 @@ import { ActionCardComponent } from '@styleguide';
   templateUrl: './scenarios-empty-list.component.html',
   styleUrl: './scenarios-empty-list.component.scss',
 })
-export class ScenariosEmptyListComponent {}
+export class ScenariosEmptyListComponent {
+  @Input() plan!: Plan | null;
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  public openProjectAreasUploadDialog() {
+    return this.dialog.open(UploadProjectAreasModalComponent,{});
+  }
+
+  public openScenarioSetupDialog(type: SCENARIO_TYPE) {
+    console.log('clicking for type:', type);
+    return this.dialog.open(ScenarioSetupModalComponent, {
+      maxWidth: '560px',
+      data: {
+        planId: this.plan?.id,
+        fromClone: false,
+        type: type,
+      },
+    });
+  }
+}
