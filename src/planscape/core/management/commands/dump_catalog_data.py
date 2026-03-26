c042731bc6fbc61ec86742f2642a0b3a37764d08
Roque Betiol Jr <jbetiol@sig-gis.com>
2026-03-26 17:32:22 -0300

    Change path and file to use word backup Creation of a shell script to run RSYNC between envs

--- src/planscape/core/management/commands/dump_catalog_data.py
+++ src/planscape/core/management/commands/dump_catalog_data.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = "Dump Dataset catalog data into json file to be loaded in another env."
+    help = "Generates backup of catalog data, by pushing it into json file to be loaded in another env."
     def handle(self, **options):
         if settings.ENV != "catalog":
             raise SystemExit(
@@ -19,18 +19,18 @@ class Command(BaseCommand):
             )
         
 
-        dumps_dir = os.path.join(settings.CATALOG_DUMPS_PATH)
-        if not os.path.exists(dumps_dir):
+        backups_dir = os.path.join(settings.CATALOG_BACKUPS_PATH)
+        if not os.path.exists(backups_dir):
             raise SystemError(
                 "\n"
                 "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
-                "!!      Error: Dumps path is not configured.          !!\n"
+                "!!     Error: Backups path is not configured.         !!\n"
                 "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
             )
         
 
         now = datetime.now()
-        output_filenames = [f"{now.strftime('%Y-%m-%d_%H:%M:%S')}_catalog_dump.json", "latest_catalog_dump.json"]
+        output_filenames = [f"{now.strftime('%Y-%m-%d_%H:%M:%S')}_catalog_backup.json", "latest_catalog_backup.json"]
         
         buf = io.StringIO()
         try:
@@ -49,7 +49,7 @@ class Command(BaseCommand):
             
             # Read from the buffer and write to the file
             for output_filename in output_filenames:
-                output_path = os.path.join(dumps_dir, output_filename)
+                output_path = os.path.join(backups_dir, output_filename)
                 buf.seek(0)
                 with open(output_path, "w") as f:
                     f.write(buf.read())
