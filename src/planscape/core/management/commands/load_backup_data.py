82eb107cbdeca1adf563928c29bf3ac7d98b3b74
Roque Betiol Jr <jbetiol@sig-gis.com>
2026-03-30 11:43:05 -0300

    Data Generation and Load commands modifications * allowing dump data from any env * allowing load data on any env except catalog * copying backup data to tmp folder and updating url fields via command line

--- src/planscape/core/management/commands/load_backup_data.py
+++ src/planscape/core/management/commands/load_backup_data.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
@@ -8,7 +9,7 @@ from datasets.models import DataLayer, DataLayerType, DataLayerStatus
 from datasets.tasks import datalayer_uploaded
 
 class Command(BaseCommand):
-    help = "Loads catalog backup and trigger Vector layers post-upload tasks."
+    help = "Loads backup data and trigger Vector layers post-upload tasks."
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -16,13 +17,20 @@ class Command(BaseCommand):
             default="latest_catalog_backup.json",
             help="Load data from specific file. Default: `latest_catalog_backup.json`"
         )
+        parser.add_argument(
+            "--source-env",
+            default="catalog",
+            help="Source which data will be copied from."
+        )
 
 
     def handle(self, **options):
+        source_env = options.get("source_env", "catalog")
         self.stdout.write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
         self.stdout.write(f"!!   WARNING: you are running this command on {settings.ENV}.    !!\n")
+        self.stdout.write(f"!!          and it will import data from {source_env}.           !!\n")
         self.stdout.write("!! It will perform the following steps:                          !!\n")
-        self.stdout.write("!! 1. Sync Catalog datalayers bucket with targeted env bucket;   !!\n")
+        self.stdout.write("!! 1. Sync source env datalayers bucket with targeted env bucket;!!\n")
         self.stdout.write("!! 2. Load data from given JSON file to targeted env database;   !!\n")
         self.stdout.write("!! 3. Update datalayers url to point to targeted env bucket;     !!\n")
         self.stdout.write("!! 4. Execute post-update process for Vector Layers (Ready only);!!\n")
@@ -47,7 +55,7 @@ class Command(BaseCommand):
                 "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
             )
 
-        backups_dir = os.path.join(settings.CATALOG_BACKUPS_PATH)
+        backups_dir = os.path.join(settings.BACKUPS_PATH)
         if not os.path.exists(backups_dir):
             raise SystemError(
                 "\n"
@@ -66,24 +74,49 @@ class Command(BaseCommand):
                 "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
             )
         
+        if source_env not in filename:
+            raise SystemError(
+                "\n"
+                "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+                "!!  Error: Backup file and source env does not match. !!\n"
+                "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+            )
+        
         try:
+            # Sync buckets
             subprocess.call(
                 [
                     "gcloud",
                     "storage", 
                     "rsync",
-                    "gs://planscape-datastore-catalog/datalayers",
+                    f"gs://planscape-datastore-{source_env}/datalayers",
                     f"gs://planscape-datastore-{settings.ENV}/datalayers",
                     "--recursive"
                 ]
             )
+
+            # Copy to tmp folder and rename all `url` fields
+            self.stdout.write(f"Copying file from {file_path} to /tmp/{filename}.")
+            shutil.copy(file_path, f"/tmp/{filename}")
+
+            self.stdout.write(f"Replacing  `planscape-datastore-{source_env}` with `planscape-datastore-{settings.ENV}` on /tmp/{filename}.")
+            subprocess.call(
+                [
+                    "sed",
+                    "-i",
+                    f"'s/planscape-datastore-{source_env}/planscape-datastore-{settings.ENV}/g'",
+                    f"/tmp/{filename}"
+                ]
+            )
             
+            # Load data to DB
+            self.stdout.write(f"Loading data from `/tmp/{filename}`")
             call_command(
                 "loaddata", 
-                str(file_path),
+                f"/tmp/{filename}",
             )
 
-            self.stdout.write(self.style.SUCCESS(f"Successfully loaded data from {file_path}"))
+            self.stdout.write(self.style.SUCCESS(f"Successfully loaded data from `/tmp/{filename}`"))
 
             ready_vector_layers = DataLayer.objects.filter(type=DataLayerType.VECTOR, status=DataLayerStatus.READY)
 
