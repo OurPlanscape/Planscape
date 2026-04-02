baeb22f3d5fa75fccb38a0d1c83aa6a332db72cd
lastminutediorama <mkinsella@sig-gis.com>
2026-04-02 10:32:07 -0500

    add list components

---
+++ src/interface/src/app/treatments/treatment-plan-cards-list/treatment-plan-cards-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-treatment-plan-cards-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './treatment-plan-cards-list.component.html',
+  styleUrl: './treatment-plan-cards-list.component.scss'
+})
+export class TreatmentPlanCardsListComponent {
+
+}
