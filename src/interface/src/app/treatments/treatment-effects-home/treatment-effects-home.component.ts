3888b71d467e065b98cfe88737e686ad8b3dc283
lastminutediorama <mkinsella@sig-gis.com>
2026-04-13 17:38:51 -0500

    send props to list

--- src/interface/src/app/treatments/treatment-effects-home/treatment-effects-home.component.ts
+++ src/interface/src/app/treatments/treatment-effects-home/treatment-effects-home.component.ts
@@ -2,16 +2,40 @@ import { Component } from '@angular/core';
 import { ToolInfoCardComponent } from '@styleguide';
 import { DashboardLayoutComponent } from '@styleguide/dashboard-layout/dashboard-layout.component';
 import { TreatmentPlansListComponent } from '../treatment-plans-list/treatment-plans-list.component';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ScenarioState } from '@app/scenario/scenario.state';
+import { PlanState } from '@app/plan/plan.state';
+import { Plan } from '@app/types';
 
 @Component({
   selector: 'app-treatment-effects-home',
   standalone: true,
-  imports: [DashboardLayoutComponent, ToolInfoCardComponent, TreatmentPlansListComponent],
+  imports: [AsyncPipe, DashboardLayoutComponent, NgIf, ToolInfoCardComponent, TreatmentPlansListComponent],
   templateUrl: './treatment-effects-home.component.html',
   styleUrl: './treatment-effects-home.component.scss'
 })
 export class TreatmentEffectsHomeComponent {
 
+  scenarioId$ = this.scenarioState.currentScenarioId$;
+
+  currentPlan!:Plan;
+
+
+constructor(
+  private scenarioState: ScenarioState,
+  private planState: PlanState,
+
+)
+{
+
+      this.planState.currentPlan$
+        .subscribe((plan: Plan) => {
+          this.currentPlan = plan;
+        });
   
+
+
+
+}
 
 }
