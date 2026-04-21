fab129d7fb8313ff1cdfe0dc9196ed5e9b2be184
George Silva <georger.silva@gmail.com>
2026-04-21 11:41:12 -0300

    adds more logging

--- src/planscape/users/backends.py
+++ src/planscape/users/backends.py
@@ -1,19 +1,24 @@
+import logging
 from datetime import timedelta
 
 from allauth.account.auth_backends import AuthenticationBackend
 from django.conf import settings
 from django.utils import timezone
 
+logger = logging.getLogger(__name__)
+
 
 def track_returning_user(user) -> None:
     from planscape.openpanel import track_openpanel
 
+    logger.info(f"user {user} logging in - last login {user.last_login}")
     now = timezone.now()
     number_of_days = settings.RETURNING_USER_THRESHOLD_DAYS
     target_date = user.date_joined + timedelta(days=number_of_days)
     if now >= target_date and (
         user.last_login is None or user.last_login < target_date
     ):
+        logger.info("fi")
         track_openpanel(
             f"users.returned_after_{number_of_days}d",
             properties={"email": user.email},
