ee81413a015c486dac78b935042f3035760f1032
lastminutediorama <mkinsella@sig-gis.com>
2026-04-22 10:52:09 -0500

    cleanup comments

--- src/interface/src/app/maplibre-map/draw.service.ts
+++ src/interface/src/app/maplibre-map/draw.service.ts
@@ -314,7 +314,6 @@ export class DrawService {
       return this._uploadedShape?.geometry;
     }
     if (this._uploadedShape.type === 'FeatureCollection') {
-      // Use turf/union to consolidate the features
       const polygonFeatures = this._uploadedShape.features.filter(
         (f) =>
           f.geometry.type === 'Polygon' || f.geometry.type === 'MultiPolygon'
@@ -324,10 +323,11 @@ export class DrawService {
       if (polygonFeatures.length === 1) {
         return this._uploadedShape.features[0].geometry;
       }
+      // Use turf/union to consolidate the features if we have more than one
       const polyCollection = {
         type: 'FeatureCollection',
         features: polygonFeatures,
-      } as FeatureCollection<Polygon | MultiPolygon>; // Cast to satisfy TS
+      } as FeatureCollection<Polygon | MultiPolygon>;
       const unioned = union(polyCollection);
       return unioned ? unioned.geometry : null;
     }
