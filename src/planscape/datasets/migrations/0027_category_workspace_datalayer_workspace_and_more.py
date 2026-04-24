c19c89e5f9d6b66e86581377ea0bbab080136914
George Silva <georger.silva@gmail.com>
2026-04-24 16:15:54 -0300

    create workspaces app and models

---
+++ src/planscape/datasets/migrations/0027_category_workspace_datalayer_workspace_and_more.py
@@ -0,0 +1,56 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("datasets", "0026_dataset_modules"),
+        ("workspaces", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="category",
+            name="workspace",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="categories",
+                to="workspaces.workspace",
+            ),
+        ),
+        migrations.AddField(
+            model_name="datalayer",
+            name="workspace",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="datalayers",
+                to="workspaces.workspace",
+            ),
+        ),
+        migrations.AddField(
+            model_name="dataset",
+            name="workspace",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="datasets",
+                to="workspaces.workspace",
+            ),
+        ),
+        migrations.AddField(
+            model_name="style",
+            name="workspace",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="styles",
+                to="workspaces.workspace",
+            ),
+        ),
+    ]
