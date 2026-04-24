2590871d72dd1a501f1be7cb313ba89a612873e6
lastminutediorama <mkinsella@sig-gis.com>
2026-04-24 09:24:38 -0500

    fix test

--- src/interface/src/app/treatments/treatment-effects-home/treatment-effects-home.component.spec.ts
+++ src/interface/src/app/treatments/treatment-effects-home/treatment-effects-home.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TreatmentEffectsHomeComponent } from './treatment-effects-home.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('TreatmentEffectsHomeComponent', () => {
   let component: TreatmentEffectsHomeComponent;
@@ -10,6 +11,9 @@ describe('TreatmentEffectsHomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, TreatmentEffectsHomeComponent],
+      providers: [
+              { provide: ActivatedRoute, useValue: { snapshot: {} } },
+            ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TreatmentEffectsHomeComponent);
