b4f6a9de7d0e0b70d3ff3566b846ac85f2099b11
Pablo <plopez@sig-gis.com>
2026-05-01 17:31:23 -0300

    addming capabilities to the sg-input field

--- src/interface/src/styleguide/input/input-field.component.ts
+++ src/interface/src/styleguide/input/input-field.component.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { InputDirective } from './input.directive';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 export type ShowSupportMessage = 'always' | 'on-error' | false;
 
 /**
@@ -23,12 +24,7 @@ export type ShowSupportMessage = 'always' | 'on-error' | false;
 @Component({
   selector: 'sg-input-field',
   standalone: true,
-  imports: [
-    MatIconModule,
-    MatProgressSpinnerModule,
-    CommonModule,
-    InputDirective,
-  ],
+  imports: [MatIconModule, MatProgressSpinnerModule, CommonModule],
   templateUrl: './input-field.component.html',
   styleUrl: './input-field.component.scss',
 })
@@ -94,6 +90,8 @@ export class InputFieldComponent implements AfterContentInit {
 
   @Input() label = '';
 
+  @Input() height: 'regular' | 'small' = 'regular';
+
   @HostBinding('class.error')
   get hasError() {
     return this.error;
@@ -110,7 +108,7 @@ export class InputFieldComponent implements AfterContentInit {
   }
 
   @HostBinding('class.full')
-  get isSmall() {
+  get isFull() {
     return this.size === 'full';
   }
 
