2ee2d904bb839361a754ec6a0e61418a1ecf6018
Pablo <plopez@sig-gis.com>
2026-05-24 09:30:02 -0300

    add theme and variants to tool info card

--- src/interface/src/styleguide/tool-info-card/tool-info-card.component.ts
+++ src/interface/src/styleguide/tool-info-card/tool-info-card.component.ts
@@ -1,4 +1,4 @@
-import { NgIf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,23 +6,38 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'sg-tool-info-card',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, NgIf],
+  imports: [MatButtonModule, MatIconModule, NgIf, NgForOf],
   templateUrl: './tool-info-card.component.html',
   styleUrl: './tool-info-card.component.scss',
 })
 export class ToolInfoCardComponent {
-  @Input() title = '';
+  /** Title shown on the banner/header */
+  @Input() cardTitle = '';
+  /** Image/icon on the banner/header */
   @Input() mainImagePath: string | null = null;
-  @Input() mainImageAlt = this.title; // default the main image alt to title
-
+  /** Alt text for the image/icon, default the main image alt to title */
+  @Input() mainImageAlt = this.cardTitle; //
+  /** credits image */
   @Input() creditImagePath: string | null = null;
+  /** additional credit text */
   @Input() creditText = '';
-  @Input() creditImageAlt = this.creditText; // default the image alt to the credit text
-
+  /** Alt text for credits image, default the image alt to the credit text */
+  @Input() creditImageAlt = this.creditText;
+  /** description text */
   @Input() description = '';
+  /** Theme for the banner/header, defaults to light */
+  @Input() theme: 'light' | 'dark' = 'light';
+  /** Whether to show the tooltip (help) button */
+  @Input() showTooltipButton = true;
+  /** Additional partners */
+  @Input() partners: { name: string; url: string; logo: string }[] = [];
 
+  /** Action when clicking on tooltip. */
   @Output() clickTooltip = new EventEmitter<void>();
 
+  /**
+   * @ignore
+   */
   handleTooltip() {
     this.clickTooltip.emit();
   }
--- src/interface/src/styleguide/tool-info-card/tool-info-card.stories.ts
+++ src/interface/src/styleguide/tool-info-card/tool-info-card.stories.ts
@@ -3,6 +3,9 @@ import { applicationConfig, argsToTemplate } from '@storybook/angular';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ToolInfoCardComponent } from './tool-info-card.component';
 
+const longDescription =
+  'The Treatment Effects module aims to show planners the direct impacts of treatments on their project areas and planning areas, respectively. Through an easy-to-use tool, planners can create any number of treatment scenarios to compare the impacts of their applied treatments and choose the optimal arrangement of treatments that meets their goals for resiliency.';
+
 const meta: Meta<ToolInfoCardComponent> = {
   title: 'Components/Tool Info Card',
   component: ToolInfoCardComponent,
@@ -23,31 +26,71 @@ type Story = StoryObj<ToolInfoCardComponent>;
 
 export const Default: Story = {
   args: {
-    title: 'Treatment Effects',
+    cardTitle: 'Treatment Effects',
     mainImagePath: '/assets/svg/treatment-effects.svg',
     creditImageAlt: 'Planscape',
     creditImagePath: 'assets/svg/icons/planscape-color-logo-w-text.svg',
-    description:
-      'The Treatment Effects module aims to show planners the direct impacts of treatments on their project areas and planning areas, respectively. Through an easy-to-use tool, planners can create any number of treatment scenarios to compare the impacts of their applied treatments and choose the optimal arrangement of treatments that meets their goals for resiliency.',
+    description: longDescription,
   },
 };
 
 export const NoMainImage: Story = {
   args: {
-    title: 'Treatment Effects',
+    cardTitle: 'Treatment Effects',
     creditImageAlt: 'Planscape',
     creditImagePath: 'assets/svg/icons/planscape-color-logo-w-text.svg',
-    description:
-      'The Treatment Effects module aims to show planners the direct impacts of treatments on their project areas and planning areas, respectively. Through an easy-to-use tool, planners can create any number of treatment scenarios to compare the impacts of their applied treatments and choose the optimal arrangement of treatments that meets their goals for resiliency.',
+    description: longDescription,
   },
 };
 
 export const NoCreditImage: Story = {
   args: {
-    title: 'Treatment Effects',
+    cardTitle: 'Treatment Effects',
     mainImagePath: '/assets/svg/treatment-effects.svg',
     creditText: 'Planscape',
-    description:
-      'The Treatment Effects module aims to show planners the direct impacts of treatments on their project areas and planning areas, respectively. Through an easy-to-use tool, planners can create any number of treatment scenarios to compare the impacts of their applied treatments and choose the optimal arrangement of treatments that meets their goals for resiliency.',
+    description: longDescription,
+  },
+};
+
+export const NoTooltip: Story = {
+  args: {
+    cardTitle: 'Treatment Effects',
+    creditText: 'Planscape',
+    description: longDescription,
+    showTooltipButton: false,
+  },
+};
+
+export const DarkTheme: Story = {
+  args: {
+    cardTitle: 'Treatment Effects',
+    creditImageAlt: 'Planscape',
+    creditImagePath: 'assets/svg/icons/planscape-color-logo-w-text.svg',
+    description: longDescription,
+    theme: 'dark',
+  },
+};
+
+export const WithPartners: Story = {
+  args: {
+    cardTitle: 'Treatment Effects',
+    creditImageAlt: 'Planscape',
+    creditImagePath: 'assets/svg/icons/planscape-color-logo-w-text.svg',
+    description: longDescription,
+    theme: 'dark',
+    showTooltipButton: false,
+    creditText: 'in partnership with:',
+    partners: [
+      {
+        name: 'Blue Forest',
+        logo: '/assets/png/blue-forest.png',
+        url: 'https://www.blueforest.org/',
+      },
+      {
+        name: 'The Freshwater Trust',
+        logo: '/assets/png/freshwater-trust.png',
+        url: 'https://thefreshwatertrust.org/',
+      },
+    ],
   },
 };
