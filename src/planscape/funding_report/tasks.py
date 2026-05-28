e71f1b1ba54c22b41ad49b798bb7555f1e3ec7ad
George Silva <georger.silva@gmail.com>
2026-05-28 11:40:46 -0300

    change status in celery task

--- src/planscape/funding_report/tasks.py
+++ src/planscape/funding_report/tasks.py
@@ -5,4 +5,19 @@ from planscape.celery import app
 
 @app.task()
 def run_funding_opportunity_report(funding_opportunity_report_id: int) -> None:
-    time.sleep(60)
+    from funding_report.models import FundingOpportunityReport, FundingOpportunityReportStatus
+
+    FundingOpportunityReport.objects.filter(pk=funding_opportunity_report_id).update(
+        status=FundingOpportunityReportStatus.RUNNING
+    )
+
+    try:
+        time.sleep(60)
+        FundingOpportunityReport.objects.filter(pk=funding_opportunity_report_id).update(
+            status=FundingOpportunityReportStatus.SUCCESS
+        )
+    except Exception:
+        FundingOpportunityReport.objects.filter(pk=funding_opportunity_report_id).update(
+            status=FundingOpportunityReportStatus.FAILED
+        )
+        raise
