40c4d313eaa57e53734414a6480e696166b5f83e
Luis Lopez <33841664+LALuis@users.noreply.github.com>
2026-06-15 14:59:28 -0300

    PLAN-3623: Displaying includes on map (#4057)

--- src/interface/src/app/services/forsys.service.ts
+++ src/interface/src/app/services/forsys.service.ts
@@ -15,8 +15,7 @@ export class ForsysService {
   );
 
   public excludedAreas$ = this.forsysData$.pipe(map((data) => data.exclusions));
-  // TODO: Update to data.inclusions once the backend is ready and before releasing ADD_INCLUDES
-  public includedAreas$ = this.forsysData$.pipe(map((data) => data.exclusions));
+  public includedAreas$ = this.forsysData$.pipe(map((data) => data.inclusions));
 
   constructor(private moduleService: ModuleService) {}
 
