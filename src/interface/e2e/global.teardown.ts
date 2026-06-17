3ddf8ed53ecb7a5d765004b5d626d3dc3fb0fd59
lastminutediorama <mkinsella@sig-gis.com>
2026-06-16 19:20:57 -0500

    prevent auto removal of files

--- src/interface/e2e/global.teardown.ts
+++ src/interface/e2e/global.teardown.ts
@@ -1,7 +1,9 @@
+
 import { unlinkSync } from 'fs';
 import { PASSWORD_FILE, EMAIL_FILE } from './fixtures/test-users';
 
 export default async function globalTeardown() {
-  try { unlinkSync(PASSWORD_FILE); } catch {}
-  try { unlinkSync(EMAIL_FILE); } catch {}
+  // keep the user files
+  // try { unlinkSync(PASSWORD_FILE); } catch {}
+  // try { unlinkSync(EMAIL_FILE); } catch {}
 }
