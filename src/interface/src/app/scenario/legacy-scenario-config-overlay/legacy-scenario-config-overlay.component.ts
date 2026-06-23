f087a3dc212714cb2ffddb8f10c358f34271f88d
Luis Lopez <33841664+LALuis@users.noreply.github.com>
2026-06-23 10:22:05 -0300

    PLAN-3626: Includes on scenario results (#4077)

--- src/interface/src/app/scenario/legacy-scenario-config-overlay/legacy-scenario-config-overlay.component.ts
+++ src/interface/src/app/scenario/legacy-scenario-config-overlay/legacy-scenario-config-overlay.component.ts
@@ -5,9 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from '@styleguide';
 import { STAND_OPTIONS } from '@plan/plan-helpers';
-import { catchError, combineLatest, map } from 'rxjs';
+import { catchError, combineLatest, map, of, startWith } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { ForsysService } from '@services/forsys.service';
+import { FeaturesModule } from '@app/features/features.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @UntilDestroy()
 @Component({
@@ -19,6 +21,8 @@ import { ForsysService } from '@services/forsys.service';
     MatIconModule,
     ButtonComponent,
     DecimalPipe,
+    FeaturesModule,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './legacy-scenario-config-overlay.component.html',
   styleUrl: './legacy-scenario-config-overlay.component.scss',
@@ -30,8 +34,27 @@ export class LegacyScenarioConfigOverlayComponent implements OnDestroy {
   displayScenarioConfigOverlay$ = this.scenarioState.displayConfigOverlay$;
   currentScenario$ = this.scenarioState.currentScenario$;
   excludedAreas$ = this.forsysService.excludedAreas$;
+  includedAreas$ = this.forsysService.includedAreas$;
 
   readonly standSizeOptions = STAND_OPTIONS;
+
+  selectedIncludedAreas$ = combineLatest([
+    this.currentScenario$,
+    this.includedAreas$,
+  ]).pipe(
+    untilDestroyed(this),
+    map(([scenario, includedAreas]) => {
+      const ids = scenario.configuration?.included_areas ?? [];
+
+      const labels = ids
+        .map((id) => includedAreas.find((a) => a.id === id)?.name)
+        .filter((v): v is string => !!v);
+
+      return labels.length ? labels.join(', ') : '--';
+    }),
+    startWith(null),
+    catchError(() => of('--'))
+  );
 
   selectedExcludedAreas$ = combineLatest([
     this.currentScenario$,
