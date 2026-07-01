7ec5eb80a9344e1df0a4f043da7f4fc8cc57606f
Foxbyte0101 <186831384+Foxbyte0101@users.noreply.github.com>
2026-07-01 11:43:28 -0400

    plan-3715-multiply-by-10000-instead-of-1000 (#4089)
    Co-authored-by: Zalo <gcrivelli@sig-gis.com>

--- src/planscape/planning/services.py
+++ src/planscape/planning/services.py
@@ -1982,7 +1982,7 @@ def calculate_and_update_rx_leverage(scenario: Scenario, features: List) -> List
         attainment = feature.get("properties").get("attainment")
         att_values = [attainment.get(name) for name in names]
         area_acres = feature.get("properties").get("area_acres")
-        rx_leverage = (sum(att_values) / area_acres) * 1000 if area_acres else None
+        rx_leverage = (sum(att_values) / area_acres) * 10000 if area_acres else None
         feature["properties"]["rx_leverage"] = rx_leverage
 
     return features
