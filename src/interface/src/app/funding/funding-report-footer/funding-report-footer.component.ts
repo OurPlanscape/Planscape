e2c5b4309754bdbfc138157c81ca18c43f50a2f6
lastminutediorama <mkinsella@sig-gis.com>
2026-07-10 09:00:24 -0500

    basic download, rename, refactors

--- src/interface/src/app/funding/funding-report-footer/funding-report-footer.component.ts
+++ src/interface/src/app/funding/funding-report-footer/funding-report-footer.component.ts
@@ -5,6 +5,9 @@ import { MatMenuModule } from '@angular/material/menu';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ButtonComponent } from '@styleguide';
 import { FeaturesModule } from '@features/features.module';
+import { FileSaverService } from '@app/services';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SNACK_ERROR_CONFIG } from '@app/shared';
 
 @Component({
   selector: 'app-funding-report-footer',
@@ -23,7 +26,9 @@ import { FeaturesModule } from '@features/features.module';
 export class FundingReportFooterComponent {
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private fileSaverService: FileSaverService,
+    private snackbar: MatSnackBar
   ) {}
   // allow for arbitrary disabling, such as when the map is still loading
   @Input() buttonDisabled = false;
@@ -32,8 +37,37 @@ export class FundingReportFooterComponent {
 
   @Output() downloadPdf = new EventEmitter<void>();
 
+  @Input() geoPackageUrl: string | null = null;
+
+  downloadingGeopackage = false;
+
+  //TODO: or just emit this to the parent?
   downloadGeopackage() {
-    // TODO
+    this.downloadingGeopackage = true;
+
+    //TODO: grab this from ... generated file or...compose it?
+    const filename = 'geopackage.gpkg';
+
+    if (this.geoPackageUrl) {
+      this.fileSaverService.downloadGeopackage(this.geoPackageUrl).subscribe({
+        next: (data) => {
+          this.downloadingGeopackage = false;
+          const blob = new Blob([data], {
+            type: 'application/zip',
+          });
+          this.fileSaverService.saveAs(blob, filename);
+        },
+        error: (e) => {
+          this.downloadingGeopackage = false;
+          console.error('Error downloading: ', e);
+          this.snackbar.open(
+            `Error: Could not generate a GeoPackage.`,
+            'Dismiss',
+            SNACK_ERROR_CONFIG
+          );
+        },
+      });
+    }
   }
 
   downloadPDF() {
