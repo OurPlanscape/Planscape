d79c286f17d882ef291632269a15fdacbfa1b686
Mark <100235124+lastminutediorama@users.noreply.github.com>
2026-07-20 09:36:22 -0500

    update FOR tooltip text (#4175)

--- src/interface/src/app/funding/funding-report/funding-report.component.ts
+++ src/interface/src/app/funding/funding-report/funding-report.component.ts
@@ -530,7 +530,7 @@ export class FundingReportComponent implements OnInit, OnChanges, OnDestroy {
         mapCanvas
       );
     } catch (error) {
-      this.displayDownloadErrorSnackbar;
+      this.displayDownloadErrorSnackbar();
     } finally {
       this.generatingPdf$.next(false);
     }
