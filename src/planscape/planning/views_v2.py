82e2a27072c9b38aff39fd39c875ef9fef926b7c
Zalo <gcrivelli@sig-gis.com>
2026-07-23 11:53:40 -0400

    prioritize-scnearios-updates

--- src/planscape/planning/views_v2.py
+++ src/planscape/planning/views_v2.py
@@ -276,16 +276,11 @@ class ScenarioViewSet(MultiSerializerMixin, viewsets.ModelViewSet):
             qs = qs.filter(parent__isnull=True)
         return qs
 
-    @action(methods=["get"], detail=True, url_path="child")
-    def child(self, request, pk=None):
+    @action(methods=["get"], detail=True, url_path="children")
+    def children(self, request, pk=None):
         parent = self.get_object()
 
-        children = (
-            self.get_queryset()
-            .filter(parent=parent)
-            .select_related("planning_area", "user", "results")
-            .prefetch_related("project_areas")
-        )
+        children = self.filter_queryset(self.get_queryset().filter(parent=parent))
 
         serializer = ListScenarioSerializer(
             children,
