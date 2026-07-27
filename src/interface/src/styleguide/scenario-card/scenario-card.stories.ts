b8052de53af1fe2bcac1ee09de9eeb6b6c0b61e2
lastminutediorama <mkinsella@sig-gis.com>
2026-07-27 12:35:06 -0500

    lint

--- src/interface/src/styleguide/scenario-card/scenario-card.stories.ts
+++ src/interface/src/styleguide/scenario-card/scenario-card.stories.ts
@@ -36,7 +36,7 @@ export const ProjectArea: Story = {
     resultStatus: 'SUCCESS',
     creator: 'Larry Larrington',
     created_at: '2024-01-01 12:34:00',
-    type: 'PROJECT_AREAS'
+    type: 'PROJECT_AREAS',
   },
 };
 
