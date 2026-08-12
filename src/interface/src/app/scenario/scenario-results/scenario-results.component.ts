bb7a28170a406bb65fb726bed04955eee3ec7041
lastminutediorama <mkinsella@sig-gis.com>
2026-08-12 16:41:12 -0500

    update heading in results view

--- src/interface/src/app/scenario/scenario-results/scenario-results.component.ts
+++ src/interface/src/app/scenario/scenario-results/scenario-results.component.ts
@@ -41,6 +41,7 @@ import { BaseLayersStateService } from '@base-layers/base-layers.state.service';
 export class ScenarioResultsComponent implements OnChanges, OnInit {
   @Input() scenarioId!: number;
   @Input() scenarioVersion!: string;
+  @Input() scenarioParent: number | null = null;
   @Input() planningApproach!: PLANNING_APPROACH;
   @Input() scenarioName = 'scenario_results';
   @Input() results: ScenarioResult | null = null;
@@ -109,6 +110,13 @@ export class ScenarioResultsComponent implements OnChanges, OnInit {
       analytics = hasAnalytics(this.results);
     }
     return analytics;
+  }
+
+  get headLineText() {
+    if (this.isPlanningApproachSubUnits && this.scenarioParent === null) {
+      return 'Top 10 Subunits';
+    }
+    return 'Project Areas';
   }
 
   get isPlanningApproachSubUnits() {
