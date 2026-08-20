c490e5c70ca5963ea052d8a5cfc61e14cb51e989
Luis Lopez <33841664+LALuis@users.noreply.github.com>
2026-08-20 12:38:54 -0300

    PLAN-3855: Workspace dashboard
    PLAN-3855: Workspace dashboard

--- src/interface/src/app/services/workspaces.service.ts
+++ src/interface/src/app/services/workspaces.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
+import { delay, Observable, of, throwError } from 'rxjs';
 import { CreateWorkspacePayload, Workspace } from '@types';
+import { HttpErrorResponse } from '@angular/common/http';
 
 /**
  * TODO: mocked until the backend endpoints exist.
@@ -16,6 +17,32 @@ export class WorkspacesService {
       id: this.mockId(),
       name: payload.name,
       creator: '',
+      created_at: new Date().toISOString(),
+    };
+
+    return of(workspace).pipe(delay(this.mockLatency));
+  }
+
+  getWorkspace(id: number): Observable<Workspace> {
+    // Mocking an error
+    if (id === 404) {
+      return throwError(
+        () =>
+          new HttpErrorResponse({
+            status: 404,
+            statusText: 'Not Found',
+            error: {
+              detail: 'Workspace not found',
+            },
+          })
+      ).pipe(delay(this.mockLatency));
+    }
+
+    // Mocking a workspace
+    const workspace: Workspace = {
+      id,
+      name: 'Mock Workspace',
+      creator: 'Han Solo',
       created_at: new Date().toISOString(),
     };
 
