5a68639b14a6b71dad4e0d396c845fa9dba33da2
Pablo <plopez@sig-gis.com>
2026-09-14 10:19:38 -0300

    card link to workspaces

--- src/interface/src/app/workspaces/workspace-dashboard/workspace-dashboard.component.spec.ts
+++ src/interface/src/app/workspaces/workspace-dashboard/workspace-dashboard.component.spec.ts
@@ -2,7 +2,8 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkspaceDashboardComponent } from './workspace-dashboard.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { MockProvider } from 'ng-mocks';
 import { WorkspaceActionsService } from '../workspace-actions.service';
 
@@ -12,7 +13,11 @@ describe('WorkspaceDashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WorkspaceDashboardComponent, HttpClientTestingModule],
+      imports: [
+        WorkspaceDashboardComponent,
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
       providers: [
         MockProvider(WorkspaceActionsService),
         {
@@ -35,5 +40,14 @@ describe('WorkspaceDashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('navigates to the workspace map viewer', () => {
+    const router = TestBed.inject(Router);
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.handleNavigation();
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/map-viewer/workspace', 1]);
   });
 });
