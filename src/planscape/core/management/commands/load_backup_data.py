c5bdf18e111776e2c5e2406863d2f106cfd1110a
Roque Betiol Jr <jbetiol@sig-gis.com>
2026-09-15 18:11:57 -0300

    Using dead_or_alive queryset to delete soft-deleted objects when loading backup data

--- src/planscape/core/management/commands/load_backup_data.py
+++ src/planscape/core/management/commands/load_backup_data.py
@@ -166,7 +166,7 @@ class Command(BaseCommand):
             batch_size = options.get("batch_size", 500)
             with transaction.atomic():
 
-                datalayers = DataLayer.objects.filter(created_at__gte=last_restore_date)
+                datalayers = DataLayer.dead_or_alive.filter(created_at__gte=last_restore_date)
 
                 # Stand metrics batch deletion
                 stand_metrics = StandMetric.objects.filter(datalayer__in=datalayers)
@@ -179,7 +179,7 @@ class Command(BaseCommand):
 
 
                 # N-N relational tables deletion by `updated_at`
-                count = TreatmentGoalUsesDataLayer.objects.filter(updated_at__gte=last_restore_date).delete()
+                count = TreatmentGoalUsesDataLayer.dead_or_alive.filter(updated_at__gte=last_restore_date).delete()
                 self.stdout.write(f"Deleted {count[1]} entry(ies) related to TreatmentGoalUsesDataLayer updated after last restore.")
 
                 count = DataLayerHasStyle.objects.filter(updated_at__gte=last_restore_date).delete()
@@ -187,22 +187,22 @@ class Command(BaseCommand):
 
 
                 # Other tables deletion by `created_at`
-                count = TreatmentGoal.objects.filter(created_at__gte=last_restore_date).delete()
+                count = TreatmentGoal.dead_or_alive.filter(created_at__gte=last_restore_date).delete()
                 self.stdout.write(f"Deleted {count[1]} entry(ies) related to TreatmentGoal created after last restore.")
 
                 count = Category.objects.filter(created_at__gte=last_restore_date).delete()
                 self.stdout.write(f"Deleted {count[1]} entry(ies) related to Category(s) created after last restore.")
 
-                count = Style.objects.filter(created_at__gte=last_restore_date).delete()
+                count = Style.dead_or_alive.filter(created_at__gte=last_restore_date).delete()
                 self.stdout.write(f"Deleted {count[1]} entry(ies) related to Style(s) created after last restore.")
 
                 count = datalayers.delete()
                 self.stdout.write(f"Deleted {count[1]} entry(ies) related to DataLayer(s) created after last restore.")
 
-                count = Dataset.objects.filter(created_at__gte=last_restore_date).delete()
+                count = Dataset.dead_or_alive.filter(created_at__gte=last_restore_date).delete()
                 self.stdout.write(f"Deleted {count[1]} entry(ies) related to Dataset(s) created after last restore.")
 
-                count = Organization.objects.filter(created_at__gte=last_restore_date).delete()
+                count = Organization.dead_or_alive.filter(created_at__gte=last_restore_date).delete()
                 self.stdout.write(f"Deleted {count[1]} entry(ies) related to Organization(s) created after last restore.")
 
             # Copy to tmp folder and rename all `url` fields
