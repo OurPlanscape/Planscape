ae9407ae4e6b5167a2db0bd119b2b1601918bb97
Roque Betiol Jr <jbetiol@sig-gis.com>
2026-09-16 17:13:06 -0300

    chore: Update DataLayer's geometry by uploading shapefile.
    Enabling uploading shapefile when editing DataLayer via Django Admin.

--- src/planscape/datasets/tests/test_admin.py
+++ src/planscape/datasets/tests/test_admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from django.test import SimpleTestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import RequestFactory, SimpleTestCase, TestCase
 
+from datasets.admin import DataLayerAdmin
+from datasets.forms import DataLayerAdminForm
 from datasets.models import DataLayer, Dataset
+from datasets.tests.factories import DataLayerFactory
+from datasets.tests.test_shapefile_geometry import make_shapefile_zip
+from planscape.tests.factories import UserFactory
 
 
 class DatasetAdminTest(SimpleTestCase):
@@ -22,3 +28,72 @@ class DataLayerAdminTest(SimpleTestCase):
         self.assertIn("updated_at", model_admin.list_display)
         self.assertIn("created_at", model_admin.readonly_fields)
         self.assertIn("updated_at", model_admin.readonly_fields)
+
+
+class DataLayerAdminShapefileUploadTest(TestCase):
+    def setUp(self):
+        self.datalayer = DataLayerFactory.create(
+            url="s3://bucket/original.zip",
+            table="datastore.original",
+            info={"original": True},
+        )
+
+    def _form_data(self):
+        return {
+            "organization": self.datalayer.organization.pk,
+            "dataset": self.datalayer.dataset.pk,
+            "name": self.datalayer.name,
+            "table": self.datalayer.table,
+            "metadata": "{}",
+        }
+
+    def test_admin_form_exposes_shapefile_zip_upload(self):
+        form = DataLayerAdminForm(instance=self.datalayer)
+
+        self.assertIn("geometry_shapefile_zip", form.fields)
+
+    def test_admin_form_rejects_non_zip_upload(self):
+        uploaded_file = SimpleUploadedFile(
+            "shape.txt",
+            make_shapefile_zip(),
+            content_type="text/plain",
+        )
+
+        form = DataLayerAdminForm(
+            data=self._form_data(),
+            files={"geometry_shapefile_zip": uploaded_file},
+            instance=self.datalayer,
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertIn("geometry_shapefile_zip", form.errors)
+
+    def test_admin_save_updates_only_geometry_fields_from_upload(self):
+        original_type = self.datalayer.type
+        original_status = self.datalayer.status
+        uploaded_file = SimpleUploadedFile(
+            "shape.zip",
+            make_shapefile_zip(),
+            content_type="application/zip",
+        )
+        form = DataLayerAdminForm(
+            data=self._form_data(),
+            files={"geometry_shapefile_zip": uploaded_file},
+            instance=self.datalayer,
+        )
+        self.assertTrue(form.is_valid(), form.errors)
+        obj = form.save(commit=False)
+
+        request = RequestFactory().post("/")
+        request.user = UserFactory.create(is_staff=True, is_superuser=True)
+        model_admin = DataLayerAdmin(DataLayer, admin.site)
+        model_admin.save_model(request, obj, form, change=True)
+
+        self.datalayer.refresh_from_db()
+        self.assertEqual(self.datalayer.geometry.geom_type, "Polygon")
+        self.assertEqual(self.datalayer.outline.geom_type, "MultiPolygon")
+        self.assertEqual(self.datalayer.url, "s3://bucket/original.zip")
+        self.assertEqual(self.datalayer.table, "datastore.original")
+        self.assertEqual(self.datalayer.info, {"original": True})
+        self.assertEqual(self.datalayer.type, original_type)
+        self.assertEqual(self.datalayer.status, original_status)
