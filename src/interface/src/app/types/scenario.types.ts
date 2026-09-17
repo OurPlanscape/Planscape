188ada8bf283599a2ad6c2b264984649233fb995
lastminutediorama <mkinsella@sig-gis.com>
2026-09-17 14:46:20 -0500

    divide codes into categories

--- src/interface/src/app/types/scenario.types.ts
+++ src/interface/src/app/types/scenario.types.ts
@@ -15,20 +15,25 @@ export type ScenarioResultStatus =
   | 'TIMED_OUT'
   | 'DRAFT'; // Creating a scenario but not completed the steps yet.
 
-// these mirror the known error codes supported on the BE
+const USER_REVISABLE_ERRORS = ['NO_AVAILABLE_RESULT'] as const;
+
+const SYSTEM_ERRORS = [
+  'TIME_OUT',
+  'GENERIC_PANIC',
+  'STAND_METRIC_FAILURE',
+  'UNKNOWN_ERROR',
+  'STAND_DATA_ERROR',
+  'FORSYS_PREPARATION_ERROR',
+  'FORSYS_EXECUTION_ERROR',
+  'RESULT_PROCESSING_ERROR',
+  'FORSYS_EMPTY_RESULT',
+  'PROJECT_AREA_UPDATE_ERROR',
+] as const;
+
 export type ScenarioResultErrorCode =
-  | 'TIME_OUT'
-  | 'GENERIC_PANIC'
-  | 'STAND_METRIC_FAILURE'
-  | 'UNKNOWN_ERROR'
-  | 'STAND_DATA_ERROR'
-  | 'PROJECT_DATA_ERROR'
-  | 'FORSYS_PREPARATION_ERROR'
-  | 'FORSYS_EXECUTION_ERROR'
-  | 'FORSYS_EMPTY_RESULT'
-  | 'RESULT_PROCESSING_ERROR'
-  | 'PROJECT_AREA_UPDATE_ERROR'
-  | 'NO_AVAILABLE_STANDS';
+  | (typeof USER_REVISABLE_ERRORS)[number]
+  | (typeof SYSTEM_ERRORS)[number];
+
 export interface ScenarioResultError {
   error_code: ScenarioResultErrorCode;
   description: string;
