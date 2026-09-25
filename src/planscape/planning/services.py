2ca4a9ab944a67d9e497ffae06b7f77309ecee4f
Roque Betiol Jr <jbetiol@sig-gis.com>
2026-09-25 11:44:27 -0300

    fix(constraints): Between constraint numeric validation without casting value to float.

--- src/planscape/planning/services.py
+++ src/planscape/planning/services.py
@@ -737,7 +737,7 @@ def _get_operation(operator: str, value: str) -> str:
             values = value.strip().replace(" ", "").split(",", maxsplit=1)
             values.sort()
             min_value, max_value = values
-            return f"value >= {int(min_value)} & value <= {int(max_value)}"
+            return f"value >= {min_value} & value <= {max_value}"
         case _:
             # normal cases
             # constraints datalayers from scenario configuration
